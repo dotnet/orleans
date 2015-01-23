3db537e0ca1a0ca88bb226d9e3af43dd66f5d577
Sergey Bykov <sbykov@microsoft.com>
2015-01-22 23:22:23 -0800

    Adding files that missed the initial commit because of .gitignore.

---
+++ src/Build/PostBuild.cmd
@@ -0,0 +1,25 @@
+@echo Begin Post-Build script
+
+if "%BuildingInsideVisualStudio%" == "true" (
+	set PKG_DIR=%SolutionDir%\NuGet.Packages
+) else (
+    set PKG_DIR=%TargetDir%\..\NuGet.Packages
+)
+
+if not exist "%PKG_DIR%" (md "%PKG_DIR%") else (del /s/q "%PKG_DIR%\*")
+
+copy /y "%SolutionDir%SDK\OrleansConfiguration.xml" "%TargetDir%"
+copy /y "%SolutionDir%SDK\ClientConfiguration.xml" "%TargetDir%"
+
+@echo BuildingInsideVisualStudio = %BuildingInsideVisualStudio% BuildOrleansNuGet = %BuildOrleansNuGet%
+
+@echo Clean old generated Orleans NuGet packages from %TargetDir%
+del /q *.nupkg
+
+copy /y "%SolutionDir%NuGet\*.props" "%TargetDir%\"
+copy /y "%SolutionDir%NuGet\EmptyFile.cs" "%TargetDir%\"
+
+@echo Build Orleans NuGet packages from %TargetDir%
+call "%SolutionDir%NuGet\CreateOrleansPackages.bat" . .\Version.txt
+@echo Copying Orleans NuGet packages to %PKG_DIR%
+xcopy /y *.nupkg "%PKG_DIR%\"
