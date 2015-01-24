e075a18a7278fef25554d532c4d7f89153b00acc
Michael Ahern <michael.ahern@gmail.com>
2015-01-24 18:01:12 -0500

    Update CreateTables.sql
    Adding missing RecentlyUsedActivations column to OrleansSiloMetricsTable. Normalizing indentation.

--- src/OrleansProviders/SQLServer/CreateTables.sql
+++ src/OrleansProviders/SQLServer/CreateTables.sql
@@ -7,7 +7,7 @@ DROP TABLE [dbo].[OrleansStatisticsTable]
 
 CREATE TABLE [dbo].[OrleansMembershipTable]
 (
-	[DeploymentId] NVARCHAR(150) NOT NULL, 
+    [DeploymentId] NVARCHAR(150) NOT NULL, 
     [Address] NVARCHAR(150) NOT NULL, 
     [Port] INT NOT NULL, 
     [Generation] INT NOT NULL, 
@@ -23,34 +23,34 @@ CREATE TABLE [dbo].[OrleansMembershipTable]
     [SuspectingTimes] NTEXT NULL, 
     [StartTime] DATETIME NOT NULL, 
     [IAmAliveTime] DATETIME NOT NULL, 
-	[ETag] NVARCHAR(50) NOT NULL,
+    [ETag] NVARCHAR(50) NOT NULL,
     PRIMARY KEY ([DeploymentId],[Address],[Port],[Generation])
 )
 
 CREATE TABLE [dbo].[OrleansMembershipVersionTable]
 (
-	[DeploymentId] NVARCHAR(150) NOT NULL, 
+    [DeploymentId] NVARCHAR(150) NOT NULL, 
     [TimeStamp] DATETIME NOT NULL, 
     [Version] BIGINT NOT NULL, 
-	[ETag] NVARCHAR(50) NOT NULL,
+    [ETag] NVARCHAR(50) NOT NULL,
     PRIMARY KEY ([DeploymentId])
 )
 
 CREATE TABLE [dbo].[OrleansRemindersTable]
 (
-	[ServiceId] NVARCHAR(150) NOT NULL, 
+    [ServiceId] NVARCHAR(150) NOT NULL, 
     [GrainId] NVARCHAR(150) NOT NULL, 
-	[ReminderName] NVARCHAR(150) NOT NULL,
+    [ReminderName] NVARCHAR(150) NOT NULL,
     [StartTime] DATETIME NOT NULL, 
     [Period] INT NOT NULL,
-	[GrainIdConsistentHash] INT NOT NULL, 
-	[ETag] NVARCHAR(50) NOT NULL,
+    [GrainIdConsistentHash] INT NOT NULL, 
+    [ETag] NVARCHAR(50) NOT NULL,
     PRIMARY KEY ([ServiceId],[GrainId],[ReminderName])
 )
 
 CREATE TABLE [dbo].[OrleansSiloMetricsTable]
 (
-	[DeploymentId] NVARCHAR(150) NOT NULL, 
+    [DeploymentId] NVARCHAR(150) NOT NULL, 
     [SiloId] NVARCHAR(150) NOT NULL, 
     [TimeStamp] DATETIME NOT NULL, 
     [Address] NVARCHAR(50) NOT NULL, 
@@ -59,39 +59,40 @@ CREATE TABLE [dbo].[OrleansSiloMetricsTable]
     [HostName] NVARCHAR(150) NOT NULL, 
     [GatewayAddress] NVARCHAR(50) NULL, 
     [GatewayPort] INT NULL, 
-	[CPU] FLOAT NOT NULL,
-	[Memory] BIGINT NOT NULL,
-    [Activations] INT NOT NULL, 
+    [CPU] FLOAT NOT NULL,
+    [Memory] BIGINT NOT NULL,
+    [Activations] INT NOT NULL,
+    [RecentlyUsedActivations] INT NOT NULL,
     [SendQueue] INT NOT NULL, 
     [ReceiveQueue] INT NOT NULL, 
-	[RequestQueue] BIGINT NOT NULL,
-	[SentMessages] BIGINT NOT NULL,
-	[ReceivedMessages] BIGINT NOT NULL,
-	[LoadShedding] BIT NOT NULL,
-	[ClientCount] BIGINT NOT NULL,
+    [RequestQueue] BIGINT NOT NULL,
+    [SentMessages] BIGINT NOT NULL,
+    [ReceivedMessages] BIGINT NOT NULL,
+    [LoadShedding] BIT NOT NULL,
+    [ClientCount] BIGINT NOT NULL,
     PRIMARY KEY ([DeploymentId],[SiloId])
 )
 
 CREATE TABLE [dbo].[OrleansClientMetricsTable]
 (
-	[DeploymentId] NVARCHAR(150) NOT NULL, 
+    [DeploymentId] NVARCHAR(150) NOT NULL, 
     [ClientId] NVARCHAR(150) NOT NULL, 
     [TimeStamp] DATETIME NOT NULL, 
     [Address] NVARCHAR(50) NOT NULL, 
     [HostName] NVARCHAR(150) NOT NULL, 
-	[CPU] FLOAT NOT NULL,
-	[Memory] BIGINT NOT NULL,
+    [CPU] FLOAT NOT NULL,
+    [Memory] BIGINT NOT NULL,
     [SendQueue] INT NOT NULL, 
     [ReceiveQueue] INT NOT NULL, 
-	[SentMessages] BIGINT NOT NULL,
-	[ReceivedMessages] BIGINT NOT NULL,
-	[ConnectedGWCount] BIGINT NOT NULL,
+    [SentMessages] BIGINT NOT NULL,
+    [ReceivedMessages] BIGINT NOT NULL,
+    [ConnectedGWCount] BIGINT NOT NULL,
     PRIMARY KEY ([DeploymentId],[ClientId])
 )
 
 CREATE TABLE [dbo].[OrleansStatisticsTable]
 (
-	[DeploymentId] NVARCHAR(150) NOT NULL, 
+    [DeploymentId] NVARCHAR(150) NOT NULL, 
     [Date] DATE NOT NULL, 
     [TimeStamp] DATETIME NOT NULL, 
     [Id] NVARCHAR(250) NOT NULL, 
@@ -99,8 +100,8 @@ CREATE TABLE [dbo].[OrleansStatisticsTable]
     [HostName] NVARCHAR(150) NOT NULL, 
     [Name] NVARCHAR(150) NULL, 
     [IsDelta] BIT NULL, 
-	[StatValue] NVARCHAR(250) NOT NULL,
-	[Statistic] NVARCHAR(250) NOT NULL,
+    [StatValue] NVARCHAR(250) NOT NULL,
+    [Statistic] NVARCHAR(250) NOT NULL,
     PRIMARY KEY ([DeploymentId],[Date],[Id],[Counter])
 )
 
