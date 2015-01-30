d536f7d022d8fb08486a2ff74884dd2a0294bb0e
SBArbeit <scott.b.arbeit@hotmail.com>
2015-01-30 15:58:11 -0800

    Documentation spelling correction: "backin store" -> "backing store".

--- src/Orleans/Core/IGrainState.cs
+++ src/Orleans/Core/IGrainState.cs
@@ -38,19 +38,19 @@ namespace Orleans
 
         /// <summary>
         /// Async method to cause the current grain state data to be cleared and reset. 
-        /// This will usually mean the state record is deleted from backin store, but the specific behavior is defined by the storage provider instance configured for this grain.
+        /// This will usually mean the state record is deleted from backing store, but the specific behavior is defined by the storage provider instance configured for this grain.
         /// If Etags do not match, then this operation will fail; Set Etag = <c>null</c> to indicate "always delete".
         /// </summary>
         Task ClearStateAsync();
 
         /// <summary>
-        /// Async method to cause write of the current grain state data into backin store.
+        /// Async method to cause write of the current grain state data into backing store.
         /// If Etags do not match, then this operation will fail; Set Etag = <c>null</c> to indicate "always overwrite".
         /// </summary>
         Task WriteStateAsync();
 
         /// <summary>
-        /// Async method to cause refresh of the current grain state data from backin store.
+        /// Async method to cause refresh of the current grain state data from backing store.
         /// Any previous contents of the grain state data will be overwritten.
         /// </summary>
         Task ReadStateAsync();
