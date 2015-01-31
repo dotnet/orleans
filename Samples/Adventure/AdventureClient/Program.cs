453755c3512ae313d74ba75bbe8b1944cb8feb18
Jakub Konecki <jkonecki@gmail.com>
2015-01-31 21:29:57 +0000

    Added Adventure sample.

---
+++ Samples/Adventure/AdventureClient/Program.cs
@@ -0,0 +1,71 @@
+﻿//*********************************************************//
+//    Copyright (c) Microsoft. All rights reserved.
+//    
+//    Apache 2.0 License
+//    
+//    You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//    
+//    Unless required by applicable law or agreed to in writing, software 
+//    distributed under the License is distributed on an "AS IS" BASIS, 
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+//    implied. See the License for the specific language governing 
+//    permissions and limitations under the License.
+//
+//*********************************************************
+
+using AdventureGrainInterfaces;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GrainClient.Initialize();
+
+            Console.WriteLine(@"
+  ___      _                 _                  
+ / _ \    | |               | |                 
+/ /_\ \ __| |_   _____ _ __ | |_ _   _ _ __ ___ 
+|  _  |/ _` \ \ / / _ \ '_ \| __| | | | '__/ _ \
+| | | | (_| |\ V /  __/ | | | |_| |_| | | |  __/
+\_| |_/\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___|");
+
+            Console.WriteLine();
+            Console.WriteLine("What's you name?");
+            string name = Console.ReadLine();
+
+            var player = GrainFactory.GetGrain<IPlayerGrain>(Guid.NewGuid());
+            player.SetName(name).Wait();
+            var room1 = GrainFactory.GetGrain<IRoomGrain>(0);
+            player.SetRoomGrain(room1).Wait();
+
+            Console.WriteLine(player.Play("look").Result);
+
+            string result="Start";
+
+            try
+            {
+                while (result!="")
+                {
+                    string command = Console.ReadLine();
+
+                    result = player.Play(command).Result;
+                    Console.WriteLine(result);
+                }
+            }
+            finally
+            {
+                player.Die().Wait();
+                Console.WriteLine("Game over!");
+            }
+        }
+    }
+}
