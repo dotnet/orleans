d262ada93820eb2c63e24fe910131217f4321f43
Gabriel Kliot <gkliot@microsoft.com>
2015-02-02 13:33:44 -0800

    Fixed references to Microsoft.WindowsAzure.ServiceRuntime in Samples. (reverted from commit f569199446cb076b8f9ef2a5a05e6e397e884133)

--- Samples/TicTacToe/OrleansXO.WorkerRole/WorkerRole.cs
+++ Samples/TicTacToe/OrleansXO.WorkerRole/WorkerRole.cs
@@ -21,6 +21,7 @@ using System.Linq;
 using System.Net;
 using System.Threading;
 using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.Storage;
 using Orleans.Runtime.Host;
