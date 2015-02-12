ca0d449ec5dae9b6ef65e322f6601f825c154b84
jthelin <github@sw-technologies.com>
2015-02-11 21:14:41 -0800

    StorageTestConstants
    Follow on changes from PR #114 and #116
    - Rename TestConstants to StorageTestConstants.
    - Add copyright statements to new test files.
    - Normalize namespaces in TesterInternals project.
    - Cleanup some unused imports.

--- src/TesterInternal/StorageTests/AzureTableDataManagerTests.cs
+++ src/TesterInternal/StorageTests/AzureTableDataManagerTests.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/*
+Project Orleans Cloud Service SDK ver. 1.0
+ 
+Copyright (c) Microsoft Corporation
+ 
+All rights reserved.
+ 
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the ""Software""), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
@@ -6,12 +29,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Orleans;
-using Orleans.Runtime;
-using Orleans.Storage;
 using Orleans.AzureUtils;
-using Microsoft.WindowsAzure.StorageClient;
-using System.Data.Services.Common;
-using System.Text;
 using UnitTests.Tester;
 
 namespace UnitTests.StorageTests
@@ -28,7 +46,7 @@ namespace UnitTests.StorageTests
             UnitTestUtils.ConfigureThreadPoolSettingsForStorageTests();
 
             // Pre-create table, if required
-            manager = new UnitTestAzureTableDataManager(TestConstants.DataConnectionString);
+            manager = new UnitTestAzureTableDataManager(StorageTestConstants.DataConnectionString);
 
             PartitionKey = "AzureTableTests-" + Guid.NewGuid();
         }
--- src/TesterInternal/StorageTests/UnitTestAzureTableDataManager.cs
+++ src/TesterInternal/StorageTests/UnitTestAzureTableDataManager.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/*
+Project Orleans Cloud Service SDK ver. 1.0
+ 
+Copyright (c) Microsoft Corporation
+ 
+All rights reserved.
+ 
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the ""Software""), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +34,6 @@ using Orleans.AzureUtils;
 
 namespace UnitTests.StorageTests
 {
-    public class TestConstants
-    {
-        // Set DataConnectionString to your actual Azure Storage DataConnectionString
-        // public static string DataConnectionString ="DefaultEndpointsProtocol=https;AccountName=XXX;AccountKey=YYY"
-        // OR:
-        // start locval stoarge emulator and UseDevelopmentStorage.
-        public static string DataConnectionString = "UseDevelopmentStorage=true";
-       
-    }
-
-
     [Serializable]
     [DataServiceKey("PartitionKey", "RowKey")]
     public class UnitTestAzureTableData : TableServiceEntity
