48f913bb083a663d95bd7e63bf051d6c2de7f670
Jason Bragg <jbragg@microsoft.com>
2015-03-04 17:48:00 -0800

    Adding Subscription Multiplicity test - incomplete - does not compile

---
+++ src/Tester/StreamingTests/SMSSubscriptionMultiplicityTests.cs
@@ -0,0 +1,74 @@
+﻿/*
+Project Orleans Cloud Service SDK ver. 1.0
+ 
+Copyright (c) Microsoft Corporation
+ 
+All rights reserved.
+ 
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the ""Software""), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Tester;
+
+namespace UnitTests.SampleStreaming
+{
+    [DeploymentItem("OrleansConfigurationForUnitTests.xml")]
+    [DeploymentItem("OrleansProviders.dll")]
+    [TestClass]
+    public class SMSSubscriptionMultiplicityTests : SubscriptionMultiplicityTests
+    {
+        public SMSSubscriptionMultiplicityTests()
+            : base(new UnitTestSiloOptions
+            {
+                StartFreshOrleans = true,
+                SiloConfigFile = new FileInfo("OrleansConfigurationForUnitTests.xml"),
+            }, "SMSProvider", TimeSpan.FromSeconds(30))
+        {
+        }
+
+        // Use ClassCleanup to run code after all tests in a class have run
+        [ClassCleanup]
+        public static void MyClassCleanup()
+        {
+            base.MyClassCleanup();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            base.TestCleanup();
+        }
+
+        [TestMethod, TestCategory("BVT"), TestCategory("Nightly"), TestCategory("Streaming")]
+        public async Task SMSMultipleSubscriptionTest()
+        {
+            logger.Info("************************ SMSMultipleSubscriptionTest *********************************");
+            await MultipleSubscriptionTest(Guid.NewGuid());
+        }
+
+        [TestMethod, TestCategory("BVT"), TestCategory("Nightly"), TestCategory("Streaming")]
+        public async Task SMSAddAndRemoveSubscriptionTest()
+        {
+            logger.Info("************************ SMSAddAndRemoveSubscriptionTest *********************************");
+            await AddAndRemoveSubscriptionTest(Guid.NewGuid());
+        }
+    }
+}
