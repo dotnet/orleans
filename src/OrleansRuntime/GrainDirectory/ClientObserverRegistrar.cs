aca267d0716fffb5c6cc059409c8bf601d8f288e
Gabriel Kliot <gkliot@microsoft.com>
2015-03-05 13:56:14 -0800

    Added retries to directory registration attempts.

--- src/OrleansRuntime/GrainDirectory/ClientObserverRegistrar.cs
+++ src/OrleansRuntime/GrainDirectory/ClientObserverRegistrar.cs
@@ -23,6 +23,7 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR TH
 
 ﻿using System;
 using System.Collections.Generic;
+﻿using System.Linq;
 ﻿using System.Linq.Expressions;
 ﻿using System.Threading.Tasks;
 using Orleans.Runtime.Configuration;
@@ -34,6 +35,10 @@ namespace Orleans.Runtime
 {
     internal class ClientObserverRegistrar : SystemTarget, IClientObserverRegistrar
     {
+        private static readonly TimeSpan EXP_BACKOFF_ERROR_MIN = TimeSpan.FromSeconds(1);
+        private static readonly TimeSpan EXP_BACKOFF_ERROR_MAX = TimeSpan.FromSeconds(30);
+        private static readonly TimeSpan EXP_BACKOFF_STEP = TimeSpan.FromSeconds(1);
+
         private readonly ILocalGrainDirectory grainDirectory;
         private readonly ISiloMessageCenter messageCenter;
         private readonly SiloAddress myAddress;
@@ -77,11 +82,11 @@ namespace Orleans.Runtime
         internal void ClientAdded(GrainId clientId)
         {
             // Use a ActivationId that is hashed from clientId, and not random ActivationId.
-            // That way, when we refresh it in the directiry, its the same one.
+            // That way, when we refresh it in the directiry, it's the same one.
             var addr = GetClientActivationAddress(clientId);
             scheduler.QueueTask(
-                () => grainDirectory.RegisterAsync(addr), this.SchedulingContext)
-                    .LogException(logger, ErrorCode.ClientRegistrarFailedToRegister, String.Format("Directory.RegisterAsync {0} failed.", addr))
+                () => ExecuteWithRetries(() => grainDirectory.RegisterAsync(addr), ErrorCode.ClientRegistrarFailedToRegister, String.Format("Directory.RegisterAsync {0} failed.", addr)), 
+                this.SchedulingContext)
                         .Ignore();
         }
 
@@ -89,16 +94,41 @@ namespace Orleans.Runtime
         {
             var addr = GetClientActivationAddress(clientId);
             scheduler.QueueTask(
-                () => grainDirectory.UnregisterAsync(addr), this.SchedulingContext)
-                    .LogException(logger, ErrorCode.ClientRegistrarFailedToUnregister, String.Format("Directory.UnregisterAsync {0} failed.", addr))
+                () => ExecuteWithRetries(() => grainDirectory.UnregisterAsync(addr), ErrorCode.ClientRegistrarFailedToUnregister, String.Format("Directory.UnRegisterAsync {0} failed.", addr)), 
+                this.SchedulingContext)
                         .Ignore();
+        }
+
+        private async Task ExecuteWithRetries(Func<Task> functionToExecute, ErrorCode errorCode, string errorStr)
+        {
+            try
+            {
+                // Try to register/unregister the client in the directory.
+                // If failed, keep retrying with exponentially increasing time intervals in between, until:
+                // either succeeds or max time of orleansConfig.Globals.ClientRegistrationRefresh has reached.
+                // If failed to register after that time, it will be retried further on by clientRefreshTimer.
+                // In the unregsiter case just drop it. At the worst, where will be garbage in the directory.
+                await AsyncExecutorWithRetries.ExecuteWithRetries(
+                    (int _) =>
+                    {
+                        return functionToExecute();
+                    },
+                    AsyncExecutorWithRetries.INFINITE_RETRIES, // Do not limit the number of on-error retries, control it via "maxExecutionTime"
+                    (exc, i) => true, // Retry on all errors.         
+                    orleansConfig.Globals.ClientRegistrationRefresh, // "maxExecutionTime"
+                    new ExponentialBackoff(EXP_BACKOFF_ERROR_MIN, EXP_BACKOFF_ERROR_MAX, EXP_BACKOFF_STEP)); // how long to wait between error retries
+            }
+            catch (Exception exc)
+            {
+                logger.Error(errorCode, errorStr, exc);
+            }
         }
 
         private async Task OnClientRefreshTimer(object data)
         {
             try
             {
-                ICollection<GrainId> clients = gateway.GetConnectedClients();
+                ICollection<GrainId> clients = gateway.GetConnectedClients().ToList();
                 List<Task> tasks = new List<Task>();
                 foreach (GrainId clientId in clients)
                 {
@@ -111,7 +141,16 @@ namespace Orleans.Runtime
             }
             catch (Exception exc)
             {
-                logger.Error(ErrorCode.ClientRegistrarTimerFailed, "OnClientRefreshTimer has thrown.", exc.GetBaseException());
+                int actualExceptions = 1;
+                if (exc is AggregateException)
+                {
+                    AggregateException aggregateException = exc as AggregateException;
+                    actualExceptions = aggregateException.InnerExceptions.Count;
+                    exc = aggregateException.InnerExceptions.First();
+                }
+                logger.Error(ErrorCode.ClientRegistrarTimerFailed, 
+                    String.Format("OnClientRefreshTimer has thrown {0} inner exceptions. Printing the first exception:", actualExceptions), 
+                    exc);
             }
         }
 
