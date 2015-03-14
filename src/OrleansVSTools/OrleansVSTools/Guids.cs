4976c712aa27fe663e1ca93ef368f6e7a2caadcc
jthelin <github@thelin.me>
2015-03-14 12:51:19 -0700

    Template-Project-Imports
    - Include the template .props and .targets import lines in project files from start.
    - Grains and Grain Interfaces also should import .Core too.
    - Changed F# and VB to use /packages/ location rather than $(OrleansSDK) environment variable. (+2 squashed commit)
    
    Squashed commit:
    
    [2adb28a] Use Orleans NuGet packages v1.0.5
    
    [cadfa1f] Combined merge of all vsix changes
    
    - Remove unused SCC binding elements and other tools upgrade cruft from project files.
    
    - Use consistent default name IGrain1 / Grain1 of template generated interfaces / grain classes regardless of language.
    
    - Change display name of this template pack to "OrleansVSTools" everywhere, rather than "OrleansVSTools11".
    
    - Clean up some old info in package metadata.
    
    - Renamed SiloHost template ID value from cryptic Guid to "Orleans.Templates.Project.SiloHost.DevTest" to aline with the other template names.
    
    - Remove MixedMode and x86 build targets - we only need Release + AnyCPU.
    
    - Reference standard GlobalAssemblyInfo.cs file with shared product / file versioning rather than duplicating separately here.
    
    - Removed explicit AssemblyInfo.cs file in PersistedGrain template builder project, because none of the other builder projects have that.
    
    - Visual Studio made change to .fsproj when VS 2013 SDK upgraded projects to current version.
    I am assuming this is correct, but don't know enough about .fsproj to know for sure.
    
    - Only set start program settings to run local silo if OrleansSDK is installed [env var is set]
    
    - Rename the base copies of packages.config file to .txt in each template builder project.
    This is to prevent VS from trying to use those "payload data" files to recreate any package links in the template builder projects themselves.
    - GrainInterfaces templates don't need to include the Templates.Grains package - only the Templates.GrainInterfaces package.
    - Fixed required version for Orleans NuGet packages to 1.0.3 because this the current stable version, and big changes in package structure earlier than that are not compatible.
    
    - Don't think we need to splatter these binaries into so many different places on disk!
    
    - Clean up template builder project files after removed the trigger for VS to keep regenerated package links into the builder project itself rather than the generated project instance.
    - Standarized on using 'None' references for payload data files and keep 'Content' for data file references used in the builder project itself.
    
    - Remove old Orleans SDK codegen hooks from instantiated project files, because those should be recreated automatically by NuGet when first built.

--- src/OrleansVSTools/OrleansVSTools/Guids.cs
+++ src/OrleansVSTools/OrleansVSTools/Guids.cs
@@ -29,9 +29,9 @@ namespace OrleansVSTools
 {
     static class GuidList
     {
-        public const string guidOrleansVSTools11PkgString = "462db41f-31a4-48f0-834c-1bdcc0578511";
-        public const string guidOrleansVSTools11CmdSetString = "e52a46d8-332c-4522-8ebc-ebc745a1e3ef";
+        public const string guidOrleansVSToolsPkgString = "462db41f-31a4-48f0-834c-1bdcc0578511";
+        public const string guidOrleansVSToolsCmdSetString = "e52a46d8-332c-4522-8ebc-ebc745a1e3ef";
 
-        public static readonly Guid guidOrleansVSTools11CmdSet = new Guid(guidOrleansVSTools11CmdSetString);
+        public static readonly Guid guidOrleansVSToolsCmdSet = new Guid(guidOrleansVSToolsCmdSetString);
     };
 }
--- src/OrleansVSTools/OrleansVSTools/Properties/AssemblyInfo.cs
+++ src/OrleansVSTools/OrleansVSTools/Properties/AssemblyInfo.cs
@@ -30,30 +30,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrleansVSTools11")]
+[assembly: AssemblyTitle("OrleansVSTools")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cloud Computing Futures")]
-[assembly: AssemblyProduct("OrleansVSTools11")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]     
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("OrleansVSTools11_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100eb81539dc2728946d4860bad0fbc91ea23d7a835e00547aa4e9a7abf185535b564d29dbe77915b779951fef2f4b1ab39cc3a246cf572c7b217b5c892835a687eb22638183d66930039ff9fa6e7e71cb880f854fb76b884dfc905bf1f3c623abad7d4732c1ce62efff937155e352170831239acbe103a41ed363a90001d3108ca")]
-
