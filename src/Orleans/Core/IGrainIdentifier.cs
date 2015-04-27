027dabd80d8bf3f891ab26ac34434312f95563ef
nbilal <nbilal@microsoft.com>
2015-04-27 15:05:15 -0700

    Add GrainLocator and GrainIdentifier properties to the Grain class.
    Adding these two properties makes it possible to unittest a grain without starting a Silo.

---
+++ src/Orleans/Core/IGrainIdentifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans.Core
+{
+    public interface IGrainIdentifier
+    {
+        Guid AsGuid();
+
+        long AsLong();
+
+        string AsString();
+    }
+}
---
+++ src/Orleans/Core/IGrainLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans.Core
+{
+    public interface IGrainLocator
+    {
+        TGrainInterface GetGrain<TGrainInterface>(Guid primaryKey, string grainClassNamePrefix = null) where TGrainInterface : IGrainWithGuidKey;
+
+        TGrainInterface GetGrain<TGrainInterface>(long primaryKey, string grainClassNamePrefix = null) where TGrainInterface : IGrainWithIntegerKey;
+
+        TGrainInterface GetGrain<TGrainInterface>(string primaryKey, string grainClassNamePrefix = null) where TGrainInterface : IGrainWithStringKey;
+    }
+}
