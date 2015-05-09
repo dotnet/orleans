66a19c237194835f01eff5a29a7e7fe5186f5144
jthelin <github@thelin.me>
2015-05-09 16:53:08 -0700

    Rename CounterControl.exe -> OrleansCounterControl.exe
    - Rename CounterControl.exe to that Orleans related .exe's are easy to identity.

--- src/CounterControl/Properties/AssemblyInfo.cs
+++ src/CounterControl/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CounterControl")]
+[assembly: AssemblyTitle("OrleansCounterControl")]
 [assembly: AssemblyDescription("Orleans - Performance Counters Registration Tool")]
 [assembly: AssemblyConfiguration("")]
 
