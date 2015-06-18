da1d4933ef6453fcf1c309b30a1eb440e401d88b
gabikliot <gkliot@microsoft.com>
2015-06-17 22:30:53 -0700

    Fixed build warnings due to broken or missing XML comments.

--- src/OrleansAzureUtils/Providers/Streams/AzureQueue/SimpleAzureQueueAdapterFactory.cs
+++ src/OrleansAzureUtils/Providers/Streams/AzureQueue/SimpleAzureQueueAdapterFactory.cs
@@ -29,37 +29,43 @@ using Orleans.Runtime;
 
 namespace Orleans.Providers.Streams.AzureQueue
 {
+    /// <summary> Factory class for Simple Azure Queue based stream provider.</summary>
     public class SimpleAzureQueueAdapterFactory : IQueueAdapterFactory
     {
         private string dataConnectionString;
         private string queueName;
         private string providerName;
 
-        public const string DATA_CONNECTION_STRING = "DataConnectionString";
+        /// <summary>"QueueName".</summary>
         public const string QUEUE_NAME_STRING = "QueueName";
-        
+
+        /// <summary> Init the factory.</summary>
         public virtual void Init(IProviderConfiguration config, string providerName, Logger logger)
         {
             if (config == null) throw new ArgumentNullException("config");
-            if (!config.Properties.TryGetValue(DATA_CONNECTION_STRING, out dataConnectionString))
-                throw new ArgumentException(String.Format("{0} property not set", DATA_CONNECTION_STRING));
+            if (!config.Properties.TryGetValue(AzureQueueAdapterFactory.DATA_CONNECTION_STRING, out dataConnectionString))
+                throw new ArgumentException(String.Format("{0} property not set", AzureQueueAdapterFactory.DATA_CONNECTION_STRING));
             if (!config.Properties.TryGetValue(QUEUE_NAME_STRING, out queueName))
                 throw new ArgumentException(String.Format("{0} property not set", QUEUE_NAME_STRING));
 
             this.providerName = providerName;
         }
 
+
+        /// <summary>Creates the Simple Azure Queue based adapter.</summary>
         public virtual Task<IQueueAdapter> CreateAdapter()
         {
             var adapter = new SimpleAzureQueueAdapter(dataConnectionString, providerName, queueName);
             return Task.FromResult<IQueueAdapter>(adapter);
         }
 
+        /// <summary>Creates the adapter cache.</summary>
         public virtual IQueueAdapterCache GetQueueAdapterCache()
         {
             throw new OrleansException("SimpleAzureQueueAdapter is a write-only adapter, it does not support reading from the queue and thus does not need cache.");
         }
 
+        /// <summary>Creates the factory stream queue mapper.</summary>
         public IStreamQueueMapper GetStreamQueueMapper()
         {
             throw new OrleansException("SimpleAzureQueueAdapter does not support multiple queues, it only writes to one queue.");
