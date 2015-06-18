529585ceb103c9a8d7c6adaafbcd52e6846465c0
gabikliot <gkliot@microsoft.com>
2015-06-17 23:18:11 -0700

    Cleaned up and restructured Membership tests.

--- src/TesterInternal/StorageTests/MembershipTablePluginTests.cs
+++ src/TesterInternal/StorageTests/MembershipTablePluginTests.cs
@@ -34,80 +34,27 @@ using Orleans.TestingHost;
 
 namespace UnitTests.StorageTests
 {
-    [TestClass]
-    [DeploymentItem(@"Data\TestDb.mdf")]
     public class MembershipTablePluginTests
     {
         public TestContext TestContext { get; set; }
-        private static int counter;
         private static string hostName = Dns.GetHostName();
         private static readonly TraceLogger logger = TraceLogger.GetLogger("MembershipTablePluginTests");
-
-        // Test methods 
-
-        [TestMethod, TestCategory("Liveness"), TestCategory("SqlServer")]
-        public async Task MT_Init_SqlServer()
-        {
-            var membership = await GetMemebershipTable_SQL(TestContext.DeploymentDirectory);
-            Assert.IsNotNull(membership, "Membership Table handler created");
-        }
-
-        [TestMethod, TestCategory("Liveness"), TestCategory("SqlServer")]
-        public async Task MT_ReadAll_SqlServer()
-        {
-            var membership = await GetMemebershipTable_SQL(TestContext.DeploymentDirectory);
-            await MembershipTable_ReadAll(membership);
-        }
-
-        [TestMethod, TestCategory("Liveness"), TestCategory("SqlServer")]
-        public async Task MT_InsertRow_SqlServer()
-        {
-            var membership = await GetMemebershipTable_SQL(TestContext.DeploymentDirectory);
-            await MembershipTable_InsertRow(membership);
-        }
-
-        [TestMethod, TestCategory("Liveness"), TestCategory("ZooKeeper")]
-        public async Task MT_Init_ZooKeeper()
-        {
-            var membership = await GetMembershipTable_ZooKeeper();
-            Assert.IsNotNull(membership, "Membership Table handler created");
-        }
-
-        [TestMethod, TestCategory("Liveness"), TestCategory("ZooKeeper")]
-        public async Task MT_ReadAll_ZooKeeper()
-        {
-            var membership = await GetMembershipTable_ZooKeeper();
-            await MembershipTable_ReadAll(membership);
-        }
-
-        [TestMethod, TestCategory("Liveness"), TestCategory("ZooKeeper")]
-        public async Task MT_InsertRow_ZooKeeper()
-        {
-            var membership = await GetMembershipTable_ZooKeeper();
-            await MembershipTable_InsertRow(membership);
-        }
-
-        // Test function methods
-
-        internal static async Task<IMembershipTable> GetMemebershipTable_Azure()
-        {
-            return await GetMembershipTable(GlobalConfiguration.LivenessProviderType.AzureTable);
-        }
-
-        internal static async Task<IMembershipTable> GetMemebershipTable_SQL(string deploymentDirectory)
-        {
-            return await GetMembershipTable(GlobalConfiguration.LivenessProviderType.SqlServer, deploymentDirectory);
-        }
-
-        internal static async Task<IMembershipTable> GetMembershipTable_ZooKeeper()
-        {
-            return await GetMembershipTable(GlobalConfiguration.LivenessProviderType.ZooKeeper);
-        }
 
         internal static async Task MembershipTable_ReadAll(IMembershipTable membership)
         {
             var membershipData = await membership.ReadAll();
             Assert.IsNotNull(membershipData, "Membership Data not null");
+
+            TableVersion tableVersion = membershipData.Version;
+            logger.Info("Membership.ReadAll returned VableVersion={0} Data={1}", tableVersion, membershipData);
+
+            Assert.AreEqual(0, membershipData.Members.Count, "Number of records returned - no table version row");
+
+            string eTag = tableVersion.VersionEtag;
+            int ver = tableVersion.Version;
+
+            Assert.IsNotNull(eTag, "ETag should not be null");
+            Assert.AreEqual(0, ver, "Initial tabel version should be zero");
         }
 
         internal static async Task MembershipTable_InsertRow(IMembershipTable membership)
@@ -125,22 +72,7 @@ namespace UnitTests.StorageTests
             Assert.AreEqual(1, membershipData.Members.Count, "Should be one row after insert: {0}", membershipData);
         }
 
-        internal static async Task MembershipTable_ReadAll_0(IMembershipTable membership)
-        {
-            MembershipTableData data = await membership.ReadAll();
-            TableVersion tableVersion = data.Version;
-            logger.Info("Membership.ReadAll returned VableVersion={0} Data={1}", tableVersion, data);
-
-            Assert.AreEqual(0, data.Members.Count, "Number of records returned - no table version row");
-
-            string eTag = tableVersion.VersionEtag;
-            int ver = tableVersion.Version;
-
-            Assert.IsNotNull(eTag, "ETag should not be null");
-            Assert.AreEqual(0, ver, "Initial tabel version should be zero");
-        }
-
-        internal static async Task MembershipTable_ReadRow_0(IMembershipTable membership, SiloAddress siloAddress)
+        internal static async Task MembershipTable_ReadRow_EmptyTable(IMembershipTable membership, SiloAddress siloAddress)
         {
             MembershipTableData data = await membership.ReadRow(siloAddress);
             TableVersion tableVersion = data.Version;
@@ -154,10 +86,10 @@ namespace UnitTests.StorageTests
             logger.Info("Membership.ReadRow returned MembershipEntry ETag={0} TableVersion={1}", eTag, tableVersion);
 
             Assert.IsNotNull(eTag, "ETag should not be null");
-            Assert.AreEqual(0, ver, "Initial tabel version should be zero");
+            Assert.AreEqual(0, ver, "Initial table version should be zero");
         }
 
-        internal static async Task MembershipTable_ReadRow_1(IMembershipTable membership, SiloAddress siloAddress)
+        internal static async Task MembershipTable_ReadRow_Insert_Read(IMembershipTable membership, SiloAddress siloAddress)
         {
             MembershipTableData data = await membership.ReadAll();
             TableVersion tableVersion = data.Version;
@@ -196,7 +128,7 @@ namespace UnitTests.StorageTests
             Assert.IsNotNull(MembershipEntry, "MembershipEntry should not be null");
         }
 
-        internal static async Task MembershipTable_ReadAll_1(IMembershipTable membership, SiloAddress siloAddress)
+        internal static async Task MembershipTable_ReadAll_Insert_ReadAll(IMembershipTable membership, SiloAddress siloAddress)
         {
             MembershipTableData data = await membership.ReadAll();
             TableVersion tableVersion = data.Version;
@@ -238,7 +170,7 @@ namespace UnitTests.StorageTests
 
         private static MembershipEntry CreateMembershipEntryForTest()
         {
-            var siloAddress = SiloAddress.NewLocalAddress(counter++);
+            var siloAddress = SiloAddress.NewLocalAddress(SiloAddress.AllocateNewGeneration());
 
             var now = DateTime.UtcNow;
 
@@ -254,40 +186,6 @@ namespace UnitTests.StorageTests
             };
 
             return membershipEntry;
-        }
-
-        private static async Task<IMembershipTable> GetMembershipTable(GlobalConfiguration.LivenessProviderType membershipType, 
-            string deploymentDirectory = null)
-        {
-            string runId = Guid.NewGuid().ToString("N");
-
-            var config = new GlobalConfiguration { LivenessType = membershipType, DeploymentId = runId };
-
-            IMembershipTable membership;
-
-            switch(membershipType)
-            {
-                case GlobalConfiguration.LivenessProviderType.AzureTable:
-                    config.DataConnectionString = StorageTestConstants.DataConnectionString;
-                    membership = new AzureBasedMembershipTable();
-                    break;
-
-                case GlobalConfiguration.LivenessProviderType.SqlServer:
-                    config.DataConnectionString = StorageTestConstants.GetSqlConnectionString(deploymentDirectory);
-                    membership = new SqlMembershipTable();
-                    break;
-
-                case GlobalConfiguration.LivenessProviderType.ZooKeeper:
-                    config.DataConnectionString = StorageTestConstants.GetZooKeeperConnectionString();
-                    membership = AssemblyLoader.LoadAndCreateInstance<IMembershipTable>(Constants.ORLEANS_ZOOKEEPER_UTILS_DLL, logger);
-                    break;
-
-                default:
-                    throw new NotImplementedException(membershipType.ToString());
-            }
-
-            await membership.InitializeMembershipTable(config, true, TraceLogger.GetLogger(membership.GetType().Name));
-            return membership;
         }
     }
 }
