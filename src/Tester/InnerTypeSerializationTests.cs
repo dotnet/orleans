c51fcee0dc0b2e06b8f1e280d991228419d88be6
Gutemberg Ribeiro <gutemberg@fgrit.com>
2015-07-27 15:31:16 -0300

    Missing nuget update

--- src/Tester/InnerTypeSerializationTests.cs
+++ src/Tester/InnerTypeSerializationTests.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Orleans.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Orleans.CodeGeneration;
 
 namespace Tester
 {
@@ -57,5 +56,92 @@ namespace Tester
         }
         public Guid Id { get; set; }
         public string Something { get; set; }
+    }
+
+    /// <summary>
+    /// Provides support for serializing JSON values.
+    /// </summary>
+    [RegisterSerializer]
+    public class JsonSerialization
+    {
+        private static JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        /// <summary>
+        /// Initializes static members of the <see cref="JsonSerialization"/> class.
+        /// </summary>
+        static JsonSerialization()
+        {
+            Register();
+        }
+
+        /// <summary>
+        /// The deep copier.
+        /// </summary>
+        /// <param name="original">
+        /// The original.
+        /// </param>
+        /// <returns>
+        /// The copy.
+        /// </returns>
+        public static object DeepCopier(object original)
+        {
+            return original;
+        }
+
+        /// <summary>
+        /// Serializes an object to a stream.
+        /// </summary>
+        /// <param name="obj">
+        /// The object being serialized.
+        /// </param>
+        /// <param name="stream">
+        /// The stream to serialize to.
+        /// </param>
+        /// <param name="expected">
+        /// The expected type.
+        /// </param>
+        public static void Serialize(object obj, BinaryTokenStreamWriter stream, Type expected)
+        {
+            var str = JsonConvert.SerializeObject(obj, expected, _settings);
+            SerializationManager.SerializeInner(str, stream, typeof(string));
+        }
+
+        /// <summary>
+        /// Deserializes a JSON object.
+        /// </summary>
+        /// <param name="expected">
+        /// The expected type.
+        /// </param>
+        /// <param name="stream">
+        /// The stream.
+        /// </param>
+        /// <returns>
+        /// The deserialized object.
+        /// </returns>
+        public static object Deserialize(Type expected, BinaryTokenStreamReader stream)
+        {
+            var str = (string)SerializationManager.DeserializeInner(typeof(string), stream);
+            return JsonConvert.DeserializeObject(str, expected);
+        }
+
+        /// <summary>
+        /// Registers this class with the <see cref="SerializationManager"/>.
+        /// </summary>
+        public static void Register()
+        {
+            foreach (
+                var type in
+                    new[]
+                    {
+                        typeof(JObject), typeof(JArray), typeof(JToken), typeof(JValue), typeof(JProperty),
+                        typeof(JConstructor), 
+                    })
+            {
+                SerializationManager.Register(type, DeepCopier, Serialize, Deserialize);
+            }
+        }
     }
 }
