84f43640db0b26ee6f7569d4f994d9fb2edeef40
gabikliot <gkliot@microsoft.com>
2015-08-15 22:31:02 -0700

    No need for OnActivateAsync.

--- src/TestGrains/InitialStateGrain.cs
+++ src/TestGrains/InitialStateGrain.cs
@@ -16,11 +16,6 @@ namespace UnitTests.Grains
     }
     public class InitialStateGrain : Grain<Initialized_State>, IInitialStateGrain
     {
-        public override Task OnActivateAsync()
-        {
-            return TaskDone.Done;
-        }
-
         public Task<List<string>> GetNames()
         {
             return Task.FromResult(State.Names);
