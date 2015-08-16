76e43f0f74a35e8f1507993a7f079701ddae3dea
gabikliot <gkliot@microsoft.com>
2015-08-15 22:28:46 -0700

    Fixed a bug of MemoryStorageProvider does not respect grain state default constructor upon initialization.

--- src/TestGrains/InitialStateGrain.cs
+++ src/TestGrains/InitialStateGrain.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Orleans;
 using UnitTests.GrainInterfaces;
+
 namespace UnitTests.Grains
 {
     public class Initialized_State : GrainState
@@ -17,6 +16,11 @@ namespace UnitTests.Grains
     }
     public class InitialStateGrain : Grain<Initialized_State>, IInitialStateGrain
     {
+        public override Task OnActivateAsync()
+        {
+            return TaskDone.Done;
+        }
+
         public Task<List<string>> GetNames()
         {
             return Task.FromResult(State.Names);
