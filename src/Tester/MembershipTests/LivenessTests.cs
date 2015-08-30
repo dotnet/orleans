adc72624cf2d131b7e76fb8e2e8542e3929aaa59
Veikko Eeva <veikko_e@hotmail.com>
2015-08-30 09:55:36 +0300

    Cleanup of relational storage code
    * Removed dependency on statics when using relational storage.
    * Use of constants clarified by grouping them by usage.
    * Constant classes saved to each of their own files.
    * Introduced SqlTestsEnvironment (by @kucheruk) also to Liveness test.
    * Refactored the relational test initialization to make it easier to
      introduce other backends to the mix.
    * Added example usage documentation on IRelationalStorage usage.
    * Enhanced the multi-insert helper.
    
    Still to do, batch insert could use a special "skip list" for columns
    that will not be batch inserted and consequently simplifying Orleans
    statistics inserts code, which is a near duplicate at the moment.
    
    Fixes #650 when ready.

--- src/Tester/MembershipTests/LivenessTests.cs
+++ src/Tester/MembershipTests/LivenessTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,8 +11,7 @@ using Orleans.TestingHost;
 using UnitTests.GrainInterfaces;
 using UnitTests.Tester;
 using Orleans.Runtime.Storage.Relational;
-using Orleans.Runtime.Storage.Management;
-using Orleans.Runtime.Storage.RelationalExtensions;
+using UnitTests.General;
 
 namespace UnitTests.MembershipTests
 {
@@ -449,33 +447,8 @@ namespace UnitTests.MembershipTests
             Console.WriteLine(DumpTestContext(context));
 
             Console.WriteLine("Initializing relational databases...");
-            relationalStorage = RelationalStorageUtilities.CreateDefaultSqlServerStorageInstance();
-
-            Console.WriteLine("Dropping and recreating database '{0}' with connectionstring '{1}'", testDatabaseName, relationalStorage.ConnectionString);
-            if(relationalStorage.ExistsDatabaseAsync(testDatabaseName).Result)
-            {
-                relationalStorage.DropDatabaseAsync(testDatabaseName).Wait();
-            }
-            relationalStorage.CreateDatabaseAsync(testDatabaseName).Wait();
-
-            //The old storage instance has the previous connection string, time have a new handle with a new connection string...
-            relationalStorage = relationalStorage.CreateNewStorageInstance(testDatabaseName);
-
-            Console.WriteLine("Creating database tables...");
-            var creationScripts = RelationalStorageUtilities.RemoveBatchSeparators(File.ReadAllText("CreateOrleansTables_SqlServer.sql"));
-            foreach(var creationScript in creationScripts)
-            {
-                var res = relationalStorage.ExecuteAsync(creationScript).Result;
-            }
-
-            //Currently there's only one database under test, SQL Server. So this as the other
-            //setup is hardcoded here: putting the database in simple recovery mode.
-            //This removes the use of recovery log in case of database crashes, which
-            //improves performance to some degree, depending on usage. For non-performance testing only.
-            var simpleModeRes = relationalStorage.ExecuteAsync(string.Format("ALTER DATABASE [{0}] SET RECOVERY SIMPLE;", testDatabaseName)).Result;
-
-            Console.WriteLine("Initializing relational databases done.");
-
+            relationalStorage = SqlTestsEnvironment.Setup(testDatabaseName);
+                        
             siloOptions.DataConnectionString = relationalStorage.ConnectionString;
         }
 
