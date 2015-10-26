1628e8d812b6415b5ff8933c69d0afdc7b98ea59
Gutemberg Ribeiro <gutemberg@fgrit.com>
2015-10-26 01:51:46 -0200

    Added Circular reference test to reproduce grain states which contains circular reference and the "Reference with no referred object" case

---
+++ src/TestGrainInterfaces/ICircularStateTestGrain.cs
@@ -0,0 +1,32 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TestGrainInterfaces
+{
+    public interface ICircularStateTestGrain : IGrainWithGuidCompoundKey
+    {
+        Task<CircularTest1> GetState();
+    }
+
+    public class CircularStateTestState : GrainState
+    {
+        public CircularTest1 CircularTest1 { get; set; }
+    }
+
+    [Serializable]
+    public class CircularTest1
+    {
+        public CircularTest2 CircularTest2 { get; set; }
+    }
+    [Serializable]
+    public class CircularTest2
+    {
+        public CircularTest2()
+        {
+            CircularTest1List = new List<CircularTest1>();
+        }
+        public List<CircularTest1> CircularTest1List { get; set; }
+    }
+}
