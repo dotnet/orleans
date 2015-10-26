b074c11ceb5a5c32a37573c97338ba4c4a3838dc
jthelin <jthelin@microsoft.com>
2015-10-26 06:28:28 -0700

    Revert "Added Circular reference test to reproduce grain states which contains circular reference and the "Reference with no referred object" case"
    This reverts commit 1628e8d812b6415b5ff8933c69d0afdc7b98ea59.

--- src/Tester/GenericGrainTests.cs
+++ src/Tester/GenericGrainTests.cs
@@ -31,7 +31,6 @@ using Orleans.TestingHost;
 using UnitTests.GrainInterfaces;
 using UnitTests.Tester;
 using System.Collections.Generic;
-using TestGrainInterfaces;
 
 namespace UnitTests.General
 {
@@ -667,14 +666,6 @@ namespace UnitTests.General
             await Task.Delay(TimeSpan.FromSeconds(6));
             var s2 = await grain.GetLastValue();
             Assert.AreEqual(s1, s2);
-        }
-
-        [TestMethod, TestCategory("Functional"), TestCategory("Generics"), TestCategory("Serialization")]
-        public async Task Generic_CircularReferenceTest()
-        {
-            var grainId = Guid.NewGuid();
-            var grain = GrainFactory.GetGrain<ICircularStateTestGrain>(primaryKey: grainId, keyExtension: grainId.ToString("N"));
-            var c1 = await grain.GetState();
         }
     }
 }
