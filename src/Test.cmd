5dbc06ecb7a0c632f8eb5cd21cb6692004f29e4f
jthelin <jthelin@microsoft.com>
2015-11-08 00:53:40 -0800

    Switch from using MSTest to use VSTest Runner for running test cases.
    - Appveyer and VSO scripted build systems both use VSTest, but Jenkins was using MSTest because of Test.cmd settings.
    
    - I am not exactly sure why Test.cmd works ok for me locally -- i suspect it is because i have VS2013 installed instead of VS2015.
    
    - Simplify TestAll.cmd to just call Test.cmd with a different TestCategory list parameter.

--- src/Test.cmd
+++ src/Test.cmd
@@ -1,20 +1,32 @@
-@REM NOTE: This script must be run from a Visual Studio command prompt window
-
 @setlocal
-@ECHO on
+@ECHO off
 
-SET CMDHOME=%~dp0.
+if .%TEST_CATEGORIES%. == .. set TEST_CATEGORIES="TestCategory=BVT"
+
+SET CONFIGURATION=Release
+
+SET CMDHOME=%~dp0
+@REM Remove trailing backslash \
+set CMDHOME=%CMDHOME:~0,-1%
+
 if "%FrameworkDir%" == "" set FrameworkDir=%WINDIR%\Microsoft.NET\Framework
 if "%FrameworkVersion%" == "" set FrameworkVersion=v4.0.30319
 
-SET MSTESTEXEDIR=%VS120COMNTOOLS%..\IDE
-SET MSTESTEXE=%MSTESTEXEDIR%\MSTest.exe
-
-SET CONFIGURATION=Release
-SET OutDir=%CMDHOME%\..\Binaries\%CONFIGURATION%
+SET VSIDEDIR=%VS120COMNTOOLS%..\IDE
+SET VSTESTEXEDIR=%VSIDEDIR%\CommonExtensions\Microsoft\TestWindow
+SET VSTESTEXE=%VSTESTEXEDIR%\VSTest.console.exe
 
 cd "%CMDHOME%"
+@cd
 
-set TEST_ARGS= /testcontainer:%OutDir%\Tester.dll /testcontainer:%OutDir%\TesterInternal.dll 
+SET OutDir=%CMDHOME%\..\Binaries\%CONFIGURATION%
 
-"%MSTESTEXE%" %TEST_ARGS% /category:"BVT"
+set TESTS=%OutDir%\Tester.dll %OutDir%\TesterInternal.dll 
+@Echo Test assemblies = %TESTS%
+
+set TEST_ARGS= /Settings:%CMDHOME%\Local.testsettings
+set TEST_ARGS= %TEST_ARGS% /TestCaseFilter:%TEST_CATEGORIES%
+
+@echo on
+
+"%VSTESTEXE%" %TEST_ARGS% %TESTS%
--- src/TestAll.cmd
+++ src/TestAll.cmd
@@ -1,20 +1,16 @@
-@REM NOTE: This script must be run from a Visual Studio command prompt window
-
 @setlocal
-@ECHO on
+@ECHO off
 
-SET CMDHOME=%~dp0.
-if "%FrameworkDir%" == "" set FrameworkDir=%WINDIR%\Microsoft.NET\Framework
-if "%FrameworkVersion%" == "" set FrameworkVersion=v4.0.30319
+SET CMDHOME=%~dp0
+@REM Remove trailing backslash \
+set CMDHOME=%CMDHOME:~0,-1%
 
-SET MSTESTEXEDIR=%VS120COMNTOOLS%..\IDE
-SET MSTESTEXE=%MSTESTEXEDIR%\MSTest.exe
+@REM Due to more of Windows .cmd script parameter passing quirks, we can't pass this value as cmdline argument, 
+@REM  so we need to pass it in through the back door as environment variable, scoped by setlocal
+set TEST_CATEGORIES="TestCategory=BVT|TestCategory=Functional"
 
-SET CONFIGURATION=Release
-SET OutDir=%CMDHOME%\..\Binaries\%CONFIGURATION%
+@REM Note: We transfer _complete_ control to the Test.cmd script here because we don't use CALL.
 
-cd "%CMDHOME%"
+"%CMDHOME%\Test.cmd"
 
-set TEST_ARGS= /testcontainer:%OutDir%\Tester.dll /testcontainer:%OutDir%\TesterInternal.dll 
-
-"%MSTESTEXE%" %TEST_ARGS% /category:"BVT|Functional"
+@REM Note: Execution will NOT return here, and the exit code returned to the caller will be whatever the other script returned.
