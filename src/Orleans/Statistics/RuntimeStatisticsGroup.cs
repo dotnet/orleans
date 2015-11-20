33124b83c7dd3130f33d9d66a48905693c9c3c8d
Ashley Mannix <Ashley.Mannix@sunshinecoast.qld.gov.au>
2015-11-20 10:22:27 +1000

    Remove accidental ifdef on setting stats

--- src/Orleans/Statistics/RuntimeStatisticsGroup.cs
+++ src/Orleans/Statistics/RuntimeStatisticsGroup.cs
@@ -182,13 +182,11 @@ namespace Orleans.Runtime
             FloatValueStatistic.FindOrCreate(StatisticNames.RUNTIME_GC_PROMOTEDMEMORYFROMGEN0KB, () => promotedFinalizationMemoryFromGen0.NextValue() / 1024f);
             FloatValueStatistic.FindOrCreate(StatisticNames.RUNTIME_GC_NUMBEROFINDUCEDGCS, () => numberOfInducedGCs.NextValue());
 
-#if DNXCORE50
             IntValueStatistic.FindOrCreate(StatisticNames.RUNTIME_MEMORY_TOTALPHYSICALMEMORYMB, () => (TotalPhysicalMemory / 1024) / 1024);
             if (availableMemoryCounter != null)
             {
                 IntValueStatistic.FindOrCreate(StatisticNames.RUNTIME_MEMORY_AVAILABLEMEMORYMB, () => (AvailableMemory/ 1024) / 1024); // Round up
             }
-#endif
 #endif
             IntValueStatistic.FindOrCreate(StatisticNames.RUNTIME_DOT_NET_THREADPOOL_INUSE_WORKERTHREADS, () =>
             {
