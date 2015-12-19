7a65abc9d9a3c15b45bc3afed028368784230984
Gabriel Kliot <gkliot@gmail.com>
2015-12-18 22:42:05 -0800

    Changed project name to OrleansGoogleUtils.

--- src/OrleansGoogleUtils/Properties/AssemblyInfo.cs
+++ src/OrleansGoogleUtils/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrleansGoogleCloudPlatformUtils")]
+[assembly: AssemblyTitle("OrleansGoogleUtils")]
 [assembly: AssemblyDescription("Protobuf Serializer for Orleans")]
-[assembly: AssemblyProduct("OrleansGoogleCloudPlatformUtils")]
+[assembly: AssemblyProduct("OrleansGoogleUtils")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
