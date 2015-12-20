a8a75088ee3c92f00d36401d0a5ce0a6ce787505
erano.of <eran.of@gigya-inc.com>
2015-12-20 09:12:43 +0200

    Bugfix: GetReminder throws exception when reminder don't exists #1167

---
+++ src/TestGrains/ReminderTestGrain.cs
@@ -0,0 +1,15 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+using UnitTests.GrainInterfaces;
+
+namespace UnitTests.Grains
+{
+    internal class ReminderTestGrain : Grain, IReminderTestGrain
+    {
+        public async Task<bool> IsReminderExists(string reminderName)
+        {
+            var reminder = await this.GetReminder(reminderName);
+            return reminder != null;
+        }
+    }
+}
