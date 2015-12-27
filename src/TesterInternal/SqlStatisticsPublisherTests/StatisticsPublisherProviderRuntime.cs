2b566db524d7f0dd4a26221dd196e536bb27c5a5
Shay Hazor <shayhatsor@gmail.com>
2015-12-27 16:30:12 +0200

    Relational Statistics Publisher tests

---
+++ src/TesterInternal/SqlStatisticsPublisherTests/StatisticsPublisherProviderRuntime.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Orleans;
+using Orleans.Providers;
+using Orleans.Runtime;
+
+namespace UnitTests.SqlStatisticsPublisherTests
+{
+    internal class StatisticsPublisherProviderRuntime : IProviderRuntime
+    {
+        private readonly Logger logger;
+
+        public StatisticsPublisherProviderRuntime(Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public Logger GetLogger(string loggerName)
+        {
+            return logger;
+        }
+
+        public Guid ServiceId
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string SiloIdentity
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IGrainFactory GrainFactory
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IServiceProvider ServiceProvider
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
