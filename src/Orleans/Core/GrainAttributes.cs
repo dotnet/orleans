d1d5005e6c529fec99e615b8cfdf7bd1a35b0bee
Alexey Shcherbak <alexey@drawboard.com>
2016-01-15 08:58:28 +1100

    fix: revert back Provider part of the change so we'll only make Namespace field public

--- src/Orleans/Core/GrainAttributes.cs
+++ src/Orleans/Core/GrainAttributes.cs
@@ -280,25 +280,11 @@ namespace Orleans
     public sealed class ImplicitStreamSubscriptionAttribute : Attribute
     {
         public string Namespace { get; private set; }
-
-        public string Provider { get; private set; }
-
+        
         // We have not yet come to an agreement whether the provider should be specified as well.
         public ImplicitStreamSubscriptionAttribute(string streamNamespace)
         {
             Namespace = streamNamespace;
-        }
-
-
-        /// <summary>
-        /// Constructor which allows you to specify both namespace and provider names
-        /// </summary>
-        /// <param name="streamNamespace"></param>
-        /// <param name="streamProvider"></param>
-        public ImplicitStreamSubscriptionAttribute(string streamNamespace, string streamProvider)
-        {
-            Namespace = streamNamespace;
-            Provider = streamProvider;
         }
     }
 }
