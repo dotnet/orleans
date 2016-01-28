b33589793bc580dceaadba9bc10c8709deeaf20e
Catalin Bocirnea <catalin@aetheris.ca>
2016-01-27 18:45:37 -1000

    Initialize SerializationManager before CodeGeneratorManager fixes #1344

--- src/OrleansRuntime/Silo/Silo.cs
+++ src/OrleansRuntime/Silo/Silo.cs
@@ -186,8 +186,8 @@ namespace Orleans.Runtime
             ActivationData.Init(config, nodeConfig);
             StatisticsCollector.Initialize(nodeConfig);
             
-            CodeGeneratorManager.GenerateAndCacheCodeForAllAssemblies();
             SerializationManager.Initialize(globalConfig.UseStandardSerializer, globalConfig.SerializationProviders, globalConfig.UseJsonFallbackSerializer);
+            CodeGeneratorManager.GenerateAndCacheCodeForAllAssemblies();
             initTimeout = globalConfig.MaxJoinAttemptTime;
             if (Debugger.IsAttached)
             {
