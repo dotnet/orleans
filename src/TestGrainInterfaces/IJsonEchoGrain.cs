ba4e9f06cc741098439ba2b0fdac8910bbe1c02a
Jorgen Thelin <jthelin@microsoft.com>
2016-02-03 10:58:30 -0800

    JSON Echo Grain
    - A test case for grains that use JSON JObject parameters for input / output.
    
    - Grain classes need to extend Grain base class.

---
+++ src/TestGrainInterfaces/IJsonEchoGrain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Orleans;
+
+namespace UnitTests.GrainInterfaces
+{
+    /// <summary>
+    /// Echo grain to test round trip of JSON data.
+    /// </summary>
+    public interface IJsonEchoGrain : IGrainWithIntegerKey
+    {
+        Task Ping();
+        Task<JObject> EchoJson(JObject data);
+    }
+}
