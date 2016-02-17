a98fbf6edabc4de812b1b8605dba821d44dce01e
Sergey Bykov <sbykov@microsoft.com>
2016-02-17 13:32:44 -0800

    Update global file versions to 1.2.0 to be consistent with the NuGet version.

--- src/Build/GlobalAssemblyInfo.fs
+++ src/Build/GlobalAssemblyInfo.fs
@@ -6,8 +6,8 @@ open System.Reflection
 [<assembly: AssemblyProduct("Orleans")>]
 [<assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2015")>]
 [<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.1.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.1.0.0")>]
+[<assembly: AssemblyFileVersion("1.2.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.2.0.0")>]
 
 do
     ()
