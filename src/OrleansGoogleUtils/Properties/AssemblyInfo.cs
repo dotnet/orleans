7111da8da84991ff5fb36c72d708d23985806ae4
Sergey Bykov <sbykov@microsoft.com>
2016-02-17 13:55:30 -0800

    Switched projects to using GlobalAssemblyInfo.cs.

--- src/OrleansGoogleUtils/Properties/AssemblyInfo.cs
+++ src/OrleansGoogleUtils/Properties/AssemblyInfo.cs
@@ -7,7 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("OrleansGoogleUtils")]
 [assembly: AssemblyDescription("Protobuf Serializer for Orleans")]
-[assembly: AssemblyProduct("OrleansGoogleUtils")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -16,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b9196cb5-d356-4228-b73f-143f2eb80c64")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
