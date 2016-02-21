cdfe0690107584b761bd335031481647cc8a64d2
Shay Hazor <shayhatsor@gmail.com>
2016-02-21 23:28:57 +0200

    Membership tests - Improved and refactored

--- src/TesterInternal/MembershipTests/AzureMembershipTableTests.cs
+++ src/TesterInternal/MembershipTests/AzureMembershipTableTests.cs
@@ -1,15 +1,10 @@
-using System;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Orleans;
 using Orleans.Runtime;
-using Orleans.Runtime.Configuration;
 using Orleans.Runtime.MembershipService;
 using Orleans.TestingHost;
-using Orleans.AzureUtils;
 using Tester;
-using UnitTests.StorageTests;
-using UnitTests.Tester;
 
 
 namespace UnitTests.MembershipTests
@@ -18,105 +13,68 @@ namespace UnitTests.MembershipTests
     /// Tests for operation of Orleans Membership Table using AzureStore - Requires access to external Azure storage
     /// </summary>
     [TestClass]
-    public class AzureMembershipTableTests
+    public class AzureMembershipTableTests : MembershipTableTestsBase
     {
-        public TestContext TestContext { get; set; }
-
-        private string deploymentId;
-        private AzureBasedMembershipTable membership;
-        private static readonly TimeSpan timeout = TimeSpan.FromMinutes(1);
-        private readonly TraceLogger logger = TraceLogger.GetLogger("AzureMembershipTableTests", TraceLogger.LoggerType.Application);
-
-        // Use ClassInitialize to run code before running the first test in the class
         [ClassInitialize]
-        public static void ClassInitialize(TestContext testContext)
+        public new static void ClassInitialize(TestContext testContext)
         {
-            TraceLogger.Initialize(new NodeConfiguration());
+            MembershipTableTestsBase.ClassInitialize();
             TraceLogger.AddTraceLevelOverride("AzureTableDataManager", Severity.Verbose3);
             TraceLogger.AddTraceLevelOverride("OrleansSiloInstanceManager", Severity.Verbose3);
             TraceLogger.AddTraceLevelOverride("Storage", Severity.Verbose3);
 
-            // Set shorter init timeout for these tests
-            OrleansSiloInstanceManager.initTimeout = TimeSpan.FromSeconds(20);
-
             TestUtils.CheckForAzureStorage();
         }
 
-        private async Task Initialize()
+        protected override IMembershipTable CreateMembershipTable(TraceLogger logger)
         {
-            deploymentId = "test-" + Guid.NewGuid();
-            logger.Info("DeploymentId={0}", deploymentId);
-
-            var config = new GlobalConfiguration
-            {
-                DeploymentId = deploymentId,
-                DataConnectionString = StorageTestConstants.DataConnectionString
-            };
-
-            var mbr = new AzureBasedMembershipTable();
-            await mbr.InitializeMembershipTable(config, true, logger).WithTimeout(timeout);
-            membership = mbr;
+            return new AzureBasedMembershipTable();
         }
 
-        // Use TestCleanup to run code after each test has run
-        [TestCleanup]
-        public void TestCleanup()
+        protected override string GetConnectionString()
         {
-            if (membership != null && SiloInstanceTableTestConstants.DeleteEntriesAfterTest)
-            {
-                membership.DeleteMembershipTableEntries(deploymentId).Wait();
-                membership = null;
-            }
-            logger.Info("Test {0} completed - Outcome = {1}", TestContext.TestName, TestContext.CurrentTestOutcome);
-        }
-
-        [ClassCleanup]
-        public static void ClassCleanup()
-        {
-            // Reset init timeout after tests
-            OrleansSiloInstanceManager.initTimeout = AzureTableDefaultPolicies.TableCreationTimeout;
+            return StorageTestConstants.DataConnectionString;
         }
 
         [TestMethod, TestCategory("Functional"), TestCategory("Membership"), TestCategory("Azure")]
-        public async Task MembershipTable_Azure_Init()
+        public void MembershipTable_Azure_Init()
         {
-            await Initialize();
-            Assert.IsNotNull(membership, "Membership Table handler created");
         }
 
         [TestMethod, TestCategory("Functional"), TestCategory("Membership"), TestCategory("Azure")]
         public async Task MembershipTable_Azure_ReadAll_EmptyTable()
         {
-            await Initialize();
-            await MembershipTablePluginTests.MembershipTable_ReadAll_EmptyTable(membership);
+            await MembershipTable_ReadAll_EmptyTable();
         }
 
         [TestMethod, TestCategory("Functional"), TestCategory("Membership"), TestCategory("Azure")]
         public async Task MembershipTable_Azure_InsertRow()
         {
-            await Initialize();
-            await MembershipTablePluginTests.MembershipTable_InsertRow(membership);
+            await MembershipTable_InsertRow();
         }
 
         [TestMethod, TestCategory("Functional"), TestCategory("Membership"), TestCategory("Azure")]
         public async Task MembershipTable_Azure_ReadRow_Insert_Read()
         {
-            await Initialize();
-            await MembershipTablePluginTests.MembershipTable_ReadRow_Insert_Read(membership);
+            await MembershipTable_ReadRow_Insert_Read();
         }
 
         [TestMethod, TestCategory("Functional"), TestCategory("Membership"), TestCategory("Azure")]
         public async Task MembershipTable_Azure_ReadAll_Insert_ReadAll()
         {
-            await Initialize();
-            await MembershipTablePluginTests.MembershipTable_ReadAll_Insert_ReadAll(membership);
+            await MembershipTable_ReadAll_Insert_ReadAll();
         }
 
         [TestMethod, TestCategory("Functional"), TestCategory("Membership"), TestCategory("Azure")]
         public async Task MembershipTable_Azure_UpdateRow()
         {
-            await Initialize();
-            await MembershipTablePluginTests.MembershipTable_UpdateRow(membership);
+            await MembershipTable_UpdateRow();
+        }
+
+        [TestMethod, TestCategory("Membership"), TestCategory("Azure")]
+        public async Task MembershipTable_Azure_UpdateRowInParallel()
+        {
+            await MembershipTable_UpdateRowInParallel();
         }
     }
 }
