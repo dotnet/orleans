d5c2cec2f9edff11d8841f463c2a150de388a6b0
Peter Rohde-Witthus <Peter Rohde-Witthus@implico.com>
2016-02-29 21:28:58 +0100

    Issue #1480 (https://github.com/dotnet/orleans/issues/1480)

---
+++ src/TesterInternal/CodeGeneratorTests_TakeSerializedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.GrainInterfaces;
+using UnitTests.Tester;
+
+namespace UnitTests.General
+{
+    /// <summary>
+    /// Unit tests for grains implementing generic interfaces
+    /// </summary>
+    [TestClass]
+    public class CodeGeneratorTests_TakeSerializedData : HostedTestClusterEnsureDefaultStarted
+    {
+        [TestMethod, TestCategory("BVT"), TestCategory("Functional"), TestCategory("CodeGen"), TestCategory("Serialization")]
+        public async Task TakeSerializedDataNotRefOrleans()
+        {
+            var grain = GrainFactory.GetGrain<ISerializerPresenceTest>(Guid.NewGuid());
+            await grain.TakeSerializedData(new Dtos.ClassNotReferencingOrleansTypeDto { MyProperty = "Test" });
+        }
+
+        [TestMethod, TestCategory("BVT"), TestCategory("Functional"), TestCategory("CodeGen"), TestCategory("Serialization")]
+        public async Task TakeSerializedDataRefOrleans()
+        {
+            var grain = GrainFactory.GetGrain<ISerializerPresenceTest>(Guid.NewGuid());
+            await grain.TakeSerializedData(new DtosRefOrleans.ClassReferencingOrleansTypeDto { MyProperty = "Test" });
+        }
+    }
+}
