872357ef8c8ecd9c2e70add9603e9a971b9e6f6f
Sergey Bykov <sbykov@microsoft.com>
2016-03-25 08:57:16 -0700

    Moved TestInternalDtos to TestInterfaces project. Removed TestInternalDtos project. Changed TakeSerializedDataRefOrleans test to SkippableFact.

--- test/TesterInternal/CodeGeneratorTests_TakeSerializedData.cs
+++ test/TesterInternal/CodeGeneratorTests_TakeSerializedData.cs
@@ -20,7 +20,7 @@ namespace UnitTests.General
             await grain.TakeSerializedData(new Dtos.ClassNotReferencingOrleansTypeDto { MyProperty = "Test" });
         }
 
-        [Fact, TestCategory("BVT"), TestCategory("Functional"), TestCategory("CodeGen"), TestCategory("Serialization")]
+        [SkippableFact, TestCategory("BVT"), TestCategory("Functional"), TestCategory("CodeGen"), TestCategory("Serialization")]
         public async Task TakeSerializedDataRefOrleans()
         {
             var grain = GrainFactory.GetGrain<ISerializerPresenceTest>(Guid.NewGuid());
