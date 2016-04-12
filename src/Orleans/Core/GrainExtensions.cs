90890072191a631aa777f705e2324accb922b476
Sergey Bykov <sbykov@microsoft.com>
2016-04-12 09:23:08 -0700

    Add an extension method GetPrimaryKeyString(this IAddressable grain).

--- src/Orleans/Core/GrainExtensions.cs
+++ src/Orleans/Core/GrainExtensions.cs
@@ -130,6 +130,7 @@ namespace Orleans
         {
             return GetGrainId(grain).GetPrimaryKeyLong();
         }
+
         /// <summary>
         /// Returns the Guid representation of a grain primary key.
         /// </summary>
@@ -149,6 +150,16 @@ namespace Orleans
         public static Guid GetPrimaryKey(this IAddressable grain)
         {
             return GetGrainId(grain).GetPrimaryKey();
+        }
+
+        /// <summary>
+        /// Returns the string primary key of the grain.
+        /// </summary>
+        /// <param name="grain">The grain to find the primary key for.</param>
+        /// <returns>A string representing the primary key for this grain.</returns>
+        public static string GetPrimaryKeyString(this IAddressable grain)
+        {
+            return GetGrainId(grain).GetPrimaryKeyString();
         }
 
         public static long GetPrimaryKeyLong(this IGrain grain, out string keyExt)
