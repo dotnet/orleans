43829b36b3470f04f22203cc7c07c996bc31b76a
Sergey Bykov <sbykov@microsoft.com>
2016-05-03 08:11:44 -0700

    Update version to 1.3.0.

--- src/Build/GlobalAssemblyInfo.fs
+++ src/Build/GlobalAssemblyInfo.fs
@@ -5,9 +5,9 @@ open System.Reflection
 [<assembly: AssemblyCompany("Microsoft Corporation")>]
 [<assembly: AssemblyProduct("Orleans")>]
 [<assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2015")>]
-[<assembly: AssemblyVersion("1.2.0.0")>]
-[<assembly: AssemblyFileVersion("1.2.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.2.0.0")>]
+[<assembly: AssemblyVersion("1.3.0.0")>]
+[<assembly: AssemblyFileVersion("1.3.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.3.0.0")>]
 
 do
     ()
