8d9598b9abbb220faa32c934db380e72ff6c3fa4
Julian Dominguez <julido@microsoft.com>
2016-05-12 16:35:19 -0700

    Switch to programmatic config

--- Samples/TicTacToe/OrleansXO.WorkerRole/WorkerRole.cs
+++ Samples/TicTacToe/OrleansXO.WorkerRole/WorkerRole.cs
@@ -29,8 +29,7 @@ namespace OrleansXO.WorkerRole
         
         public override void Run()
         {
-            var config = new ClusterConfiguration();
-            config.StandardLoad();
+            var config = AzureSilo.DefaultConfiguration();
             
             // It is IMPORTANT to start the silo not in OnStart but in Run. 
             // Azure may not have the firewalls open yet (on the remote silos) at the OnStart phase.
