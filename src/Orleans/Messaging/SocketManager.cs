69ca3de297d9785f38a410a6945e763c809d7f6a
Julian Dominguez <julido@microsoft.com>
2016-05-19 17:44:36 -0700

    Avoid disconnect and abort

--- src/Orleans/Messaging/SocketManager.cs
+++ src/Orleans/Messaging/SocketManager.cs
@@ -193,6 +193,7 @@ namespace Orleans.Runtime
                 // Ignore
             }
 
+#if !NETSTANDARD1_6
             try
             {
                 s.Disconnect(false);
@@ -201,6 +202,7 @@ namespace Orleans.Runtime
             {
                 // Ignore
             }
+#endif
             try
             {
                 s.Dispose();
