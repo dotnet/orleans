fc5924ed3d1b163807abcb7d88f3f9b4347f45b5
Julian Dominguez <julido@microsoft.com>
2016-06-22 15:52:03 -0700

    Ignoring failing test (that didn't work correctly for a very long time)

--- test/Tester/RequestContextTest.cs
+++ test/Tester/RequestContextTest.cs
@@ -3,8 +3,6 @@ using Xunit;
 using Orleans.Runtime;
 using UnitTests.GrainInterfaces;
 using UnitTests.Tester;
-using System;
-using Tester;
 
 namespace UnitTests.General
 {
@@ -22,20 +20,16 @@ namespace UnitTests.General
             Assert.True((int)infoFromGrain == 10);
         }
 
-        [Fact, TestCategory("RequestContext"), TestCategory("Functional")]
+        [Fact(Skip = "Was failing before (just masked as a Pass), needs fixing or removing"), TestCategory("RequestContext"), TestCategory("Functional")]
         public async Task RequestContextCalleeToCallerFlow()
         {
-            await Xunit.Assert.ThrowsAsync(typeof(Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException), async () =>
-            {
-                var grain = GrainFactory.GetGrain<ISimplePersistentGrain>(random.Next());
-                // This method in the grain does RequestContext.Set
-                await grain.SetRequestContext(15);
-                // Read the info set in the grain
-                var infoFromGrain = RequestContext.Get("GrainInfo");
-                Assert.NotNull(infoFromGrain);
-                Assert.True((int)infoFromGrain == 15);
-            });
+            var grain = GrainFactory.GetGrain<ISimplePersistentGrain>(random.Next());
+            // This method in the grain does RequestContext.Set
+            await grain.SetRequestContext(15);
+            // Read the info set in the grain
+            var infoFromGrain = RequestContext.Get("GrainInfo");
+            Assert.NotNull(infoFromGrain);
+            Assert.True((int)infoFromGrain == 15);
         }
-
     }
 }
