4d302f78c2116d6391bf9e43279f61ddaa3fc8c5
Julian Dominguez <julido@microsoft.com>
2016-07-07 14:31:55 -0700

    Update version to 1.2.3

--- src/Build/GlobalAssemblyInfo.fs
+++ src/Build/GlobalAssemblyInfo.fs
@@ -5,9 +5,9 @@ open System.Reflection
 [<assembly: AssemblyCompany("Microsoft Corporation")>]
 [<assembly: AssemblyProduct("Orleans")>]
 [<assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2016")>]
-[<assembly: AssemblyVersion("1.2.2.0")>]
-[<assembly: AssemblyFileVersion("1.2.2.0")>]
-[<assembly: AssemblyInformationalVersion("1.2.2.0")>]
+[<assembly: AssemblyVersion("1.2.3.0")>]
+[<assembly: AssemblyFileVersion("1.2.3.0")>]
+[<assembly: AssemblyInformationalVersion("1.2.3.0")>]
 
 do
     ()
