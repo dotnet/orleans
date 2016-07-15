117e405ff621c3628ae66197ec1b7765bcfc720a
Norman H. Harebottle III <nharebottl@usxpress.com>
2016-07-15 07:25:29 -0400

    Removed class constraint because it was overly broad

--- src/Orleans/Core/Grain.cs
+++ src/Orleans/Core/Grain.cs
@@ -250,7 +250,7 @@ namespace Orleans
     /// Base class for a Grain with declared persistent state.
     /// </summary>
     /// <typeparam name="TGrainState">The class of the persistent state object</typeparam>
-    public class Grain<TGrainState> : Grain, IStatefulGrain where TGrainState : class, new()
+    public class Grain<TGrainState> : Grain, IStatefulGrain where TGrainState : new()
     {
         private readonly GrainState<TGrainState> grainState;
 
