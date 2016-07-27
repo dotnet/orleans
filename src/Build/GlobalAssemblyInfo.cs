0196bf8be9d6bee3ec86dd5dbc3ecda2db917eee
Julian Dominguez <julido@microsoft.com>
2016-07-27 14:05:33 -0700

    Apply "-beta1" suffix to version

--- src/Build/GlobalAssemblyInfo.cs
+++ src/Build/GlobalAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2015")]
 [assembly: AssemblyVersion("1.3.0.0")]
 [assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: AssemblyInformationalVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0-beta1")]
