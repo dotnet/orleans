f2933f89cee587d965715f1833805eaf991efe0e
Gutemberg Ribeiro <gutemberg@outlook.com>
2016-08-13 12:39:42 -0300

    Orleans PowerShell Client

---
+++ src/OrleansPSUtils/StopGrainClient.cs
@@ -0,0 +1,15 @@
+﻿using Orleans;
+using System.Management.Automation;
+
+namespace OrleansPSUtils
+{
+    [Cmdlet(VerbsLifecycle.Stop, "GrainClient")]
+    public class StopGrainClient : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            if (GrainClient.IsInitialized)
+                GrainClient.Uninitialize();
+        }
+    }
+}
