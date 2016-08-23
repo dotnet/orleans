7c2e6bdba45f1b5a8afc0d0ac587394c92dfb1c9
Julian Dominguez <julido@microsoft.com>
2016-08-23 14:28:41 -0700

    Clean up unit tests in HelloWorld sample

--- Samples/HelloWorld/SiloTests/HelloWorldSiloTests.cs
+++ Samples/HelloWorld/SiloTests/HelloWorldSiloTests.cs
@@ -4,37 +4,40 @@ using System.Threading.Tasks;
 using HelloWorld.Interfaces;
 using Orleans;
 using Orleans.Runtime.Configuration;
-using Orleans.Serialization;
 using Orleans.TestingHost;
 using Xunit;
 
 namespace SiloTests
 {
     /// <summary>
-    /// -----------------------------------------------------------------
     /// How this works:
     /// 
     /// 1. The class fixture will create an in-process test silo and client
     ///    environment that will be shared by all tests within this class.
-    /// 2. The default test environment will contain a mini cluster of 1 silo, 
+    /// 2. The default test environment will contain a mini cluster of 2 silos, 
     ///    running in separate AppDomains from the test process. The silo
-    ///    will be named Primary.
+    ///    will be named Primary and Secondary_1.
     /// 3. The Orleans client environment will be initialized in the main AppDomain
     ///    where this test class is running, so each of the test cases can assume 
     ///    that everything is initialized at the start of their execution.
     /// 4. The configuration used for the test silos and test client are based on 
     ///    the <c>TestClusterOptions</c> config object which can be configured 
     ///    in the <c>OrleansSiloFixture</c>
-    /// 5. There are also various utility methods in the <c>TestingSiloHost</c> class
+    /// 5. There are also various utility methods in the <c>TestCluster</c> class
     ///    that allow silos to be stopped or restarted to allow tests to programmatically 
     ///    simulate some simple failure mode conditions.
+    /// 6. Be aware that if you want to have multiple fixtures that create and tear down clusters,
+    ///    you should disable parallelization if within the same test project, as it is not possible
+    ///    to have 2 different grain clients talking to different silos.
     /// Note: These tests are an example of using xUnit to write unit tests, although
     ///       similar testing frameworks such as NUnit or MsTest could have been used.
-    /// ----------------------------------------------------------------- */
+    ///       Consider using a collection fixture (using <see cref="ICollectionFixture{TFixture}"/> as 
+    ///       opposed to implementing <see cref="IClassFixture{TFixture}"/> in the test class) if you want
+    ///       to reuse the same cluster accross many test classes.
     /// </summary>
     public class HelloWorldSiloTests : IClassFixture<OrleansSiloFixture>
     {
-        private OrleansSiloFixture _fixture;
+        private readonly OrleansSiloFixture _fixture;
 
         public HelloWorldSiloTests(OrleansSiloFixture fixture)
         {
@@ -48,7 +51,7 @@ namespace SiloTests
         {
             // The Orleans silo / client test environment is already set up at this point.
 
-            const long id = 0;
+            long id = new Random().Next();
             const string greeting = "Bonjour";
 
             IHello grain = GrainFactory.GetGrain<IHello>(id);
@@ -57,20 +60,17 @@ namespace SiloTests
             string reply = await grain.SayHello(greeting);
             
             Assert.NotNull(reply);
-            string expected = string.Format("You said: '{0}', I say: Hello!", greeting);
-            Assert.Equal(expected, reply);
+            Assert.Equal($"You said: '{greeting}', I say: Hello!", reply);
         }
 
         [Fact]
         public async Task SiloSayHelloArchiveTest()
         {
-            // The mocked Orleans runtime is already set up at this point
-
-            const long id = 0;
+            long id = new Random().Next();
             const string greeting1 = "Bonjour";
             const string greeting2 = "Hei";
 
-            IHelloArchive grain = GrainFactory.GetGrain <IHelloArchive>(id);
+            IHelloArchive grain = GrainFactory.GetGrain<IHelloArchive>(id);
 
             // This will directly call the grain under test.
             await grain.SayHello(greeting1);
@@ -78,13 +78,13 @@ namespace SiloTests
 
             var greetings = (await grain.GetGreetings()).ToList();
 
-            Assert.True(greetings.Contains(greeting1));
-            Assert.True(greetings.Contains(greeting2));
+            Assert.Contains(greeting1, greetings);
+            Assert.Contains(greeting2, greetings);
         }
     }
 
     /// <summary>
-    /// Class fixture used to share the silos between multiple tests within a specific test class
+    /// Class fixture used to share the silos between multiple tests within a specific test class.
     /// </summary>
     public class OrleansSiloFixture : IDisposable
     {
@@ -94,7 +94,7 @@ namespace SiloTests
         {
             GrainClient.Uninitialize();
 
-            var options = new TestClusterOptions();
+            var options = new TestClusterOptions(initialSilosCount: 2);
             options.ClusterConfiguration.AddMemoryStorageProvider("Default");
             options.ClusterConfiguration.AddMemoryStorageProvider("MemoryStore");
             Cluster = new TestCluster(options);
