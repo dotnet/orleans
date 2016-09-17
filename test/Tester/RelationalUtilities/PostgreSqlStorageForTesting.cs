96bcae278196351fda28ea93a935f63ff4eb6831
Clay Lenhart <xclayl@users.noreply.github.com>
2016-09-17 15:34:04 -0400

    Restore code to reliably drop the PostgreSql testing database.

--- test/Tester/RelationalUtilities/PostgreSqlStorageForTesting.cs
+++ test/Tester/RelationalUtilities/PostgreSqlStorageForTesting.cs
@@ -37,7 +37,11 @@ namespace Tester.RelationalUtilities
         {
             get
             {
-                return @"DROP DATABASE ""{0}"";";
+                return @"SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = '{0}'
+  AND pid <> pg_backend_pid();
+DROP DATABASE ""{0}"";";
             }
         }
         
