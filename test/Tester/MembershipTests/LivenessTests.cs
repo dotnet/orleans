bebdd80d627f8572583a87b243be2cde1f7b873f
Clay Lenhart <xclayl@users.noreply.github.com>
2016-09-19 15:21:03 +0100

    PostgreSql membership tests (#2113)
    ADO.NET: PostgreSql added.

--- test/Tester/MembershipTests/LivenessTests.cs
+++ test/Tester/MembershipTests/LivenessTests.cs
@@ -455,6 +455,58 @@ namespace UnitTests.MembershipTests
         }
     }
 
+
+
+    public class LivenessTests_PostgreSql : LivenessTestsBase
+    {
+        public const string TestDatabaseName = "orleanstest";
+        public LivenessTests_PostgreSql(ITestOutputHelper output) : base(output)
+        {
+        }
+        public override TestCluster CreateTestCluster()
+        {
+            var relationalStorage = RelationalStorageForTesting.SetupInstance(AdoNetInvariants.InvariantNamePostgreSql, TestDatabaseName).Result;
+            var options = new TestClusterOptions(2);
+            options.ClusterConfiguration.Globals.DataConnectionString = relationalStorage.CurrentConnectionString;
+            options.ClusterConfiguration.Globals.LivenessType = GlobalConfiguration.LivenessProviderType.SqlServer;
+            options.ClusterConfiguration.Globals.AdoInvariant = AdoNetInvariants.InvariantNamePostgreSql;
+            options.ClusterConfiguration.PrimaryNode = null;
+            options.ClusterConfiguration.Globals.SeedNodes.Clear();
+            return new TestCluster(options);
+        }
+
+        [Fact, TestCategory("Membership"), TestCategory("PostgreSql")]
+        public async Task Liveness_PostgreSql_1()
+        {
+            await Do_Liveness_OracleTest_1();
+        }
+
+        [Fact, TestCategory("Membership"), TestCategory("PostgreSql")]
+        public async Task Liveness_PostgreSql_2_Restart_Primary()
+        {
+            await Do_Liveness_OracleTest_2(0);
+        }
+
+        [Fact, TestCategory("Membership"), TestCategory("PostgreSql")]
+        public async Task Liveness_PostgreSql_3_Restartl_GW()
+        {
+            await Do_Liveness_OracleTest_2(1);
+        }
+
+        [Fact, TestCategory("Membership"), TestCategory("PostgreSql")]
+        public async Task Liveness_PostgreSql_4_Restart_Silo_1()
+        {
+            await Do_Liveness_OracleTest_2(2);
+        }
+
+        [Fact, TestCategory("Membership"), TestCategory("PostgreSql")]
+        public async Task Liveness_PostgreSql_5_Kill_Silo_1_With_Timers()
+        {
+            await Do_Liveness_OracleTest_2(2, false, true);
+        }
+    }
+
+
     public class LivenessTests_MySql : LivenessTestsBase
     {
         public const string TestDatabaseName = "OrleansTest";
---
+++ test/TesterInternal/RemindersTest/PostgreSqlRemindersTableTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using Orleans;
+using Orleans.Runtime;
+using Orleans.Runtime.ReminderService;
+using Orleans.SqlUtils;
+using UnitTests.General;
+using Xunit;
+
+namespace UnitTests.RemindersTest
+{
+    public class PostgreSqlRemindersTableTests : ReminderTableTestsBase
+    {
+        public PostgreSqlRemindersTableTests(ConnectionStringFixture fixture) : base(fixture)
+        {
+            LogManager.AddTraceLevelOverride(nameof(PostgreSqlRemindersTableTests), Severity.Verbose3);
+        }
+
+        protected override IReminderTable CreateRemindersTable()
+        {
+            return new SqlReminderTable();
+        }
+
+        protected override string GetAdoInvariant()
+        {
+            return AdoNetInvariants.InvariantNamePostgreSql;
+        }
+
+        protected override string GetConnectionString()
+        {
+            return RelationalStorageForTesting.SetupInstance(GetAdoInvariant(), testDatabaseName)
+                    .Result.CurrentConnectionString;
+        }
+
+
+        [Fact, TestCategory("Reminders"), TestCategory("PostgreSql")]
+        public void RemindersTable_PostgreSql_Init()
+        {
+        }
+
+
+        [Fact, TestCategory("Reminders"), TestCategory("PostgreSql")]
+        public async Task RemindersTable_PostgreSql_RemindersRange()
+        {
+            await RemindersRange();
+        }
+
+        [Fact, TestCategory("Reminders"), TestCategory("PostgreSql")]
+        public async Task RemindersTable_PostgreSql_RemindersParallelUpsert()
+        {
+            await RemindersParallelUpsert();
+        }
+
+        [Fact, TestCategory("Reminders"), TestCategory("PostgreSql")]
+        public async Task RemindersTable_PostgreSql_ReminderSimple()
+        {
+            await ReminderSimple();
+        }
+    }
+}
