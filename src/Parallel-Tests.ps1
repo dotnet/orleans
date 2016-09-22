c103207adaa4a40a6fd288206d40a09bee720a20
Benjamin Petit <bpetit@microsoft.com>
2016-09-22 10:20:28 -0700

    In tests, if there is multiple xunit packages installed, take the latest one

--- src/Parallel-Tests.ps1
+++ src/Parallel-Tests.ps1
@@ -3,7 +3,11 @@ param(
     [string] $testFilter,
     [string] $outDir)
 
-$xunitRunner = $(Join-Path $PSScriptRoot 'packages\xunit.runner.console.2.1.0\tools\xunit.console.exe ')
+# If there is multiple xunit packages installed, take the latest one
+$xunitRunner = Get-ChildItem packages -Directory -Filter "xunit.runner.console.*" | 
+                ForEach-Object { Get-ChildItem $_.FullName -Recurse -File -Filter "xunit.console.exe" } | 
+                Sort-Object -Property VersionInfo | 
+                Select-Object -Last 1
 
 $ExecuteCmd =
 {
@@ -19,7 +23,7 @@ foreach ($a in $assemblies)
 {
     $xmlName = 'xUnit-Results-' + [System.IO.Path]::GetFileNameWithoutExtension($a) + '.xml'
     $outXml = $(Join-Path $outDir $xmlName)
-    $cmdLine = $xunitRunner + $a + ' ' + $testFilter + ' -xml ' + $outXml + ' -parallel none -noshadow -verbose' 
+    $cmdLine = $xunitRunner.FullName + ' ' + $a + ' ' + $testFilter + ' -xml ' + $outXml + ' -parallel none -noshadow -verbose' 
     Write-Host $cmdLine
     Start-Job $ExecuteCmd -ArgumentList $cmdLine -Name $([System.IO.Path]::GetFileNameWithoutExtension($a))
 }
