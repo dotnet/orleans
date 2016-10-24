2e6ca1f2cdda088a24ac743c7b2dd58a826d406b
xiazen <xiazen@microsoft.com>
2016-10-24 13:21:38 -0700

    port over OrleansProviders project to vNext

--- test/TestExtensions/DefaultClusterFixture.cs
+++ test/TestExtensions/DefaultClusterFixture.cs
@@ -8,11 +8,8 @@ namespace TestExtensions
         protected override TestCluster CreateTestCluster()
         {
             var options = new TestClusterOptions();
-#if !NETSTANDARD_TODO
-            // waiting for OrleansProviders project to be ported over to vNext
             options.ClusterConfiguration.AddMemoryStorageProvider("Default");
             options.ClusterConfiguration.AddMemoryStorageProvider("MemoryStore");
-#endif
             return new TestCluster(options);
         }
     }
