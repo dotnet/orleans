c5ddaad437b89d8153c21f780d1244987f480e3f
xiazen <xiazen@microsoft.com>
2016-11-28 11:50:10 -0800

    add Tester.AzureUtils and TesterInternal to test.cmd, and Add GenerateDocumentationFiler to OrleansAzureUtils

--- vNext/src/Test.cmd
+++ vNext/src/Test.cmd
@@ -13,7 +13,7 @@ pushd "%CMDHOME%"
 SET OutDir=%CMDHOME%\..\Binaries\%CONFIGURATION%
 
 REM set TESTS=%OutDir%\Tester.dll,%OutDir%\TesterInternal.dll,%OutDir%\Orleans.NonSiloTests.dll
-set TESTS=%OutDir%\Tester.dll,%OutDir%\Orleans.NonSiloTests.dll
+set TESTS=%OutDir%\Tester.dll,%OutDir%\Orleans.NonSiloTests.dll,%OutDir%\Tester.AzureUtils.dll,%OutDir%\TesterInternal.dll
 if []==[%TEST_FILTERS%] set TEST_FILTERS=-trait 'Category=BVT' -trait 'Category=SlowBVT'
 
 @Echo Test assemblies = %TESTS%
