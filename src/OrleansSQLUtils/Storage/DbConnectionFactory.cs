35ad08cfce9f5feeca90335975006456c1613879
Gutemberg Ribeiro <gutemberg@outlook.com>
2016-12-03 18:25:00 -0200

    Final feedback.

--- src/OrleansSQLUtils/Storage/DbConnectionFactory.cs
+++ src/OrleansSQLUtils/Storage/DbConnectionFactory.cs
@@ -19,7 +19,7 @@ namespace Orleans.SqlUtils
                 { AdoNetInvariants.InvariantNameSqlServer, new Tuple<string, string>("System.Data.SqlClient", "System.Data.SqlClient.SqlClientFactory") },
                 { AdoNetInvariants.InvariantNameMySql, new Tuple<string, string>("MySql.Data", "MySql.Data.MySqlClient.MySqlClientFactory") },
                 { AdoNetInvariants.InvariantNameOracleDatabase, new Tuple<string, string>("System.Data.SqlClient", "System.Data.SqlClient.SqlClientFactory") },
-                { AdoNetInvariants.InvariantNamePostgreSql, new Tuple<string, string>("Npgsql", "Npgsql.NpgsqlFactory ") },
+                { AdoNetInvariants.InvariantNamePostgreSql, new Tuple<string, string>("Npgsql", "Npgsql.NpgsqlFactory") },
                 { AdoNetInvariants.InvariantNameSqlLite, new Tuple<string, string>("Microsoft.Data.SQLite", "Microsoft.Data.SQLite.SqliteFactory") },
             };
 
