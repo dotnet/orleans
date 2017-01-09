cbc12e10aa98d290fc0afcda088552385e4643c9
Reuben Bond <a-rebond@microsoft.com>
2017-01-10 10:44:41 +1100

    Fix vNext build failure

--- src/OrleansRuntime/Silo/Silo.cs
+++ src/OrleansRuntime/Silo/Silo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Runtime;
 using System.Text;
 using System.Threading;
