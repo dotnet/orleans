45aeabb13b4ef2f0d6e7f4d032db9083d425fa5d
Attila Hajdrik <athajdri@microsoft.com>
2017-01-17 14:19:02 -0800

    Update all image assets in package and templates. (#2602)
    Cleanup project and template files.
    Update F# project template to use FSharp Core v4.4.
    Enable VS 2017 RC2 support.

--- src/SDK/InstallOrleansVSTools.cmd
+++ src/SDK/InstallOrleansVSTools.cmd
@@ -2,27 +2,33 @@
 @prompt $G$S
 @set CMDHOME=%~dp0.
 
-@if NOT "%VS120COMNTOOLS%"=="" (
-  @set VSTOOLSDIR="%VS120COMNTOOLS%"
-  @set VSVER=Visual Studio 2013
+@if NOT "%VS150COMNTOOLS%"=="" (
+  @set VSTOOLSDIR="%VS150COMNTOOLS%"
+  @set VSVER=Visual Studio 2017
 ) else (
-  @set VSTOOLSDIR="%VS110COMNTOOLS%"
-  @set VSVER=Visual Studio 2012
+  @if NOT "%VS140COMNTOOLS%"=="" (
+    @set VSTOOLSDIR="%VS140COMNTOOLS%"
+    @set VSVER=Visual Studio 2015
+  ) else (
+    @if NOT "%VS120COMNTOOLS%"=="" (
+      @set VSTOOLSDIR="%VS120COMNTOOLS%"
+      @set VSVER=Visual Studio 2013
+    ) else (
+      @set VSTOOLSDIR="%VS110COMNTOOLS%"
+      @set VSVER=Visual Studio 2012
+    )
+  )
 )
 
 @set PKG=Orleans Tools for %VSVER%
 
 @echo --- Installing %PKG%
-@Echo -- VS Tools directory = %VSTOOLSDIR%
+@echo -- VS Tools directory = %VSTOOLSDIR%
 
-@set SDKHOME=%CMDHOME%
-@Echo - Setting OrleansSDK environment variable to %SDKHOME%
-SetX.exe OrleansSDK "%SDKHOME%"
-
-@Echo - Removing any old copy of Visual Studio extension package %PKG%
+@echo - Uninstalling any old copy of Visual Studio extension package %PKG%
 %VSTOOLSDIR%\..\IDE\vsixinstaller.exe /q /uninstall:462db41f-31a4-48f0-834c-1bdcc0578511
 
-@Echo - Installing Visual Studio extension package %PKG%
+@echo - Installing Visual Studio extension package %PKG%
 %VSTOOLSDIR%\..\IDE\vsixinstaller.exe "%SDKHOME%\VisualStudioTemplates\OrleansVSTools.vsix"
 
 @echo --- Finished installing %PKG%
