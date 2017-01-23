7c33426736a0a5f0936161464d87729a2a0b0f8f
Gutemberg Ribeiro <gutemberg@outlook.com>
2017-01-23 21:28:07 -0200

    BondUtils added to vNext solution

---
+++ vNext/src/OrleansBondUtils/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OrleansBondUtils")]
+[assembly: AssemblyDescription("Bond Serializer for Orleans")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a6b8051-5d99-483c-b9b7-444ed57918ec")]
+
