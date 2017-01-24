662623aa72ac4c5992c26ceaf536bf4bd27e7008
Gutemberg Ribeiro <gutemberg@outlook.com>
2017-01-23 22:49:54 -0200

    AI Telemetry Consumer added to vNext Small fix with Bond install scripts

---
+++ vNext/src/OrleansTelemetryConsumers.AI/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Orleans.CodeGeneration;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OrleansTelemetryConsumers.AI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99a8e011-3a30-423a-9f67-d1349fd8b1cd")]
+[assembly: SkipCodeGeneration]
