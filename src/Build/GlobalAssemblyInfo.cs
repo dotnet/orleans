1337a8285172bb971eae26187e222c9cd3a7cb12
Sergey Bykov <sbykov@microsoft.com>
2017-01-30 09:58:19 -0800

    Update version to 1.4.0-beta.

--- src/Build/GlobalAssemblyInfo.cs
+++ src/Build/GlobalAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2016")]
 [assembly: AssemblyVersion("1.4.0.0")]
 [assembly: AssemblyFileVersion("1.4.0.0")]
-[assembly: AssemblyInformationalVersion("1.4.0.0")]
+[assembly: AssemblyInformationalVersion("1.4.0-beta")]
