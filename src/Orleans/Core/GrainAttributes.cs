a2117334e9e66f33c500586e4566c72572c1e363
Reuben Bond <a-rebond@microsoft.com>
2017-01-31 15:10:21 +1100

    Allow overriding MethodId using [MethodId(id)] on interface methods

--- src/Orleans/Core/GrainAttributes.cs
+++ src/Orleans/Core/GrainAttributes.cs
@@ -226,7 +226,36 @@ namespace Orleans
             {
                 TypeCode = typeCode;
             }
-    }
+        }
+
+        /// <summary>
+        /// Specifies the method id for the interface method which this attribute is declared on.
+        /// </summary>
+        /// <remarks>
+        /// Method ids must be unique for all methods in a given interface.
+        /// This attribute is only applicable for interface method declarations, not for method definitions on classes.
+        /// </remarks>
+        [AttributeUsage(AttributeTargets.Method)]
+        public sealed class MethodIdAttribute : Attribute
+        {
+            /// <summary>
+            /// Gets the method id for the interface method this attribute is declared on.
+            /// </summary>
+            public int MethodId { get; }
+
+            /// <summary>
+            /// Specifies the method id for the interface method which this attribute is declared on.
+            /// </summary>
+            /// <remarks>
+            /// Method ids must be unique for all methods in a given interface.
+            /// This attribute is only valid only on interface method declarations, not on method definitions.
+            /// </remarks>
+            /// <param name="methodId">The method id.</param>
+            public MethodIdAttribute(int methodId)
+            {
+                this.MethodId = methodId;
+            }
+        }
 
     /// <summary>
     /// Used to mark a method as providing a copier function for that type.
