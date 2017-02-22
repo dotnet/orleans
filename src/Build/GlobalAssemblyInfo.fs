b9515fb619a6df2cbe734646009e172be36771b3
Sergey Bykov <sbykov@microsoft.com>
2017-02-22 14:24:39 -0800

    Update version to 1.5.0. (#2753)

--- src/Build/GlobalAssemblyInfo.fs
+++ src/Build/GlobalAssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 
 [<assembly: AssemblyCompany("Microsoft Corporation")>]
 [<assembly: AssemblyProduct("Orleans")>]
-[<assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2016")>]
-[<assembly: AssemblyVersion("1.4.0.0")>]
-[<assembly: AssemblyFileVersion("1.4.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.4.0.0")>]
+[<assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2017")>]
+[<assembly: AssemblyVersion("1.5.0.0")>]
+[<assembly: AssemblyFileVersion("1.5.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.5.0.0")>]
 
 do
     ()
