7e7a90dc699ca15e9e509b2dd3cc971e1c86863d
Attila Hajdrik <athajdri@microsoft.com>
2017-02-28 21:00:46 -0800

    Build process cleanup for newer tooling (#2798)
    * Move common build properties to common props
    * Reduce logic in build.cmd
    * Enable auto generation of AssemblyInfo.cs
    * Remove Version.txt
    * Remove GlobalAssemblyInfo.cs
    * Bump DotnetCLIVersion.txt
    * Cleanup AssemblyInfo.cs files
    * Upgrade build version to 2.0.0-preview2
    * Debug nuget packages include a date-based suffix in the version

--- Build.cmd
+++ Build.cmd
@@ -13,14 +13,14 @@ set exitcode=%errorlevel%
 GOTO END
 
 :VNEXT
-cmd /c "%~dp0vNext\src\Build.cmd"
+cmd /c "%~dp0vNext\Build.cmd"
 set exitcode=%errorlevel%
 GOTO END
 
 :ALL
 cmd /c "%~dp0src\Build.cmd"
 set exitcode=%errorlevel%
-cmd /c "%~dp0vNext\src\Build.cmd"
+cmd /c "%~dp0vNext\Build.cmd"
 set /a exitcode=%errorlevel%+%exitcode%
 
 :END
