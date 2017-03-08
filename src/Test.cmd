fca0e4c4ceecd69f78bc86f4f055a78b0bb575f2
Julian Dominguez <julido@microsoft.com>
2017-03-08 11:47:30 -0800

    Allow echoing build script commands

--- src/Test.cmd
+++ src/Test.cmd
@@ -1,5 +1,6 @@
-@setlocal
-@ECHO off
+@if not defined _echo @echo off
+setlocal
+ECHO off
 
 SET CONFIGURATION=Release
 
@@ -17,7 +18,7 @@ if []==[%TEST_FILTERS%] set TEST_FILTERS=-trait 'Category=BVT' -trait 'Category=
 
 @Echo Test assemblies = %TESTS%
 @Echo Test filters = %TEST_FILTERS%
-@echo on
+@if not defined _echo @echo on
 call "%CMDHOME%\SetupTestScript.cmd" "%OutDir%"
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& ./Parallel-Tests.ps1 -assemblies %TESTS% -testFilter \"%TEST_FILTERS%\" -outDir '%OutDir%'"
