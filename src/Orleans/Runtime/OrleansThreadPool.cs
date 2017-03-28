8e9909d1ea505c296b4e0bb317ddc6be5ec98ebb
Dmitry Vakulenko <dmitry.vakulenko@outlook.com>
2017-03-28 09:58:43 +0300

    Use fast random

--- src/Orleans/Runtime/OrleansThreadPool.cs
+++ src/Orleans/Runtime/OrleansThreadPool.cs
@@ -1503,5 +1503,29 @@ namespace Orleans.Runtime
 
         #endregion
     }
+    // Simple random number generator. We don't need great randomness, we just need a little and for it to be fast.
+    internal struct FastRandom // xorshift prng
+    {
+        private uint _w, _x, _y, _z;
+
+        public FastRandom(int seed)
+        {
+            _x = (uint)seed;
+            _w = 88675123;
+            _y = 362436069;
+            _z = 521288629;
+        }
+
+        public int Next(int maxValue)
+        {
+            Debug.Assert(maxValue > 0);
+
+            uint t = _x ^ (_x << 11);
+            _x = _y; _y = _z; _z = _w;
+            _w = _w ^ (_w >> 19) ^ (t ^ (t >> 8));
+
+            return (int)(_w % (uint)maxValue);
+        }
+    }
 
 }
