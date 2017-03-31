1ccec7b25b724a9ac2676d1365bdf8d567a11b84
Julian Dominguez <julido@microsoft.com>
2017-03-31 16:05:51 -0700

    Account for additional assemblies being modified while in use. (#2908)
    This fixes non-deterministic issue when a silo is stopped and another one started in parallel, such as in Ring_1F1J

--- src/OrleansTestingHost/TestCluster.cs
+++ src/OrleansTestingHost/TestCluster.cs
@@ -12,6 +12,7 @@ using Orleans.Runtime.Configuration;
 using Orleans.Serialization;
 using Orleans.Streams;
 using Orleans.TestingHost.Utils;
+using System.Collections.Concurrent;
 
 namespace Orleans.TestingHost
 {
@@ -39,7 +40,7 @@ namespace Orleans.TestingHost
 
         private readonly List<SiloHandle> additionalSilos = new List<SiloHandle>();
 
-        private readonly Dictionary<string, GeneratedAssembly> additionalAssemblies = new Dictionary<string, GeneratedAssembly>();
+        private readonly IDictionary<string, GeneratedAssembly> additionalAssemblies = new ConcurrentDictionary<string, GeneratedAssembly>();
 
         /// <summary>
         /// Client configuration to use when initializing the client
