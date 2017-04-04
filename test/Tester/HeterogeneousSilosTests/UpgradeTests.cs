9b034e43ce868e3cd66b63f7d10d58ff358cc338
Benjamin Petit <bpetit@microsoft.com>
2017-04-04 13:19:10 -0700

    Add tests

--- test/Tester/HeterogeneousSilosTests/UpgradeTests.cs
+++ test/Tester/HeterogeneousSilosTests/UpgradeTests.cs
@@ -87,6 +87,51 @@ namespace Tester.HeterogeneousSilosTests
         }
 
         [Fact, TestCategory("SlowBVT"), TestCategory("Functional")]
+        public async Task AlwaysCreateActivationWithMinimumVersionTest()
+        {
+            await DeployCluster(MinimumVersionPlacement.Singleton, BackwardCompatible.Singleton);
+
+            const int numberOfGrains = 100;
+
+            // Only V1 exist for now
+            for (var i = 0; i < numberOfGrains; i++)
+            {
+                var grain = grainFactory.GetGrain<IVersionUpgradeTestGrain>(i);
+                Assert.Equal(1, await grain.GetVersion());
+            }
+
+            // Start a new silo with V2
+            await StartSiloV2();
+
+            // We still only use V1
+            for (var i = 0; i < numberOfGrains * 2; i++)
+            {
+                var grain = grainFactory.GetGrain<IVersionUpgradeTestGrain>(i);
+                Assert.Equal(1, await grain.GetVersion());
+            }
+        }
+
+        [Fact, TestCategory("SlowBVT"), TestCategory("Functional")]
+        public async Task DoNotUpgradeIfCompatibleVersionsTest()
+        {
+            await DeployCluster(LatestVersionPlacement.Singleton, AllVersionsCompatible.Singleton);
+
+            // Only V1 exist for now
+            var grain0 = grainFactory.GetGrain<IVersionUpgradeTestGrain>(0);
+            Assert.Equal(1, await grain0.GetVersion());
+
+            await StartSiloV2();
+
+            // New activation should be V2
+            var grain1 = grainFactory.GetGrain<IVersionUpgradeTestGrain>(1);
+            Assert.Equal(2, await grain1.GetVersion());
+
+            // Since grain are compatible, no upgrade this time
+            Assert.Equal(1, await grain1.ProxyGetVersion(grain0));
+            Assert.Equal(1, await grain0.GetVersion());
+        }
+
+        [Fact, TestCategory("SlowBVT"), TestCategory("Functional")]
         public async Task AlwaysCreateNewActivationWithLatestVersionTest()
         {
             await DeployCluster(LatestVersionPlacement.Singleton, BackwardCompatible.Singleton);
