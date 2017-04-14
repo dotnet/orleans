68d44fee4eadcef2f1a65063822f0bae5c90fb13
Reuben Bond <a-rebond@microsoft.com>
2017-04-14 10:00:50 +1000

    Remove deprecated [RegisterSerializer] and support for serializer Register methods

--- src/Orleans/Core/GrainAttributes.cs
+++ src/Orleans/Core/GrainAttributes.cs
@@ -296,16 +296,6 @@ namespace Orleans
         public sealed class DeserializerMethodAttribute : Attribute
         {
         }
-
-        /// <summary>
-        /// Used to make a class for auto-registration as a serialization helper.
-        /// </summary>
-        [AttributeUsage(AttributeTargets.Class)]
-        [Obsolete(
-            "[RegisterSerializer] is obsolete, please use [Serializer(typeof(TargetType))] instead. Note that the signature of Register has changed to 'void Register(SerializationManager sm)'.")]
-        public sealed class RegisterSerializerAttribute : Attribute
-        {
-        }
     }
 
     namespace Providers
