1fdff11c3dae58bf8125d1908d30fa3c56e2b98c
xiazen <xiazen@microsoft.com>
2017-04-17 14:33:40 -0700

    add necessary xml comment and make test EHSlowConsuming_ShouldFavorSlowConsumer more reliable

--- src/OrleansServiceBus/Providers/Streams/EventHub/IEventHubQueueCacheFactory.cs
+++ src/OrleansServiceBus/Providers/Streams/EventHub/IEventHubQueueCacheFactory.cs
@@ -8,6 +8,13 @@ namespace Orleans.ServiceBus.Providers
     /// </summary>
     public interface IEventHubQueueCacheFactory
     {
+        /// <summary>
+        /// Function used to create a IEventHubQueueCache
+        /// </summary>
+        /// <param name="partition"></param>
+        /// <param name="checkpointer"></param>
+        /// <param name="cacheLogger"></param>
+        /// <returns></returns>
         IEventHubQueueCache CreateCache(string partition, IStreamQueueCheckpointer<string> checkpointer, 
             Logger cacheLogger);
     }
