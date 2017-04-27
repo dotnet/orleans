e09675c6874d7696d023a2a930ec952d2f62a90d
Julian Dominguez <julido@microsoft.com>
2017-04-27 16:23:34 -0700

    Update version to 1.5.0-beta1

--- src/Build/GlobalAssemblyInfo.cs
+++ src/Build/GlobalAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2017")]
 [assembly: AssemblyVersion("1.5.0.0")]
 [assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0.0")]
+[assembly: AssemblyInformationalVersion("1.5.0.0-beta1")]
