8d59dbfe00d119b2ee96222acb52a1738fc22b83
Julian Dominguez <julido@microsoft.com>
2017-05-02 11:50:06 -0700

    - Include missing Orleans.dll in build-time codegen package - Update HelloWorld.NetCore sample to download packages from MyGet feed

--- Samples/HelloWorld.NetCore/BuildAndRun.ps1
+++ Samples/HelloWorld.NetCore/BuildAndRun.ps1
@@ -3,14 +3,16 @@ if((Test-Path "..\..\vNext\Binaries\Debug\") -eq $false) {
      # this will only work in Windows. 
      # Alternatively build the nuget packages and place them in the <root>/vNext/Binaries/Debug folder 
      # (or make sure there is a package source available with the Orleans 2.0 TP nugets)
-    ..\..\Build.cmd netstandard
+    #..\..\Build.cmd netstandard
 }
 
 # Uncomment the following to clear the nuget cache if rebuilding the packages doesn't seem to take effect.
 #dotnet nuget locals all --clear
 
 dotnet restore
+if ($LastExitCode -ne 0) { return; }
 dotnet build
+if ($LastExitCode -ne 0) { return; }
 
 # Run the 2 console apps in different windows
 
