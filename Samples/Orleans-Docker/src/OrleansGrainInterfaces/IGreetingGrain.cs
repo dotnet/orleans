c135c1bfab21475078ce178fcc84a36614c9f178
Gutemberg Ribeiro <gutemberg@outlook.com>
2017-05-09 19:39:38 -0300

    Initial Orleans-Docker sample

---
+++ Samples/Orleans-Docker/src/OrleansGrainInterfaces/IGreetingGrain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace OrleansGrainInterfaces
+{
+    public interface IGreetingGrain : IGrainWithGuidKey
+    {
+        Task<string> SayHello(string name);
+    }
+}
