3133d4c3b50f0d5da17bbe598db1ab33e961bcff
Dmytro Dziuma <dmytro.dzyuma@lebara.com>
2017-05-16 01:00:16 +0300

    Xml docs for ImplicitConsumerGrainExtensions

--- src/Orleans/Streams/Core/ImplicitConsumerGrainExtensions.cs
+++ src/Orleans/Streams/Core/ImplicitConsumerGrainExtensions.cs
@@ -1,7 +1,15 @@
 ﻿namespace Orleans.Streams
 {
+    /// <summary>
+    /// Extension methods for grains implicitly subscribed to streams.
+    /// </summary>
     public static class ImplicitConsumerGrainExtensions
     {
+        /// <summary>
+        /// Constructs <see cref="StreamIdentity"/> of the stream that the grain is implicitly subscribed to.
+        /// </summary>
+        /// <param name="grain">The implicitly subscribed grain.</param>
+        /// <returns>The stream identity (key + namespace).</returns>
         public static StreamIdentity GetImplicitStreamIdentity(this IGrainWithGuidCompoundKey grain)
         {
             string keyExtension;
