90a86c3892118d7ca33b8acd49eddd9aeedc23cd
Sergey Bykov <sbykov@microsoft.com>
2017-05-25 09:14:27 -0700

    Update version to 1.4.2.

--- src/Build/GlobalAssemblyInfo.fs
+++ src/Build/GlobalAssemblyInfo.fs
@@ -5,9 +5,9 @@ open System.Reflection
 [<assembly: AssemblyCompany("Microsoft Corporation")>]
 [<assembly: AssemblyProduct("Orleans")>]
 [<assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2017")>]
-[<assembly: AssemblyVersion("1.4.1.0")>]
-[<assembly: AssemblyFileVersion("1.4.1.0")>]
-[<assembly: AssemblyInformationalVersion("1.4.1.0")>]
+[<assembly: AssemblyVersion("1.4.2.0")>]
+[<assembly: AssemblyFileVersion("1.4.2.0")>]
+[<assembly: AssemblyInformationalVersion("1.4.2.0")>]
 
 do
     ()
