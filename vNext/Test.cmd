e28b817811d008e44bac4034db7e82831d2d5f08
Attila Hajdrik <athajdri@microsoft.com>
2017-06-14 00:42:37 +0200

    Updates to vNext tree in preparation for trees merge (#3091)
    - Migrate to new Build, Test, Tools, they are the same as in "new" 2017 master
    - Remove redundant AssemblyInfo.cs files, use from the master src
    - Remove redundant App.config files, use linked files from master src
    - Remove redundant xUnit runner files, use linked files from master src
    - Update csproj files to have the same content as "new" master files
    - Fix Versioning test assembly file locator code
    - Fix HierarchicalKeyStoreTests client config file loading
    - Move NuGet.config to solution root folder

--- vNext/Test.cmd
+++ vNext/Test.cmd
@@ -10,18 +10,27 @@ set CMDHOME=%CMDHOME:~0,-1%
 pushd "%CMDHOME%"
 @cd
 
-SET OutDir=%CMDHOME%\Binaries\%CONFIGURATION%
+SET TestResultDir=%CMDHOME%\Binaries\%CONFIGURATION%
 
-REM set TESTS=%OutDir%\Tester.dll,%OutDir%\TesterInternal.dll,%OutDir%\NonSilo.Tests.dll,%OutDir%\Tester.AzureUtils.dll
-set TESTS=%OutDir%\net462\Tester.dll,%OutDir%\net462\NonSilo.Tests.dll,%OutDir%\net462\Tester.AzureUtils.dll,%OutDir%\net462\TesterInternal.dll,%OutDir%\net462\Tester.SQLUtils.dll,%OutDir%\net462\DefaultCluster.Tests.dll,%OutDir%\net462\Consul.Tests.dll,%OutDir%\net462\BondUtils.Tests.dll,%OutDir%\net462\AWSUtils.Tests.dll,%OutDir%\net462\GoogleUtils.Tests.dll,%OutDir%\net462\ServiceBus.Tests.dll,%OutDir%\net462\Tester.ZooKeeperUtils.dll
+if not exist %TestResultDir% md %TestResultDir%
+
+SET _Directory=bin\%CONFIGURATION%\net462\win
+
+rem copy Versioning dlls to the appropriate place to make Versioning tests pass.
+if not exist %CMDHOME%\test\Tester\%_Directory%\TestVersionGrainsV1\ mkdir %CMDHOME%\test\Tester\%_Directory%\TestVersionGrainsV1
+if not exist %CMDHOME%\test\Tester\%_Directory%\TestVersionGrainsV2\ mkdir %CMDHOME%\test\Tester\%_Directory%\TestVersionGrainsV2
+
+copy %CMDHOME%\test\Versions\TestVersionGrains\%_Directory%\* %CMDHOME%\test\Tester\%_Directory%\TestVersionGrainsV1\
+copy %CMDHOME%\test\Versions\TestVersionGrains2\%_Directory%\* %CMDHOME%\test\Tester\%_Directory%\TestVersionGrainsV2\
+
+set TESTS=%CMDHOME%\test\AWSUtils.Tests\%_Directory%\AWSUtils.Tests.dll,%CMDHOME%\test\BondUtils.Tests\%_Directory%\BondUtils.Tests.dll,%CMDHOME%\test\Consul.Tests\%_Directory%\Consul.Tests.dll,%CMDHOME%\test\DefaultCluster.Tests\%_Directory%\DefaultCluster.Tests.dll,%CMDHOME%\test\GoogleUtils.Tests\%_Directory%\GoogleUtils.Tests.dll,%CMDHOME%\test\NonSilo.Tests\%_Directory%\NonSilo.Tests.dll,%CMDHOME%\test\ServiceBus.Tests\%_Directory%\ServiceBus.Tests.dll,%CMDHOME%\test\Tester\%_Directory%\Tester.dll,%CMDHOME%\test\TesterAzureUtils\%_Directory%\Tester.AzureUtils.dll,%CMDHOME%\test\TesterInternal\%_Directory%\TesterInternal.dll,%CMDHOME%\test\TesterSQLUtils\%_Directory%\Tester.SQLUtils.dll,%CMDHOME%\test\TesterZooKeeperUtils\%_Directory%\Tester.ZooKeeperUtils.dll
+
 if []==[%TEST_FILTERS%] set TEST_FILTERS=-trait 'Category=BVT' -trait 'Category=SlowBVT'
 
 @Echo Test assemblies = %TESTS%
 @Echo Test filters = %TEST_FILTERS%
-@if not defined _echo @echo on
-REM call "%CMDHOME%\SetupTestScript.cmd" "%OutDir%"
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& ./Parallel-Tests.ps1 -assemblies %TESTS% -testFilter \"%TEST_FILTERS%\" -outDir '%OutDir%'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& ./Parallel-Tests.ps1 -assemblies %TESTS% -testFilter \"%TEST_FILTERS%\" -outDir '%TestResultDir%'"
 set testresult=%errorlevel%
 popd
 endlocal&set testresult=%testresult%
--- vNext/init-tools.cmd
+++ vNext/init-tools.cmd
@@ -1,8 +1,7 @@
-@if "%_echo%" neq "on" echo off
+@if not defined _echo @echo off
 setlocal
 
 set INIT_TOOLS_LOG=%~dp0init-tools.log
-if [%PACKAGES_DIR%]==[] set PACKAGES_DIR=%~dp0packages\
 if [%TOOLRUNTIME_DIR%]==[] set TOOLRUNTIME_DIR=%~dp0Tools
 set DOTNET_PATH=%TOOLRUNTIME_DIR%\dotnetcli\
 if [%DOTNET_CMD%]==[] set DOTNET_CMD=%DOTNET_PATH%dotnet.exe
@@ -21,18 +20,20 @@ if exist "%BUILD_TOOLS_SEMAPHORE%" (
 
 if exist "%TOOLRUNTIME_DIR%" rmdir /S /Q "%TOOLRUNTIME_DIR%"
 
+echo Running %0 > "%INIT_TOOLS_LOG%"
+
+set /p DOTNET_VERSION=< "%~dp0DotnetCLIVersion.txt"
 if exist "%DOTNET_CMD%" goto :afterdotnetrestore
 
 echo Installing dotnet cli...
 if NOT exist "%DOTNET_PATH%" mkdir "%DOTNET_PATH%"
-set /p DOTNET_VERSION=< "%~dp0DotnetCLIVersion.txt"
 set DOTNET_ZIP_NAME=dotnet-dev-win-x64.%DOTNET_VERSION%.zip
-set DOTNET_REMOTE_PATH=https://dotnetcli.blob.core.windows.net/dotnet/Sdk/%DOTNET_VERSION%/%DOTNET_ZIP_NAME%
+set DOTNET_REMOTE_PATH=https://dotnetcli.azureedge.net/dotnet/Sdk/%DOTNET_VERSION%/%DOTNET_ZIP_NAME%
 set DOTNET_LOCAL_PATH=%DOTNET_PATH%%DOTNET_ZIP_NAME%
 echo Installing '%DOTNET_REMOTE_PATH%' to '%DOTNET_LOCAL_PATH%' >> "%INIT_TOOLS_LOG%"
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "$retryCount = 0; $success = $false; do { try { (New-Object Net.WebClient).DownloadFile('%DOTNET_REMOTE_PATH%', '%DOTNET_LOCAL_PATH%'); $success = $true; } catch { if ($retryCount -ge 6) { throw; } else { $retryCount++; Start-Sleep -Seconds (5 * $retryCount); } } } while ($success -eq $false); Add-Type -Assembly 'System.IO.Compression.FileSystem' -ErrorVariable AddTypeErrors; if ($AddTypeErrors.Count -eq 0) { [System.IO.Compression.ZipFile]::ExtractToDirectory('%DOTNET_LOCAL_PATH%', '%DOTNET_PATH%') } else { (New-Object -com shell.application).namespace('%DOTNET_PATH%').CopyHere((new-object -com shell.application).namespace('%DOTNET_LOCAL_PATH%').Items(),16) }" >> "%INIT_TOOLS_LOG%"
 if NOT exist "%DOTNET_LOCAL_PATH%" (
-  echo ERROR: Could not install dotnet cli correctly. See '%INIT_TOOLS_LOG%' for more details.
+  echo ERROR: Could not install dotnet cli correctly. See '%INIT_TOOLS_LOG%' for more details. 1>&2
   exit /b 1
 )
 
@@ -40,4 +41,5 @@ if NOT exist "%DOTNET_LOCAL_PATH%" (
 
 :: Create sempahore file
 echo Done initializing tools.
-echo Init-Tools.cmd completed > "%BUILD_TOOLS_SEMAPHORE%"
+echo Init-Tools.cmd completed. > "%BUILD_TOOLS_SEMAPHORE%"
+exit /b 0
