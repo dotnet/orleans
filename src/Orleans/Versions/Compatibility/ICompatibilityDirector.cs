46e9015ef7abd3565ed643af59ac3de5610d5385
Benjamin Petit <benjaminpetit@users.noreply.github.com>
2017-07-05 10:40:19 -0700

    Add compatibility director and version selector change in OrleansManager (#3194)

--- src/Orleans/Versions/Compatibility/ICompatibilityDirector.cs
+++ src/Orleans/Versions/Compatibility/ICompatibilityDirector.cs
@@ -14,5 +14,15 @@ namespace Orleans.Versions.Compatibility
     [Serializable]
     public abstract class CompatibilityStrategy
     {
+        public static CompatibilityStrategy Parse(string str)
+        {
+            if (str.Equals(typeof(AllVersionsCompatible).Name))
+                return AllVersionsCompatible.Singleton;
+            if (str.Equals(typeof(BackwardCompatible).Name))
+                return BackwardCompatible.Singleton;
+            if (str.Equals(typeof(StrictVersionCompatible).Name))
+                return StrictVersionCompatible.Singleton;
+            return null;
+        }
     }
 }
