80fe9792ed5d4af350dc368ad98603251da0472a
xiazen <xiazen@microsoft.com>
2017-07-05 14:19:39 -0700

    PR feedback

--- src/Orleans/Providers/ILeaseProvider.cs
+++ src/Orleans/Providers/ILeaseProvider.cs
@@ -7,21 +7,48 @@ using System.Threading.Tasks;
 namespace Orleans.LeaseProviders
 {
     /// <summary>
-    /// Aquired lease, which contains the resource key, duration of the lease and the token you can use to renew the lease with
+    /// Acquired lease
     /// </summary>
     public class AcquiredLease
     {
+        /// <summary>
+        /// The resource key which the lease is attached to 
+        /// </summary>
         public string ResourceKey { get; }
+        /// <summary>
+        /// Duration of the aquired lease
+        /// </summary>
         public TimeSpan Duration { get; }
+        /// <summary>
+        /// Lease token, whcih will be null if acquiring or renewing the lease failed
+        /// </summary>
         public string Token { get; }
+        /// <summary>
+        /// Whether successfully acquire or renew the lease
+        /// </summary>
+        public bool Success { get; }
+        /// <summary>
+        /// If acquiring or renewing the lease failed, this is the exception which caused it
+        /// </summary>
+        public Exception FailureException { get; }
+        /// <summary>
+        /// Start time for this lease, which is when the lease is acquired or renewed
+        /// </summary>
+        public DateTime StartTimeUtc { get; }
     }
 
     /// <summary>
-    /// Lease request where you can specify ResourceKey, which you want to apply a lease on, and duration of your lease. 
+    /// Lease request where you can specify ResourceKey and duration of your lease. 
     /// </summary>
     public class LeaseRequest
     {
+        /// <summary>
+        /// The key of the resource where you want to apply the lease on
+        /// </summary>
         public string ResourceKey { get; set; }
+        /// <summary>
+        /// Duration of the lease
+        /// </summary>
         public TimeSpan? Duration { get; set; }
     }
 
@@ -30,8 +57,26 @@ namespace Orleans.LeaseProviders
     /// </summary>
     public interface ILeaseProvider
     {
-        Task<AcquiredLease[]> Acquire(string category, LeaseRequest[] requestedLeases);
+        /// <summary>
+        /// Batch acquire leases operation
+        /// </summary>
+        /// <param name="category">resource category</param>
+        /// <param name="leaseRequests"></param>
+        /// <returns>Lease acquiring results array, whose order is the same with leaseRequstes</returns>
+        Task<AcquiredLease[]> Acquire(string category, LeaseRequest[] leaseRequests);
+        /// <summary>
+        /// Batch renew lease operation
+        /// </summary>
+        /// <param name="category">resource category</param>
+        /// <param name="aquiredLeases"></param>
+        /// <returns>Lease renew results array, whose order is the same with acquiredLeases</returns>
         Task<AcquiredLease[]> Renew(string category, AcquiredLease[] aquiredLeases);
+        /// <summary>
+        /// Batch release lease operation
+        /// </summary>
+        /// <param name="category">resource category</param>
+        /// <param name="aquiredLeases"></param>
+        /// <returns></returns>
         Task Release(string category, AcquiredLease[] aquiredLeases);
     }
 
