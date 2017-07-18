60d867d9cfd698a071f53fcd0364b974346421b3
Dmitry Vakulenko <dmitry.vakulenko@outlook.com>
2017-07-18 00:09:03 +0000

    Move to GrainExtensions class

--- test/DefaultCluster.Tests/OneWayCallTests.cs
+++ test/DefaultCluster.Tests/OneWayCallTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using Orleans;
-using Orleans.Runtime.GrainMethodInvocationExtensions;
 using TestExtensions;
 using UnitTests.GrainInterfaces;
 using Xunit;
