371688650f00286bb6b4a21b995f17aac384f313
Reuben Bond <reuben.bond@gmail.com>
2017-08-18 01:39:34 +1000

    ClusterClient: Dispose() is equivalent to Abort() (#3306)

--- src/Orleans/Runtime/OutsideRuntimeClient.cs
+++ src/Orleans/Runtime/OutsideRuntimeClient.cs
@@ -33,6 +33,7 @@ namespace Orleans
         private bool listenForMessages;
         private CancellationTokenSource listeningCts;
         private bool firstMessageReceived;
+        private bool disposing;
 
         private ClientProviderRuntime clientProviderRuntime;
         private StatisticsProviderManager statisticsProviderManager;
@@ -780,7 +781,9 @@ namespace Orleans
                 }
             }
             catch (Exception) { }
-            Utils.SafeExecute(() => (this.ServiceProvider as IDisposable)?.Dispose());
+
+            Utils.SafeExecute(() => this.Dispose());
+
             try
             {
                 LogManager.UnInitialize();
@@ -867,22 +870,25 @@ namespace Orleans
 
         public void Dispose()
         {
+            if (this.disposing) return;
+            this.disposing = true;
+
             if (listeningCts != null)
             {
-                listeningCts.Dispose();
+                Utils.SafeExecute(() => listeningCts.Dispose());
                 listeningCts = null;
-
-                this.assemblyProcessor?.Dispose();
             }
 
-            transport?.Dispose();
+            Utils.SafeExecute(() => this.assemblyProcessor?.Dispose());
+
+            Utils.SafeExecute(() => transport?.Dispose());
             if (ClientStatistics != null)
             {
-                ClientStatistics.Dispose();
+                Utils.SafeExecute(() => ClientStatistics.Dispose());
                 ClientStatistics = null;
             }
 
-            (this.ServiceProvider as IDisposable)?.Dispose();
+            Utils.SafeExecute(() => (this.ServiceProvider as IDisposable)?.Dispose());
             this.ServiceProvider = null;
             GC.SuppressFinalize(this);
         }
