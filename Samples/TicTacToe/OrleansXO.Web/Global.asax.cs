501fb414f666505e5764cf9eabefc894a38d8a7d
Julian Dominguez <julido@microsoft.com>
2017-08-30 16:15:59 -0700

    Retry when initializing client

--- Samples/TicTacToe/OrleansXO.Web/Global.asax.cs
+++ Samples/TicTacToe/OrleansXO.Web/Global.asax.cs
@@ -4,6 +4,10 @@ using Orleans.Runtime.Host;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Orleans.Runtime.Configuration;
+using System.Diagnostics;
+using System.Threading;
+using Orleans.Runtime;
+using System;
 
 namespace OrleansXO.Web
 {
@@ -13,15 +17,35 @@ namespace OrleansXO.Web
     {
         protected void Application_Start()
         {
-            if (RoleEnvironment.IsAvailable)
+            const int InitializeAttemptsBeforeFailing = 5;
+            int attempt = 0;
+            while (true)
             {
-                // running in Azure
-                AzureClient.Initialize(AzureClient.DefaultConfiguration());
-            }
-            else
-            {
-                // not running in Azure
-                GrainClient.Initialize(ClientConfiguration.LocalhostSilo());
+                try
+                {
+                    if (RoleEnvironment.IsAvailable)
+                    {
+                        // running in Azure
+                        AzureClient.Initialize(AzureClient.DefaultConfiguration());
+                    }
+                    else
+                    {
+                        // not running in Azure
+                        GrainClient.Initialize(ClientConfiguration.LocalhostSilo());
+                    }
+                    Trace.WriteLine("Client successfully connect to silo host");
+                    break;
+                }
+                catch (SiloUnavailableException)
+                {
+                    attempt++;
+                    Trace.TraceWarning($"Attempt {attempt} of {InitializeAttemptsBeforeFailing} failed to initialize the Orleans client.");
+                    if (attempt > InitializeAttemptsBeforeFailing)
+                    {
+                        throw;
+                    }
+                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                }
             }
 
             AreaRegistration.RegisterAllAreas();
