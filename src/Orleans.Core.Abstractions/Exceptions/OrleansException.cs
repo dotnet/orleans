ca5bbb9caa53c524ed55fca785ccfbd8d424d0f5
Benjamin Petit <bpetit@microsoft.com>
2017-09-25 13:34:30 -0700

    Move OrleansException to another file and move it to Abstractions

---
+++ src/Orleans.Core.Abstractions/Exceptions/OrleansException.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Orleans.Runtime
+{
+    /// <summary>
+    /// An exception class used by the Orleans runtime for reporting errors.
+    /// </summary>
+    /// <remarks>
+    /// This is also the base class for any more specific exceptions 
+    /// raised by the Orleans runtime.
+    /// </remarks>
+    [Serializable]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1058:TypesShouldNotExtendCertainBaseTypes")]
+    public class OrleansException : Exception
+    {
+        public OrleansException() : base("Unexpected error.") { }
+
+        public OrleansException(string message) : base(message) { }
+
+        public OrleansException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected OrleansException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
