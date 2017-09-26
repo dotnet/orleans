6af49208c23f443d07a38f7daff1c72a6243b1e3
Benjamin Petit <bpetit@microsoft.com>
2017-09-26 15:03:22 -0700

    Revert TickStatus TickStatus.NewStruct to internal and make Abstractions internals visible to OrleansRuntime

--- src/Orleans.Core.Abstractions/Timers/IRemindable.cs
+++ src/Orleans.Core.Abstractions/Timers/IRemindable.cs
@@ -55,7 +55,7 @@ namespace Orleans
             /// </summary>
             public DateTime CurrentTickTime { get; private set; }
 
-            public static TickStatus NewStruct(DateTime firstTickTime, TimeSpan period, DateTime timeStamp)
+            internal static TickStatus NewStruct(DateTime firstTickTime, TimeSpan period, DateTime timeStamp)
             {
                 return
                     new TickStatus
