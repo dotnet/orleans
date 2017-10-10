7794f9ba98dea8e8b524522cd287c636d3f010dd
Benjamin Petit <bpetit@microsoft.com>
2017-10-10 14:48:38 -0700

    Inline ActivationId.GetClientGWActivation in ClientObserverRegistrar

--- src/Orleans.Core.Abstractions/IDs/ActivationId.cs
+++ src/Orleans.Core.Abstractions/IDs/ActivationId.cs
@@ -47,19 +47,6 @@ namespace Orleans.Runtime
             return FindOrCreate(grain.Key);
         }
 
-        public static ActivationId GetClientGWActivation(GrainId grain, SiloAddress location)
-        {
-            if (!grain.IsClient)
-                throw new ArgumentException("ClientGW activation IDs can only be created for client grains");
-
-            // Construct a unique and deterministic ActivationId based on GrainId and SiloAddress.
-            string stringToHash = grain.ToParsableString() + location.Endpoint + location.Generation.ToString(System.Globalization.CultureInfo.InvariantCulture);
-            Guid hash = Utils.CalculateGuidHash(stringToHash);
-            UniqueKey key = UniqueKey.NewKey(hash);
-
-            return FindOrCreate(key);
-        }
-
         internal static ActivationId GetActivationId(UniqueKey key)
         {
             return FindOrCreate(key);
