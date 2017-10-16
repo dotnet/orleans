2304e068bf4d5fd39bfc45b5e3554f95dcc161b9
Benjamin Petit <bpetit@microsoft.com>
2017-10-16 14:38:20 -0700

    Move attributes related to multicluster to Abstractions

--- src/Orleans.Core/GrainDirectory/MultiClusterRegistrationStrategy.cs
+++ src/Orleans.Core/GrainDirectory/MultiClusterRegistrationStrategy.cs
@@ -7,19 +7,6 @@ using System.Collections.Generic;
 
 namespace Orleans.GrainDirectory
 {
-    /// <summary>
-    /// Interface for multi-cluster registration strategies. Used by protocols that coordinate multiple instances.
-    /// </summary>
-    public interface IMultiClusterRegistrationStrategy {
-
-        /// <summary>
-        /// Determines which remote clusters have instances.
-        /// </summary>
-        /// <param name="mcConfig">The multi-cluster configuration</param>
-        /// <param name="myClusterId">The cluster id of this cluster</param>
-        /// <returns></returns>
-        IEnumerable<string> GetRemoteInstances(IReadOnlyList<string> clusters, string myClusterId);
-    }
 
     internal class MultiClusterRegistrationStrategyManager
     {
@@ -55,16 +42,6 @@ namespace Orleans.GrainDirectory
                             grainClass.Name));
             }
         }
-    }
-
-    /// <summary>
-    /// A superclass for all multi-cluster registration strategies.
-    /// Strategy object which is used as keys to select the proper registrar.
-    /// </summary>
-    [Serializable]
-    internal abstract class MultiClusterRegistrationStrategy : IMultiClusterRegistrationStrategy
-    {
-        public abstract IEnumerable<string> GetRemoteInstances(IReadOnlyList<string> clusters, string myClusterId);
     }
 
     public static class MultiClusterRegistrationStrategyExtensions
