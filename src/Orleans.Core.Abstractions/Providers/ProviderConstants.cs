d7b068bee493b4ab8992e44bbf20c7362f810244
Benjamin Petit <bpetit@microsoft.com>
2017-10-16 12:57:40 -0700

    Move provider related grain attributes in Abstractions

---
+++ src/Orleans.Core.Abstractions/Providers/ProviderConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orleans.Providers
+{
+    public static class ProviderConstants
+    {
+        public const string DEFAULT_STORAGE_PROVIDER_NAME = "Default";
+
+        public const string DEFAULT_LOG_CONSISTENCY_PROVIDER_NAME = "Default";
+    }
+}
---
+++ src/Orleans.Core.Abstractions/Providers/ProviderGrainAttributes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orleans.Providers
+{
+    /// <summary>
+    /// The [Orleans.Providers.StorageProvider] attribute is used to define which storage provider to use for persistence of grain state.
+    /// <para>
+    /// Specifying [Orleans.Providers.StorageProvider] property is recommended for all grains which extend Grain&lt;T&gt;.
+    /// If no [Orleans.Providers.StorageProvider] attribute is  specified, then a "Default" strorage provider will be used.
+    /// If a suitable storage provider cannot be located for this grain, then the grain will fail to load into the Silo.
+    /// </para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class StorageProviderAttribute : Attribute
+    {
+        /// <summary>
+        /// The name of the provider to be used for persisting of grain state
+        /// </summary>
+        public string ProviderName { get; set; }
+
+        public StorageProviderAttribute()
+        {
+            ProviderName = ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME;
+        }
+    }
+
+    /// <summary>
+    /// The [Orleans.Providers.LogConsistencyProvider] attribute is used to define which consistency provider to use for grains using the log-view state abstraction.
+    /// <para>
+    /// Specifying [Orleans.Providers.LogConsistencyProvider] property is recommended for all grains that derive
+    /// from LogConsistentGrain, such as JournaledGrain.
+    /// If no [Orleans.Providers.LogConsistencyProvider] attribute is  specified, then the runtime tries to locate
+    /// one as follows. First, it looks for a
+    /// "Default" provider in the configuration file, then it checks if the grain type defines a default.
+    /// If a consistency provider cannot be located for this grain, then the grain will fail to load into the Silo.
+    /// </para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class LogConsistencyProviderAttribute : Attribute
+    {
+        /// <summary>
+        /// The name of the provider to be used for consistency
+        /// </summary>
+        public string ProviderName { get; set; }
+
+        public LogConsistencyProviderAttribute()
+        {
+            ProviderName = ProviderConstants.DEFAULT_LOG_CONSISTENCY_PROVIDER_NAME;
+        }
+    }
+}
