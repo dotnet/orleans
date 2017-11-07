d1b5df13a80db35609ec8ddb3f2265869090cc88
Reuben Bond <reuben.bond@gmail.com>
2017-11-08 10:00:50 +1100

    Code Generation: do not generate KnownTypes for nested private or compiler-generated classes (#3643)

--- test/TestGrains/MultipleGenericParameterInterfaceImpl.cs
+++ test/TestGrains/MultipleGenericParameterInterfaceImpl.cs
@@ -7,18 +7,21 @@ using System.Threading.Tasks;
 
 namespace UnitTests.Grains
 {
-    interface IFactory<TInput, TOutput>
-    {
-        Task<TOutput> Product(TInput input);
-    }
-
-    class Factory
+    public class CodeGenTestPoco
     {
         public int SomeProperty { get; set; }
     }
 
-    class AFactory : Factory, IFactory<int, double>
+    // This class forms a code generation test case.
+    // If the code generator does generate any code for the async state machine in the Product
+    // method, it must generate valid C# syntax. See: https://github.com/dotnet/orleans/pull/3639
+    public class FeaturePopulatorCodeGenTestClass : CodeGenTestPoco, FeaturePopulatorCodeGenTestClass.IFactory<int, double>
     {
+        public interface IFactory<TInput, TOutput>
+        {
+            Task<TOutput> Product(TInput input);
+        }
+
         async Task<double> IFactory<int, double>.Product(int input)
         {
             await Task.Delay(100);
