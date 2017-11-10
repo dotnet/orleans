ef908d1cf605e6fe862c50c279ce38c838afe3d4
Dmitry Vakulenko <dmitry.vakulenko@outlook.com>
2017-11-10 20:11:04 +0200

    Access modifier for AsynchAgentTask

--- src/Orleans.Core/Runtime/AsynchAgentTask.cs
+++ src/Orleans.Core/Runtime/AsynchAgentTask.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace Orleans.Runtime
 {
-    class AsynchAgentTask : Task
+    internal class AsynchAgentTask : Task
     {
         public string Name { get; }
 
