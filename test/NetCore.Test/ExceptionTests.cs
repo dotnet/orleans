27b2b99470bcf2cb545ce6055c5efbc8d3e616f5
Reuben Bond <a-rebond@microsoft.com>
2017-11-20 15:06:30 +1100

    Add initial .NET Core test project

---
+++ test/NetCore.Test/ExceptionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Runtime.Configuration;
+using Xunit;
+
+namespace NetCore.Test
+{
+    [Trait("Category", "BVT")]
+    public class ExceptionTests
+    {
+        private readonly ISiloHost silo;
+        private readonly IClusterClient client;
+
+        public ExceptionTests()
+        {
+            this.silo = SiloHostBuilder.CreateDefault().AddApplicationPartsFromAppDomain().ConfigureLocalHostPrimarySilo().Build();
+            this.silo.StartAsync().GetAwaiter().GetResult();
+
+            this.client = ClientBuilder.CreateDefault().AddApplicationPartsFromAppDomain().UseConfiguration(ClientConfiguration.LocalhostSilo()).Build();
+            this.client.Connect().GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public async Task ExceptionsPropagatedFromGrainToClient()
+        {
+            var grain = this.client.GetGrain<UnitTests.GrainInterfaces.IExceptionGrain>(0);
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => grain.ThrowsInvalidOperationException());
+            Assert.Equal("Test exception", exception.Message);
+        }
+    }
+}
---
+++ test/TestLibrary/ExceptionGrain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.Threading.Tasks;
+using Orleans;
+
+namespace TestLibrary
+{
+    public interface IExceptionGrain : IGrainWithGuidKey
+    {
+        Task Throw();
+    }
+    
+    public class ExceptionGrain : Grain, IExceptionGrain
+    {
+        public Task Throw() => throw new InvalidOperationException("Throwing");
+    }
+}
