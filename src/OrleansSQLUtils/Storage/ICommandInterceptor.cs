a399ee013ca6726d6b0a52f0a82fa8d28b5b2f2f
Roman Eisendle <r.eisendle@gmail.com>
2017-12-05 20:20:50 +0100

    Oracle support for storage and system store (#3576)

---
+++ src/OrleansSQLUtils/Storage/ICommandInterceptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace OrleansSQLUtils.Storage
+{
+    internal interface ICommandInterceptor
+    {
+        void Intercept(IDbCommand command);
+    }
+}
