6f70b37270137dc42dff137d4bc21dd01281a478
Julian Dominguez <julido@microsoft.com>
2017-12-14 13:27:31 -0800

    Move SiloOptions to its own file

--- src/Orleans.Runtime/Silo/SiloInitializationParameters.cs
+++ src/Orleans.Runtime/Silo/SiloInitializationParameters.cs
@@ -1,30 +1,8 @@
-using System;
 using Microsoft.Extensions.Options;
 using Orleans.Runtime.Configuration;
 
 namespace Orleans.Runtime
 {
-    /// <summary>
-    /// Silo identity configuration options.
-    /// </summary>
-    public class SiloOptions
-    {
-        /// <summary>
-        /// Gets or sets the silo name.
-        /// </summary>
-        public string SiloName { get; set; }
-
-        /// <summary>
-        /// Gets or sets the cluster identity. This used to be called DeploymentId before Orleans 2.0 name.
-        /// </summary>
-        public string ClusterId { get; set; }
-
-        /// <summary>
-        /// Gets or sets a unique identifier for this service, which should survive deployment and redeployment, where as <see cref="ClusterId"/> might not.
-        /// </summary>
-        public Guid ServiceId { get; set; }
-    }
-
     /// <summary>
     /// Parameters used to initialize a silo and values derived from those parameters.
     /// </summary>
