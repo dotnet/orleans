b9ff9e24879ebd2b4d07bf5d496e36730b92a323
Lari Hotari <lari@hotari.net>
2017-12-28 12:41:40 +0200

    Add test for invalid preamble size handling

---
+++ test/Tester/ClientConnectionTests/InvalidPreambleConnectionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using TestExtensions;
+using Xunit;
+
+namespace Tester.ClientConnectionTests
+{
+    public class InvalidPreambleConnectionTests : TestClusterPerTest
+    {
+        [Fact, TestCategory("Functional")]
+        public void ShouldCloseConnectionWhenClientSendsInvalidPreambleSize()
+        {
+            var gwEndpoint = this.HostedCluster.Primary.NodeConfiguration.ProxyGatewayEndpoint;
+
+            using (Socket s = new Socket(gwEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                s.Connect(gwEndpoint);
+
+                Int32 invalidSize = 99999;
+                s.Send(BitConverter.GetBytes(invalidSize));
+
+                bool socketClosed = s.Poll(100000, SelectMode.SelectRead) && s.Available == 0;
+                Assert.True(socketClosed);
+            }
+        }
+    }
+}
