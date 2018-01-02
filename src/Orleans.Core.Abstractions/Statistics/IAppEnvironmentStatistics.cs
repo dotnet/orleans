2e8a012631b60c92d103349bfb25e81993f29148
Benjamin Petit <bpetit@microsoft.com>
2018-01-02 10:46:14 -0800

    Add interface to abstract previous implementation of RuntimeStatisticsGroup

---
+++ src/Orleans.Core.Abstractions/Statistics/IAppEnvironmentStatistics.cs
@@ -0,0 +1,7 @@
+﻿namespace Orleans.Statistics
+{
+    public interface IAppEnvironmentStatistics
+    {
+        long MemoryUsage { get; }
+    }
+}
---
+++ src/Orleans.Core.Abstractions/Statistics/IHostEnvironmentStatistics.cs
@@ -0,0 +1,11 @@
+﻿namespace Orleans.Statistics
+{
+    public interface IHostEnvironmentStatistics
+    {
+        long TotalPhysicalMemory { get; }
+
+        float CpuUsage { get; }
+
+        long AvailableMemory { get; }
+    }
+}
