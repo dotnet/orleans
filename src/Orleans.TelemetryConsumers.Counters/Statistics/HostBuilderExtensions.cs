865ac74b9e6046a9deb6ad60b8e6643e8d9fa93d
Benjamin Petit <bpetit@microsoft.com>
2018-01-02 14:01:05 -0800

    Add a perf counter based implementation of IHostEnvironmentStatistics

---
+++ src/Orleans.TelemetryConsumers.Counters/Statistics/HostBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿#define LOG_MEMORY_PERF_COUNTERS
+
+using Microsoft.Extensions.DependencyInjection;
+using Orleans.Hosting;
+
+namespace Orleans.Statistics
+{
+    public static class SiloHostBuilderExtensions
+    {
+        public static ISiloHostBuilder UsePerfCounterEnvironmentStatistics(this ISiloHostBuilder builder)
+        {
+            return builder.ConfigureServices(services => services.AddSingleton<IHostEnvironmentStatistics, PerfCounterEnvironmentStatistics>());
+        }
+    }
+
+    public static class ClientBuilderExtensions
+    {
+        public static IClientBuilder UsePerfCounterEnvironmentStatistics(this IClientBuilder builder)
+        {
+            return builder.ConfigureServices(services => services.AddSingleton<IHostEnvironmentStatistics, PerfCounterEnvironmentStatistics>());
+        }
+    }
+}
