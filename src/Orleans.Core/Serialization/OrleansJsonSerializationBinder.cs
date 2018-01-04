5354ebe7bd4b991535c95fcaaa4a43ed8b3ad979
Reuben Bond <reuben.bond@gmail.com>
2018-01-05 06:19:18 +0900

    Support resolving 1.5.x type names to 2.x types. (#3805)
    * [WIP] support resolving 1.5.x type names to 2.x types.
    
    * limit broad assembly remapping to types in Orleans namespace

---
+++ src/Orleans.Core/Serialization/OrleansJsonSerializationBinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json.Serialization;
+using Orleans.Runtime;
+
+namespace Orleans.Serialization
+{
+    /// <inheritdoc />
+    public class OrleansJsonSerializationBinder : DefaultSerializationBinder
+    {
+        private readonly ITypeResolver typeResolver;
+
+        /// <inheritdoc />
+        public OrleansJsonSerializationBinder(ITypeResolver typeResolver)
+        {
+            this.typeResolver = typeResolver;
+        }
+
+        /// <inheritdoc />
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            var fullName = !string.IsNullOrWhiteSpace(assemblyName) ? typeName + ',' + assemblyName : typeName;
+            if (typeResolver.TryResolveType(fullName, out var type)) return type;
+
+            return base.BindToType(assemblyName, typeName);
+        }
+    }
+}
