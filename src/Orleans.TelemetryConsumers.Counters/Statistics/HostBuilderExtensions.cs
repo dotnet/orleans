bd9577013d2fe28a4fb870f5d60fca56e289cf0d
Benjamin Petit <bpetit@microsoft.com>
2018-01-23 16:25:58 -0800

    Add XML comments

--- src/Orleans.TelemetryConsumers.Counters/Statistics/HostBuilderExtensions.cs
+++ src/Orleans.TelemetryConsumers.Counters/Statistics/HostBuilderExtensions.cs
@@ -1,4 +1,4 @@
-﻿#define LOG_MEMORY_PERF_COUNTERS
+#define LOG_MEMORY_PERF_COUNTERS
 
 using Microsoft.Extensions.DependencyInjection;
 using Orleans.Hosting;
@@ -7,6 +7,9 @@ namespace Orleans.Statistics
 {
     public static class SiloHostBuilderExtensions
     {
+        /// <summary>
+        /// Use Windows performance counters as source for host environment statistics
+        /// </summary>
         public static ISiloHostBuilder UsePerfCounterEnvironmentStatistics(this ISiloHostBuilder builder)
         {
             return builder.ConfigureServices(services => services.AddSingleton<IHostEnvironmentStatistics, PerfCounterEnvironmentStatistics>());
@@ -15,6 +18,9 @@ namespace Orleans.Statistics
 
     public static class ClientBuilderExtensions
     {
+        /// <summary>
+        /// Use Windows performance counters as source for host environment statistics
+        /// </summary>
         public static IClientBuilder UsePerfCounterEnvironmentStatistics(this IClientBuilder builder)
         {
             return builder.ConfigureServices(services => services.AddSingleton<IHostEnvironmentStatistics, PerfCounterEnvironmentStatistics>());
