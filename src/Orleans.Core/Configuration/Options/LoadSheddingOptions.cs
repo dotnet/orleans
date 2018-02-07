edcfa7415e0cad900a9e4f4436f4182a63deb1e8
Sergey Bykov <sbykov@microsoft.com>
2018-02-06 18:20:27 -0800

    Improve xml docs.

--- src/Orleans.Core/Configuration/Options/LoadSheddingOptions.cs
+++ src/Orleans.Core/Configuration/Options/LoadSheddingOptions.cs
@@ -7,13 +7,13 @@ namespace Orleans.Hosting
     public class LoadSheddingOptions
     {
         /// <summary>
-        /// The LoadShedding element specifies the gateway load shedding configuration for the node.
-        /// If it does not appear, gateway load shedding is disabled.
+        /// Specifies whether or not load shedding in the client gateway and stream providers is enabled.
+        /// The default value is false, meaning that load shedding is disabled.
         /// </summary>
         public bool LoadSheddingEnabled { get; set; }
 
         /// <summary>
-        /// The LoadLimit attribute specifies the system load, in CPU%, at which load begins to be shed.
+        /// Specifies the system load, in CPU%, at which load begins to be shed.
         /// Note that this value is in %, so valid values range from 1 to 100, and a reasonable value is
         /// typically between 80 and 95.
         /// This value is ignored if load shedding is disabled, which is the default.
