c4fb49a7aec8f9b1bdd13c4e669d2eac908471b4
ReubenBond <a-rebond@microsoft.com>
2018-02-14 07:01:06 +1100

    Include more clustering providers

--- src/Orleans.Core/Configuration/Validators/ClientClusteringValidator.cs
+++ src/Orleans.Core/Configuration/Validators/ClientClusteringValidator.cs
@@ -10,12 +10,14 @@ namespace Orleans.Configuration.Validators
     internal class ClientClusteringValidator : IConfigurationValidator
     {
         internal const string ClusteringNotConfigured =
-            "Clustering has not been configured. Configure clustering using one of the clustering packages, such as: "
-            + "\n  * Microsoft.Orleans.Clustering.AzureStorage for Microsoft Azure Storage"
+            "Clustering has not been configured. Configure clustering using one of the clustering packages, such as:"
+            + "\n  * Microsoft.Orleans.Clustering.AzureStorage"
             + "\n  * Microsoft.Orleans.Clustering.AdoNet for ADO.NET systems such as SQL Server, MySQL, PostgreSQL, and Oracle"
             + "\n  * Microsoft.Orleans.Clustering.DynamoDB"
             + "\n  * Microsoft.Orleans.Clustering.ServiceFabric"
-            + "\n  * See: https://www.nuget.org/packages?q=orleans+clustering";
+            + "\n  * Microsoft.Orleans.Clustering.Consul"
+            + "\n  * Microsoft.Orleans.Clustering.ZooKeeper"
+            + "\n  * Others, see: https://www.nuget.org/packages?q=Microsoft.Orleans.Clustering.";
 
         private readonly IServiceProvider serviceProvider;
 
