7f6a1f6006a0835e3da7e24ee359a1950fac055f
Benjamin Petit <bpetit@microsoft.com>
2018-02-26 13:11:08 -0800

    Add basic sample using docker and asp.net core

---
+++ Samples/docker-aspnet-core/Grains/ValueGrain.cs
@@ -0,0 +1,23 @@
+﻿using GrainInterfaces;
+using Orleans;
+using System;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public class ValueGrain : Grain, IValueGrain
+    {
+        private string value = "none";
+
+        public Task<string> GetValue()
+        {
+            return Task.FromResult(this.value);
+        }
+
+        public Task SetValue(string value)
+        {
+            this.value = value;
+            return Task.CompletedTask;
+        }
+    }
+}
