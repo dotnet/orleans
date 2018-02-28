5c8d4e658899451375e9fb42cefc5fbdba85cee5
Reuben Bond <reuben.bond@gmail.com>
2018-03-01 05:00:54 +1000

    Make equality requirements of IApplicationPart explicit. (#4092)

--- src/Orleans.Core/ApplicationParts/AssemblyPart.cs
+++ src/Orleans.Core/ApplicationParts/AssemblyPart.cs
@@ -43,15 +43,19 @@ namespace Orleans.ApplicationParts
         /// </returns>
         protected bool Equals(AssemblyPart other)
         {
-            return Equals(this.Assembly, other.Assembly);
+            return Equals(this.Assembly, other?.Assembly);
         }
+
+        /// <inheritdoc />
+        public bool Equals(IApplicationPart other) => this.Equals(other as AssemblyPart);
 
         /// <inheritdoc />
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            return obj.GetType() == this.GetType() && this.Equals((AssemblyPart) obj);
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((AssemblyPart) obj);
         }
 
         /// <inheritdoc />
