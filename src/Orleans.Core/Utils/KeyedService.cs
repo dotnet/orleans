5b97b655fd05343d59e58c6c085cb9df3b4ac19d
Jason Bragg <jbragg@microsoft.com>
2018-03-22 19:03:57 -0700

    Overridable options (#4277)

--- src/Orleans.Core/Utils/KeyedService.cs
+++ src/Orleans.Core/Utils/KeyedService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Orleans.Configuration;
 
 namespace Orleans.Runtime
 {
@@ -84,6 +86,25 @@ namespace Orleans.Runtime
 
     public static class KeyedServiceExtensions
     {
+
+        /// <summary>
+        /// Gets <see cref="ClusterOptions"/> which may have been overridden on a per-provider basis.
+        /// Note: This is intended for migration purposes as a means to handle previously inconsistent behaviors in how providers used ServiceId and ClusterId.
+        /// </summary>
+        public static IOptions<ClusterOptions> GetProviderClusterOptions(this IServiceProvider services, string key) => services.GetOverridableOption<ClusterOptions>(key);
+
+        /// <summary>
+        /// Gets option that can be overriden by named service.
+        /// </summary>
+        private static IOptions<TOptions> GetOverridableOption<TOptions>(this IServiceProvider services, string key)
+            where TOptions : class, new()
+        {
+            TOptions option = services.GetServiceByName<TOptions>(key);
+            return option != null
+                ? Options.Create(option)
+                : services.GetRequiredService<IOptions<TOptions>>();
+        }
+
         /// <summary>
         /// Register a transient keyed service
         /// </summary>
