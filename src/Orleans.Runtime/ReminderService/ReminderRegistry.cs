57898a03192f54f06ceb936e913316416f23e0e1
Reuben Bond <reuben.bond@gmail.com>
2018-03-23 12:25:25 +1100

    Improve error message when attempting to use reminders without configuring a reminder table (#4287)

--- src/Orleans.Runtime/ReminderService/ReminderRegistry.cs
+++ src/Orleans.Runtime/ReminderService/ReminderRegistry.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
 using Orleans.Runtime.Services;
 using Orleans.Timers;
 
@@ -8,14 +10,27 @@ namespace Orleans.Runtime.ReminderService
 {
     internal class ReminderRegistry : GrainServiceClient<IReminderService>, IReminderRegistry
     {
+        private const string ReminderServiceNotConfigured =
+           "The reminder service has not been configured. Reminders can be configured using extension methods from the following packages:"
+           + "\n  * Microsoft.Orleans.Reminders.AzureStorage via ISiloHostBuilder.UseAzureTableReminderService(...)"
+           + "\n  * Microsoft.Orleans.Reminders.AdoNet via ISiloHostBuilder.UseAdoNetReminderService(...)"
+           + "\n  * Microsoft.Orleans.Reminders.DynamoDB via via ISiloHostBuilder.UseDynamoDBReminderService(...)"
+           + "\n  * Microsoft.Orleans.OrleansRuntime via ISiloHostBuilder.UseInMemoryReminderService(...) (Note: for development purposes only)"
+           + "\n  * Others, see: https://www.nuget.org/packages?q=Microsoft.Orleans.Reminders.";
+
         private const uint MaxSupportedTimeout = 0xfffffffe;
+        private readonly IServiceProvider serviceProvider;
+        private bool reminderServiceRegistered;
 
         public ReminderRegistry(IServiceProvider serviceProvider) : base(serviceProvider)
         {
+            this.serviceProvider = serviceProvider;
         }
 
         public Task<IGrainReminder> RegisterOrUpdateReminder(string reminderName, TimeSpan dueTime, TimeSpan period)
         {
+            this.EnsureReminderServiceRegistered();
+
             // Perform input volatility checks that are consistent with System.Threading.Timer
             // http://referencesource.microsoft.com/#mscorlib/system/threading/timer.cs,c454f2afe745d4d3,references
             var dueTm = (long) dueTime.TotalMilliseconds;
@@ -54,11 +69,13 @@ namespace Orleans.Runtime.ReminderService
 
         public Task UnregisterReminder(IGrainReminder reminder)
         {
+            this.EnsureReminderServiceRegistered();
             return GrainService.UnregisterReminder(reminder);
         }
 
         public Task<IGrainReminder> GetReminder(string reminderName)
         {
+            this.EnsureReminderServiceRegistered();
             if (string.IsNullOrEmpty(reminderName))
             {
                 throw new ArgumentException("Cannot use null or empty name for the reminder", nameof(reminderName));
@@ -69,7 +86,20 @@ namespace Orleans.Runtime.ReminderService
 
         public Task<List<IGrainReminder>> GetReminders()
         {
+            this.EnsureReminderServiceRegistered();
             return GrainService.GetReminders(CallingGrainReference);
+        }
+
+        private void EnsureReminderServiceRegistered()
+        {
+            if (this.reminderServiceRegistered) return;
+            var reminderTable = this.serviceProvider.GetService<IReminderTable>();
+            if (reminderTable == null)
+            {
+                throw new OrleansConfigurationException(ReminderServiceNotConfigured);
+            }
+
+            this.reminderServiceRegistered = true;
         }
     }
 }
