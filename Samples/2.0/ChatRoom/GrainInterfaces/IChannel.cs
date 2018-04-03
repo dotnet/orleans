d804a25e7c2852fd4ca267d0aa2969f6a05968a3
Sergey Bykov <sbykov@microsoft.com>
2018-04-03 14:29:43 -0700

    Fix indentation

--- Samples/2.0/ChatRoom/GrainInterfaces/IChannel.cs
+++ Samples/2.0/ChatRoom/GrainInterfaces/IChannel.cs
@@ -7,9 +7,9 @@ namespace GrainInterfaces
 {
 	public interface IChannel : IGrainWithStringKey
 	{
-		Task<Guid> Join(string nickname);
-		Task<Guid> Leave(string nickname);
-		Task<bool> Message(ChatMsg msg);
+	    Task<Guid> Join(string nickname);
+	    Task<Guid> Leave(string nickname);
+	    Task<bool> Message(ChatMsg msg);
 	    Task<ChatMsg[]> ReadHistory(int numberOfMessages);
 	    Task<string[]> GetMembers();
 	}
