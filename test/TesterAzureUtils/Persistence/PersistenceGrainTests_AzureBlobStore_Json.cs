e4f4c40fbe7eb2ab74b955c01264def39df1bee3
Reuben Bond <reuben.bond@gmail.com>
2018-04-14 08:34:19 +1000

    Azure blob storage provider: respect UseJson setting (#4455)

---
+++ test/TesterAzureUtils/Persistence/PersistenceGrainTests_AzureBlobStore_Json.cs
@@ -0,0 +1,123 @@
+using System.Threading.Tasks;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.TestingHost;
+using TestExtensions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tester.AzureUtils.Persistence
+{
+    /// <summary>
+    /// PersistenceGrainTests using AzureStore - Requires access to external Azure blob storage
+    /// </summary>
+    [TestCategory("Persistence"), TestCategory("Azure")]
+    public class PersistenceGrainTests_AzureBlobStore_Json : Base_PersistenceGrainTests_AzureStore, IClassFixture<PersistenceGrainTests_AzureBlobStore_Json.Fixture>
+    {
+        public class Fixture : BaseAzureTestClusterFixture
+        {
+            private class StorageSiloBuilderConfigurator : ISiloBuilderConfigurator
+            {
+                public void Configure(ISiloHostBuilder hostBuilder)
+                {
+                    hostBuilder
+                        .AddAzureBlobGrainStorage("GrainStorageForTest", (AzureBlobStorageOptions options) =>
+                        {
+                            options.ConnectionString = TestDefaultConfiguration.DataConnectionString;
+                            options.UseJson = true;
+                        });
+                }
+            }
+
+            protected override void ConfigureTestCluster(TestClusterBuilder builder)
+            {
+                builder.Options.InitialSilosCount = 4;
+                builder.Options.UseTestClusterMembership = false;
+                builder.AddSiloBuilderConfigurator<SiloBuilderConfigurator>();
+                builder.AddSiloBuilderConfigurator<StorageSiloBuilderConfigurator>();
+                builder.AddClientBuilderConfigurator<ClientBuilderConfigurator>();
+            }
+        }
+
+        public PersistenceGrainTests_AzureBlobStore_Json(ITestOutputHelper output, Fixture fixture) : base(output, fixture)
+        {
+            fixture.EnsurePreconditionsMet();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_AzureBlobStore_Delete_Json()
+        {
+            await base.Grain_AzureStore_Delete();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_AzureBlobStore_Read_Json()
+        {
+            await base.Grain_AzureStore_Read();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_GuidKey_AzureBlobStore_Read_Write_Json()
+        {
+            await base.Grain_GuidKey_AzureStore_Read_Write();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_LongKey_AzureBlobStore_Read_Write_Json()
+        {
+            await base.Grain_LongKey_AzureStore_Read_Write();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_LongKeyExtended_AzureBlobStore_Read_Write_Json()
+        {
+            await base.Grain_LongKeyExtended_AzureStore_Read_Write();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_GuidKeyExtended_AzureBlobStore_Read_Write_Json()
+        {
+            await base.Grain_GuidKeyExtended_AzureStore_Read_Write();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_Generic_AzureBlobStore_Read_Write_Json()
+        {
+            StorageEmulatorUtilities.EnsureEmulatorIsNotUsed();
+
+            await base.Grain_Generic_AzureStore_Read_Write();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_Generic_AzureBlobStore_DiffTypes_Json()
+        {
+            StorageEmulatorUtilities.EnsureEmulatorIsNotUsed();
+
+            await base.Grain_Generic_AzureStore_DiffTypes();
+        }
+
+        [SkippableFact, TestCategory("Functional")]
+        public async Task Grain_AzureBlobStore_SiloRestart_Json()
+        {
+            await base.Grain_AzureStore_SiloRestart();
+        }
+
+        [SkippableFact, TestCategory("CorePerf"), TestCategory("Performance"), TestCategory("Stress")]
+        public void Persistence_Perf_Activate_AzureBlobStore_Json()
+        {
+            base.Persistence_Perf_Activate();
+        }
+
+        [SkippableFact, TestCategory("CorePerf"), TestCategory("Performance"), TestCategory("Stress")]
+        public void Persistence_Perf_Write_AzureBlobStore_Json()
+        {
+            base.Persistence_Perf_Write();
+        }
+
+        [SkippableFact, TestCategory("CorePerf"), TestCategory("Performance"), TestCategory("Stress")]
+        public void Persistence_Perf_Write_Reread_AzureBlobStore_Json()
+        {
+            base.Persistence_Perf_Write_Reread();
+        }
+    }
+}
