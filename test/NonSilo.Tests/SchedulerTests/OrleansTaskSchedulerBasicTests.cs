d706d5adb4104851675cb3e56c5786da91fd7051
ReubenBond <a-rebond@microsoft.com>
2018-04-16 14:19:27 +1000

    Improve reliability of Sched_Task_SubTaskExecutionSequencing test

--- test/NonSilo.Tests/SchedulerTests/OrleansTaskSchedulerBasicTests.cs
+++ test/NonSilo.Tests/SchedulerTests/OrleansTaskSchedulerBasicTests.cs
@@ -504,7 +504,7 @@ namespace UnitTests.SchedulerTests
         }
 
         [Fact]
-        public void Sched_Task_SubTaskExecutionSequencing()
+        public async Task Sched_Task_SubTaskExecutionSequencing()
         {
             UnitTestSchedulingContext context = new UnitTestSchedulingContext();
             this.scheduler.RegisterWorkContext(context);
@@ -512,7 +512,8 @@ namespace UnitTests.SchedulerTests
             LogContext("Main-task " + Task.CurrentId);
 
             int n = 0;
-
+            TaskCompletionSource<int> finished = new TaskCompletionSource<int>();
+            var numCompleted = new[] {0};
             Action closure = () =>
             {
                 LogContext("ClosureWorkItem-task " + Task.CurrentId);
@@ -539,6 +540,10 @@ namespace UnitTests.SchedulerTests
                         LogContext("Sub-task " + id + "-ContinueWith");
 
                         this.output.WriteLine("Sub-task " + id + " Done");
+                        if (Interlocked.Increment(ref numCompleted[0]) == 10)
+                        {
+                            finished.SetResult(0);
+                        }
                     });
                 }
             };
@@ -549,7 +554,7 @@ namespace UnitTests.SchedulerTests
 
             // Pause to let things run
             this.output.WriteLine("Main-task sleeping");
-            Thread.Sleep(TimeSpan.FromSeconds(2));
+            await Task.WhenAny(Task.Delay(TimeSpan.FromSeconds(10)), finished.Task);
             this.output.WriteLine("Main-task awake");
 
             // N should be 10, because all tasks should execute serially
