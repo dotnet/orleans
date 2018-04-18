1f9049fdc6d281fe16746bae227e02eb69e71840
xiazen <xiazen@microsoft.com>
2018-04-18 15:43:29 -0700

    fix build errors

--- test/DependencyInjection.Tests/DependencyInjectionDisambiguationTests.cs
+++ test/DependencyInjection.Tests/DependencyInjectionDisambiguationTests.cs
@@ -13,7 +13,7 @@ namespace DependencyInjection.Tests
         [Fact]
         public void DisambiguateByKeyTest()
         {
-            IServiceProvider services = BuildeServiceProvider(ConfigureServices());
+            IServiceProvider services = BuildServiceProvider(ConfigureServices());
             int actual0 = services.GetServiceByKey<int, IValue<int>>(0).Value;
             Assert.StrictEqual(0, actual0);
             int actual1 = services.GetServiceByKey<int, IValue<int>>(1).Value;
@@ -23,7 +23,7 @@ namespace DependencyInjection.Tests
         [Fact]
         public void DisambiguateByNameTest()
         {
-            IServiceProvider services = BuildeServiceProvider(ConfigureServices());
+            IServiceProvider services = BuildServiceProvider(ConfigureServices());
             string actualThis = services.GetServiceByName<IValue<string>>("this").Value;
             Assert.Equal("this", actualThis);
             string actualThat = services.GetServiceByName<IValue<string>>("that").Value;
