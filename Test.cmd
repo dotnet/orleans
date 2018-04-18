d8e135abb5cd5679d4832c56199d796c01829c36
Sergey Bykov <sbykov@microsoft.com>
2018-04-18 15:02:36 -0700

    Rearrange test projects for easier consolidation of references (#4485)
    * Rearrange test projects for easier consolidation of references.
    
    * Update Test.cmd.
    
    * Restore build time codegen in TestingHost tests.
    
    * Update solution structure to match source folders.

--- Test.cmd
+++ Test.cmd
@@ -19,24 +19,24 @@ if not exist %TestResultDir% md %TestResultDir%
 SET _Directory=bin\%BuildConfiguration%\net461\win10-x64
 
 set TESTS=^
-%CMDHOME%\test\TesterAzureUtils,^
+%CMDHOME%\test\Extensions\TesterAzureUtils,^
 %CMDHOME%\test\TesterInternal,^
 %CMDHOME%\test\Tester,^
 %CMDHOME%\test\DefaultCluster.Tests,^
 %CMDHOME%\test\NonSilo.Tests,^
-%CMDHOME%\test\AWSUtils.Tests,^
-%CMDHOME%\test\BondUtils.Tests,^
-%CMDHOME%\test\Consul.Tests,^
-%CMDHOME%\test\GoogleUtils.Tests,^
-%CMDHOME%\test\ServiceBus.Tests,^
-%CMDHOME%\test\TestServiceFabric,^
-%CMDHOME%\test\TesterAdoNet,^
-%CMDHOME%\test\TesterZooKeeperUtils,^
+%CMDHOME%\test\Extensions\AWSUtils.Tests,^
+%CMDHOME%\test\Extensions\Serializers\BondUtils.Tests,^
+%CMDHOME%\test\Extensions\Consul.Tests,^
+%CMDHOME%\test\Extensions\Serializers\GoogleUtils.Tests,^
+%CMDHOME%\test\Extensions\ServiceBus.Tests,^
+%CMDHOME%\test\Extensions\TestServiceFabric,^
+%CMDHOME%\test\Extensions\TesterAdoNet,^
+%CMDHOME%\test\Extensions\TesterZooKeeperUtils,^
 %CMDHOME%\test\RuntimeCodeGen.Tests,^
-%CMDHOME%\test\Orleans.Transactions.Tests,^
-%CMDHOME%\test\Orleans.Transactions.Azure.Test,^
-%CMDHOME%\test\Orleans.Transactions.DynamoDB.Test,^
-%CMDHOME%\test\Orleans.TestingHost.Tests,^
+%CMDHOME%\test\Transactions\Orleans.Transactions.Tests,^
+%CMDHOME%\test\Transactions\Orleans.Transactions.Azure.Test,^
+%CMDHOME%\test\Transactions\Orleans.Transactions.DynamoDB.Test,^
+%CMDHOME%\test\TestInfrastructure\Orleans.TestingHost.Tests,^
 %CMDHOME%\test\DependencyInjection.Tests
 
 if []==[%TEST_FILTERS%] set TEST_FILTERS=-trait Category=BVT -trait Category=SlowBVT
