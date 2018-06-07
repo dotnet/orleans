5c5257e8c3e102a65ef1b0734d9d9e9e17226eb0
Benjamin Petit <benjaminpetit@users.noreply.github.com>
2018-06-07 16:22:38 -0700

    Disable multilevel lookup (#4665)

--- Test.cmd
+++ Test.cmd
@@ -7,6 +7,9 @@ SET CMDHOME=%~dp0
 @REM Remove trailing backslash \
 set CMDHOME=%CMDHOME:~0,-1%
 
+:: Disable multilevel lookup https://github.com/dotnet/core-setup/blob/master/Documentation/design-docs/multilevel-sharedfx-lookup.md
+set DOTNET_MULTILEVEL_LOOKUP=0 
+
 call Ensure-DotNetSdk.cmd
 
 pushd "%CMDHOME%"
