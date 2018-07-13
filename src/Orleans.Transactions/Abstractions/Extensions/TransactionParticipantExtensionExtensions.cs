01b05f2e349de3d4385845b02ce1dea6641ada99
jason-bragg <jbragg@microsoft.com>
2018-07-13 12:15:38 -0700

    Transaction wrapper was not serializting the patrisipant when using json.

--- src/Orleans.Transactions/Abstractions/Extensions/TransactionParticipantExtensionExtensions.cs
+++ src/Orleans.Transactions/Abstractions/Extensions/TransactionParticipantExtensionExtensions.cs
@@ -33,6 +33,7 @@ namespace Orleans.Transactions.Abstractions.Extensions
         [Immutable]
         internal sealed class TransactionParticipantExtensionWrapper : ITransactionParticipant
         {
+            [JsonProperty]
             private readonly ITransactionParticipantExtension extension;
             [JsonProperty]
             private readonly string resourceId;
