1d0d79583d4f2d231d4289ae25009205134854a7
ReubenBond <a-rebond@microsoft.com>
2018-08-29 09:58:29 +1000

    Propagate message serialization errors to callers

--- test/TestGrainInterfaces/IExceptionGrain.cs
+++ test/TestGrainInterfaces/IExceptionGrain.cs
@@ -1,3 +1,8 @@
+using System;
+using Orleans.CodeGeneration;
+using Orleans.Runtime;
+using Orleans.Serialization;
+
 namespace UnitTests.GrainInterfaces
 {
     using System.Threading.Tasks;
@@ -31,10 +36,76 @@ namespace UnitTests.GrainInterfaces
 
     public interface IMessageSerializationGrain : IGrainWithIntegerKey
     {
-        Task<object> EchoObject(object input);
+        Task Send(UndeserializableType input);
+        Task<UnserializableType> Get();
 
-        Task GetUnserializableObjectChained();
+        Task SendToOtherSilo();
+        Task GetFromOtheSilo();
+
+        Task SendToClient(IMessageSerializationClientObject obj);
+        Task GetFromClient(IMessageSerializationClientObject obj);
 
         Task<string> GetSiloIdentity();
+    }
+
+    public interface IMessageSerializationClientObject : IAddressable
+    {
+        Task Send(UndeserializableType input);
+        Task<UnserializableType> Get();
+    }
+
+    [Serializable]
+    public struct UndeserializableType
+    {
+        public const string FailureMessage = "Can't do it, sorry.";
+
+        public UndeserializableType(int num)
+        {
+            this.Number = num;
+        }
+
+        public int Number { get; }
+
+        [CopierMethod]
+        public static object DeepCopy(object original, ICopyContext context)
+        {
+            var typed = (UndeserializableType) original;
+            return new UndeserializableType(typed.Number);
+        }
+
+        [SerializerMethod]
+        public static void Serialize(object untypedInput, ISerializationContext context, Type expected)
+        {
+            var typed = (UndeserializableType) untypedInput;
+            context.StreamWriter.Write(typed.Number);
+        }
+
+        [DeserializerMethod]
+        public static object Deserialize(Type expected, IDeserializationContext context)
+        {
+            throw new NotSupportedException(FailureMessage);
+        }
+    }
+
+    [Serializable]
+    public class UnserializableType
+    {
+        [CopierMethod]
+        public static object DeepCopy(object original, ICopyContext context)
+        {
+            return original;
+        }
+
+        [SerializerMethod]
+        public static void Serialize(object untypedInput, ISerializationContext context, Type expected)
+        {
+            throw new NotSupportedException(UndeserializableType.FailureMessage);
+        }
+
+        [DeserializerMethod]
+        public static object Deserialize(Type expected, IDeserializationContext context)
+        {
+            return null;
+        }
     }
 }
