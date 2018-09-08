15e667c7e86509e2ce06a1533983bc755b3ed09e
Rajesh Kumar Aggani <raaggani@microsoft.com>
2018-09-09 01:24:28 +0530

    Removed CollectionAgeLimitConstant which is no longer required.

--- src/Orleans.Core/Configuration/CollectionAgeLimitConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Orleans.Configuration
-{
-    internal class CollectionAgeLimitConstants
-    {
-        public static readonly double DefaultCollectionAgeLimitInMinutes = 120;
-    }
-}
