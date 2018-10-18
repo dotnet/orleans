83011aa03b147f278982cd326a46fc4b37b0b87b
Xiao Zeng <xiazen@microsoft.com>
2018-10-18 11:31:25 -0700

    Add Orleans.Transaction.Testkit project structure (#5103)
    * Add Orleans.Transaction.Testkit
    
    * add correct nuget related settings
    
    * remove unnecessary diff which tries to use AzureDataManager
    
    * rename Testkit to TestKit
    
    * add xml comments

---
+++ src/Orleans.Transactions.TestkitBase/Grains/TransactionTestConstants.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.Extensions.Logging;
+using Orleans.Hosting;
+
+namespace Orleans.Transactions.TestKit.Base
+{
+    public static class TransactionTestConstants
+    {
+        /// <summary>
+        /// Max number of grains to include in a transaction for test purposes.  Not a hard limit of the transaction system.
+        /// </summary>
+        public const int MaxCoordinatedTransactions = 8;
+
+        // storage providers
+        public const string TransactionStore = "TransactionStore";
+
+        // committer service
+        public const string RemoteCommitService = "RemoteCommitService";
+        
+        // grain implementations
+        public const string NoStateTransactionalGrain = "NoStateTransactionalGrain";
+        public const string SingleStateTransactionalGrain = "SingleStateTransactionalGrain";
+        public const string DoubleStateTransactionalGrain = "DoubleStateTransactionalGrain";
+        public const string MaxStateTransactionalGrain = "MaxStateTransactionalGrain";
+    }
+}
---
+++ src/Orleans.Transactions.TestkitBase/TransactionalStateStorageTestRunner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Orleans.Transactions.Abstractions;
+
+namespace Orleans.Transactions.TestKit.Base
+{
+    public abstract class TransactionalStateStorageTestRunner<TState> : TransactionTestRunnerBase
+        where TState : class, new()
+    {
+        protected Func<Task<ITransactionalStateStorage<TState>>> stateStorageFactory;
+        protected Func<TState> stateFactory;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="stateStorageFactory">factory to create ITransactionalStateStorage, the test runner are assuming the state 
+        /// in storage is empty when ITransactionalStateStorage was created </param>
+        /// <param name="stateFactory">factory to create TState for test</param>
+        /// <param name="grainFactory">grain Factory needed for test runner</param>
+        /// <param name="testOutput">test output to helpful messages</param>
+        protected TransactionalStateStorageTestRunner(Func<Task<ITransactionalStateStorage<TState>>> stateStorageFactory, Func<TState> stateFactory, 
+            IGrainFactory grainFactory, Action<string> testOutput)
+            :base(grainFactory, testOutput)
+        {
+            this.stateStorageFactory = stateStorageFactory;
+            this.stateFactory = stateFactory;
+        }
+
+        public virtual async Task FirstTime_Load_ShouldReturnEmptyLoadResponse()
+        {
+            var stateStorage = await this.stateStorageFactory();
+            var response = await stateStorage.Load();
+
+            //Assertion
+            response.Should().NotBeNull();
+            response.ETag.Should().BeNull();
+            response.CommittedSequenceId.Should().Be(0);
+            response.PendingStates.Should().BeEmpty();
+        }
+
+    }
+}
