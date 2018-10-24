e5655d27f0a4ecd69118a7537b646db6610588ad
Aleksander Pruks <aprooks@live.ru>
2018-10-24 20:25:04 +0200

    compilable 2.1 sample

--- Samples/1.x/FSharp.NetCore/src/FSharp.NetCore.Interfaces/Interfaces.fs
+++
@@ -1,9 +0,0 @@
-namespace FSharp.NetCore
-
-module Interfaces =
-
-    open System.Threading.Tasks
-
-    type IHello = 
-        inherit Orleans.IGrainWithIntegerKey
-        abstract member SayHello : string -> Task<string>
