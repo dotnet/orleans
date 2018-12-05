e680964c48414ea9f7ed96538abe7a227a1b1bfa
Benjamin Petit <benjaminpetit@users.noreply.github.com>
2018-12-05 12:51:37 -0800

    Remove added type to fix backward compatibility (#5225)

--- src/Orleans.Core/Messaging/RequestInvocationHistory.cs
+++ src/Orleans.Core/Messaging/RequestInvocationHistory.cs
@@ -2,38 +2,24 @@ using System;
 
 namespace Orleans.Runtime
 {
-    // used for tracking request invocation history for deadlock detection.
+    // used for tracking request invocation history for deadlock detection and call chain reentrancy
     [Serializable]
-    internal sealed class RequestInvocationHistory : RequestInvocationHistorySummary
+    internal sealed class RequestInvocationHistory
     {
         public GrainId GrainId { get; private set; }
+        public ActivationId ActivationId { get; private set; }
         public string DebugContext { get; private set; }
 
-        public RequestInvocationHistory(GrainId grainId, ActivationId activationId, string debugContext) : base(activationId)
+        public RequestInvocationHistory(GrainId grainId, ActivationId activationId, string debugContext)
         {
             this.GrainId = grainId;
-            DebugContext = debugContext;
+            this.ActivationId = activationId;
+            this.DebugContext = debugContext;
         }
 
         public override string ToString()
         {
             return $"RequestInvocationHistory {GrainId}:{ActivationId}:{DebugContext}";
-        }
-    }
-    // used for tracking request invocation history for call chain reentrancy
-    [Serializable]
-    internal class RequestInvocationHistorySummary
-    {
-        public ActivationId ActivationId { get; private set; }
-
-        public RequestInvocationHistorySummary(ActivationId activationId)
-        {
-            this.ActivationId = activationId;
-        }
-
-        public override string ToString()
-        {
-            return $"RequestInvocationHistorySummary {ActivationId}";
         }
     }
 }
