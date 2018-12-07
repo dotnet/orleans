2fa0db907c1f257d30aaf7f4d1ebebc0b83c9ab2
xiazen <xiazen@microsoft.com>
2018-12-06 17:47:11 -0800

    move runners and related to testkit.base proj

---
+++ src/Orleans.Transactions.TestkitBase/FaultInjection/ITransactionFaultInjector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orleans.Transactions.TestKit.Base
+{
+    public interface ITransactionFaultInjector
+    {
+        void BeforeStore();
+        void AfterStore();
+    }
+}
--- test/Transactions/Orleans.Transactions.Tests/Runners/TransactionAttributionTestRunner.cs
+++ test/Transactions/Orleans.Transactions.Tests/Runners/TransactionAttributionTestRunner.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Orleans.Transactions.TestKit.Base;
 using Xunit;
 using Xunit.Abstractions;
 
