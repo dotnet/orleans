24ac8f096d5e449163003ebd8e1d32b039b603ff
xiazen <xiazen@microsoft.com>
2019-01-02 15:50:03 -0800

    add more validation to GrainCollectionOptions

--- src/Orleans.Runtime/Configuration/Validators/CollectionAgeLimitValidator.cs
+++ src/Orleans.Runtime/Configuration/Validators/CollectionAgeLimitValidator.cs
@@ -4,17 +4,24 @@ using Orleans.Runtime;
 
 namespace Orleans.Configuration
 {
-    internal class CollectionAgeLimitValidator : IConfigurationValidator
+    internal class GrainCollectionOptionsValidator : IConfigurationValidator
     {
         private IOptions<GrainCollectionOptions> options;
 
-        public CollectionAgeLimitValidator(IOptions<GrainCollectionOptions> options)
+        public GrainCollectionOptionsValidator(IOptions<GrainCollectionOptions> options)
         {
             this.options = options;
         }
 
         public void ValidateConfiguration()
         {
+            if (this.options.Value.CollectionAge <= this.options.Value.CollectionQuantum)
+            {
+                throw new OrleansConfigurationException(
+                    $"{nameof(GrainCollectionOptions.CollectionAge)} is set to {options.Value.CollectionAge}. " +
+                    $"{nameof(GrainCollectionOptions.CollectionAge)} must be bigger than {nameof(GrainCollectionOptions.CollectionQuantum)}, " +
+                    $"which is set to {this.options.Value.CollectionQuantum}");
+            }
             foreach(var classSpecificCollectionAge in this.options.Value.ClassSpecificCollectionAge)
             {
                 if(classSpecificCollectionAge.Value <= TimeSpan.Zero)
