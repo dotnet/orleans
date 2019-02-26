84b5f7c59234ac27fc9a519f9bdb3336dc60bd41
Sergey Bykov <sbykov@microsoft.com>
2019-02-26 15:45:32 -0800

    Update version to 1.5.7.

--- src/Build/GlobalAssemblyInfo.cs
+++ src/Build/GlobalAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("Orleans")]
-[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2018")]
-[assembly: AssemblyVersion("1.5.6.0")]
-[assembly: AssemblyFileVersion("1.5.6.0")]
-[assembly: AssemblyInformationalVersion("1.5.6.0")]
+[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2019")]
+[assembly: AssemblyVersion("1.5.7.0")]
+[assembly: AssemblyFileVersion("1.5.7.0")]
+[assembly: AssemblyInformationalVersion("1.5.7.0")]
