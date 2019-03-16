c75491ae4534e55df883f73c0b4ad22c4f61588a
Jorge Candeias <candeias@fastmail.fm>
2019-03-16 19:23:05 +0000

    Migrated silo startup code.

--- Samples/2.2/Presence/src/Silo/Program.cs
+++ Samples/2.2/Presence/src/Silo/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Hosting;
+using Presence.Grains;
 
-namespace Silo
+namespace Presence.Silo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .ConfigureApplicationParts(_ =>
+                {
+                    _.AddApplicationPart(typeof(GameGrain).Assembly).WithReferences();
+                })
+                .ConfigureLogging(_ =>
+                {
+                    _.AddConsole();
+                })
+                .Build();
+
+            await host.StartAsync();
+
+            Console.CancelKeyPress += async (sender, eargs) =>
+            {
+                eargs.Cancel = true;
+                await host.StopAsync();
+            };
+
+            await host.Stopped;
         }
     }
 }
