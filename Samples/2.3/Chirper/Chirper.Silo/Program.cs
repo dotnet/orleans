edc7cdf20c544e79439da605489740dcdc4f325a
Jorge Candeias <candeias@fastmail.fm>
2019-03-23 15:12:39 +0000

    Migrated startup code to generic host.

--- Samples/2.3/Chirper/Chirper.Silo/Program.cs
+++ Samples/2.3/Chirper/Chirper.Silo/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using Chirper.Grains;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Hosting;
@@ -8,33 +10,22 @@ namespace Chirper.Silo
 {
     class Program
     {
-        static void Main(string[] args)
+        static Task Main(string[] args)
         {
             Console.Title = nameof(Silo);
 
-            var host = new SiloHostBuilder()
-                .UseLocalhostClustering()
-                .ConfigureApplicationParts(_ => _.AddApplicationPart(typeof(ChirperAccount).Assembly).WithReferences())
-                .ConfigureLogging(_ =>
-                {
-                    _.AddFilter("Orleans.Runtime.Management.ManagementGrain", LogLevel.Warning);
-                    _.AddFilter("Orleans.Runtime.SiloControl", LogLevel.Warning);
-                    _.AddConsole();
-                })
-                .AddMemoryGrainStorageAsDefault()
-                .AddMemoryGrainStorage("PubSubStore")
-                .UseDashboard()
-                .Build();
-
-            host.StartAsync().Wait();
-
-            Console.CancelKeyPress += (sender, e) =>
-            {
-                e.Cancel = true;
-                host.StopAsync().Wait();
-            };
-
-            host.Stopped.Wait();
+            return new HostBuilder()
+                .UseOrleans(builder => builder
+                    .UseLocalhostClustering()
+                    .ConfigureApplicationParts(_ => _.AddApplicationPart(typeof(ChirperAccount).Assembly).WithReferences())
+                    .AddMemoryGrainStorageAsDefault()
+                    .AddMemoryGrainStorage("PubSubStore")
+                    .UseDashboard())
+                .ConfigureLogging(builder => builder
+                    .AddFilter("Orleans.Runtime.Management.ManagementGrain", LogLevel.Warning)
+                    .AddFilter("Orleans.Runtime.SiloControl", LogLevel.Warning)
+                    .AddConsole())
+                .RunConsoleAsync();
         }
     }
 }
