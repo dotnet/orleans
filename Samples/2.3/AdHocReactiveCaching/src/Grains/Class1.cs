4075abe88c1ba7c84b477dec8d27ea8f208bec40
Jorge Candeias <candeias@fastmail.fm>
2019-03-23 17:59:45 +0000

    Added silo startup code.

--- Samples/2.3/AdHocReactiveCaching/src/Grains/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Grains
-{
-    public class Class1
-    {
-    }
-}
--- Samples/2.3/AdHocReactiveCaching/src/Silo/Program.cs
+++ Samples/2.3/AdHocReactiveCaching/src/Silo/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+using System.Threading.Tasks;
+using Grains;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Hosting;
 
 namespace Silo
 {
     class Program
     {
-        static void Main(string[] args)
+        static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return new HostBuilder()
+                .UseOrleans(builder =>
+                {
+                    builder
+                        .UseLocalhostClustering()
+                        .ConfigureApplicationParts(manager =>
+                        {
+                            manager.AddApplicationPart(typeof(DummyGrain).Assembly).WithReferences();
+                        });
+                })
+                .ConfigureLogging(builder =>
+                {
+                    builder.AddConsole();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.Configure<ConsoleLifetimeOptions>(options =>
+                    {
+                        options.SuppressStatusMessages = true;
+                    });
+                })
+                .RunConsoleAsync();
         }
     }
 }
