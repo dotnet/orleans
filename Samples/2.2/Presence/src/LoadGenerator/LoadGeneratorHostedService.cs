301cb6c37ff65b90a7d936530511361ffa0ae7b1
Jorge Candeias <candeias@fastmail.fm>
2019-03-23 13:34:49 +0000

    Cleanup.

--- Samples/2.2/Presence/src/LoadGenerator/LoadGeneratorHostedService.cs
+++ Samples/2.2/Presence/src/LoadGenerator/LoadGeneratorHostedService.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
