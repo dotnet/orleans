46c0995f1b50acd2a5ddeea07d9c6975da067467
Jorge Candeias <candeias@fastmail.fm>
2019-03-31 18:03:14 +0100

    Migrated client to hosted service model.

--- Samples/2.3/HelloWorld/src/OrleansClient/Program.cs
+++ Samples/2.3/HelloWorld/src/OrleansClient/Program.cs
@@ -1,10 +1,8 @@
-﻿using HelloWorld.Interfaces;
-using Orleans;
-using Orleans.Runtime;
-using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Orleans.Configuration;
+using Orleans;
 
 namespace OrleansClient
 {
@@ -13,76 +11,27 @@ namespace OrleansClient
     /// </summary>
     public class Program
     {
-        const int initializeAttemptsBeforeFailing = 5;
-        private static int attempt = 0;
-
-        static int Main(string[] args)
+        public static Task Main(string[] args)
         {
-            return RunMainAsync().Result;
-        }
-
-        private static async Task<int> RunMainAsync()
-        {
-            try
-            {
-                using (var client = await StartClientWithRetries())
+            return new HostBuilder()
+                .ConfigureServices(services =>
                 {
-                    await DoClientWork(client);
-                    Console.ReadKey();
-                }
+                    services.AddSingleton<ClusterClientHostedService>();
+                    services.AddSingleton<IHostedService>(_ => _.GetService<ClusterClientHostedService>());
+                    services.AddSingleton(_ => _.GetService<ClusterClientHostedService>().Client);
 
-                return 0;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                Console.ReadKey();
-                return 1;
-            }
-        }
+                    services.AddHostedService<HelloWorldClientHostedService>();
 
-        private static async Task<IClusterClient> StartClientWithRetries()
-        {
-            attempt = 0;
-            IClusterClient client;
-            client = new ClientBuilder()
-                .UseLocalhostClustering()
-                .Configure<ClusterOptions>(options =>
-                {
-                    options.ClusterId = "dev";
-                    options.ServiceId = "HelloWorldApp";
+                    services.Configure<ConsoleLifetimeOptions>(options =>
+                    {
+                        options.SuppressStatusMessages = true;
+                    });
                 })
-                .ConfigureLogging(logging => logging.AddConsole())
-                .Build();
-
-            await client.Connect(RetryFilter);
-            Console.WriteLine("Client successfully connect to silo host");
-            return client;
-        }
-
-        private static async Task<bool> RetryFilter(Exception exception)
-        {
-            if (exception.GetType() != typeof(SiloUnavailableException))
-            {
-                Console.WriteLine($"Cluster client failed to connect to cluster with unexpected error.  Exception: {exception}");
-                return false;
-            }
-            attempt++;
-            Console.WriteLine($"Cluster client attempt {attempt} of {initializeAttemptsBeforeFailing} failed to connect to cluster.  Exception: {exception}");
-            if (attempt > initializeAttemptsBeforeFailing)
-            {
-                return false;
-            }
-            await Task.Delay(TimeSpan.FromSeconds(4));
-            return true;
-        }
-
-        private static async Task DoClientWork(IClusterClient client)
-        {
-            // example of calling grains from the initialized client
-            var friend = client.GetGrain<IHello>(0);
-            var response = await friend.SayHello("Good morning, my friend!");
-            Console.WriteLine("\n\n{0}\n\n", response);
+                .ConfigureLogging(builder =>
+                {
+                    builder.AddConsole();
+                })
+                .RunConsoleAsync();
         }
     }
 }
