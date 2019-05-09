f584d5a9f0cf8fda66954f30cf0b400d63ac98be
Reuben Bond <rebond@microsoft.com>
2019-05-08 17:02:14 -0700

    Add test categories and add to Test.cmd

--- test/Analyzers.Tests/AlwaysInterleaveDiagnosticAnalyzerTests.cs
+++ test/Analyzers.Tests/AlwaysInterleaveDiagnosticAnalyzerTests.cs
@@ -6,6 +6,7 @@ using Xunit;
 
 namespace Analyzers.Tests
 {
+    [TestCategory("BVT"), TestCategory("Analyzer")]
     public class AlwaysInterleaveDiagnosticAnalyzerTest : DiagnosticAnalyzerTestBase<AlwaysInterleaveDiagnosticAnalyzer>
     {
         protected override Task<(Diagnostic[], string)> GetDiagnosticsAsync(string source, params string[] extraUsings)
