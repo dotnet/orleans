aaa579109405a2a148cc7fe2a6ce4ec80da7df61
Jorge Candeias <candeias@fastmail.fm>
2019-05-11 23:32:49 +0100

    Split single set benchmarks

---
+++ Samples/2.3/FasterSample/src/Silo/FasterSimpleSingleSetBenchmarks.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Grains;
+using Grains.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Runtime;
+
+namespace Silo
+{
+    [ShortRunJob, EvaluateOverhead(false), AllStatisticsColumn, MarkdownExporter, RunOncePerIteration]
+    [GcServer(true), GcConcurrent(true)]
+    public class FasterSimpleSingleSetBenchmarks
+    {
+        private readonly IHost host = Program.BuildHost();
+        private IFasterSimpleGrain grain;
+        private LookupItem[] data;
+        private const int Items = 1 << 13;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            data = Enumerable.Range(0, Items)
+                .Select(x => new LookupItem(x, x, DateTime.UtcNow))
+                .ToArray();
+
+            host.StartAsync().Wait();
+
+            grain = host.Services.GetService<IGrainFactory>()
+                .GetGrain<IFasterSimpleGrain>(Guid.Empty);
+        }
+
+        [IterationSetup]
+        public void IterationSetup() => grain.StartAsync().Wait();
+
+        [IterationCleanup]
+        public void IterationCleanup() => grain.StopAsync().Wait();
+
+        [GlobalCleanup]
+        public void GlobalCleanup() => host.StopAsync().Wait();
+
+        [Params(1, 2, 4)]
+        public int Concurrency { get; set; }
+
+        [Benchmark(OperationsPerInvoke = Items)]
+        public void Benchmark()
+        {
+            var pipeline = new AsyncPipeline(Concurrency);
+            pipeline.AddRange(data.Select(_ => grain.SetAsync(_)));
+            pipeline.Wait();
+        }
+    }
+}
