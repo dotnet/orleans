025845a21a0036e43155f0d378b55ddfc035db13
Sergey Bykov <sbykov@microsoft.com>
2019-06-13 09:36:15 -0700

    Add IGrainServiceFactory/GrainServiceFactory with methods needed for Indexing.

--- src/Orleans.Runtime/Services/GrainService.cs
+++ src/Orleans.Runtime/Services/GrainService.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Orleans.Core;
-using Orleans.Runtime.Configuration;
 using Orleans.Runtime.ConsistentRing;
 using Orleans.Runtime.Scheduler;
 using Orleans.Services;
