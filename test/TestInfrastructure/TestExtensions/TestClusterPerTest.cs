393a992793d2715168d1b930b055df34ad209bd2
Reuben Bond <rebond@microsoft.com>
2019-06-26 08:36:27 -0700

    Dispose TestCluster after tests

--- test/TestInfrastructure/TestExtensions/TestClusterPerTest.cs
+++ test/TestInfrastructure/TestExtensions/TestClusterPerTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Orleans;
@@ -7,7 +8,7 @@ using Orleans.TestingHost;
 
 namespace TestExtensions
 {
-    public abstract class TestClusterPerTest : OrleansTestingBase, IDisposable
+    public abstract class TestClusterPerTest : OrleansTestingBase, IDisposable, Xunit.IAsyncLifetime
     {
         private readonly ExceptionDispatchInfo preconditionsException;
         static TestClusterPerTest()
@@ -66,6 +67,20 @@ namespace TestExtensions
         public virtual void Dispose()
         {
             this.HostedCluster?.StopAllSilos();
+        }
+
+        public virtual Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task DisposeAsync()
+        {
+            var cluster = this.HostedCluster;
+            if (cluster != null)
+            {
+                await cluster.StopAllSilosAsync();
+            }
         }
     }
 }
