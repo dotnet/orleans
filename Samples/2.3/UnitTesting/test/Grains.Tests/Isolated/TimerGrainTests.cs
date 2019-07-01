c934d5fe2bd7983a5cf3c47c98f0d15cc437b494
Jorge Candeias <candeias@fastmail.fm>
2019-07-01 10:55:37 +0100

    Fixed timer assertion

--- Samples/2.3/UnitTesting/test/Grains.Tests/Isolated/TimerGrainTests.cs
+++ Samples/2.3/UnitTesting/test/Grains.Tests/Isolated/TimerGrainTests.cs
@@ -18,14 +18,21 @@ namespace Grains.Tests.Isolated
 
             // mock the timer registration method and capture the action
             Func<object, Task> action = null;
-            grain.Setup(_ => _.RegisterTimer(It.IsAny<Func<object, Task>>(), null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1)))
-                .Callback<Func<object, Task>, object, TimeSpan, TimeSpan>((a, b, c, d) => action = a);
+            object state = null;
+            var dueTime = TimeSpan.FromSeconds(1);
+            var period = TimeSpan.FromSeconds(1);
+            grain.Setup(_ => _.RegisterTimer(It.IsAny<Func<object, Task>>(), It.IsAny<object>(), It.IsAny<TimeSpan>(), It.IsAny<TimeSpan>()))
+                .Callback<Func<object, Task>, object, TimeSpan, TimeSpan>((a, b, c, d) => { action = a; state = b; dueTime = c; period = d; })
+                .Returns(Mock.Of<IDisposable>());
 
             // simulate activation
             await grain.Object.OnActivateAsync();
 
             // assert the timer was registered
             Assert.NotNull(action);
+            Assert.Null(state);
+            Assert.Equal(TimeSpan.FromSeconds(1), dueTime);
+            Assert.Equal(TimeSpan.FromSeconds(1), period);
 
             // assert the initial value is zero
             Assert.Equal(0, await grain.Object.GetValueAsync());
