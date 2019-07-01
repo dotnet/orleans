2b46acbe0c9fba04e8652571a5e5d4bfb141afdc
Reuben Bond <rebond@microsoft.com>
2019-07-01 15:19:23 -0700

    Add validator for DevelopmentClusterMembershipOptions (#5721)

---
+++ src/Orleans.Runtime/MembershipService/DevelopmentClusterMembershipOptionsValidator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using Orleans.Runtime;
+using Orleans.Runtime.MembershipService;
+
+namespace Orleans.Configuration
+{
+    internal class DevelopmentClusterMembershipOptionsValidator : IConfigurationValidator
+    {
+        private readonly DevelopmentClusterMembershipOptions options;
+        private readonly IMembershipTable membershipTable;
+
+        public DevelopmentClusterMembershipOptionsValidator(IOptions<DevelopmentClusterMembershipOptions> options, IMembershipTable membershipTable)
+        {
+            this.options = options.Value;
+            this.membershipTable = membershipTable;
+        }
+
+        public void ValidateConfiguration()
+        {
+            if (this.membershipTable is SystemTargetBasedMembershipTable && this.options.PrimarySiloEndpoint is null)
+            {
+                throw new OrleansConfigurationException("Development clustering is enabled but no value is specified ");
+            }
+        }
+    }
+}
