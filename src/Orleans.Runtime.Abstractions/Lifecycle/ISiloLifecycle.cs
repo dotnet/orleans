e537c054d7a430185e2ed12ad173de2b18fbb22b
Reuben Bond <rebond@microsoft.com>
2019-07-02 12:27:20 -0700

    Improve lifecycle logging (#5711)
    (cherry picked from commit cbef78afbbe8a460b727a2438b0d6e5add947b12)

--- src/Orleans.Runtime.Abstractions/Lifecycle/ISiloLifecycle.cs
+++ src/Orleans.Runtime.Abstractions/Lifecycle/ISiloLifecycle.cs
@@ -1,7 +1,15 @@
-
 namespace Orleans.Runtime
 {
     public interface ISiloLifecycle : ILifecycleObservable
     {
+        /// <summary>
+        /// The highest lifecycle stage which has completed starting.
+        /// </summary>
+        int HighestCompletedStage { get; }
+
+        /// <summary>
+        /// The lowest lifecycle stage which has completed stopping.
+        /// </summary>
+        int LowestStoppedStage { get; }
     }
 }
