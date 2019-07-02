ce09e2dbf7391e4d81190e1cd38ac32a3fd2233a
Reuben Bond <rebond@microsoft.com>
2019-07-02 12:27:20 -0700

    Improve lifecycle logging (#5711)

--- src/Orleans.Runtime/Catalog/GrainLifecycle.cs
+++ src/Orleans.Runtime/Catalog/GrainLifecycle.cs
@@ -1,12 +1,21 @@
-﻿
+
+using System.Collections.Immutable;
 using Microsoft.Extensions.Logging;
 
 namespace Orleans.Runtime
 {
     internal class GrainLifecycle : LifecycleSubject, IGrainLifecycle
     {
+        private static readonly ImmutableDictionary<int, string> StageNames = GetStageNames(typeof(GrainLifecycleStage));
+
         public GrainLifecycle(ILogger<LifecycleSubject> logger) : base(logger)
         {
+        }
+
+        protected override string GetStageName(int stage)
+        {
+            if (StageNames.TryGetValue(stage, out var result)) return result;
+            return base.GetStageName(stage);
         }
     }
 }
