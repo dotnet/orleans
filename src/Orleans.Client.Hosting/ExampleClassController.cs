098da7810d1a035e608e822200b852571e90e320
Ross Buggins <20972856+rossbuggins@users.noreply.github.com>
2019-07-17 09:47:50 +0100

    summary comments

--- src/Orleans.Client.Hosting/ExampleClassController.cs
+++ src/Orleans.Client.Hosting/ExampleClassController.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Orleans.Client.Hosting
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ExampleClassController
     {
         private IClusterClient client;
