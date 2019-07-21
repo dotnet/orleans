76f229ddb08f2b638b6e91a548c8e5f399c9f66d
Jorge Candeias <candeias@fastmail.fm>
2019-07-21 14:15:24 +0100

    Added sample files

---
+++ Samples/2.3/Blazor/Sample.Silo/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Orleans;
+using Orleans.Hosting;
+using Sample.Grains;
+using Sample.Silo.Api;
+using System.Threading.Tasks;
+
+namespace Sample.Silo
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+        {
+            return new HostBuilder()
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddCommandLine(args);
+                })
+                .ConfigureLogging(builder =>
+                {
+                    builder.AddConsole();
+                    builder.AddFilter("Orleans.Runtime.Management.ManagementGrain", LogLevel.Warning);
+                    builder.AddFilter("Orleans.Runtime.SiloControl", LogLevel.Warning);
+                })
+                .ConfigureServices(services =>
+                {
+                    services.Configure<ConsoleLifetimeOptions>(options =>
+                    {
+                        options.SuppressStatusMessages = true;
+                    });
+
+                    services.AddHostedService<ApiService>();
+                })
+                .UseOrleans(builder =>
+                {
+                    builder.ConfigureApplicationParts(manager =>
+                    {
+                        manager.AddApplicationPart(typeof(WeatherGrain).Assembly).WithReferences();
+                    });
+                    builder.UseLocalhostClustering();
+                    builder.AddMemoryGrainStorageAsDefault();
+                    builder.AddSimpleMessageStreamProvider("SMS");
+                    builder.AddMemoryGrainStorage("PubSubStore");
+                    builder.UseDashboard(options =>
+                    {
+                        options.HideTrace = true;
+                    });
+                })
+                .RunConsoleAsync();
+        }
+    }
+}
