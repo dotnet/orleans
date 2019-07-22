f16047d3b40ed2c176acda25f2242c75e9e06363
Reuben Bond <rebond@microsoft.com>
2019-07-22 16:16:16 -0700

    Remove concurrency cap for Xunit (#5713)
    (cherry picked from commit 0ed497d4243d5ee860660e7fd57c90b994ebe71d)

--- test/NetCore.Tests/Properties/AssemblyInfo.cs
+++ test/NetCore.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,4 @@
-using System.Runtime.CompilerServices;
 using Xunit;
 
 // Disable XUnit concurrency limit
 [assembly: CollectionBehavior(MaxParallelThreads = -1)]
-
-[assembly: InternalsVisibleTo("TesterInternal")]
