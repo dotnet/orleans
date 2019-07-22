2145dafd32073274307293758a69eb196928bab6
Jorge Candeias <candeias@fastmail.fm>
2019-07-22 20:45:57 +0100

    Added sample files (#5773)

---
+++ Samples/2.3/Blazor/Sample.Grains/TodoGrain.cs
@@ -0,0 +1,74 @@
+﻿using Orleans;
+using Orleans.Runtime;
+using Sample.Grains.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Sample.Grains
+{
+    public class TodoGrain : Grain, ITodoGrain
+    {
+        private readonly IPersistentState<State> state;
+
+        private Guid GrainKey => this.GetPrimaryKey();
+
+        public TodoGrain([PersistentState("State")] IPersistentState<State> state)
+        {
+            this.state = state;
+        }
+
+        public Task<TodoItem> GetAsync() => Task.FromResult(state.State.Item);
+
+        public async Task SetAsync(TodoItem item)
+        {
+            // ensure the key is consistent
+            if (item.Key != GrainKey)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // save the item
+            state.State.Item = item;
+            await state.WriteStateAsync();
+
+            // register the item with its owner list
+            await GrainFactory.GetGrain<ITodoManagerGrain>(item.OwnerKey)
+                .RegisterAsync(item.Key);
+
+            // notify listeners - best effort only
+            GetStreamProvider("SMS").GetStream<TodoNotification>(item.OwnerKey, nameof(ITodoGrain))
+                .OnNextAsync(new TodoNotification(item.Key, item))
+                .Ignore();
+        }
+
+        public async Task ClearAsync()
+        {
+            // fast path for already cleared state
+            if (state.State.Item == null) return;
+
+            // hold on to the keys
+            var itemKey = state.State.Item.Key;
+            var ownerKey = state.State.Item.OwnerKey;
+
+            // unregister from the registry
+            await GrainFactory.GetGrain<ITodoManagerGrain>(ownerKey)
+                .UnregisterAsync(itemKey);
+
+            // clear the state
+            await state.ClearStateAsync();
+
+            // notify listeners - best effort only
+            GetStreamProvider("SMS").GetStream<TodoNotification>(ownerKey, nameof(ITodoGrain))
+                .OnNextAsync(new TodoNotification(itemKey, null))
+                .Ignore();
+
+            // no need to stay alive anymore
+            DeactivateOnIdle();
+        }
+
+        public class State
+        {
+            public TodoItem Item { get; set; }
+        }
+    }
+}
