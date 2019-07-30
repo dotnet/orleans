91c478897bfad10eb3f5f1eb3a4066c47a1146a1
jason-bragg <jbragg@microsoft.com>
2019-07-30 11:59:48 -0700

    Support configurable supported roles in transactional state.

--- src/Orleans.Transactions/Abstractions/ITransactionalStateFactory.cs
+++ src/Orleans.Transactions/Abstractions/ITransactionalStateFactory.cs
@@ -1,8 +1,28 @@
-﻿
+
+using System.Xml.Serialization;
+using Orleans.Concurrency;
+
 namespace Orleans.Transactions.Abstractions
 {
+    public class TransactionalStateConfiguration : ITransactionalStateConfiguration
+    {
+        private readonly string name;
+        private readonly string storage;
+        public TransactionalStateConfiguration(ITransactionalStateConfiguration config, ParticipantId.Role supportedRoles = ParticipantId.Role.Resource | ParticipantId.Role.Manager)
+        {
+            this.name = config.StateName;
+            this.storage = config.StorageName;
+            this.SupportedRoles = supportedRoles;
+        }
+        public string StateName => this.name;
+
+        public string StorageName => this.storage;
+
+        public ParticipantId.Role SupportedRoles { get; }
+    }
+
     public interface ITransactionalStateFactory
     {
-        ITransactionalState<TState> Create<TState>(ITransactionalStateConfiguration config) where TState : class, new();
+        ITransactionalState<TState> Create<TState>(TransactionalStateConfiguration config) where TState : class, new();
     }
 }
--- src/Orleans.Transactions/Hosting/TransactionalStateAttributeMapper.cs
+++ src/Orleans.Transactions/Hosting/TransactionalStateAttributeMapper.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans;
 using Orleans.Runtime;
 using Orleans.Transactions.Abstractions;
+using System;
 using System.Reflection;
 
 namespace Orleans.Transactions
 {
-    internal class TransactionalStateAttributeMapper : IAttributeToFactoryMapper<TransactionalStateAttribute>
+    public class TransactionalStateAttributeMapper : TransactionalStateAttributeMapper<TransactionalStateAttribute>
+    {
+        protected override TransactionalStateConfiguration AttributeToConfig(TransactionalStateAttribute attribute)
+        {
+            return new TransactionalStateConfiguration(attribute);
+        }
+    }
+
+    public abstract class TransactionalStateAttributeMapper<TAttribute> : IAttributeToFactoryMapper<TAttribute>
+        where TAttribute : IFacetMetadata, ITransactionalStateConfiguration
     {
         private static readonly MethodInfo create = typeof(ITransactionalStateFactory).GetMethod("Create");
 
-        public Factory<IGrainActivationContext, object> GetFactory(ParameterInfo parameter, TransactionalStateAttribute attribute)
+        public Factory<IGrainActivationContext, object> GetFactory(ParameterInfo parameter, TAttribute attribute)
         {
-            ITransactionalStateConfiguration config = attribute;
+            TransactionalStateConfiguration config = AttributeToConfig(attribute);
             // use generic type args to define collection type.
             MethodInfo genericCreate = create.MakeGenericMethod(parameter.ParameterType.GetGenericArguments());
             object[] args = new object[] { config };
@@ -23,5 +34,7 @@ namespace Orleans.Transactions
             ITransactionalStateFactory factory = context.ActivationServices.GetRequiredService<ITransactionalStateFactory>();
             return genericCreate.Invoke(factory, args);
         }
+
+        protected abstract TransactionalStateConfiguration AttributeToConfig(TAttribute attribute);
     }
 }
