963c75ca750b07097117be31f5c93c1a0dbb9d40
Reuben Bond <rebond@microsoft.com>
2019-08-22 08:39:49 -0700

    Transactions: support more graceful deactivation of grains performing transaction work

--- src/Orleans.Transactions/State/ConfirmationWorker.cs
+++ src/Orleans.Transactions/State/ConfirmationWorker.cs
@@ -80,17 +80,21 @@ namespace Orleans.Transactions.State
 
             // attempts to confirm all, will retry every ConfirmationRetryDelay until all succeed
             var ct = this.activationLifetime.OnDeactivating;
-            while (!ct.IsCancellationRequested && await HasPendingConfirmations(confirmations))
-            {
-                await this.timerManager.Delay(this.options.ConfirmationRetryDelay);
-            }
-
-            async Task<bool> HasPendingConfirmations(List<Confirmation> values)
+            var hasPendingConfirmations = true;
+            while (!ct.IsCancellationRequested && hasPendingConfirmations)
             {
                 using (this.activationLifetime.BlockDeactivation())
                 {
-                    var results = await Task.WhenAll(confirmations.Select(c => c.Confirmed()));
-                    return results.Any(b => !b);
+                    var confirmationResults = await Task.WhenAll(confirmations.Select(c => c.Confirmed()));
+                    hasPendingConfirmations = false;
+                    foreach (var confirmed in confirmationResults)
+                    {
+                        hasPendingConfirmations |= !confirmed;
+                    }
+
+                    if (!hasPendingConfirmations) break;
+
+                    await this.timerManager.Delay(this.options.ConfirmationRetryDelay, ct);
                 }
             }
         }
@@ -104,9 +108,9 @@ namespace Orleans.Transactions.State
                 using (this.activationLifetime.BlockDeactivation())
                 {
                     if (await TryCollect(transactionId)) break;
-                }
 
-                await this.timerManager.Delay(this.options.ConfirmationRetryDelay);
+                    await this.timerManager.Delay(this.options.ConfirmationRetryDelay, ct);
+                }
             }
         }
 
