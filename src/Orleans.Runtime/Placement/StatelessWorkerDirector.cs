8fc7608e52f208588a7a1005f43454efa18b2387
John Morman <jmorman@microsoft.com>
2019-08-28 16:30:46 -0700

    Stateless worker now checks whether local silo is compatible before activating on local silo

--- src/Orleans.Runtime/Placement/StatelessWorkerDirector.cs
+++ src/Orleans.Runtime/Placement/StatelessWorkerDirector.cs
@@ -25,12 +25,21 @@ namespace Orleans.Runtime.Placement
 
         public Task<SiloAddress> OnAddActivation(PlacementStrategy strategy, PlacementTarget target, IPlacementContext context)
         {
-            // If the current silo is not shutting down, place locally
+            var compatibleSilos = context.GetCompatibleSilos(target);
+
+            // If the current silo is not shutting down, place locally if we are compatible
             if (!context.LocalSiloStatus.IsTerminating())
-                return Task.FromResult(context.LocalSilo);
+            {
+                foreach (var silo in compatibleSilos)
+                {
+                    if (silo == context.LocalSilo)
+                    {
+                        return Task.FromResult(context.LocalSilo);
+                    }
+                }
+            }
 
             // otherwise, place somewhere else
-            var compatibleSilos = context.GetCompatibleSilos(target);
             return Task.FromResult(compatibleSilos[random.Next(compatibleSilos.Count)]);
         }
 
