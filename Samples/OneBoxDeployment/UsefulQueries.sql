31c13dcce69b50eeb0ff50e729ef8575fd794da2
Veikko Eeva <veikko_e@hotmail.com>
2019-09-16 22:14:18 +0300

    Fix useful queries database name (#5953)
    Before this sample was committed to the repository, it was
    renamed from BigSample to OneBoxDeployment. However, these queries
    had the previous name. This commit fixes that.

--- Samples/OneBoxDeployment/UsefulQueries.sql
+++ Samples/OneBoxDeployment/UsefulQueries.sql
@@ -1,10 +1,10 @@
 -- If the database gets stuck to single user mode.
-ALTER DATABASE [BigSample.Database] SET MULTI_USER;
+ALTER DATABASE [OneBoxDeployment.Database] SET MULTI_USER;
 
 -- Check what data there is in version tables if the system takes long time to start.
-SELECT * FROM [BigSample.Database].Orleans.OrleansMembershipTable;
-SELECT * FROM [BigSample.Database].Orleans.OrleansMembershipVersionTable;
+SELECT * FROM [OneBoxDeployment.Database].[Orleans].[OrleansMembershipTable];
+SELECT * FROM [OneBoxDeployment.Database].[Orleans].[OrleansMembershipVersionTable];
 
 -- Just remove everything.
-DELETE [BigSample.Database].Orleans.OrleansMembershipTable;
-DELETE [BigSample.Database].Orleans.OrleansMembershipVersionTable;
+DELETE [OneBoxDeployment.Database].[Orleans].[OrleansMembershipTable];
+DELETE [OneBoxDeployment.Database].[Orleans].[OrleansMembershipVersionTable];
