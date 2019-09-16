65fa36342d2e77f2da1e2d877b84827d359c63d6
Sergey Bykov <sbykov@microsoft.com>
2019-09-16 15:11:17 -0700

    Update version to 1.5.10.

--- src/Build/GlobalAssemblyInfo.fs
+++ src/Build/GlobalAssemblyInfo.fs
@@ -5,9 +5,9 @@ open System.Reflection
 [<assembly: AssemblyCompany("Microsoft Corporation")>]
 [<assembly: AssemblyProduct("Orleans")>]
 [<assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation 2019")>]
-[<assembly: AssemblyVersion("1.5.9.0")>]
-[<assembly: AssemblyFileVersion("1.5.9.0")>]
-[<assembly: AssemblyInformationalVersion("1.5.9.0")>]
+[<assembly: AssemblyVersion("1.5.10.0")>]
+[<assembly: AssemblyFileVersion("1.5.10.0")>]
+[<assembly: AssemblyInformationalVersion("1.5.10.0")>]
 
 do
     ()
