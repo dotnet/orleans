5982c173f1ae6818afcc89b1ea01ffb2d60cdae9
Mike Lloyd <mike@reboot3times.org>
2019-11-18 14:33:07 -0700

    samples: added aspnet core with hosted services sample.
    Signed-off-by: Mike Lloyd <mike@reboot3times.org>

---
+++ Samples/3.0/ASPNetCoreHostedServices/Interfaces/IHelloWorld.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AspNetCoreHostedServices.Interfaces
+{
+    public interface IHelloWorld : Orleans.IGrainWithIntegerKey
+    {
+        Task<string> SayHello();
+    }
+}
