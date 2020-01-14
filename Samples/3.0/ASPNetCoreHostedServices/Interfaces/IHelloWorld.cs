f20059407d4a15c0a8c6c96ecd50e3e2ecfbbe36
Reuben Bond <rebond@microsoft.com>
2020-01-14 10:39:37 -0800

    Remove accidental commit of ASPNetCoreHostedServices folder (#6233)

--- Samples/3.0/ASPNetCoreHostedServices/Interfaces/IHelloWorld.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace AspNetCoreHostedServices.Interfaces
-{
-    public interface IHelloWorld : Orleans.IGrainWithIntegerKey
-    {
-        Task<string> SayHello();
-    }
-}
