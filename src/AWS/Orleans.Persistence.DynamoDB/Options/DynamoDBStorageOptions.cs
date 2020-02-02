b3c4b00eccf7ba8570d617b271437a911ce4035e
Amir Sasson <Amirsas@gmail.com>
2020-02-02 14:04:21 +0200

    Update DynamoDBStorageOptions.cs

--- src/AWS/Orleans.Persistence.DynamoDB/Options/DynamoDBStorageOptions.cs
+++ src/AWS/Orleans.Persistence.DynamoDB/Options/DynamoDBStorageOptions.cs
@@ -67,7 +67,6 @@ namespace Orleans.Configuration
         /// </summary>
         public TimeSpan? TimeToLive { get; set; }
         public Action<JsonSerializerSettings> ConfigureJsonSerializerSettings { get; set; }
-
     }
 
     /// <summary>
