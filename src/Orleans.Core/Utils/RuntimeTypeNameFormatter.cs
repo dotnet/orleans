b4f0097a3ade50c75f784d297baddf1abc8e1b75
ReubenBond <rebond@microsoft.com>
2020-05-22 16:05:46 -0700

    Implement CLR type name parser and rewriter

--- src/Orleans.Core/Utils/RuntimeTypeNameFormatter.cs
+++ src/Orleans.Core/Utils/RuntimeTypeNameFormatter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Reflection;
 using System.Text;
 using Orleans.Runtime;
@@ -7,15 +8,24 @@ namespace Orleans.Utilities
 {
     /// <summary>
     /// Utility methods for formatting <see cref="Type"/> instances in a way which can be parsed by
-    /// <see cref="Type.GetType(string)"/>.
+    /// <see cref="Type.GetType(string)"/> and <see cref="RuntimeTypeNameParser"/>.
     /// </summary>
     public static class RuntimeTypeNameFormatter
     {
         private static readonly Assembly SystemAssembly = typeof(int).Assembly;
         private static readonly char[] SimpleNameTerminators = { '`', '*', '[', '&' };
 
-        private static readonly CachedReadConcurrentDictionary<Type, string> Cache =
-            new CachedReadConcurrentDictionary<Type, string>();
+        private static readonly ConcurrentDictionary<Type, string> Cache = new ConcurrentDictionary<Type, string>();
+
+        static RuntimeTypeNameFormatter()
+        {
+            IncludeAssemblyName = type => !SystemAssembly.Equals(type.Assembly);
+        }
+
+        /// <summary>
+        /// Gets or sets the delegate used to determine whether an assembly name should be printed for the provided type.
+        /// </summary>
+        public static Func<Type, bool> IncludeAssemblyName { get; set; }
 
         /// <summary>
         /// Returns a <see cref="string"/> form of <paramref name="type"/> which can be parsed by <see cref="Type.GetType(string)"/>.
@@ -65,7 +75,10 @@ namespace Orleans.Utilities
 
             // Types which are used as elements are not formatted with their assembly name, since that is added after the
             // element type's adornments.
-            if (!isElementType) AddAssembly(builder, type);
+            if (!isElementType && IncludeAssemblyName(type))
+            {
+                AddAssembly(builder, type);
+            }
         }
 
         private static void AddNamespace(StringBuilder builder, Type type)
@@ -96,7 +109,7 @@ namespace Orleans.Utilities
         {
             // Generic type definitions (eg, List<> without parameters) and non-generic types do not include any
             // parameters in their formatting.
-            if (!type.IsConstructedGenericType) return;
+            if (!type.IsConstructedGenericType || type.ContainsGenericParameters) return;
 
             var args = type.GetGenericArguments();
             builder.Append('[');
