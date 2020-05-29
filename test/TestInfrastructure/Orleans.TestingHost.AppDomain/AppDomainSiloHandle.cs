9df2bfe2e23728b210a00c01bb6b74d68e15bae6
ReubenBond <rebond@microsoft.com>
2020-05-28 19:39:55 -0700

    Remove unused AppDomainTestHooks type

--- test/TestInfrastructure/Orleans.TestingHost.AppDomain/AppDomainSiloHandle.cs
+++ test/TestInfrastructure/Orleans.TestingHost.AppDomain/AppDomainSiloHandle.cs
@@ -65,7 +65,6 @@ namespace Orleans.TestingHost
                     SiloAddress = siloHost.SiloAddress,
                     GatewayAddress = siloHost.GatewayAddress,
                     AppDomain = appDomain,
-                    AppDomainTestHook = siloHost.AppDomainTestHook,
                 };
 
                 return Task.FromResult(retValue);
@@ -157,11 +156,6 @@ namespace Orleans.TestingHost
             // TODO: replace
             Console.WriteLine(value.ToString());
         }
-
-        /// <summary>Gets the Silo test hook that uses AppDomain remoting
-        /// (NOTE: this will be removed really soon, and was migrated here temporarily. It does not respect the abstraction
-        /// as this only works with AppDomains for now, but we'll be removing TestHooks with AppDomains entirely)</summary>
-        internal AppDomainTestHooks AppDomainTestHook { get; set; }
 
         internal static AppDomainSetup GetAppDomainSetupInfo(string applicationBase)
         {
