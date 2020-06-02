0d463d99e756608fb51ce6d33a31968e817403e0
ReubenBond <rebond@microsoft.com>
2020-06-02 15:40:23 -0700

    Split out tests a little

--- test/Analyzers.Tests/NoRefParamsDiagnosticAnalyzerTest.cs
+++ test/Analyzers.Tests/NoRefParamsDiagnosticAnalyzerTest.cs
@@ -10,18 +10,36 @@ namespace Analyzers.Tests
     public class NoRefParamsDiagnosticAnalyzerTest : DiagnosticAnalyzerTestBase<NoRefParamsDiagnosticAnalyzer>
     {
         [Fact]
-        public async Task NoRefParamsDiagnosticAnalyzerInInterface()
+        public async Task NoRefParamsAllowedInGrainInterfaceMethods()
         {
             var code = @"public interface I : IGrain
                         {
-                            Task GetSomeThing(ref int i);
+                            Task GetSomeOtherThing(int a, ref int i);
+                        }";
+
+            var (diagnostics, _) = await this.GetDiagnosticsAsync(code, new string[0]);
+
+            Assert.NotEmpty(diagnostics);
+            Assert.Single(diagnostics);
+
+            var diagnostic = diagnostics.First();
+            Assert.Equal(NoRefParamsDiagnosticAnalyzer.DiagnosticId, diagnostic.Id);
+            Assert.Equal(DiagnosticSeverity.Error, diagnostic.Severity);
+            Assert.Equal(NoRefParamsDiagnosticAnalyzer.MessageFormat, diagnostic.GetMessage());
+        }
+
+        [Fact]
+        public async Task NoOutParamsAllowedInGrainInterfaceMethods()
+        {
+            var code = @"public interface I : IGrain
+                        {
                             Task GetSomeOtherThing(out int i);
                         }";
 
             var (diagnostics, _) = await this.GetDiagnosticsAsync(code, new string[0]);
 
             Assert.NotEmpty(diagnostics);
-            Assert.Equal(2, diagnostics.Length);
+            Assert.Single(diagnostics);
 
             var diagnostic = diagnostics.First();
             Assert.Equal(NoRefParamsDiagnosticAnalyzer.DiagnosticId, diagnostic.Id);
@@ -71,6 +89,12 @@ namespace Analyzers.Tests
                         public class Imp : I
                         {
                             public Task GetSomething(int i) => Task.CompletedTask;
+                            public Task SomeNonInterfaceMethod(ref int value, out int value2, in int value3)
+                            {
+                                value = 0;
+                                value2 = 0;
+                                return Task.CompletedTask;
+                            }
                         }
                         ";
 
