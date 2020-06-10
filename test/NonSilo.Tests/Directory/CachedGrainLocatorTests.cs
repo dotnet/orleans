066c5ba1cc3da182bafc746d7d00e5c5cb0cc489
Benjamin Petit <benjaminpetit@users.noreply.github.com>
2020-06-10 09:29:55 -0700

    LocalGrainDirectory.UnregisterManyAsync should always be called from RemoteGrainDirectory context (#6575) (#6590)

--- test/NonSilo.Tests/Directory/CachedGrainLocatorTests.cs
+++ test/NonSilo.Tests/Directory/CachedGrainLocatorTests.cs
@@ -11,7 +11,11 @@ using Orleans.GrainDirectory;
 using Orleans.Metadata;
 using Orleans.Runtime;
 using Orleans.Runtime.GrainDirectory;
+using Orleans.Runtime.Scheduler;
+using Orleans.Runtime.Utilities;
 using TestExtensions;
+using UnitTests.SchedulerTests;
+using UnitTests.TesterInternal;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -25,9 +29,7 @@ namespace UnitTests.Directory
 
         private readonly IGrainDirectory grainDirectory;
         private readonly GrainDirectoryResolver grainDirectoryResolver;
-        private readonly ILocalGrainDirectory localGrainDirectory;
         private readonly MockClusterMembershipService mockMembershipService;
-
         private readonly CachedGrainLocator grainLocator;
 
         public CachedGrainLocatorTests(ITestOutputHelper output)
@@ -45,11 +47,10 @@ namespace UnitTests.Directory
                 services,
                 new GrainPropertiesResolver(new NoOpClusterManifestProvider()),
                 Array.Empty<IGrainDirectoryResolver>());
-            this.localGrainDirectory = Substitute.For<ILocalGrainDirectory>();
             this.mockMembershipService = new MockClusterMembershipService();
 
             this.grainLocator = new CachedGrainLocator(
-                this.grainDirectoryResolver,
+                this.grainDirectoryResolver, 
                 this.mockMembershipService.Target);
 
             this.grainLocator.Participate(this.lifecycle);
