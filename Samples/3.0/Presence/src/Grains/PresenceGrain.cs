30d8fe2f9fe1cbb00743fba0e76d1f61243442df
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2020-06-24 17:46:41 -0400

    Spelling error then => them (#6611)

--- Samples/3.0/Presence/src/Grains/PresenceGrain.cs
+++ Samples/3.0/Presence/src/Grains/PresenceGrain.cs
@@ -6,7 +6,7 @@ using Presence.Grains.Models;
 namespace Presence.Grains
 {
     /// <summary>
-    /// Stateless grain that decodes binary blobs and routes then to the appropriate game grains based on the blob content.
+    /// Stateless grain that decodes binary blobs and routes them to the appropriate game grains based on the blob content.
     /// Simulates how a cloud service receives raw data from a device and needs to preprocess it before forwarding for the actial computation.
     /// </summary>
     [StatelessWorker]
