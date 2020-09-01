147b214c235b5528f6c7ed7837c559a4b0906816
Reuben Bond <203839+ReubenBond@users.noreply.github.com>
2020-09-01 11:16:11 -0700

    Kubernetes hosting integration (#6707)
    * Add IClusterMembershipService.TryKill method for unilaterally declaring a silo dead
    
    * Add Orleans.Hosting.Kubernetes extension
    
    * Use PostConfigure for configuring EndpointOptions
    
    * Extras
    
    * Scope pod labels
    
    * Mark Orleans.Hosting.Kubernetes package as beta
    
    Co-authored-by: Benjamin Petit <bpetit@microsoft.com>

---
+++ src/Orleans.Hosting.Kubernetes/KubernetesHostingOptionsValidator.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.Options;
+using Orleans.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Orleans.Hosting.Kubernetes
+{
+    /// <summary>
+    /// Validates <see cref="KubernetesHostingOptions"/>.
+    /// </summary>
+    internal class KubernetesHostingOptionsValidator : IValidateOptions<KubernetesHostingOptions>
+    {
+        private readonly IOptions<SiloOptions> _siloOptions;
+
+        public KubernetesHostingOptionsValidator(IOptions<SiloOptions> siloOptions) => _siloOptions = siloOptions;
+
+        public ValidateOptionsResult Validate(string name, KubernetesHostingOptions options)
+        {
+            List<string> failures = default;
+            if (string.IsNullOrWhiteSpace(options.Namespace))
+            {
+                failures ??= new List<string>();
+                failures.Add($"{nameof(KubernetesHostingOptions)}.{nameof(KubernetesHostingOptions.Namespace)} is not set. Set it via the {KubernetesHostingOptions.PodNamespaceEnvironmentVariable} environment variable");
+            }
+
+            if (string.IsNullOrWhiteSpace(options.PodName))
+            {
+                failures ??= new List<string>();
+                failures.Add($"{nameof(KubernetesHostingOptions)}.{nameof(KubernetesHostingOptions.PodName)} is not set. Set it via the {KubernetesHostingOptions.PodNameEnvironmentVariable} environment variable");
+            }
+
+            if (!string.Equals(_siloOptions.Value.SiloName, options.PodName, StringComparison.Ordinal))
+            {
+                failures ??= new List<string>();
+                failures.Add($"{nameof(SiloOptions)}.{nameof(SiloOptions.SiloName)} is not equal to the current pod name as defined by {nameof(KubernetesHostingOptions)}.{nameof(KubernetesHostingOptions.PodName)}");
+            }
+
+            if (failures is object) return ValidateOptionsResult.Fail(failures);
+
+            return ValidateOptionsResult.Success;
+        }
+    }
+}
--- src/Orleans.Runtime/MembershipService/ClusterMember.cs
+++ src/Orleans.Runtime/MembershipService/ClusterMember.cs
@@ -5,21 +5,26 @@ namespace Orleans.Runtime
     [Serializable]
     public sealed class ClusterMember : IEquatable<ClusterMember>
     {
-        public ClusterMember(SiloAddress siloAddress, SiloStatus status)
+        public ClusterMember(SiloAddress siloAddress, SiloStatus status, string name)
         {
             this.SiloAddress = siloAddress ?? throw new ArgumentNullException(nameof(siloAddress));
             this.Status = status;
+            this.Name = name;
         }
 
         public SiloAddress SiloAddress { get; }
         public SiloStatus Status { get; }
+        public string Name { get; }
 
         public override bool Equals(object obj) => this.Equals(obj as ClusterMember);
 
-        public bool Equals(ClusterMember other) => other != null && this.SiloAddress.Equals(other.SiloAddress) && this.Status == other.Status;
+        public bool Equals(ClusterMember other) => other != null
+            && this.SiloAddress.Equals(other.SiloAddress)
+            && this.Status == other.Status
+            && string.Equals(this.Name, other.Name, StringComparison.Ordinal);
 
         public override int GetHashCode() => this.SiloAddress.GetConsistentHashCode();
 
-        public override string ToString() => $"{this.SiloAddress}/{this.Status}";
+        public override string ToString() => $"{this.SiloAddress}/{this.Name}/{this.Status}";
     }
 }
