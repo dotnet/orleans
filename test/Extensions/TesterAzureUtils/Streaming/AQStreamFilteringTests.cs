2d95ad65ce65c8fa78eefdd543e1ad36efc31569
Benjamin Petit <bpetit@microsoft.com>
2020-09-03 16:44:58 -0700

    Add tests

---
+++ test/Extensions/TesterAzureUtils/Streaming/AQStreamFilteringTests.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Providers.Streams.AzureQueue;
+using Orleans.TestingHost;
+using Tester.StreamingTests.Filtering;
+using TestExtensions;
+using UnitTests.StreamingTests;
+using Xunit;
+
+namespace Tester.AzureUtils.Streaming
+{
+    public class AQStreamFilteringTests : StreamFilteringTestsBase, IClassFixture<AQStreamFilteringTests.Fixture>, IAsyncLifetime
+    {
+        private const int queueCount = 1;
+
+        public AQStreamFilteringTests(Fixture fixture) : base(fixture)
+        {
+        }
+
+        public class Fixture : BaseAzureTestClusterFixture
+        {
+            protected override void ConfigureTestCluster(TestClusterBuilder builder)
+            {
+                TestUtils.CheckForAzureStorage();
+                builder.AddClientBuilderConfigurator<ClientConfigurator>();
+                builder.AddSiloBuilderConfigurator<SiloConfigurator>();
+            }
+
+            public class SiloConfigurator : ISiloConfigurator
+            {
+                public void Configure(ISiloBuilder hostBuilder)
+                {
+                    hostBuilder
+                        .AddAzureQueueStreams(StreamTestsConstants.AZURE_QUEUE_STREAM_PROVIDER_NAME, ob => ob.Configure<IOptions<ClusterOptions>>(
+                            (options, dep) =>
+                            {
+                                options.ConfigureTestDefaults();
+                                options.QueueNames = AzureQueueUtilities.GenerateQueueNames(dep.Value.ClusterId, queueCount);
+                            }))
+                        .AddMemoryGrainStorage("MemoryStore")
+                        .AddMemoryGrainStorage("PubSubStore")
+                        .AddStreamFilter<CustomStreamFilter>(StreamTestsConstants.AZURE_QUEUE_STREAM_PROVIDER_NAME);
+                }
+            }
+
+            public class ClientConfigurator : IClientBuilderConfigurator
+            {
+                public void Configure(IConfiguration configuration, IClientBuilder clientBuilder)
+                {
+                    clientBuilder
+                        .AddAzureQueueStreams(StreamTestsConstants.AZURE_QUEUE_STREAM_PROVIDER_NAME, ob => ob.Configure<IOptions<ClusterOptions>>(
+                            (options, dep) =>
+                            {
+                                options.ConfigureTestDefaults();
+                                options.QueueNames = AzureQueueUtilities.GenerateQueueNames(dep.Value.ClusterId, queueCount);
+                            }))
+                        .AddStreamFilter<CustomStreamFilter>(StreamTestsConstants.AZURE_QUEUE_STREAM_PROVIDER_NAME);
+                }
+            }
+        }
+
+        protected override string ProviderName => StreamTestsConstants.AZURE_QUEUE_STREAM_PROVIDER_NAME;
+
+        protected override TimeSpan WaitTime => TimeSpan.FromSeconds(2);
+
+        [Fact, TestCategory("BVT"), TestCategory("Streaming"), TestCategory("Filters")]
+        public async override Task IgnoreBadFilter() => await base.IgnoreBadFilter();
+
+        [Fact, TestCategory("BVT"), TestCategory("Streaming"), TestCategory("Filters")]
+        public async override Task OnlyEvenItems() => await base.OnlyEvenItems();
+
+        [Fact, TestCategory("BVT"), TestCategory("Streaming"), TestCategory("Filters")]
+        public async override Task MultipleSubscriptionsDifferentFilterData() => await base.MultipleSubscriptionsDifferentFilterData();
+
+        public Task InitializeAsync() => Task.CompletedTask;
+
+        public async Task DisposeAsync()
+        {
+            if (!string.IsNullOrWhiteSpace(TestDefaultConfiguration.DataConnectionString))
+            {
+                await AzureQueueStreamProviderUtils.ClearAllUsedAzureQueues(
+                  NullLoggerFactory.Instance,
+                  AzureQueueUtilities.GenerateQueueNames(this.fixture.HostedCluster.Options.ClusterId, queueCount),
+                  new AzureQueueOptions().ConfigureTestDefaults());
+            }
+        }
+    }
+}
---
+++ test/Grains/TestGrains/StreamingHistoryGrain.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Runtime;
+using Orleans.Streams;
+using UnitTests.GrainInterfaces;
+
+namespace UnitTests.Grains
+{
+    public class StreamingHistoryGrain : Grain, IStreamingHistoryGrain, IAsyncObserver<int>
+    {
+        private List<int> receivedItems = new List<int>();
+        private List<StreamSubscriptionHandle<int>> subscriptionHandles = new List<StreamSubscriptionHandle<int>>();
+
+        public async Task BecomeConsumer(StreamId streamId, string provider, string filterData = null)
+        {
+            var stream = base.GetStreamProvider(provider).GetStream<int>(streamId);
+            this.subscriptionHandles.Add(await stream.SubscribeAsync(this, null, filterData));
+        }
+
+        public Task<List<int>> GetReceivedItems() => Task.FromResult(this.receivedItems);
+
+        public async Task StopBeingConsumer()
+        {
+            foreach (var sub in this.subscriptionHandles)
+            {
+                await sub.UnsubscribeAsync();
+            }
+        }
+
+        public Task OnCompletedAsync() => Task.CompletedTask;
+
+        public Task OnErrorAsync(Exception ex) => Task.CompletedTask;
+
+        public Task OnNextAsync(int item, StreamSequenceToken token = null)
+        {
+            this.receivedItems.Add(item);
+            return Task.CompletedTask;
+        }
+    }
+}
