c5a08608a77b8e807906b716b8b25712ae0127e4
Benjamin Petit <bpetit@microsoft.com>
2020-10-28 17:24:20 -0700

    Revert "Use stream instead"
    This reverts commit 1d244608e9d51ce98c23fb24169e5e28451ce98f.

--- src/Orleans.Core/Providers/IGrainStateSerializer.cs
+++ src/Orleans.Core/Providers/IGrainStateSerializer.cs
@@ -1,35 +1,13 @@
 using System;
-using System.Buffers;
-using System.IO;
 using Orleans.Serialization;
 
 namespace Orleans.Storage
 {
     public interface IGrainStateSerializer
     {
-        void Serialize(Stream s, Type t, object value);
+        ReadOnlyMemory<byte> Serialize(Type t, object value);
 
-        object Deserialize(Type expected, Stream s);
-    }
-
-    public static class GrainStateSerializerExtentions
-    {
-        public static ReadOnlyMemory<byte> Serialize(this IGrainStateSerializer serializer, Type t, object value)
-        {
-            using (var stream = new MemoryStream())
-            {
-                serializer.Serialize(stream, t, value);
-                return stream.GetBuffer();
-            }
-        }
-
-        public static object Deserialize(this IGrainStateSerializer serializer, Type expected, byte[] input)
-        {
-            using (var stream = new MemoryStream(input))
-            {
-                return serializer.Deserialize(expected, stream);
-            }
-        }
+        object Deserialize(Type expected, ReadOnlyMemory<byte> value);
     }
 
     internal class OrleansGrainStateSerializer : IGrainStateSerializer
@@ -41,34 +19,8 @@ namespace Orleans.Storage
             this.serializationManager = serializationManager;
         }
 
-        public void Serialize(Stream s, Type t, object value)
-        {
-            var buffer = this.serializationManager.SerializeToByteArray(value);
-            s.Write(buffer, 0, buffer.Length);
-        }
+        public ReadOnlyMemory<byte> Serialize(Type t, object value) => this.serializationManager.SerializeToByteArray(value);
 
-        public object Deserialize(Type expected, Stream s)
-        {
-            if (s is MemoryStream memStream && memStream.TryGetBuffer(out var buffer))
-            {
-                return Deserialize(buffer);
-            }
-            else
-            {
-                // We have to copy to get the buffer
-                using (var tmpStream = new MemoryStream())
-                {
-                    s.CopyTo(tmpStream);
-                    tmpStream.TryGetBuffer(out var buffer2);
-                    return Deserialize(buffer2);
-                }
-            }
-        }
-
-        private object Deserialize(ArraySegment<byte> buffer)
-        {
-            var reader = new BinaryTokenStreamReader(buffer);
-            return this.serializationManager.Deserialize(reader);
-        }
+        public object Deserialize(Type expected, ReadOnlyMemory<byte> value) => this.serializationManager.DeserializeFromMemoryByte(typeof(object), value);
     }
 }
