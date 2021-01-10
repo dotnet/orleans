ed7f54bc92978ae08f8d35b1466abb06aa5876db
ReubenBond <rebond@microsoft.com>
2021-01-09 16:22:14 -0800

    Remove IGatewayListListener and IGatewayListObservable

--- src/Orleans.Core/Messaging/IGatewayListProvider.cs
+++ src/Orleans.Core/Messaging/IGatewayListProvider.cs
@@ -13,6 +13,7 @@ namespace Orleans.Messaging
         /// Initializes the provider, will be called before all other methods
         /// </summary>
         Task InitializeGatewayListProvider();
+
         /// <summary>
         /// Returns the list of gateways (silos) that can be used by a client to connect to Orleans cluster.
         /// The Uri is in the form of: "gwy.tcp://IP:port/Generation". See Utils.ToGatewayUri and Utils.ToSiloAddress for more details about Uri format.
@@ -29,26 +30,5 @@ namespace Orleans.Messaging
         /// </summary>
         [Obsolete("This attribute is no longer used and all providers are considered updatable")]
         bool IsUpdatable { get; }
-    }
-
-    /// <summary>
-    /// A listener interface for optional GatewayList notifications provided by the IGatewayListObservable interface.
-    /// </summary>
-    public interface IGatewayListListener
-    {
-        void GatewayListNotification(IEnumerable<Uri> gateways);
-    }
-
-    /// <summary>
-    /// An optional interface that GatewayListProvider may implement if it support out of band gw update notifications.
-    /// By default GatewayListProvider should support pull based queries (GetGateways).
-    /// Optionally, some GatewayListProviders may be able to notify a listener if an updated gw information is available.
-    /// This is optional and not required.
-    /// </summary>
-    public interface IGatewayListObservable
-    {
-        bool SubscribeToGatewayNotificationEvents(IGatewayListListener listener);
-
-        bool UnSubscribeFromGatewayNotificationEvents(IGatewayListListener listener);
     }
 }
