172b3b3dbdab8faa5aa2b911931bea48c16d5617
Maryam <43354588+maryammadzadeh@users.noreply.github.com>
2021-01-14 05:06:25 -0800

    Only log that a message is being forwarded if it is being forwarded (#6892)

--- src/Orleans.Runtime/Core/Dispatcher.cs
+++ src/Orleans.Runtime/Core/Dispatcher.cs
@@ -82,8 +82,6 @@ namespace Orleans.Runtime
             Exception exc = null,
             bool rejectMessages = false)
         {
-            this.messagingTrace.OnDispatcherForwardingMultiple(messages.Count, oldAddress, forwardingAddress, failedOperation, exc);
-
             // IMPORTANT: do not do anything on activation context anymore, since this activation is invalid already.
             scheduler.QueueAction(
                 () =>
@@ -96,6 +94,7 @@ namespace Orleans.Runtime
                         }
                         else
                         {
+                            this.messagingTrace.OnDispatcherForwardingMultiple(messages.Count, oldAddress, forwardingAddress, failedOperation, exc);
                             TryForwardRequest(message, oldAddress, forwardingAddress, failedOperation, exc);
                         }
                     }
