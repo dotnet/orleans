3d40fdbc283029674c54ba9c2444991f116e5e18
Andy Petralli <andres.petralli@microsoft.com>
2021-08-13 14:16:43 -0700

    Add selector for the client (#7144)

--- src/Orleans.Connections.Security/Security/TlsClientAuthenticationOptions.cs
+++ src/Orleans.Connections.Security/Security/TlsClientAuthenticationOptions.cs
@@ -5,6 +5,8 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Orleans.Connections.Security
 {
+    public delegate X509Certificate ClientCertificateSelectionCallback(object sender, string targetHost, X509CertificateCollection localCertificates, X509Certificate remoteCertificate, string[] acceptableIssuers);
+
     public class TlsClientAuthenticationOptions
     {
 #if NETCOREAPP
@@ -15,6 +17,12 @@ namespace Orleans.Connections.Security
                 OrleansApplicationProtocol.Orleans1
             }
         };
+
+        public ClientCertificateSelectionCallback LocalCertificateSelectionCallback
+        {
+            get => Value.LocalCertificateSelectionCallback is null ? null : new ClientCertificateSelectionCallback(Value.LocalCertificateSelectionCallback);
+            set => Value.LocalCertificateSelectionCallback = value is null ? null : new System.Net.Security.LocalCertificateSelectionCallback(value);
+        }
 
         public X509CertificateCollection ClientCertificates
         {
