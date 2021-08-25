2b39ab2f3c89a2896fcf7a48687a3c2f22c56150
Reuben Bond <203839+ReubenBond@users.noreply.github.com>
2021-08-25 13:32:52 -0700

    Fix ResponseCompletionSource (#7221)

--- src/Orleans.Serialization/Invocation/ResponseCompletionSource.cs
+++ src/Orleans.Serialization/Invocation/ResponseCompletionSource.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Runtime.CompilerServices;
-using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Sources;
 
@@ -26,7 +25,17 @@ namespace Orleans.Serialization.Invocation
 
         public void SetException(Exception exception) => _core.SetException(exception);
 
-        public void SetResult(Response result) => _core.SetResult(result);
+        public void SetResult(Response result)
+        {
+            if (result.Exception is not { } exception)
+            {
+                _core.SetResult(result);
+            }
+            else
+            {
+                _core.SetException(exception);
+            }
+        }
         public void Complete(Response value) => SetResult(value);
         public void Complete() => SetResult(Response.Completed);
 
@@ -118,7 +127,7 @@ namespace Orleans.Serialization.Invocation
         {
             var exception = new InvalidCastException($"Cannot cast object of type {result.GetType()} to {typeof(TResult)}");
 #if NET5_0
-            ExceptionDispatchInfo.SetCurrentStackTrace(exception);
+            System.Runtime.ExceptionServices.ExceptionDispatchInfo.SetCurrentStackTrace(exception);
             SetException(exception);
 #else
             try
