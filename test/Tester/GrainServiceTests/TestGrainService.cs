57a32339b82ebb31c8caf70c6d4247ac34acc877
ReubenBond <rebond@microsoft.com>
2021-09-02 14:50:44 -0700

    Implement GetGrainId() for SystemTargets & GrainService

--- test/Tester/GrainServiceTests/TestGrainService.cs
+++ test/Tester/GrainServiceTests/TestGrainService.cs
@@ -56,11 +56,13 @@ namespace Tester
     {
         private readonly IGrainIdentity id;
         private TestGrainServiceOptions config;
+        private ILogger<TestGrainService> log;
 
         public TestGrainService(IGrainIdentity id, Silo silo, ILoggerFactory loggerFactory, IOptions<TestGrainServiceOptions> options) : base(id, silo, loggerFactory)
         {
             this.id = id;
             this.config = options.Value;
+            this.log = loggerFactory.CreateLogger<TestGrainService>();
         }
 
         private bool started = false;
@@ -69,44 +71,53 @@ namespace Tester
 
         public async override Task Init(IServiceProvider serviceProvider)
         {
+            this.log.LogInformation("Calling Init on grain service with id {GrainServiceId}", this.GetPrimaryKeyLong(out _));
             await base.Init(serviceProvider);
             init = true;
         }
 
         public override Task Start()
         {
+            this.log.LogInformation("Calling Start on grain service with id {GrainServiceId}", this.GetPrimaryKeyLong(out _));
             started = true;
             return base.Start();
         }
 
         public Task<string> GetHelloWorldUsingCustomService(GrainReference reference)
         {
+            this.log.LogInformation("Calling GetHelloWorldUsingCustomService on grain service with id {GrainServiceId}", this.GetPrimaryKeyLong(out _));
             return Task.FromResult("Hello World from Test Grain Service");
         }
 
         protected override Task StartInBackground()
         {
+            this.log.LogInformation("Calling StartInBackground on grain service with id {GrainServiceId}", this.GetPrimaryKeyLong(out _));
             startedInBackground = true;
             return Task.CompletedTask;
         }
 
         public Task<bool> HasStarted()
         {
+            this.log.LogInformation("Calling HasStarted on grain service with id {GrainServiceId}", this.GetPrimaryKeyLong(out _));
             return Task.FromResult(started);
         }
 
         public Task<bool> HasStartedInBackground()
         {
+            this.log.LogInformation("Calling HasStartedInBackground on grain service with id {GrainServiceId}", this.GetPrimaryKeyLong(out _));
             return Task.FromResult(startedInBackground);
         }
 
         public Task<bool> HasInit()
         {
+            var id = this.GetPrimaryKeyLong(out _);
+            this.log.LogInformation("Calling HasInit on grain service with id {GrainServiceId}", id);
             return Task.FromResult(init);
         }
 
         public Task<string> GetServiceConfigProperty()
         {
+            this.log.LogInformation("Calling GetServiceConfigProperty on grain service with id {GrainServiceId}", this.GetPrimaryKeyLong(out _));
             return Task.FromResult(config.ConfigProperty);
         }
     }
