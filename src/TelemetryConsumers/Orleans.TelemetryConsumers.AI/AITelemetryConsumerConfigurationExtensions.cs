5318ad256d8532b5287dc844d270fdde7fdc2293
Xinyi Joffre <xiou@microsoft.com>
2021-09-21 08:50:13 -0700

    Add TelemetryConfiguration support for AITelemetryConsumer (#7278)
    * Add TelemetryConfiguration support for AITelemetryConsumer
    
    * Ensure back compatibility
    
    * Update src/TelemetryConsumers/Orleans.TelemetryConsumers.AI/AITelemetryConsumerConfigurationExtensions.cs
    
    * Update src/TelemetryConsumers/Orleans.TelemetryConsumers.AI/AITelemetryConsumerConfigurationExtensions.cs
    
    * Update src/TelemetryConsumers/Orleans.TelemetryConsumers.AI/ApplicationInsightsTelemetryConsumerOptions.cs
    
    * Update src/TelemetryConsumers/Orleans.TelemetryConsumers.AI/ApplicationInsightsTelemetryConsumerOptions.cs
    
    * Update src/TelemetryConsumers/Orleans.TelemetryConsumers.AI/ApplicationInsightsTelemetryConsumerOptions.cs
    
    * Update src/TelemetryConsumers/Orleans.TelemetryConsumers.AI/ApplicationInsightsTelemetryConsumerOptions.cs
    
    Co-authored-by: Reuben Bond <203839+ReubenBond@users.noreply.github.com>

--- src/TelemetryConsumers/Orleans.TelemetryConsumers.AI/AITelemetryConsumerConfigurationExtensions.cs
+++ src/TelemetryConsumers/Orleans.TelemetryConsumers.AI/AITelemetryConsumerConfigurationExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.DependencyInjection;
 using Orleans.Configuration;
 using Orleans.TelemetryConsumers.AI;
@@ -13,7 +14,17 @@ namespace Orleans.Hosting
         /// <param name="instrumentationKey">The Application Insights instrumentation key.</param>
         public static ISiloBuilder AddApplicationInsightsTelemetryConsumer(this ISiloBuilder hostBuilder, string instrumentationKey = null)
         {
-            return hostBuilder.ConfigureServices((context, services) => ConfigureServices(context, services, instrumentationKey));
+            return hostBuilder.ConfigureServices((context, services) => ConfigureServices(context, services, null, instrumentationKey));
+        }
+
+        /// <summary>
+        /// Adds a metrics telemetric consumer provider of type <see cref="AITelemetryConsumer"/> using a predefined <see cref="TelemetryConfiguration"/>.
+        /// </summary>
+        /// <param name="hostBuilder"></param>
+        /// <param name="telemetryConfiguration">The Application Insights TelemetryConfiguration.</param>
+        public static ISiloBuilder AddApplicationInsightsTelemetryConsumer(this ISiloBuilder hostBuilder, TelemetryConfiguration telemetryConfiguration)
+        {
+            return hostBuilder.ConfigureServices((context, services) => ConfigureServices(context, services, telemetryConfiguration, null));
         }
 
         /// <summary>
@@ -23,23 +34,45 @@ namespace Orleans.Hosting
         /// <param name="instrumentationKey">The Application Insights instrumentation key.</param>
         public static IClientBuilder AddApplicationInsightsTelemetryConsumer(this IClientBuilder clientBuilder, string instrumentationKey = null)
         {
-            return clientBuilder.ConfigureServices((context, services) => ConfigureServices(context, services, instrumentationKey));
+            return clientBuilder.ConfigureServices((context, services) => ConfigureServices(context, services, null, instrumentationKey));
         }
 
-        private static void ConfigureServices(Microsoft.Extensions.Hosting.HostBuilderContext context, IServiceCollection services, string instrumentationKey)
+        /// <summary>
+        /// Adds a metrics telemetric consumer provider of type <see cref="AITelemetryConsumer"/> using a predefined <see cref="TelemetryConfiguration"/>.
+        /// </summary>
+        /// <param name="clientBuilder"></param>
+        /// <param name="telemetryConfiguration">The Application Insights TelemetryConfiguration.</param>
+        public static IClientBuilder AddApplicationInsightsTelemetryConsumer(this IClientBuilder clientBuilder, TelemetryConfiguration telemetryConfiguration)
+        {
+            return clientBuilder.ConfigureServices((context, services) => ConfigureServices(context, services, telemetryConfiguration, null));
+        }
+
+        private static void ConfigureServices(Microsoft.Extensions.Hosting.HostBuilderContext context, IServiceCollection services, TelemetryConfiguration telemetryConfiguration, string instrumentationKey)
         {
             services.ConfigureFormatter<ApplicationInsightsTelemetryConsumerOptions>();
             services.Configure<TelemetryOptions>(options => options.AddConsumer<AITelemetryConsumer>());
-            if (!string.IsNullOrWhiteSpace(instrumentationKey))
+            if (telemetryConfiguration != null)
+            {
+                services.Configure<ApplicationInsightsTelemetryConsumerOptions>(options => options.TelemetryConfiguration = telemetryConfiguration);
+            }
+            else if (!string.IsNullOrWhiteSpace(instrumentationKey))
+            {
                 services.Configure<ApplicationInsightsTelemetryConsumerOptions>(options => options.InstrumentationKey = instrumentationKey);
+            }
         }
 
-        private static void ConfigureServices(HostBuilderContext context, IServiceCollection services, string instrumentationKey)
+        private static void ConfigureServices(HostBuilderContext context, IServiceCollection services, TelemetryConfiguration telemetryConfiguration, string instrumentationKey)
         {
             services.ConfigureFormatter<ApplicationInsightsTelemetryConsumerOptions>();
             services.Configure<TelemetryOptions>(options => options.AddConsumer<AITelemetryConsumer>());
-            if (!string.IsNullOrWhiteSpace(instrumentationKey))
+            if (telemetryConfiguration != null)
+            {
+                services.Configure<ApplicationInsightsTelemetryConsumerOptions>(options => options.TelemetryConfiguration = telemetryConfiguration);
+            }
+            else if (!string.IsNullOrWhiteSpace(instrumentationKey))
+            {
                 services.Configure<ApplicationInsightsTelemetryConsumerOptions>(options => options.InstrumentationKey = instrumentationKey);
+            }
         }
     }
 }
