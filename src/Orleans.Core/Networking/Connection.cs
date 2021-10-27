d1b7119cb069117b6755b74f4fb4835ee8d6d06f
ReubenBond <rebond@microsoft.com>
2021-10-27 11:19:24 -0700

    No abort

--- src/Orleans.Core/Networking/Connection.cs
+++ src/Orleans.Core/Networking/Connection.cs
@@ -249,7 +249,7 @@ namespace Orleans.Runtime.Messaging
 
             try
             {
-                this.Context.Abort();
+                //this.Context.Abort();
                 await this.Context.DisposeAsync();
             }
             catch (Exception abortException)
