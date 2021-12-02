c61ea4245b769950e000d6234126ac59f6fcdf67
Benjamin Petit <bpetit@microsoft.com>
2021-12-02 17:49:03 +0100

    Use a custom storage factory in PubSubRendezVousGrain

--- src/Orleans.Core.Abstractions/Providers/ProviderConstants.cs
+++ src/Orleans.Core.Abstractions/Providers/ProviderConstants.cs
@@ -1,9 +1,11 @@
-﻿namespace Orleans.Providers
+namespace Orleans.Providers
 {
     public static class ProviderConstants
     {
         public const string DEFAULT_STORAGE_PROVIDER_NAME = "Default";
 
         public const string DEFAULT_LOG_CONSISTENCY_PROVIDER_NAME = "Default";
+
+        public const string DEFAULT_PUBSUB_PROVIDER_NAME = "PubSubStore";
     }
 }
