f39c0b0f3e2f4b5071cb66abc97dcc5775a7145c
Ede Meijer <ede@elastict.nl>
2022-01-12 18:46:52 +0100

    Create PostgreSQL migrations for version 3.6.0 (#7490)
    * Create PostgreSQL migrations for version 3.6.0
    
    The changes in https://github.com/dotnet/orleans/pull/7402 are fine for new deployments, but existing deployments need a way to migrate tables and routines in order to apply the same changes.
    
    Users can run the SQL in the 3 migration files when upgrading to 3.6.0
    
    Co-authored-by: Reuben Bond <203839+ReubenBond@users.noreply.github.com>

---
+++ src/AdoNet/Orleans.Persistence.AdoNet/Migrations/PostgreSQL-Persistence-3.6.0.sql
@@ -0,0 +1,137 @@
+-- Run this migration for upgrading the PostgreSQL persistence table and routines for deployments created before 3.6.0
+
+BEGIN;
+
+-- Change date type
+
+ALTER TABLE OrleansStorage
+ALTER COLUMN modifiedon TYPE TIMESTAMPTZ USING modifiedon AT TIME ZONE 'UTC';
+
+-- Recreate routines
+
+CREATE OR REPLACE FUNCTION writetostorage(
+    _grainidhash integer,
+    _grainidn0 bigint,
+    _grainidn1 bigint,
+    _graintypehash integer,
+    _graintypestring character varying,
+    _grainidextensionstring character varying,
+    _serviceid character varying,
+    _grainstateversion integer,
+    _payloadbinary bytea,
+    _payloadjson text,
+    _payloadxml xml)
+    RETURNS TABLE(newgrainstateversion integer)
+    LANGUAGE 'plpgsql'
+AS $function$
+    DECLARE
+     _newGrainStateVersion integer := _GrainStateVersion;
+     RowCountVar integer := 0;
+
+    BEGIN
+
+    -- Grain state is not null, so the state must have been read from the storage before.
+    -- Let's try to update it.
+    --
+    -- When Orleans is running in normal, non-split state, there will
+    -- be only one grain with the given ID and type combination only. This
+    -- grain saves states mostly serially if Orleans guarantees are upheld. Even
+    -- if not, the updates should work correctly due to version number.
+    --
+    -- In split brain situations there can be a situation where there are two or more
+    -- grains with the given ID and type combination. When they try to INSERT
+    -- concurrently, the table needs to be locked pessimistically before one of
+    -- the grains gets @GrainStateVersion = 1 in return and the other grains will fail
+    -- to update storage. The following arrangement is made to reduce locking in normal operation.
+    --
+    -- If the version number explicitly returned is still the same, Orleans interprets it so the update did not succeed
+    -- and throws an InconsistentStateException.
+    --
+    -- See further information at https://dotnet.github.io/orleans/Documentation/Core-Features/Grain-Persistence.html.
+    IF _GrainStateVersion IS NOT NULL
+    THEN
+        UPDATE OrleansStorage
+        SET
+            PayloadBinary = _PayloadBinary,
+            PayloadJson = _PayloadJson,
+            PayloadXml = _PayloadXml,
+            ModifiedOn = (now() at time zone 'utc'),
+            Version = Version + 1
+
+        WHERE
+            GrainIdHash = _GrainIdHash AND _GrainIdHash IS NOT NULL
+            AND GrainTypeHash = _GrainTypeHash AND _GrainTypeHash IS NOT NULL
+            AND GrainIdN0 = _GrainIdN0 AND _GrainIdN0 IS NOT NULL
+            AND GrainIdN1 = _GrainIdN1 AND _GrainIdN1 IS NOT NULL
+            AND GrainTypeString = _GrainTypeString AND _GrainTypeString IS NOT NULL
+            AND ((_GrainIdExtensionString IS NOT NULL AND GrainIdExtensionString IS NOT NULL AND GrainIdExtensionString = _GrainIdExtensionString) OR _GrainIdExtensionString IS NULL AND GrainIdExtensionString IS NULL)
+            AND ServiceId = _ServiceId AND _ServiceId IS NOT NULL
+            AND Version IS NOT NULL AND Version = _GrainStateVersion AND _GrainStateVersion IS NOT NULL;
+
+        GET DIAGNOSTICS RowCountVar = ROW_COUNT;
+        IF RowCountVar > 0
+        THEN
+            _newGrainStateVersion := _GrainStateVersion + 1;
+        END IF;
+    END IF;
+
+    -- The grain state has not been read. The following locks rather pessimistically
+    -- to ensure only one INSERT succeeds.
+    IF _GrainStateVersion IS NULL
+    THEN
+        INSERT INTO OrleansStorage
+        (
+            GrainIdHash,
+            GrainIdN0,
+            GrainIdN1,
+            GrainTypeHash,
+            GrainTypeString,
+            GrainIdExtensionString,
+            ServiceId,
+            PayloadBinary,
+            PayloadJson,
+            PayloadXml,
+            ModifiedOn,
+            Version
+        )
+        SELECT
+            _GrainIdHash,
+            _GrainIdN0,
+            _GrainIdN1,
+            _GrainTypeHash,
+            _GrainTypeString,
+            _GrainIdExtensionString,
+            _ServiceId,
+            _PayloadBinary,
+            _PayloadJson,
+            _PayloadXml,
+           now(),
+            1
+        WHERE NOT EXISTS
+         (
+            -- There should not be any version of this grain state.
+            SELECT 1
+            FROM OrleansStorage
+            WHERE
+                GrainIdHash = _GrainIdHash AND _GrainIdHash IS NOT NULL
+                AND GrainTypeHash = _GrainTypeHash AND _GrainTypeHash IS NOT NULL
+                AND GrainIdN0 = _GrainIdN0 AND _GrainIdN0 IS NOT NULL
+                AND GrainIdN1 = _GrainIdN1 AND _GrainIdN1 IS NOT NULL
+                AND GrainTypeString = _GrainTypeString AND _GrainTypeString IS NOT NULL
+                AND ((_GrainIdExtensionString IS NOT NULL AND GrainIdExtensionString IS NOT NULL AND GrainIdExtensionString = _GrainIdExtensionString) OR _GrainIdExtensionString IS NULL AND GrainIdExtensionString IS NULL)
+                AND ServiceId = _ServiceId AND _ServiceId IS NOT NULL
+         );
+
+        GET DIAGNOSTICS RowCountVar = ROW_COUNT;
+        IF RowCountVar > 0
+        THEN
+            _newGrainStateVersion := 1;
+        END IF;
+    END IF;
+
+    RETURN QUERY SELECT _newGrainStateVersion AS NewGrainStateVersion;
+END
+
+$function$;
+
+COMMIT;
