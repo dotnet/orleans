f66bff50ddd0dc67a4b1e4b9c107f070427ebd0e
Reuben Bond <203839+ReubenBond@users.noreply.github.com>
2022-01-12 10:37:17 -0800

    Create PostgreSQL migrations for version 3.6.0 (#7490) (#7492)
    * Create PostgreSQL migrations for version 3.6.0
    
    The changes in https://github.com/dotnet/orleans/pull/7402 are fine for new deployments, but existing deployments need a way to migrate tables and routines in order to apply the same changes.
    
    Users can run the SQL in the 3 migration files when upgrading to 3.6.0
    
    Co-authored-by: Reuben Bond <203839+ReubenBond@users.noreply.github.com>
    
    Co-authored-by: Ede Meijer <ede@elastict.nl>

---
+++ src/AdoNet/Orleans.Clustering.AdoNet/Migrations/PostgreSQL-Clustering-3.6.0.sql
@@ -0,0 +1,168 @@
+-- Run this migration for upgrading the PostgreSQL clustering table and routines for deployments created before 3.6.0
+
+BEGIN;
+
+-- Change date type
+
+ALTER TABLE OrleansMembershipVersionTable
+ALTER COLUMN Timestamp TYPE TIMESTAMPTZ(3) USING Timestamp AT TIME ZONE 'UTC';
+
+ALTER TABLE OrleansMembershipTable
+ALTER COLUMN StartTime TYPE TIMESTAMPTZ(3) USING StartTime AT TIME ZONE 'UTC',
+ALTER COLUMN IAmAliveTime TYPE TIMESTAMPTZ(3) USING IAmAliveTime AT TIME ZONE 'UTC';
+
+-- Recreate routines
+
+CREATE OR REPLACE FUNCTION update_i_am_alive_time(
+    deployment_id OrleansMembershipTable.DeploymentId%TYPE,
+    address_arg OrleansMembershipTable.Address%TYPE,
+    port_arg OrleansMembershipTable.Port%TYPE,
+    generation_arg OrleansMembershipTable.Generation%TYPE,
+    i_am_alive_time OrleansMembershipTable.IAmAliveTime%TYPE)
+  RETURNS void AS
+$func$
+BEGIN
+    -- This is expected to never fail by Orleans, so return value
+    -- is not needed nor is it checked.
+    UPDATE OrleansMembershipTable as d
+    SET
+        IAmAliveTime = i_am_alive_time
+    WHERE
+        d.DeploymentId = deployment_id AND deployment_id IS NOT NULL
+        AND d.Address = address_arg AND address_arg IS NOT NULL
+        AND d.Port = port_arg AND port_arg IS NOT NULL
+        AND d.Generation = generation_arg AND generation_arg IS NOT NULL;
+END
+$func$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION insert_membership(
+    DeploymentIdArg OrleansMembershipTable.DeploymentId%TYPE,
+    AddressArg      OrleansMembershipTable.Address%TYPE,
+    PortArg         OrleansMembershipTable.Port%TYPE,
+    GenerationArg   OrleansMembershipTable.Generation%TYPE,
+    SiloNameArg     OrleansMembershipTable.SiloName%TYPE,
+    HostNameArg     OrleansMembershipTable.HostName%TYPE,
+    StatusArg       OrleansMembershipTable.Status%TYPE,
+    ProxyPortArg    OrleansMembershipTable.ProxyPort%TYPE,
+    StartTimeArg    OrleansMembershipTable.StartTime%TYPE,
+    IAmAliveTimeArg OrleansMembershipTable.IAmAliveTime%TYPE,
+    VersionArg      OrleansMembershipVersionTable.Version%TYPE)
+  RETURNS TABLE(row_count integer) AS
+$func$
+DECLARE
+    RowCountVar int := 0;
+BEGIN
+
+    BEGIN
+        INSERT INTO OrleansMembershipTable
+        (
+            DeploymentId,
+            Address,
+            Port,
+            Generation,
+            SiloName,
+            HostName,
+            Status,
+            ProxyPort,
+            StartTime,
+            IAmAliveTime
+        )
+        SELECT
+            DeploymentIdArg,
+            AddressArg,
+            PortArg,
+            GenerationArg,
+            SiloNameArg,
+            HostNameArg,
+            StatusArg,
+            ProxyPortArg,
+            StartTimeArg,
+            IAmAliveTimeArg
+        ON CONFLICT (DeploymentId, Address, Port, Generation) DO
+            NOTHING;
+
+
+        GET DIAGNOSTICS RowCountVar = ROW_COUNT;
+
+        UPDATE OrleansMembershipVersionTable
+        SET
+            Timestamp = now(),
+            Version = Version + 1
+        WHERE
+            DeploymentId = DeploymentIdArg AND DeploymentIdArg IS NOT NULL
+            AND Version = VersionArg AND VersionArg IS NOT NULL
+            AND RowCountVar > 0;
+
+        GET DIAGNOSTICS RowCountVar = ROW_COUNT;
+
+        ASSERT RowCountVar <> 0, 'no rows affected, rollback';
+
+
+        RETURN QUERY SELECT RowCountVar;
+    EXCEPTION
+    WHEN assert_failure THEN
+        RETURN QUERY SELECT RowCountVar;
+    END;
+
+END
+$func$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION update_membership(
+    DeploymentIdArg OrleansMembershipTable.DeploymentId%TYPE,
+    AddressArg      OrleansMembershipTable.Address%TYPE,
+    PortArg         OrleansMembershipTable.Port%TYPE,
+    GenerationArg   OrleansMembershipTable.Generation%TYPE,
+    StatusArg       OrleansMembershipTable.Status%TYPE,
+    SuspectTimesArg OrleansMembershipTable.SuspectTimes%TYPE,
+    IAmAliveTimeArg OrleansMembershipTable.IAmAliveTime%TYPE,
+    VersionArg      OrleansMembershipVersionTable.Version%TYPE
+  )
+  RETURNS TABLE(row_count integer) AS
+$func$
+DECLARE
+    RowCountVar int := 0;
+BEGIN
+
+    BEGIN
+
+    UPDATE OrleansMembershipVersionTable
+    SET
+        Timestamp = now(),
+        Version = Version + 1
+    WHERE
+        DeploymentId = DeploymentIdArg AND DeploymentIdArg IS NOT NULL
+        AND Version = VersionArg AND VersionArg IS NOT NULL;
+
+
+    GET DIAGNOSTICS RowCountVar = ROW_COUNT;
+
+    UPDATE OrleansMembershipTable
+    SET
+        Status = StatusArg,
+        SuspectTimes = SuspectTimesArg,
+        IAmAliveTime = IAmAliveTimeArg
+    WHERE
+        DeploymentId = DeploymentIdArg AND DeploymentIdArg IS NOT NULL
+        AND Address = AddressArg AND AddressArg IS NOT NULL
+        AND Port = PortArg AND PortArg IS NOT NULL
+        AND Generation = GenerationArg AND GenerationArg IS NOT NULL
+        AND RowCountVar > 0;
+
+
+        GET DIAGNOSTICS RowCountVar = ROW_COUNT;
+
+        ASSERT RowCountVar <> 0, 'no rows affected, rollback';
+
+
+        RETURN QUERY SELECT RowCountVar;
+    EXCEPTION
+    WHEN assert_failure THEN
+        RETURN QUERY SELECT RowCountVar;
+    END;
+
+END
+$func$ LANGUAGE plpgsql;
+
+COMMIT;
