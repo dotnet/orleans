fbca3fff350a0c9d30b4861ff4718919daf146e9
David Pine <david.pine@microsoft.com>
2022-02-04 14:57:22 -0600

    Upgrade to .NET 6, and modernize (#7537)

--- samples/Adventure/AdventureGrains/MonsterGrain.cs
+++ samples/Adventure/AdventureGrains/MonsterGrain.cs
@@ -1,78 +1,80 @@
 using AdventureGrainInterfaces;
 using Orleans;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AdventureGrains
+namespace AdventureGrains;
+
+public class MonsterGrain : Grain, IMonsterGrain
 {
-    public class MonsterGrain : Orleans.Grain, IMonsterGrain
+    private MonsterInfo _monsterInfo = new();
+    private IRoomGrain? _roomGrain; // Current room
+
+    public override Task OnActivateAsync()
     {
-        MonsterInfo monsterInfo = new MonsterInfo();
-        IRoomGrain roomGrain; // Current room
+        _monsterInfo = _monsterInfo with { Id = this.GetPrimaryKeyLong() };
 
-        public override Task OnActivateAsync()
+        RegisterTimer(
+            _ => Move(),
+            null,
+            TimeSpan.FromSeconds(150),
+            TimeSpan.FromMinutes(150));
+
+        return base.OnActivateAsync();
+    }
+
+    Task IMonsterGrain.SetInfo(MonsterInfo info)
+    {
+        _monsterInfo = info;
+        return Task.CompletedTask;
+    }
+
+    Task<string?> IMonsterGrain.Name() => Task.FromResult(_monsterInfo.Name);
+
+    async Task IMonsterGrain.SetRoomGrain(IRoomGrain room)
+    {
+        if (_roomGrain is not null)
         {
-            this.monsterInfo.Id = this.GetPrimaryKeyLong();
-
-            RegisterTimer((_) => Move(), null, TimeSpan.FromSeconds(150), TimeSpan.FromMinutes(150));
-            return base.OnActivateAsync();
+            await _roomGrain.Exit(_monsterInfo);
         }
 
-        Task IMonsterGrain.SetInfo(MonsterInfo info)
+        _roomGrain = room;
+        await _roomGrain.Enter(_monsterInfo);
+    }
+
+    Task<IRoomGrain> IMonsterGrain.RoomGrain() => Task.FromResult(_roomGrain!);
+
+    private async Task Move()
+    {
+        if (_roomGrain is not null)
         {
-            this.monsterInfo = info;
-            return Task.CompletedTask;
-        }
+            var directions = new[] { "north", "south", "west", "east" };
+            var rand = Random.Shared.Next(0, 4);
 
-        Task<string> IMonsterGrain.Name()
-        {
-            return Task.FromResult(this.monsterInfo.Name);
-        }
-
-        async Task IMonsterGrain.SetRoomGrain(IRoomGrain room)
-        {
-            if (this.roomGrain != null)
-                await this.roomGrain.Exit(this.monsterInfo);
-            this.roomGrain = room;
-            await this.roomGrain.Enter(this.monsterInfo);
-        }
-
-        Task<IRoomGrain> IMonsterGrain.RoomGrain()
-        {
-            return Task.FromResult(roomGrain);
-        }
-
-        async Task Move()
-        {
-            var directions = new string [] { "north", "south", "west", "east" };
-
-            var rand = new Random().Next(0, 4);
-            IRoomGrain nextRoom = await this.roomGrain.ExitTo(directions[rand]);
-
-            if (null == nextRoom) 
-                return;
-
-            await this.roomGrain.Exit(this.monsterInfo);
-            await nextRoom.Enter(this.monsterInfo);
-
-            this.roomGrain = nextRoom;
-        }
-
-
-        Task<string> IMonsterGrain.Kill(IRoomGrain room)
-        {
-            if (this.roomGrain != null)
+            var nextRoom = await _roomGrain.ExitTo(directions[rand]);
+            if (nextRoom is null)
             {
-                if (this.roomGrain.GetPrimaryKey() != room.GetPrimaryKey())
-                {
-                    return Task.FromResult(monsterInfo.Name + " snuck away. You were too slow!");
-                }
-                return this.roomGrain.Exit(this.monsterInfo).ContinueWith(t => monsterInfo.Name + " is dead.");
+                return;
             }
-            return Task.FromResult(monsterInfo.Name + " is already dead. You were too slow and someone else got to him!");
+
+            await _roomGrain.Exit(_monsterInfo);
+            await nextRoom.Enter(_monsterInfo);
+
+            _roomGrain = nextRoom;
         }
+    }
+
+
+    Task<string> IMonsterGrain.Kill(IRoomGrain room)
+    {
+        if (_roomGrain is not null)
+        {
+            return _roomGrain.GetPrimaryKey() != room.GetPrimaryKey()
+                ? Task.FromResult($"{_monsterInfo.Name} snuck away. You were too slow!")
+                : _roomGrain.Exit(_monsterInfo)
+                    .ContinueWith(t => $"{_monsterInfo.Name} is dead.");
+        }
+
+        return Task.FromResult(
+            $"{_monsterInfo.Name} is already dead. " +
+            "You were too slow and someone else got to him!");
     }
 }
