365a9e0e22a27eee8e5287a40533cdbd706fd965
ReubenBond <rebond@microsoft.com>
2022-02-13 11:00:13 -0800

    Orleans.Runtime.Abstractions docs

--- src/Orleans.Runtime.Abstractions/Facet/IAttributeToFactoryMapper.cs
+++ src/Orleans.Runtime.Abstractions/Facet/IAttributeToFactoryMapper.cs
@@ -11,6 +11,9 @@ namespace Orleans.Runtime
         /// <summary>
         /// Responsible for mapping a facet metadata to a cachable factory from the parameter and facet metadata.
         /// </summary>
+        /// <param name="parameter">The parameter info.</param>
+        /// <param name="metadata">The metadata.</param>
+        /// <returns>The factory used to create facet instances for a grain.</returns>
         Factory<IGrainContext, object> GetFactory(ParameterInfo parameter, TMetadata metadata);
     }
 }
