95b0b02c8ed1405fa100077891d466613a1ff11b
ReubenBond <rebond@microsoft.com>
2022-02-19 07:27:16 -0800

    Merge Orleans.Streaming.Abstractions into Orleans.Streaming

--- src/Orleans.Streaming.Abstractions/Properties/AssemblyInfo.cs
+++ src/Orleans.Streaming.Abstractions/Properties/AssemblyInfo.cs
@@ -1,10 +1,2 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Orleans.Streaming")]
-[assembly: InternalsVisibleTo("TestInternalGrains")]
-[assembly: InternalsVisibleTo("NonSilo.Tests")]
-[assembly: InternalsVisibleTo("Tester.AzureUtils")]
-[assembly: InternalsVisibleTo("Tester.AdoNet")]
-[assembly: InternalsVisibleTo("TesterInternal")]
-[assembly: InternalsVisibleTo("TestExtensions")]
-[assembly: InternalsVisibleTo("DefaultCluster.Tests")]
