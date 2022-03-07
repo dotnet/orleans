3a99362cc28694d9cb9ea46b72038d44564e4be5
David Pine <david.pine@microsoft.com>
2022-03-07 12:49:01 -0600

    Upgrade TicTacToe game sample

--- samples/TicTacToe/Controllers/ControllerExtensions.cs
+++ samples/TicTacToe/Controllers/ControllerExtensions.cs
@@ -1,20 +1,18 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TicTacToe.Controllers
-{
-    public static class ControllerExtensions
-    {
-        public static Guid GetGuid(this ControllerBase controller)
-        {
-            if (controller.Request.Cookies["playerId"] is { Length: > 0 } idCookie)
-            {
-                return Guid.Parse(idCookie);
-            }
+namespace TicTacToe.Controllers;
 
-            var guid = Guid.NewGuid();
-            controller.Response.Cookies.Append("playerId", guid.ToString());
-            return guid;
+public static class ControllerExtensions
+{
+    public static Guid GetGuid(this ControllerBase controller)
+    {
+        if (controller.Request.Cookies["playerId"] is { Length: > 0 } idCookie)
+        {
+            return Guid.Parse(idCookie);
         }
+
+        var guid = Guid.NewGuid();
+        controller.Response.Cookies.Append("playerId", guid.ToString());
+        return guid;
     }
 }
