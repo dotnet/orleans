efee4ecb1268dac3ee03b9322330af1aac941920
Mark Nickeson <mark@nickeson.com>
2022-05-11 16:39:39 -0600

    Eliminated additional client-side log noise during gateway refresh by limiting to debug level logging instead of information level.

--- src/Orleans.Core/Messaging/GatewayManager.cs
+++ src/Orleans.Core/Messaging/GatewayManager.cs
@@ -312,9 +312,9 @@ namespace Orleans.Messaging
 
                 DateTime prevRefresh = lastRefreshTime;
                 lastRefreshTime = now;
-                if (logger.IsEnabled(LogLevel.Information))
+                if (logger.IsEnabled(LogLevel.Debug))
                 {
-                    logger.Info(ErrorCode.GatewayManager_FoundKnownGateways,
+                    logger.Debug(ErrorCode.GatewayManager_FoundKnownGateways,
                             "Refreshed the live gateway list. Found {0} gateways from gateway list provider: {1}. Picked only known live out of them. Now has {2} live gateways: {3}. Previous refresh time was = {4}",
                             knownGateways.Count,
                             Utils.EnumerableToString(knownGateways),
