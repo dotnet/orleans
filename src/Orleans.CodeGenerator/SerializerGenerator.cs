ec174c76dfe2ce9d2777c89f43b88b18c68598e6
Morten Mertner <morten@mertner.com>
2022-08-31 12:40:16 +0200

    Corrected spelling

--- src/Orleans.CodeGenerator/SerializerGenerator.cs
+++ src/Orleans.CodeGenerator/SerializerGenerator.cs
@@ -340,7 +340,7 @@ namespace Orleans.CodeGenerator
                             .WithInitializer(EqualsValueClause(LiteralExpression(SyntaxKind.NumericLiteralExpression, Literal(0))))))));
             }
 
-            if (type.SupportsPrimaryContstructorParameters)
+            if (type.SupportsPrimaryConstructorParameters)
             {
                 AddSerializationMembers(type, serializerFields, members.Where(m => m.IsPrimaryConstructorParameter), libraryTypes, writerParam, instanceParam, previousFieldIdVar, body);
                 body.Add(ExpressionStatement(InvocationExpression(writerParam.Member("WriteEndBase"), ArgumentList())));
@@ -486,7 +486,7 @@ namespace Orleans.CodeGenerator
 
             AddSerializationCallbacks(type, instanceParam, "OnDeserializing", body);
 
-            if (type.SupportsPrimaryContstructorParameters)
+            if (type.SupportsPrimaryConstructorParameters)
             {
                 body.Add(WhileStatement(LiteralExpression(SyntaxKind.TrueLiteralExpression), Block(GetDeserializerLoopBody(members.Where(m => m.IsPrimaryConstructorParameter)))));
                 body.Add(ExpressionStatement(AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, IdentifierName(idVar.Identifier), LiteralExpression(SyntaxKind.NumericLiteralExpression, Literal(0)))));
