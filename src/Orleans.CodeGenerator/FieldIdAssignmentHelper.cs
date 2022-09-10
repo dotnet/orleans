9cd8390ede2477a47db2a9c91d266642f5a36117
Morten Mertner <morten@mertner.com>
2022-09-10 22:51:21 +0200

    Copied MIT-licensed implementation of XxHash32 and its dependencies to avoid referencing external assembly.

--- src/Orleans.CodeGenerator/FieldIdAssignmentHelper.cs
+++ src/Orleans.CodeGenerator/FieldIdAssignmentHelper.cs
@@ -3,13 +3,12 @@ using System;
 using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.IO.Hashing;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
+using Orleans.CodeGenerator.Hashing;
 using Orleans.CodeGenerator.SyntaxGeneration;
 
 namespace Orleans.CodeGenerator;
---
+++ src/Orleans.CodeGenerator/Hashing/BitOperations.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+
+namespace Orleans.CodeGenerator.Hashing
+{
+    internal static class BitOperations
+    {
+        /// <summary>
+        /// Rotates the specified value left by the specified number of bits.
+        /// Similar in behavior to the x86 instruction ROL.
+        /// </summary>
+        /// <param name="value">The value to rotate.</param>
+        /// <param name="offset">The number of bits to rotate by.
+        /// Any value outside the range [0..31] is treated as congruent mod 32.</param>
+        /// <returns>The rotated value.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint RotateLeft(uint value, int offset) => (value << offset) | (value >> (32 - offset));
+    }
+}
