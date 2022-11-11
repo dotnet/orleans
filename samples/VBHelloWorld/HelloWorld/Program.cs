debf291c80f2a3be9146a2d1b88d8f254984ef4f
David Pine <david.pine@microsoft.com>
2022-11-11 15:12:18 -0600

    Upgrade 'VB Hello World' sample (#8137)

--- samples/VBHelloWorld/HelloWorld/Program.cs
+++ samples/VBHelloWorld/HelloWorld/Program.cs
@@ -3,28 +3,26 @@ using Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Orleans;
-using Orleans.CodeGeneration;
 using Orleans.Hosting;
 using VBGrains;
 
-[assembly: KnownAssembly(typeof(HelloGrain))]
-[assembly: KnownAssembly(typeof(IHelloGrain))]
+[assembly: GenerateCodeForDeclaringAssembly(typeof(HelloGrain))]
+[assembly: GenerateCodeForDeclaringAssembly(typeof(IHelloGrain))]
 
 using var host = new HostBuilder()
     .UseOrleans(builder =>
     {
         builder.UseLocalhostClustering();
     })
+    .UseConsoleLifetime()
     .Build();
 
 await host.StartAsync();
 
 var grainFactory = host.Services.GetRequiredService<IGrainFactory>();
 var friend = grainFactory.GetGrain<IHelloGrain>(0);
-Console.WriteLine("\n\n{0}\n\n", friend.SayHello("Good morning!").Result);
+Console.WriteLine("\n\n{0}\n\n", await friend.SayHello("Good morning!"));
 
 Console.WriteLine("Orleans is running.\nPress Enter to terminate...");
 Console.ReadLine();
 Console.WriteLine("Orleans is stopping...");
-
-await host.StopAsync();
