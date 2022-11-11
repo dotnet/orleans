3beebf2fae6a61d805214f6bd446b54a609ecb51
David Pine <david.pine@microsoft.com>
2022-11-11 11:47:45 -0600

    Address concerns with Id values

--- samples/ChatRoom/ChatRoom.Common/ChatMsg.cs
+++ samples/ChatRoom/ChatRoom.Common/ChatMsg.cs
@@ -5,9 +5,9 @@ public record class ChatMsg(
     string? Author,
     string Text)
 {
-    [Id(2)]
+    [Id(0)]
     public string Author { get; init; } = Author ?? "Alexey";
 
-    [Id(3)]
+    [Id(1)]
     public DateTimeOffset Created { get; init; } = DateTimeOffset.Now;
 }
