3eefecb7f086708f55079554ef36bcc8c9440a9a
David Pine <david.pine@microsoft.com>
2022-11-11 13:26:58 -0600

    Fix issues with F# example

--- samples/FSharpHelloWorld/HelloWorld/Program.cs
+++ samples/FSharpHelloWorld/HelloWorld/Program.cs
@@ -5,20 +5,21 @@ using Microsoft.Extensions.Hosting;
 using Orleans;
 using Orleans.Hosting;
 
-//[assembly: KnownAssembly(typeof(Grains.HelloGrain))]
+[assembly: GenerateCodeForDeclaringAssembly(typeof(Grains.HelloGrain))]
 
 using var host = new HostBuilder()
     .UseOrleans(builder =>
     {
         builder.UseLocalhostClustering();
     })
+    .UseConsoleLifetime()
     .Build();
 
 await host.StartAsync();
 
 var grainFactory = host.Services.GetRequiredService<IGrainFactory>();
 var friend = grainFactory.GetGrain<IHelloGrain>(0);
-Console.WriteLine("\n\n{0}\n\n", friend.SayHello("Good morning!").Result);
+Console.WriteLine("\n\n{0}\n\n", await friend.SayHello("Good morning!"));
 
 Console.WriteLine("Orleans is running.\nPress Enter to terminate...");
 Console.ReadLine();
