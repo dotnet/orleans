4a1c5500ffd89f8ba1301a1cfbe9d2bb825ab315
David Pine <david.pine@microsoft.com>
2022-11-11 14:38:15 -0600

    Upgrade 'Voting' sample

--- samples/Voting/Data/DemoService.cs
+++ samples/Voting/Data/DemoService.cs
@@ -1,42 +1,37 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using Orleans;
 using VotingContract;
 
-namespace Voting.Data
+namespace Voting.Data;
+
+public partial class DemoService
 {
-    public partial class DemoService
+    private readonly IGrainFactory _grainFactory;
+    private readonly ILogger<DemoService> _logger;
+
+    public DemoService(IGrainFactory grainFactory, ILogger<DemoService> logger)
     {
-        private readonly IGrainFactory _grainFactory;
-        private readonly ILogger<DemoService> _logger;
+        _grainFactory = grainFactory;
+        _logger = logger;
+    }
 
-        public DemoService(IGrainFactory grainFactory, ILogger<DemoService> logger)
+    public async Task SimulateVoters(string pollId, int numVotes)
+    {
+        try
         {
-            _grainFactory = grainFactory;
-            _logger = logger;
+            var pollGrain = _grainFactory.GetGrain<IPollGrain>(pollId);
+            var results = await pollGrain.GetCurrentResults();
+            var random = new Random();
+            while (numVotes-- > 0)
+            {
+                var optionId = random.Next(0, results.Options.Count);
+                await pollGrain.AddVote(optionId);
+
+                // Wait some time.
+                await Task.Delay(random.Next(100, 1000));
+            }
         }
-
-        public async Task SimulateVoters(string pollId, int numVotes)
+        catch (Exception ex)
         {
-            try
-            {
-                var pollGrain = _grainFactory.GetGrain<IPollGrain>(pollId);
-                var results = await pollGrain.GetCurrentResults();
-                var random = new Random();
-                while (numVotes-- > 0)
-                {
-                    var optionId = random.Next(0, results.Options.Count);
-                    await pollGrain.AddVote(optionId);
-
-                    // Wait some time.
-                    await Task.Delay(random.Next(100, 1000));
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error while simulating voters");
-            }
+            _logger.LogError(ex, "Error while simulating voters");
         }
     }
 }
--- samples/Voting/Helpers/ThrottlingException.cs
+++ samples/Voting/Helpers/ThrottlingException.cs
@@ -1,13 +1,11 @@
-using System;
 using System.Runtime.Serialization;
 
-namespace VotingContract
+namespace VotingContract;
+
+[GenerateSerializer]
+public class ThrottlingException : Exception
 {
-    [Serializable]
-    public class ThrottlingException : Exception
-    {
-        public ThrottlingException(string message) : base(message) { }
-        public ThrottlingException(string message, Exception innerException) : base(message, innerException) { }
-        protected ThrottlingException(SerializationInfo info, StreamingContext context) : base(info, context) { }
-    }
+    public ThrottlingException(string message) : base(message) { }
+    public ThrottlingException(string message, Exception innerException) : base(message, innerException) { }
+    protected ThrottlingException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 }
