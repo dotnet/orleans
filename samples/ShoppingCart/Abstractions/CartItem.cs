3000c57ea4a03299506f5b956695277865eabe85
David Pine <david.pine@microsoft.com>
2022-11-17 19:09:57 -0600

    Upgrade 'Shopping Cart' sample (#8132)
    * Upgrade 'Shopping Cart' sample
    
    * Fix readme

--- samples/ShoppingCart/Abstractions/CartItem.cs
+++ samples/ShoppingCart/Abstractions/CartItem.cs
@@ -1,9 +1,9 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT License.
 
 namespace Orleans.ShoppingCart.Abstractions;
 
-[Serializable, Immutable]
+[GenerateSerializer, Immutable]
 public sealed record class CartItem(
     string UserId,
     int Quantity,
