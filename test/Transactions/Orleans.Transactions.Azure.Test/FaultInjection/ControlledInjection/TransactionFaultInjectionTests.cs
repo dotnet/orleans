885387ec16ce9aae5bebc0070d543a901cbdf152
Reuben Bond <203839+ReubenBond@users.noreply.github.com>
2022-11-29 17:05:58 -0800

    Add TransactionFaultInjectionTests to Functional runs (#8168)

--- test/Transactions/Orleans.Transactions.Azure.Test/FaultInjection/ControlledInjection/TransactionFaultInjectionTests.cs
+++ test/Transactions/Orleans.Transactions.Azure.Test/FaultInjection/ControlledInjection/TransactionFaultInjectionTests.cs
@@ -1,11 +1,11 @@
-﻿using Orleans.Transactions.AzureStorage.Tests;
+using Orleans.Transactions.AzureStorage.Tests;
 using Orleans.Transactions.TestKit.xUnit;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Orleans.Transactions.Azure.Tests
 {
-    [TestCategory("AzureStorage"), TestCategory("Transactions")]
+    [TestCategory("AzureStorage"), TestCategory("Transactions"), TestCategory("Functional")]
     public class TransactionFaultInjectionTests : ControlledFaultInjectionTransactionTestRunnerxUnit, IClassFixture<ControlledFaultInjectionTestFixture>
     {
         public TransactionFaultInjectionTests(ControlledFaultInjectionTestFixture fixture, ITestOutputHelper output)
