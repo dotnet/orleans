073f101d94ee1fe9d9e23ee0e37d187625312582
Reuben Bond <203839+ReubenBond@users.noreply.github.com>
2023-01-29 08:26:27 -0800

    Update SerializerGenerator.cs

--- src/Orleans.CodeGenerator/SerializerGenerator.cs
+++ src/Orleans.CodeGenerator/SerializerGenerator.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
