53d2eadf4aad7780e256dbd078c2ec61fccb1b70
Thomas Boby <thomas@boby.uk>
2023-03-24 19:53:13 +0000

    Prevent analyzer exception on aliased attribute names (#8355)
    * Add missing case to switch and add tests
    
    * Fail safe instead of stopping build with an exception
    
    * Clearer test name

---
+++ test/Analyzers.Tests/AbstractPropertiesCannotBeSerializedAnalyzerTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Orleans.Analyzers;
+using Xunit;
+
+namespace Analyzers.Tests;
+
+[TestCategory("BVT"), TestCategory("Analyzer")]
+public class AbstractPropertiesCannotBeSerializedAnalyzerTest : DiagnosticAnalyzerTestBase<AbstractPropertiesCannotBeSerializedAnalyzer>
+{
+    async Task VerifyGeneratedDiagnostic(string code)
+    {
+        var (diagnostics, _) = await GetDiagnosticsAsync(code, new string[0]);
+
+        Assert.NotEmpty(diagnostics);
+        Assert.Single(diagnostics);
+
+        var diagnostic = diagnostics.First();
+        Assert.Equal(AbstractPropertiesCannotBeSerializedAnalyzer.RuleId, diagnostic.Id);
+        Assert.Equal(DiagnosticSeverity.Error, diagnostic.Severity);
+    }
+
+    [Fact]
+    public Task AliasedAttribute()
+        => VerifyGeneratedDiagnostic("""
+using alias = Orleans;
+[alias::GenerateSerializer]
+public abstract class D { [alias::Id(0)] public abstract int F { get; set; } }
+""");
+
+    [Fact]
+    public Task GloballyQualifiedAttribute()
+        => VerifyGeneratedDiagnostic("""
+[global::Orleans.GenerateSerializer]
+public abstract class D { [global::Orleans.Id(0)] public abstract int F { get; set; } }
+""");
+
+    [Fact]
+    public Task SimpleAttribute()
+        => VerifyGeneratedDiagnostic("""
+[GenerateSerializer] public abstract class D { [Id(0)] public abstract int F { get; set; } }
+""");
+
+    [Fact]
+    public Task UnrelatedGenericAttribute()
+        => VerifyGeneratedDiagnostic("""
+public class GenericAttribute<T> : Attribute { }
+[GenerateSerializer]
+public abstract class D {
+    [GenericAttribute<bool>] [Id(0)] public abstract int F { get; set; }
+}
+""");
+}
