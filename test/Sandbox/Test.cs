dcb7227923f219d61a671f9a12a50f1061d43fde
Ledjon Behluli <Ledjon@notiphy.io>
2023-09-19 23:05:53 +0200

    Code fixer working

--- test/Sandbox/Test.cs
+++ test/Sandbox/Test.cs
@@ -2,33 +2,13 @@ using Orleans;
 
 namespace MyNs;
 
-[GenerateSerializer]
-public class TestClass
-{
-    [Id(0)]
-    public string A { get; set; } = "";
-}
-
-[GenerateSerializer]
-public struct TestStruct
-{
-    public string A;
-}
-
-[GenerateSerializer]
-public record TestRecord { }
-
-[GenerateSerializer]
-public record struct TestRecordStruct { }
-
-[Alias("TestGrain")]
+[Alias("ITestGrain")]
 public interface ITestGrain : IGrainWithStringKey
 {
     [Alias("Void")]
     Task Void();
-}
+    [Alias("Int")]
+    Task<int> Int();
 
-public class MyClass
-{
-
+    static Task<long> StaticLong() => Task.FromResult(0L);
 }
