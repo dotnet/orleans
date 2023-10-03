88b625de8f4c596146e502297ee70f7583929896
Gutemberg Ribeiro <gutemberg@outlook.com>
2023-10-03 18:50:02 -0300

    EFCore Reminders

---
+++ src/EFCore/Orleans.Reminders.EntityFrameworkCore/Data/ReminderDbContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Orleans.Reminders.EntityFrameworkCore.Data;
+
+public class ReminderDbContext : DbContext
+{
+    public DbSet<ReminderRecord> Reminders { get; set; } = default!;
+
+    public ReminderDbContext(DbContextOptions<ReminderDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ReminderRecord>(c =>
+        {
+            c.HasKey(p => new {p.ServiceId, p.GrainId, p.Name});
+            c.Property(p => p.ServiceId).HasMaxLength(150).IsRequired();
+            c.Property(p => p.GrainId).HasMaxLength(150).IsRequired();
+            c.Property(p => p.Name).HasMaxLength(150).IsRequired();
+            c.Property(p => p.StartAt).IsRequired();
+            c.Property(p => p.Period).IsRequired();
+            c.Property(p => p.GrainHash).IsRequired();
+            c.Property(p => p.ETag).IsRequired().IsRowVersion();
+        });
+    }
+}
