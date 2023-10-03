9e9374ae4c177f2febf956897a0d2c8a0ea4397e
Gutemberg Ribeiro <gutemberg@outlook.com>
2023-10-03 00:16:24 -0300

    EFCore Cluster and Storage providers

---
+++ src/EFCore/Orleans.Clustering.EntityFrameworkCore/EFGatewayListProvider.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Orleans.Runtime;
+using Orleans.Messaging;
+using Orleans.Configuration;
+using Orleans.Clustering.EntityFrameworkCore.Data;
+
+namespace Orleans.Clustering.EntityFrameworkCore;
+
+internal class EFGatewayListProvider<TDbContext> : IGatewayListProvider where TDbContext : ClusterDbContext
+{
+    private readonly ILogger _logger;
+    private readonly string _clusterId;
+    private readonly IDbContextFactory<TDbContext> _dbContextFactory;
+
+    public TimeSpan MaxStaleness { get; }
+
+    public bool IsUpdatable => true;
+
+    public EFGatewayListProvider(
+        ILoggerFactory loggerFactory,
+        IOptions<ClusterOptions> clusterOptions,
+        IOptions<GatewayOptions> gatewayOptions,
+        IDbContextFactory<TDbContext> dbContextFactory)
+    {
+        this._logger = loggerFactory.CreateLogger<EFMembershipTable<TDbContext>>();
+        this._clusterId = clusterOptions.Value.ClusterId;
+        this._dbContextFactory = dbContextFactory;
+        this.MaxStaleness = gatewayOptions.Value.GatewayListRefreshPeriod;
+    }
+
+    public Task InitializeGatewayListProvider()
+    {
+        if (this._logger.IsEnabled(LogLevel.Debug))
+        {
+            this._logger.LogDebug("EFCore Gateway list provider initialized!");
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public async Task<IList<Uri>> GetGateways()
+    {
+        try
+        {
+            var ctx = this._dbContextFactory.CreateDbContext();
+
+            var records = await ctx.Silos.Where(r =>
+                    r.ClusterId == this._clusterId &&
+                    r.Status == SiloStatus.Active &&
+                    r.ProxyPort.HasValue && r.ProxyPort.Value != 0)
+                .ToArrayAsync().ConfigureAwait(false);
+
+            return records.Select(ConvertToGatewayUri).ToArray();
+        }
+        catch (Exception ex)
+        {
+            this._logger.LogError(ex, "Error reading gateway list");
+            WrappedException.CreateAndRethrow(ex);
+            throw;
+        }
+    }
+
+    private static Uri ConvertToGatewayUri(SiloRecord record) => SiloAddress.New(new IPEndPoint(IPAddress.Parse(record.Address), record.ProxyPort!.Value), record.Generation).ToGatewayUri();
+}
---
+++ src/EFCore/Orleans.Persistence.EntityFrameworkCore/EFGrainStorage.cs
@@ -0,0 +1,198 @@
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans.Runtime;
+using Orleans.Storage;
+using Orleans.Configuration;
+using Orleans.Persistence.EntityFrameworkCore.Data;
+
+namespace Orleans.Persistence.EntityFrameworkCore;
+
+internal class EFGrainStorage<TDbContext> : IGrainStorage, ILifecycleParticipant<ISiloLifecycle> where TDbContext : GrainStateDbContext
+{
+    private readonly ILogger _logger;
+    private readonly string _name;
+    private readonly string _serviceId;
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IDbContextFactory<TDbContext> _dbContextFactory;
+
+    public EFGrainStorage(
+        string name,
+        ILoggerFactory loggerFactory,
+        IOptions<ClusterOptions> clusterOptions,
+        IServiceProvider serviceProvider)
+    {
+        this._serviceProvider = serviceProvider;
+        this._name = name;
+        this._serviceId = clusterOptions.Value.ServiceId;
+        this._logger = loggerFactory.CreateLogger<EFGrainStorage<TDbContext>>();
+
+        var dbContextFactory = this._serviceProvider.GetService<IDbContextFactory<TDbContext>>();
+        this._dbContextFactory = dbContextFactory ?? throw new OrleansConfigurationException("There are no GrainStateDbContext registered");
+    }
+
+    public async Task ReadStateAsync<T>(string stateName, GrainId grainId, IGrainState<T> grainState)
+    {
+        var grainType = grainId.Type.ToString()!;
+
+        var id = grainId.Key.ToString()!;
+
+        try
+        {
+            var ctx = this._dbContextFactory.CreateDbContext();
+
+            var record = await ctx.GrainState.AsNoTracking().SingleOrDefaultAsync(r =>
+                    r.ServiceId == this._serviceId &&
+                    r.GrainType == grainType &&
+                    r.StateType == stateName &&
+                    r.GrainId == id)
+                .ConfigureAwait(false);
+
+            if (record is null)
+            {
+                grainState.State = ActivatorUtilities.CreateInstance<T>(_serviceProvider);
+                grainState.RecordExists = false;
+                return;
+            }
+
+            grainState.State = !string.IsNullOrEmpty(record.Data) ? JsonSerializer.Deserialize<T>(record.Data)! : Activator.CreateInstance<T>();
+
+            grainState.RecordExists = true;
+            grainState.ETag = BitConverter.ToUInt64(record.ETag).ToString();
+        }
+        catch (Exception ex)
+        {
+            this._logger.LogError(ex,
+                "Unable to read state. State: {State} | Grain: {GrainType} | GrainId: {GrainId}",
+                stateName, grainType, id);
+            throw;
+        }
+    }
+
+    public async Task WriteStateAsync<T>(string stateName, GrainId grainId, IGrainState<T> grainState)
+    {
+        var grainType = grainId.Type.ToString()!;
+
+        var id = grainId.Key.ToString()!;
+
+        var ctx = this._dbContextFactory.CreateDbContext();
+
+        var record = new GrainStateRecord
+        {
+            ServiceId = this._serviceId,
+            GrainType = grainType,
+            StateType = stateName,
+            GrainId = id,
+            Data = JsonSerializer.Serialize(grainState.State),
+        };
+
+        if (grainState.RecordExists)
+        {
+            record.ETag = BitConverter.GetBytes(ulong.Parse(grainState.ETag));
+            ctx.GrainState.Update(record);
+        }
+        else
+        {
+            ctx.GrainState.Add(record);
+        }
+
+        try
+        {
+            await ctx.SaveChangesAsync().ConfigureAwait(false);
+            grainState.ETag = BitConverter.ToUInt64(record.ETag).ToString();
+            grainState.RecordExists = true;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            var found = await ctx.GrainState.AsNoTracking().SingleOrDefaultAsync(r => r.StateType == grainType && r.GrainId == id).ConfigureAwait(false);
+            var foundETag = found is not null ? found.ETag.ToString() : "<null>";
+
+            var isEx = new InconsistentStateException(
+                $"Inconsistent state. Operation: Write | State: {stateName} | Grain: {grainType} | GrainId: {id}",
+                foundETag, grainState.ETag, ex);
+
+            this._logger.LogError(isEx,
+                "Inconsistent state. Operation: {Operation} | State: {State} | Grain: {GrainType} | GrainId: {GrainId} | Expected ETag: {ExpectedETag} | Actual ETag: {ActualETag} ",
+                "Write", stateName, grainType, id, grainState.ETag, found?.ETag);
+
+            throw isEx;
+        }
+        catch (Exception ex)
+        {
+            this._logger.LogError(ex,
+                "Unable to write grain state. Operation: {Operation} | State: {State} | Grain: {GrainType} | GrainId: {GrainId}",
+                "Write", stateName, grainType, id);
+            throw;
+        }
+    }
+
+    public async Task ClearStateAsync<T>(string stateName, GrainId grainId, IGrainState<T> grainState)
+    {
+        var id = grainId.Key.ToString()!;
+
+        var grainType = grainId.Type.ToString()!;
+
+        if (!grainState.RecordExists || string.IsNullOrWhiteSpace(grainState.ETag))
+        {
+            grainState.ETag = null;
+            grainState.State = Activator.CreateInstance<T>();
+            return;
+        }
+
+        var ctx = this._dbContextFactory.CreateDbContext();
+
+        try
+        {
+            var record = await ctx.GrainState
+                .Where(r =>
+                    r.ServiceId == this._serviceId &&
+                    r.StateType == stateName &&
+                    r.GrainType == grainType &&
+                    r.GrainId == id)
+                .SingleOrDefaultAsync()
+                .ConfigureAwait(false);
+
+            if (record is null)
+            {
+                throw new DbUpdateConcurrencyException();
+            }
+
+            ctx.GrainState.Remove(record);
+            await ctx.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            var found = await ctx.GrainState.AsNoTracking()
+                .SingleOrDefaultAsync(r => r.StateType == stateName && r.GrainId == id).ConfigureAwait(false);
+
+            var foundETag = found is not null ? found.ETag.ToString() : "<null>";
+
+            var isEx = new InconsistentStateException(
+                $"Inconsistent state. Operation: Clear | State: {stateName} | GrainType: {grainType} | GrainId: {id}",
+                foundETag, grainState.ETag, ex);
+
+            this._logger.LogError(isEx,
+                "Inconsistent state. Operation: {Operation} | State: {State} | GrainType: {GrainType} | GrainId: {GrainId} | Expected ETag: {ExpectedETag} | Actual ETag: {ActualETag} ",
+                "Clear", stateName, grainType, id, grainState.ETag, found?.ETag);
+
+            throw isEx;
+        }
+        catch (Exception ex)
+        {
+            this._logger.LogError(ex, "Unable to write grain state. State: {State} | GrainType: {GrainType} GrainId: {GrainId}", stateName, grainType, grainId);
+            throw;
+        }
+
+        grainState.ETag = null;
+        grainState.State = Activator.CreateInstance<T>();
+        grainState.RecordExists = false;
+    }
+
+    public void Participate(ISiloLifecycle lifecycle) =>
+        this._logger.LogInformation("EFCore Grain Storage {Storage} initialized!", this._name);
+}
