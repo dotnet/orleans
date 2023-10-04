0a85805672c3852e3386e832b27c52d69a61f9b2
Gutemberg Ribeiro <gutemberg@outlook.com>
2023-10-03 21:45:09 -0300

    EFCore Hosting extensions

---
+++ src/EFCore/Orleans.Clustering.EntityFrameworkCore/EFClusteringExtensions.cs
@@ -0,0 +1,100 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans.Hosting;
+using Orleans.Messaging;
+using Orleans.Clustering.EntityFrameworkCore;
+using Orleans.Clustering.EntityFrameworkCore.Data;
+
+namespace Orleans.Clustering;
+
+public static class EFClusteringExtensions
+{
+    /// <summary>
+    /// Configures the silo to use Entity Framework Core for clustering.
+    /// </summary>
+    /// <param name="builder">
+    /// The silo builder.
+    /// </param>
+    /// <param name="configureDatabase">
+    /// The database configuration delegate.
+    /// </param>
+    /// <returns>
+    /// The provided <see cref="ISiloBuilder"/>.
+    /// </returns>
+    public static ISiloBuilder UseEntityFrameworkCoreClustering<TDbContext>(
+        this ISiloBuilder builder,
+        Action<DbContextOptionsBuilder> configureDatabase) where TDbContext : ClusterDbContext
+    {
+        return builder
+            .ConfigureServices(services =>
+            {
+                services.AddPooledDbContextFactory<TDbContext>(configureDatabase);
+            })
+            .UseEntityFrameworkCoreClustering<TDbContext>();
+    }
+
+    /// <summary>
+    /// Configures the silo to use Entity Framework Core for clustering.
+    /// This overload expects a <see cref="ClusterDbContext"/> to be registered already.
+    /// </summary>
+    /// <param name="builder">
+    /// The silo builder.
+    /// </param>
+    /// <returns>
+    /// The provided <see cref="ISiloBuilder"/>.
+    /// </returns>
+    public static ISiloBuilder UseEntityFrameworkCoreClustering<TDbContext>(
+        this ISiloBuilder builder) where TDbContext : ClusterDbContext
+    {
+        return builder
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<IMembershipTable, EFMembershipTable<TDbContext>>();
+            });
+    }
+
+    /// <summary>
+    /// Configures the silo to use Entity Framework Core for clustering.
+    /// </summary>
+    /// <param name="builder">
+    /// The silo builder.
+    /// </param>
+    /// <param name="configureDatabase">
+    /// The database configuration delegate.
+    /// </param>
+    /// <returns>
+    /// The provided <see cref="ISiloBuilder"/>.
+    /// </returns>
+    public static IClientBuilder UseEntityFrameworkCoreClustering<TDbContext>(
+        this IClientBuilder builder,
+        Action<DbContextOptionsBuilder> configureDatabase) where TDbContext : ClusterDbContext
+    {
+        return builder
+            .ConfigureServices(services =>
+            {
+                services.AddPooledDbContextFactory<TDbContext>(configureDatabase);
+            })
+            .UseEntityFrameworkCoreClustering<TDbContext>();
+    }
+
+    /// <summary>
+    /// Configures the silo to use Entity Framework Core for clustering.
+    /// This overload expects a <see cref="ClusterDbContext"/> to be registered already.
+    /// </summary>
+    /// <param name="builder">
+    /// The silo builder.
+    /// </param>
+    /// <returns>
+    /// The provided <see cref="ISiloBuilder"/>.
+    /// </returns>
+    public static IClientBuilder UseEntityFrameworkCoreClustering<TDbContext>(
+        this IClientBuilder builder) where TDbContext : ClusterDbContext
+    {
+        return builder
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<IGatewayListProvider, EFGatewayListProvider<TDbContext>>();
+            });
+    }
+}
