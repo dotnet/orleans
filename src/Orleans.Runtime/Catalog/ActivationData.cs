9134522c5ad9f8a12555ff7885d3ba36e1e8c78c
b.nichvolodov <b.nichvolodov@vk.team>
2023-11-09 22:21:49 +0300

    simplify code grain delay activation

--- src/Orleans.Runtime/Catalog/ActivationData.cs
+++ src/Orleans.Runtime/Catalog/ActivationData.cs
@@ -396,15 +396,15 @@ namespace Orleans.Runtime
 
         public void DelayDeactivation(TimeSpan timespan)
         {
-            if (timespan <= TimeSpan.Zero && timespan != Timeout.InfiniteTimeSpan)
-            {
-                // reset any current keepAliveUntil
-                ResetKeepAliveRequest();
-            }
-            else if (timespan == TimeSpan.MaxValue || timespan == Timeout.InfiniteTimeSpan)
+            if (timespan == TimeSpan.MaxValue || timespan == Timeout.InfiniteTimeSpan)
             {
                 // otherwise creates negative time.
                 KeepAliveUntil = DateTime.MaxValue;
+            }
+            else if (timespan <= TimeSpan.Zero)
+            {
+                // reset any current keepAliveUntil
+                ResetKeepAliveRequest();
             }
             else
             {
