ca777e77e3b7ef5ca99cb716a511b29b64d1e6e1
Reuben Bond <reuben.bond@gmail.com>
2023-12-14 10:03:09 -0800

    Fix build

--- src/Orleans.Core/GrainReferences/GrainReferenceActivator.cs
+++ src/Orleans.Core/GrainReferences/GrainReferenceActivator.cs
@@ -122,7 +122,8 @@ namespace Orleans.GrainReferences
             CodecProvider codecProvider,
             CopyContextPool copyContextPool,
             IServiceProvider serviceProvider,
-            GrainFactory grainFactory,
+            GrainInterfaceTypeToGrainTypeResolver grainTypeResolver,
+            IClusterManifestProvider clusterManifestProvider,
             TimeProvider timeProvider)
         {
             _versionManifest = manifest;
@@ -130,7 +131,7 @@ namespace Orleans.GrainReferences
             _codecProvider = codecProvider;
             _copyContextPool = copyContextPool;
             _serviceProvider = serviceProvider;
-            _stubRuntime = new StubGrainReferenceRuntime(runtimeClient, grainFactory, timeProvider);
+            _stubRuntime = new StubGrainReferenceRuntime(runtimeClient, grainTypeResolver, clusterManifestProvider, timeProvider);
         }
 
         public bool TryGet(GrainType grainType, GrainInterfaceType interfaceType, out IGrainReferenceActivator activator)
@@ -192,13 +193,12 @@ namespace Orleans.GrainReferences
 
         private sealed class StubGrainReferenceRuntime(
             IRuntimeClient runtimeClient,
-            GrainFactory grainFactory,
             GrainInterfaceTypeToGrainTypeResolver interfaceTypeToGrainTypeResolver,
             IClusterManifestProvider clusterManifestProvider,
             TimeProvider timeProvider) : IGrainReferenceRuntime
         {
             private readonly IRuntimeClient _runtimeClient = runtimeClient;
-            private readonly GrainFactory _grainFactory = grainFactory;
+            private readonly IGrainFactory _grainFactory = runtimeClient.InternalGrainFactory;
             private readonly GrainInterfaceTypeToGrainTypeResolver _interfaceTypeToGrainTypeResolver = interfaceTypeToGrainTypeResolver;
             private readonly IClusterManifestProvider _clusterManifestProvider = clusterManifestProvider;
             private readonly TimeProvider _timeProvider = timeProvider;
