58e834874b81fb8aacfab0dbcb84a416723f5ca1
Ledjon Behluli <Ledjon@notiphy.io>
2023-12-19 00:16:46 +0100

    some more usings removed

--- src/Orleans.CodeGenerator/ProxyGenerator.cs
+++ src/Orleans.CodeGenerator/ProxyGenerator.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
