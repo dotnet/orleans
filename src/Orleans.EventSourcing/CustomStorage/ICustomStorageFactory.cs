589b752197dc51a05d0d5b7039677dfe5bbf2683
Sam Fields <smfields896@gmail.com>
2023-12-26 22:55:58 -0500

    feat(event-sourcing): add injectable custom storage

---
+++ src/Orleans.EventSourcing/CustomStorage/ICustomStorageFactory.cs
@@ -0,0 +1,11 @@
+﻿using Orleans.EventSourcing.CustomStorage;
+using Orleans.Runtime;
+
+namespace OrleansEventSourcing.CustomStorage;
+
+public interface ICustomStorageFactory
+{
+    public ICustomStorageInterface<TState, TDelta> CreateCustomStorage<TState, TDelta>(GrainId grainId)
+        where TState : class, new()
+        where TDelta : class;
+}
