71d93bd7b1c720d3a221702005485c5e98135c37
Manuel Menegazzo <65919883+m3nax@users.noreply.github.com>
2023-12-29 22:24:37 +0100

    Update SQLServer-Persistence.sql

--- src/AdoNet/Orleans.Persistence.AdoNet/SQLServer-Persistence.sql
+++ src/AdoNet/Orleans.Persistence.AdoNet/SQLServer-Persistence.sql
@@ -43,6 +43,7 @@
 --
 -- 7. In the storage operations queries the columns need to be in the exact same order
 -- since the storage table operations support optionally streaming.
+IF OBJECT_ID(N'[OrleansStorage]', 'U') IS NULL
 CREATE TABLE OrleansStorage
 (
     -- These are for the book keeping. Orleans calculates
@@ -92,8 +93,7 @@ BEGIN
 END
 
 INSERT INTO OrleansQuery(QueryKey, QueryText)
-VALUES
-(
+SELECT
     'WriteToStorageKey',
     '-- When Orleans is running in normal, non-split state, there will
     -- be only one grain with the given ID and type combination only. This
@@ -190,11 +190,15 @@ VALUES
 
     SELECT @NewGrainStateVersion AS NewGrainStateVersion;
     COMMIT TRANSACTION;'
+WHERE NOT EXISTS 
+( 
+    SELECT 1 
+	FROM OrleansQuery oqt
+    WHERE oqt.[QueryKey] = 'WriteToStorageKey'
 );
 
 INSERT INTO OrleansQuery(QueryKey, QueryText)
-VALUES
-(
+SELECT
     'ClearStorageKey',
     'BEGIN TRANSACTION;
     SET XACT_ABORT, NOCOUNT ON;
@@ -218,11 +222,15 @@ VALUES
 
     SELECT @NewGrainStateVersion;
     COMMIT TRANSACTION;'
+WHERE NOT EXISTS 
+( 
+    SELECT 1 
+	FROM OrleansQuery oqt
+    WHERE oqt.[QueryKey] = 'ClearStorageKey'
 );
 
 INSERT INTO OrleansQuery(QueryKey, QueryText)
-VALUES
-(
+SELECT
     'ReadFromStorageKey',
     '-- The application code will deserialize the relevant result. Not that the query optimizer
     -- estimates the result of rows based on its knowledge on the index. It does not know there
@@ -245,4 +253,9 @@ VALUES
         AND ((@GrainIdExtensionString IS NOT NULL AND GrainIdExtensionString IS NOT NULL AND GrainIdExtensionString = @GrainIdExtensionString) OR @GrainIdExtensionString IS NULL AND GrainIdExtensionString IS NULL)
         AND ServiceId = @ServiceId AND @ServiceId IS NOT NULL
         OPTION(FAST 1, OPTIMIZE FOR(@GrainIdHash UNKNOWN, @GrainTypeHash UNKNOWN));'
+WHERE NOT EXISTS 
+( 
+    SELECT 1 
+	FROM OrleansQuery oqt
+    WHERE oqt.[QueryKey] = 'ReadFromStorageKey'
 );
