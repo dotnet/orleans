524c8a3f2ae9bf449e249468564f57c063c36508
Sam Fields <smfields896@gmail.com>
2024-01-02 22:13:42 -0500

    refactor(event-sourcing): remove custom storage type constraints

--- src/Orleans.EventSourcing/CustomStorage/ICustomStorageFactory.cs
+++ src/Orleans.EventSourcing/CustomStorage/ICustomStorageFactory.cs
@@ -5,7 +5,5 @@ namespace OrleansEventSourcing.CustomStorage;
 
 public interface ICustomStorageFactory
 {
-    public ICustomStorageInterface<TState, TDelta> CreateCustomStorage<TState, TDelta>(GrainId grainId)
-        where TState : class, new()
-        where TDelta : class;
+    public ICustomStorageInterface<TState, TDelta> CreateCustomStorage<TState, TDelta>(GrainId grainId);
 }
