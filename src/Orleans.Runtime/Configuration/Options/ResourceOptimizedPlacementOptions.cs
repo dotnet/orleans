0a7237ca302431c9d3c64671c0c17ffd6367965e
Ledjon Behluli <Ledjon@notiphy.io>
2024-01-11 00:59:42 +0100

    fixing some validations

--- src/Orleans.Runtime/Configuration/Options/ResourceOptimizedPlacementOptions.cs
+++ src/Orleans.Runtime/Configuration/Options/ResourceOptimizedPlacementOptions.cs
@@ -11,32 +11,32 @@ public sealed class ResourceOptimizedPlacementOptions
     /// <summary>
     /// The importance of the CPU utilization by the silo.
     /// </summary>
-    /// <remarks><i>Valid range of values are [0-1]</i></remarks>
+    /// <remarks><i>Valid range of values are [0.00-1.00]</i></remarks>
     public float CpuUsageWeight { get; set; } = DEFAULT_CPU_USAGE_WEIGHT;
     /// <summary>
     /// The default value of <see cref="CpuUsageWeight"/>.
     /// </summary>
-    public const float DEFAULT_CPU_USAGE_WEIGHT = 0.4f;
+    public const float DEFAULT_CPU_USAGE_WEIGHT = 0.34f;
 
     /// <summary>
     /// The importance of the memory utilization by the silo.
     /// </summary>
-    /// <remarks><i>Valid range of values are [0-1]</i></remarks>
+    /// <remarks><i>Valid range of values are [0.00-1.00]</i></remarks>
     public float MemoryUsageWeight { get; set; } = DEFAULT_MEMORY_USAGE_WEIGHT;
     /// <summary>
     /// The default value of <see cref="MemoryUsageWeight"/>.
     /// </summary>
-    public const float DEFAULT_MEMORY_USAGE_WEIGHT = 0.4f;
+    public const float DEFAULT_MEMORY_USAGE_WEIGHT = 0.33f;
 
     /// <summary>
     /// The importance of the available memory to the silo.
     /// </summary>
-    /// <remarks><i>Valid range of values are [0-1]</i></remarks>
+    /// <remarks><i>Valid range of values are [0.00-1.00]</i></remarks>
     public float AvailableMemoryWeight { get; set; } = DEFAULT_AVAILABLE_MEMORY_WEIGHT;
     /// <summary>
     /// The default value of <see cref="AvailableMemoryWeight"/>.
     /// </summary>
-    public const float DEFAULT_AVAILABLE_MEMORY_WEIGHT = 0.1f;
+    public const float DEFAULT_AVAILABLE_MEMORY_WEIGHT = 0.33f;
 
     /// <summary>
     /// The specified margin for which: if two silos (one of them being the local to the current pending activation), have a utilization score that should be considered "the same" within this margin.
@@ -45,7 +45,7 @@ public sealed class ResourceOptimizedPlacementOptions
     /// <item>When this value is 100, then the policy will always favor the local silo, regardless of its relative utilization score. This policy essentially becomes equivalent to <see cref="PreferLocalPlacement"/>.</item>
     /// </list>
     /// </summary>
-    /// <remarks><i>Valid range of values are [0-1]</i></remarks>
+    /// <remarks><i>Valid range of values are [0.00-1.00]</i></remarks>
     public float LocalSiloPreferenceMargin { get; set; }
     /// <summary>
     /// The default value of <see cref="LocalSiloPreferenceMargin"/>.
@@ -80,7 +80,9 @@ internal sealed class ResourceOptimizedPlacementOptionsValidator
             ThrowOutOfRange(nameof(ResourceOptimizedPlacementOptions.LocalSiloPreferenceMargin));
         }
 
-        if (_options.CpuUsageWeight + _options.MemoryUsageWeight + _options.AvailableMemoryWeight != 1f)
+        if (Truncate(_options.CpuUsageWeight) +
+            Truncate(_options.MemoryUsageWeight) +
+            Truncate(_options.AvailableMemoryWeight) != 1)
         {
             throw new OrleansConfigurationException(
                 $"The total sum across all the weights of {nameof(ResourceOptimizedPlacementOptions)} must equal 1");
@@ -88,5 +90,8 @@ internal sealed class ResourceOptimizedPlacementOptionsValidator
 
         static void ThrowOutOfRange(string propertyName)
             => throw new OrleansConfigurationException($"{propertyName} must be inclusive between [0-1]");
+
+        static double Truncate(double value)
+            => Math.Floor(value * 100) / 100;
     }
 }
