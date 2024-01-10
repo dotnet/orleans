621ccf4dbfd65cdcf8ce7fb8d53305208f83cd57
Ledjon Behluli <Ledjon@notiphy.io>
2024-01-11 00:42:37 +0100

    wip

--- src/Orleans.Core.Abstractions/Placement/ResourceOptimizedPlacement.cs
+++ src/Orleans.Core.Abstractions/Placement/ResourceOptimizedPlacement.cs
@@ -11,16 +11,8 @@ namespace Orleans.Runtime;
 /// In addition to normalization, an online adaptive filter provides a smoothing effect 
 /// (filters out high frequency components) and avoids rapid signal drops by transforming it into a polynomial alike decay process.
 /// This contributes to avoiding resource saturation on the silos and especially newly joined silos.</para>
-/// <para>Details of the properties used to make the placement decisions and their default values are given below:
-/// <list type="number">
-/// <item><b>Cpu usage:</b> The default weight (0.3), indicates that CPU usage is important but not the sole determinant in placement decisions.</item>
-/// <item><b>Available memory:</b> The default weight (0.4), emphasizes the importance of nodes with ample available memory.</item>
-/// <item><b>Memory usage:</b> Is important for understanding the current load on a node. The default weight (0.2), ensures consideration without making it overly influential.</item>
-/// <item><b>Total physical memory:</b> Represents the overall capacity of a node. The default weight (0.1), contributes to a more long-term resource planning perspective.</item>
-/// </list></para>
 /// <para><i>This placement strategy is configured by adding the <see cref="Placement.ResourceOptimizedPlacementAttribute"/> attribute to a grain.</i></para>
 /// </remarks>
-[Immutable, GenerateSerializer, SuppressReferenceTracking]
 internal sealed class ResourceOptimizedPlacement : PlacementStrategy
 {
     internal static readonly ResourceOptimizedPlacement Singleton = new();
--- src/Orleans.Runtime/Configuration/Options/ResourceOptimizedPlacementOptions.cs
+++ src/Orleans.Runtime/Configuration/Options/ResourceOptimizedPlacementOptions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Extensions.Options;
 
 namespace Orleans.Runtime.Configuration.Options;
@@ -10,51 +11,41 @@ public sealed class ResourceOptimizedPlacementOptions
     /// <summary>
     /// The importance of the CPU utilization by the silo.
     /// </summary>
-    /// <remarks><i>Expressed as percentage.</i></remarks>
+    /// <remarks><i>Valid range of values are [0-1]</i></remarks>
     public float CpuUsageWeight { get; set; } = DEFAULT_CPU_USAGE_WEIGHT;
     /// <summary>
     /// The default value of <see cref="CpuUsageWeight"/>.
     /// </summary>
-    public const float DEFAULT_CPU_USAGE_WEIGHT = 0.3f;
+    public const float DEFAULT_CPU_USAGE_WEIGHT = 0.4f;
 
     /// <summary>
-    /// The importance of the amount of memory available to the silo.
+    /// The importance of the memory utilization by the silo.
     /// </summary>
-    /// <remarks><i>Expressed as percentage.</i></remarks>
-    public float AvailableMemoryWeight { get; set; } = DEFAULT_AVAILABLE_MEMORY_WEIGHT;
-    /// <summary>
-    /// The default value of <see cref="AvailableMemoryWeight"/>.
-    /// </summary>
-    public const float DEFAULT_AVAILABLE_MEMORY_WEIGHT = 0.4f;
-
-    /// <summary>
-    /// The importance of the used memory by the silo.
-    /// </summary>
-    /// <remarks><i>Expressed as percentage.</i></remarks>
+    /// <remarks><i>Valid range of values are [0-1]</i></remarks>
     public float MemoryUsageWeight { get; set; } = DEFAULT_MEMORY_USAGE_WEIGHT;
     /// <summary>
     /// The default value of <see cref="MemoryUsageWeight"/>.
     /// </summary>
-    public const float DEFAULT_MEMORY_USAGE_WEIGHT = 0.2f;
+    public const float DEFAULT_MEMORY_USAGE_WEIGHT = 0.4f;
 
     /// <summary>
-    /// The importance of the total physical memory of the silo.
+    /// The importance of the available memory to the silo.
     /// </summary>
-    /// <remarks><i>Expressed as percentage.</i></remarks>
-    public float TotalPhysicalMemoryWeight { get; set; } = DEFAULT_TOTAL_PHYSICAL_MEMORY_WEIGHT;
+    /// <remarks><i>Valid range of values are [0-1]</i></remarks>
+    public float AvailableMemoryWeight { get; set; } = DEFAULT_AVAILABLE_MEMORY_WEIGHT;
     /// <summary>
-    /// The default value of <see cref="MemoryUsageWeight"/>.
+    /// The default value of <see cref="AvailableMemoryWeight"/>.
     /// </summary>
-    public const float DEFAULT_TOTAL_PHYSICAL_MEMORY_WEIGHT = 0.1f;
+    public const float DEFAULT_AVAILABLE_MEMORY_WEIGHT = 0.1f;
 
     /// <summary>
-    /// The specified margin for which: if two silos (one of them being the local silo), have a utilization score that should be considered "the same" within this margin.
+    /// The specified margin for which: if two silos (one of them being the local to the current pending activation), have a utilization score that should be considered "the same" within this margin.
     /// <list type="bullet">
     /// <item>When this value is 0, then the policy will always favor the silo with the higher utilization score, even if that silo is remote to the current pending activation.</item>
     /// <item>When this value is 100, then the policy will always favor the local silo, regardless of its relative utilization score. This policy essentially becomes equivalent to <see cref="PreferLocalPlacement"/>.</item>
     /// </list>
     /// </summary>
-    /// <remarks><i>Expressed as percentage.</i></remarks>
+    /// <remarks><i>Valid range of values are [0-1]</i></remarks>
     public float LocalSiloPreferenceMargin { get; set; }
     /// <summary>
     /// The default value of <see cref="LocalSiloPreferenceMargin"/>.
@@ -69,13 +60,33 @@ internal sealed class ResourceOptimizedPlacementOptionsValidator
 
     public void ValidateConfiguration()
     {
-        if (_options.CpuUsageWeight +
-            _options.MemoryUsageWeight +
-            _options.AvailableMemoryWeight +
-            _options.TotalPhysicalMemoryWeight != 1.0f)
+        if (_options.CpuUsageWeight < 0f || _options.CpuUsageWeight > 1f)
+        {
+            ThrowOutOfRange(nameof(ResourceOptimizedPlacementOptions.CpuUsageWeight));
+        }
+
+        if (_options.MemoryUsageWeight < 0f || _options.MemoryUsageWeight > 1f)
+        {
+            ThrowOutOfRange(nameof(ResourceOptimizedPlacementOptions.MemoryUsageWeight));
+        }
+
+        if (_options.AvailableMemoryWeight < 0f || _options.AvailableMemoryWeight > 1f)
+        {
+            ThrowOutOfRange(nameof(ResourceOptimizedPlacementOptions.AvailableMemoryWeight));
+        }
+
+        if (_options.LocalSiloPreferenceMargin < 0f || _options.LocalSiloPreferenceMargin > 1f)
+        {
+            ThrowOutOfRange(nameof(ResourceOptimizedPlacementOptions.LocalSiloPreferenceMargin));
+        }
+
+        if (_options.CpuUsageWeight + _options.MemoryUsageWeight + _options.AvailableMemoryWeight != 1f)
         {
             throw new OrleansConfigurationException(
-                $"The total sum across all the weights of {nameof(ResourceOptimizedPlacementOptions)} must equal 1.0");
+                $"The total sum across all the weights of {nameof(ResourceOptimizedPlacementOptions)} must equal 1");
         }
+
+        static void ThrowOutOfRange(string propertyName)
+            => throw new OrleansConfigurationException($"{propertyName} must be inclusive between [0-1]");
     }
 }
