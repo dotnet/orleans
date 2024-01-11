5c5d256111aa5c8536415629dc949cb17f18f3fe
Manuel Menegazzo <65919883+m3nax@users.noreply.github.com>
2024-01-11 11:35:24 +0100

    Add missing query CleanupDefunctSiloEntriesKey to SQLServer clustering script

--- src/AdoNet/Orleans.Clustering.AdoNet/SQLServer-Clustering.sql
+++ src/AdoNet/Orleans.Clustering.AdoNet/SQLServer-Clustering.sql
@@ -277,3 +277,19 @@ WHERE NOT EXISTS
     FROM OrleansQuery oqt
     WHERE oqt.[QueryKey] = 'DeleteMembershipTableEntriesKey'
 );
+
+INSERT INTO OrleansQuery(QueryKey, QueryText)
+SELECT
+    'CleanupDefunctSiloEntriesKey',
+    'DELETE FROM OrleansMembershipTable
+    WHERE DeploymentId = @DeploymentId
+        AND @DeploymentId IS NOT NULL
+        AND IAmAliveTime < @IAmAliveTime
+        AND Status != 3;
+    '
+WHERE NOT EXISTS 
+( 
+    SELECT 1 
+    FROM OrleansQuery oqt
+    WHERE oqt.[QueryKey] = 'CleanupDefunctSiloEntriesKey'
+);
