2a0622c44fa3f4d626c818e9aa1336eac5a7e847
Ledjon Behluli <Ledjon@notiphy.io>
2024-01-11 22:56:01 +0100

    some tests for options and validator, and XML docs

--- src/Orleans.Core.Abstractions/Placement/ResourceOptimizedPlacement.cs
+++ src/Orleans.Core.Abstractions/Placement/ResourceOptimizedPlacement.cs
@@ -1,15 +1,14 @@
 namespace Orleans.Runtime;
 
 /// <summary>
-/// A placement strategy which attempts to achieve approximately even load based on cluster resources.
+/// A placement strategy which attempts to optimize resource distribution across the cluster.
 /// </summary>
 /// <remarks>
-/// <para>It assigns weights to runtime statistics to prioritize different properties and calculates a normalized score for each silo.
-/// The silo with the highest score is chosen for placing the activation. 
-/// Normalization ensures that each property contributes proportionally to the overall score.
-/// You can adjust the weights based on your specific requirements and priorities for load balancing.
-/// In addition to normalization, an online adaptive filter provides a smoothing effect 
-/// (filters out high frequency components) and avoids rapid signal drops by transforming it into a polynomial alike decay process.
+/// <para>It assigns weights to runtime statistics to prioritize different resources and calculates a normalized score for each silo.
+/// The silo with the lowest score is chosen for placing the activation. Normalization ensures that each property contributes proportionally
+/// to the overall score. You can adjust the weights based on your specific requirements and priorities for load balancing.
+/// In addition to normalization, an <a href="https://en.wikipedia.org/wiki/Online_algorithm">online</a> <a href="https://en.wikipedia.org/wiki/Adaptive_algorithm">adaptive</a>
+/// algorithm provides a smoothing effect (filters out high frequency components) and avoids rapid signal drops by transforming it into a polynomial alike decay process.
 /// This contributes to avoiding resource saturation on the silos and especially newly joined silos.</para>
 /// <para><i>This placement strategy is configured by adding the <see cref="Placement.ResourceOptimizedPlacementAttribute"/> attribute to a grain.</i></para>
 /// </remarks>
