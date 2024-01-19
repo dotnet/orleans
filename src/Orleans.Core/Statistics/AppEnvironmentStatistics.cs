057e24133a828fd2a931a793103ed74ec359b3c8
Ledjon Behluli <Ledjon@notiphy.io>
2024-01-19 14:58:28 +0100

    added fragmented memory to 'MemoryUsage' since `GetTotalMemory` does not account for it.

--- src/Orleans.Core/Statistics/AppEnvironmentStatistics.cs
+++ src/Orleans.Core/Statistics/AppEnvironmentStatistics.cs
@@ -4,6 +4,8 @@ namespace Orleans.Statistics
 {
     internal class AppEnvironmentStatistics : IAppEnvironmentStatistics
     {
-        public long? MemoryUsage => GC.GetTotalMemory(false);
+        public long? MemoryUsage =>
+            GC.GetTotalMemory(false) +
+            GC.GetGCMemoryInfo().FragmentedBytes; // add fragmented memory since `GetTotalMemory` does not account for it.
     }
 }
