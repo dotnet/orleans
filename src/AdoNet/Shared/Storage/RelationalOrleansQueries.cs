a9d40996c7600ca8253a48d1af5096bf98442819
Jorge Candeias <jorge.candeias@outcompute.com>
2024-04-09 16:58:08 +0100

    Added Orleans.Streaming.AdoNet project skeleton

--- src/AdoNet/Shared/Storage/RelationalOrleansQueries.cs
+++ src/AdoNet/Shared/Storage/RelationalOrleansQueries.cs
@@ -11,10 +11,10 @@ namespace Orleans.Clustering.AdoNet.Storage
 #elif PERSISTENCE_ADONET
 namespace Orleans.Persistence.AdoNet.Storage
 #elif REMINDERS_ADONET
-
 namespace Orleans.Reminders.AdoNet.Storage
+#elif STREAMING_ADONET
+namespace Orleans.Streaming.AdoNet.Storage
 #elif TESTER_SQLUTILS
-
 namespace Orleans.Tests.SqlUtils
 #else
 // No default namespace intentionally to cause compile errors if something is not defined
