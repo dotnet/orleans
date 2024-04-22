7dc1991b3705ad501968dcbeee0a7071bbffde1d
Jorge Candeias <jorge.candeias@outcompute.com>
2024-04-22 21:09:32 +0100

    Added AdoNetQueueDataAdapter

--- src/AdoNet/Orleans.Streaming.AdoNet/AdoNetQueueDataAdapter.cs
+++ src/AdoNet/Orleans.Streaming.AdoNet/AdoNetQueueDataAdapter.cs
@@ -2,15 +2,23 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Orleans.Streaming.AdoNet;
 
+/// <summary>
+/// Implements a <see cref="IQueueDataAdapter{TQueueMessage, TMessageBatch}"/> for the ADO.NET streaming provider.
+/// </summary>
+[GenerateSerializer]
 [SerializationCallbacks(typeof(OnDeserializedCallbacks))]
+[Alias("Orleans.Streaming.AdoNet.AdoNetQueueDataAdapter")]
 internal class AdoNetQueueDataAdapter(Serializer<AdoNetBatchContainer> serializer) : IQueueDataAdapter<byte[], IBatchContainer>, IOnDeserialized
 {
+    [NonSerialized]
+    private Serializer<AdoNetBatchContainer> _serializer = serializer;
+
     public byte[] ToQueueMessage<T>(StreamId streamId, IEnumerable<T> events, StreamSequenceToken token, Dictionary<string, object> requestContext)
     {
         ArgumentNullException.ThrowIfNull(events);
 
         var container = new AdoNetBatchContainer(streamId, events.Cast<object>().ToList(), requestContext);
-        var bytes = serializer.SerializeToArray(container);
+        var bytes = _serializer.SerializeToArray(container);
 
         return bytes;
     }
@@ -19,10 +27,10 @@ internal class AdoNetQueueDataAdapter(Serializer<AdoNetBatchContainer> serialize
     {
         ArgumentNullException.ThrowIfNull(queueMessage);
 
-        var container = serializer.Deserialize(queueMessage);
+        var container = _serializer.Deserialize(queueMessage);
         container.SequenceToken = new(sequenceId);
         return container;
     }
 
-    public void OnDeserialized(DeserializationContext context) => serializer = context.ServiceProvider.GetRequiredService<Serializer<AdoNetBatchContainer>>();
+    public void OnDeserialized(DeserializationContext context) => _serializer = context.ServiceProvider.GetRequiredService<Serializer<AdoNetBatchContainer>>();
 }
--- test/Extensions/TesterAdoNet/Streaming/AdoNetQueueDataAdapterTests.cs
+++ test/Extensions/TesterAdoNet/Streaming/AdoNetQueueDataAdapterTests.cs
@@ -82,4 +82,71 @@ public class AdoNetQueueDataAdapterTests(TestEnvironmentFixture environment)
         Assert.True(adonet.ImportRequestContext());
         Assert.Equal(context["SomeKey"], RequestContext.Get("SomeKey"));
     }
+
+    [Fact]
+    public void Cycles_AfterSelfDeserialization()
+    {
+        // arrange
+        var serializer = _environment.Services.GetRequiredService<Serializer<AdoNetBatchContainer>>();
+        var adapter = new AdoNetQueueDataAdapter(serializer);
+        var streamId = StreamId.Create("SomeNamespace", "SomeKey");
+        var context = new Dictionary<string, object> { { "SomeKey", 123 } };
+        var sequenceId = Random.Shared.Next();
+
+        // act - roundtrip the data adapter itself
+        var serialized = _environment.Services.GetRequiredService<Serializer<AdoNetQueueDataAdapter>>().SerializeToArray(adapter);
+        adapter = _environment.Services.GetRequiredService<Serializer<AdoNetQueueDataAdapter>>().Deserialize(serialized);
+
+        // act - convert to message
+        var events = new[] { new TestModel(1), new TestModel(2), new TestModel(3) };
+        var message = adapter.ToQueueMessage(streamId, events, null, context);
+
+        // assert - message was produced
+        Assert.NotNull(message);
+        Assert.NotEmpty(message);
+
+        // act - convert to container
+        var container = adapter.FromQueueMessage(message, sequenceId);
+
+        // assert - container was produced
+        Assert.NotNull(container);
+
+        // assert - container is of known type
+        var adonet = Assert.IsType<AdoNetBatchContainer>(container);
+        Assert.Equal(streamId, adonet.StreamId);
+        Assert.Equal(events, adonet.Events);
+
+        // assert - token was generated
+        var token = Assert.IsType<EventSequenceTokenV2>(adonet.SequenceToken);
+        Assert.Equal(sequenceId, token.SequenceNumber);
+        Assert.Equal(0, token.EventIndex);
+
+        // assert - events are filtered
+        Assert.Empty(adonet.GetEvents<Version>());
+        Assert.Collection(adonet.GetEvents<TestModel>(),
+            x =>
+            {
+                Assert.Equal(x.Item1, events[0]);
+                Assert.Equal(sequenceId, x.Item2.SequenceNumber);
+                Assert.Equal(0, x.Item2.EventIndex);
+            },
+            x =>
+            {
+                Assert.Equal(x.Item1, events[1]);
+                Assert.Equal(sequenceId, x.Item2.SequenceNumber);
+                Assert.Equal(1, x.Item2.EventIndex);
+            },
+            x =>
+            {
+                Assert.Equal(x.Item1, events[2]);
+                Assert.Equal(sequenceId, x.Item2.SequenceNumber);
+                Assert.Equal(2, x.Item2.EventIndex);
+            });
+
+        // assert - context was preserved
+        Assert.NotNull(adonet.RequestContext);
+        Assert.Equal(context.OrderBy(x => x.Key), adonet.RequestContext.OrderBy(x => x.Key));
+        Assert.True(adonet.ImportRequestContext());
+        Assert.Equal(context["SomeKey"], RequestContext.Get("SomeKey"));
+    }
 }
