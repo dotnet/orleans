a7276e355c165a7fc306a66990da14cc5e22d78b
Jorge Candeias <jorge.candeias@outcompute.com>
2024-05-24 11:19:44 +0100

    Refactored

--- src/AdoNet/Orleans.AdoNet.Core/NoOpDatabaseCommandInterceptor.cs
+++ src/AdoNet/Orleans.AdoNet.Core/NoOpDatabaseCommandInterceptor.cs
@@ -1,32 +1,17 @@
-using System.Data;
 using Orleans.AdoNet.Storage;
 
-#if CLUSTERING_ADONET
-namespace Orleans.Clustering.AdoNet.Storage
-#elif PERSISTENCE_ADONET
-namespace Orleans.Persistence.AdoNet.Storage
-#elif REMINDERS_ADONET
-namespace Orleans.Reminders.AdoNet.Storage
-#elif STREAMING_ADONET
-namespace Orleans.Streaming.AdoNet.Storage
-#elif TESTER_SQLUTILS
-namespace Orleans.Tests.SqlUtils
-#else
-// No default namespace intentionally to cause compile errors if something is not defined
-#endif
+namespace Orleans.AdoNet.Core;
+
+internal class NoOpCommandInterceptor : ICommandInterceptor
 {
-    internal class NoOpCommandInterceptor : ICommandInterceptor
+    public static readonly ICommandInterceptor Instance = new NoOpCommandInterceptor();
+
+    private NoOpCommandInterceptor()
     {
-        public static readonly ICommandInterceptor Instance = new NoOpCommandInterceptor();
+    }
 
-        private NoOpCommandInterceptor()
-        {
-            
-        }
-
-        public void Intercept(IDbCommand command)
-        {
-            //NOP
-        }
+    public void Intercept(IDbCommand command)
+    {
+        //NOP
     }
 }
