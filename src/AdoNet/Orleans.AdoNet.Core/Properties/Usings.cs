66fe9c3284cee70e174dd0bfc78aa02e5a8f6714
Jorge Candeias <jorge.candeias@outcompute.com>
2024-05-24 11:26:13 +0100

    Refactored

--- src/AdoNet/Orleans.AdoNet.Core/Properties/Usings.cs
+++ src/AdoNet/Orleans.AdoNet.Core/Properties/Usings.cs
@@ -3,4 +3,5 @@ global using System.Collections.ObjectModel;
 global using System.Data;
 global using System.Data.Common;
 global using System.Globalization;
+global using System.Linq.Expressions;
 global using System.Reflection;
