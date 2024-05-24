5f4a81495440b3af7beafa350524121acb28cc7a
Jorge Candeias <jorge.candeias@outcompute.com>
2024-05-24 11:12:23 +0100

    Refactored

--- src/AdoNet/Orleans.AdoNet.Core/DbConnectionFactory.cs
+++ src/AdoNet/Orleans.AdoNet.Core/DbConnectionFactory.cs
@@ -1,24 +1,4 @@
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Reflection;
-using Orleans.AdoNet.Core;
-
-#if CLUSTERING_ADONET
-namespace Orleans.Clustering.AdoNet.Storage
-#elif PERSISTENCE_ADONET
-namespace Orleans.Persistence.AdoNet.Storage
-#elif REMINDERS_ADONET
-namespace Orleans.Reminders.AdoNet.Storage
-#elif STREAMING_ADONET
-namespace Orleans.Streaming.AdoNet.Storage
-#elif TESTER_SQLUTILS
-namespace Orleans.Tests.SqlUtils
-#else
-// No default namespace intentionally to cause compile errors if something is not defined
-#endif
+namespace Orleans.AdoNet.Core
 {
     /// This class caches the references to all loaded factories
     internal static class DbConnectionFactory
@@ -45,14 +25,15 @@ namespace Orleans.Tests.SqlUtils
                 throw new ArgumentNullException(nameof(invariantName));
             }
 
-            List<Tuple<string, string>> providerFactoryDefinitions;
-            if (!providerFactoryTypeMap.TryGetValue(invariantName, out providerFactoryDefinitions) || providerFactoryDefinitions.Count == 0)
+            if (!providerFactoryTypeMap.TryGetValue(invariantName, out var providerFactoryDefinitions) || providerFactoryDefinitions.Count == 0)
+            {
                 throw new InvalidOperationException($"Database provider factory with '{invariantName}' invariant name not supported.");
+            }
 
-            List<Exception> exceptions = null;
+            List<Exception>? exceptions = null;
             foreach (var providerFactoryDefinition in providerFactoryDefinitions)
             {
-                Assembly asm = null;
+                Assembly? asm = null;
                 try
                 {
                     var asmName = new AssemblyName(providerFactoryDefinition.Item1);
@@ -60,42 +41,39 @@ namespace Orleans.Tests.SqlUtils
                 }
                 catch (Exception exc)
                 {
-                    AddException(new InvalidOperationException($"Unable to find and/or load a candidate assembly '{providerFactoryDefinition.Item1}' for '{invariantName}' invariant name.", exc));
+                    AddException(ref exceptions, new InvalidOperationException($"Unable to find and/or load a candidate assembly '{providerFactoryDefinition.Item1}' for '{invariantName}' invariant name.", exc));
                     continue;
                 }
 
                 if (asm == null)
                 {
-                    AddException(new InvalidOperationException($"Can't find database provider factory with '{invariantName}' invariant name. Please make sure that your ADO.Net provider package library is deployed with your application."));
+                    AddException(ref exceptions, new InvalidOperationException($"Can't find database provider factory with '{invariantName}' invariant name. Please make sure that your ADO.Net provider package library is deployed with your application."));
                     continue;
                 }
 
                 var providerFactoryType = asm.GetType(providerFactoryDefinition.Item2);
                 if (providerFactoryType == null)
                 {
-                    AddException(new InvalidOperationException($"Unable to load type '{providerFactoryDefinition.Item2}' for '{invariantName}' invariant name."));
+                    AddException(ref exceptions, new InvalidOperationException($"Unable to load type '{providerFactoryDefinition.Item2}' for '{invariantName}' invariant name."));
                     continue;
                 }
 
                 var prop = providerFactoryType.GetFields().SingleOrDefault(p => string.Equals(p.Name, "Instance", StringComparison.OrdinalIgnoreCase) && p.IsStatic);
                 if (prop == null)
                 {
-                    AddException(new InvalidOperationException($"Invalid provider type '{providerFactoryDefinition.Item2}' for '{invariantName}' invariant name."));
+                    AddException(ref exceptions, new InvalidOperationException($"Invalid provider type '{providerFactoryDefinition.Item2}' for '{invariantName}' invariant name."));
                     continue;
                 }
 
-                var factory = (DbProviderFactory)prop.GetValue(null);
-                return new CachedFactory(factory, providerFactoryType.Name, "", providerFactoryType.AssemblyQualifiedName);
+                var factory = (DbProviderFactory)prop.GetValue(null)!;
+                return new CachedFactory(factory, providerFactoryType.Name, "", providerFactoryType.AssemblyQualifiedName!);
             }
 
-            throw new AggregateException(exceptions);
+            throw new AggregateException(exceptions ?? []);
 
-            void AddException(Exception ex)
+            static void AddException(ref List<Exception>? exceptions, Exception ex)
             {
-                if (exceptions == null)
-                {
-                    exceptions = new List<Exception>();
-                }
+                exceptions ??= [];
                 exceptions.Add(ex);
             }
         }
@@ -113,48 +91,34 @@ namespace Orleans.Tests.SqlUtils
             }
 
             var factory = factoryCache.GetOrAdd(invariantName, GetFactory).Factory;
-            var connection = factory.CreateConnection();
-
-            if (connection == null)
-            {
-                throw new InvalidOperationException($"Database provider factory: '{invariantName}' did not return a connection object.");
-            }
-
+            var connection = factory.CreateConnection() ?? throw new InvalidOperationException($"Database provider factory: '{invariantName}' did not return a connection object.");
             connection.ConnectionString = connectionString;
             return connection;
         }
 
-        private class CachedFactory
+        private class CachedFactory(DbProviderFactory factory, string factoryName, string factoryDescription, string factoryAssemblyQualifiedNameKey)
         {
-            public CachedFactory(DbProviderFactory factory, string factoryName, string factoryDescription, string factoryAssemblyQualifiedNameKey)
-            {
-                Factory = factory;
-                FactoryName = factoryName;
-                FactoryDescription = factoryDescription;
-                FactoryAssemblyQualifiedNameKey = factoryAssemblyQualifiedNameKey;
-            }
-
             /// <summary>
             /// The factory to provide vendor specific functionality.
             /// </summary>
             /// <remarks>For more about <see href="http://florianreischl.blogspot.fi/2011/08/adonet-connection-pooling-internals-and.html">ConnectionPool</see>
             /// and issues with using this factory. Take these notes into account when considering robustness of Orleans!</remarks>
-            public readonly DbProviderFactory Factory;
+            public readonly DbProviderFactory Factory = factory;
 
             /// <summary>
             /// The name of the loaded factory, set by a database connector vendor.
             /// </summary>
-            public readonly string FactoryName;
+            public readonly string FactoryName = factoryName;
 
             /// <summary>
             /// The description of the loaded factory, set by a database connector vendor.
             /// </summary>
-            public readonly string FactoryDescription;
+            public readonly string FactoryDescription = factoryDescription;
 
             /// <summary>
             /// The description of the loaded factory, set by a database connector vendor.
             /// </summary>
-            public readonly string FactoryAssemblyQualifiedNameKey;
+            public readonly string FactoryAssemblyQualifiedNameKey = factoryAssemblyQualifiedNameKey;
         }
     }
 }
--- src/AdoNet/Orleans.AdoNet.Core/Properties/Usings.cs
+++ src/AdoNet/Orleans.AdoNet.Core/Properties/Usings.cs
@@ -1,2 +1,5 @@
+global using System.Collections.Concurrent;
 global using System.Collections.ObjectModel;
+global using System.Data.Common;
 global using System.Globalization;
+global using System.Reflection;
