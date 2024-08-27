050c1c9a921d4975308c35a12a156385b55c4bde
Reuben Bond <reuben.bond@gmail.com>
2024-08-27 14:52:31 -0700

    Add additional test cases for generic & readonly scenario

--- test/Orleans.Serialization.UnitTests/GeneratedSerializerTests.cs
+++ test/Orleans.Serialization.UnitTests/GeneratedSerializerTests.cs
@@ -151,7 +151,6 @@ public class GeneratedSerializerTests : IDisposable
     }
 
 #if NET6_0_OR_GREATER
-
     [Fact]
     public void GeneratedLibExternalRecordWithPCtorSerializersRoundTripThroughCodec()
     {
@@ -169,6 +168,79 @@ public class GeneratedSerializerTests : IDisposable
         Assert.Equal(original.StarSign, result.StarSign);
     }
 
+    [Fact]
+    public void GeneratedLibExternalGenericRecordStructWithPCtorSerializersRoundTripThroughCodec()
+    {
+        var originalInner = new Person2External(2, "harry")
+        {
+            FavouriteColor = "redborine",
+            StarSign = "Aquaricorn"
+        };
+
+        var original = new GenericPersonExternalStruct<Person2External>(originalInner, "harry")
+        {
+            BodyParam = originalInner,
+            StarSign = "Aquaricorn"
+        };
+
+        var result = RoundTripThroughCodec(original);
+
+        Assert.Equal(original.CtorParam, result.CtorParam);
+        Assert.Equal(original.Name, result.Name);
+        Assert.Equal(original.BodyParam, result.BodyParam);
+        Assert.Equal(original.StarSign, result.StarSign);
+        Assert.Same(result.CtorParam, result.BodyParam);
+    }
+
+    [Fact]
+    public void GeneratedReadonlyLibExternalGenericRecordStructWithPCtorSerializersRoundTripThroughCodec()
+    {
+        var originalInner = new Person2External(2, "harry")
+        {
+            FavouriteColor = "redborine",
+            StarSign = "Aquaricorn"
+        };
+
+        var original = new ReadonlyGenericPersonExternalStruct<Person2External>(originalInner, "harry")
+        {
+            BodyParam = originalInner,
+            StarSign = "Aquaricorn"
+        };
+
+        var result = RoundTripThroughCodec(original);
+
+        Assert.Equal(original.CtorParam, result.CtorParam);
+        Assert.Equal(original.Name, result.Name);
+        Assert.Equal(original.BodyParam, result.BodyParam);
+        Assert.Equal(original.StarSign, result.StarSign);
+        Assert.Same(result.CtorParam, result.BodyParam);
+    }
+#endif
+
+#if NET9_0_OR_GREATER
+    [Fact]
+    public void GeneratedLibExternalGenericRecordWithPCtorSerializersRoundTripThroughCodec()
+    {
+        var originalInner = new Person2External(2, "harry")
+        {
+            FavouriteColor = "redborine",
+            StarSign = "Aquaricorn"
+        };
+
+        var original = new GenericPersonExternal<Person2External>(originalInner, "harry")
+        {
+            BodyParam = originalInner,
+            StarSign = "Aquaricorn"
+        };
+
+        var result = RoundTripThroughCodec(original);
+
+        Assert.Equal(original.CtorParam, result.CtorParam);
+        Assert.Equal(original.Name, result.Name);
+        Assert.Equal(original.BodyParam, result.BodyParam);
+        Assert.Equal(original.StarSign, result.StarSign);
+        Assert.Same(result.CtorParam, result.BodyParam);
+    }
 #endif
 
 #if NET6_0_OR_GREATER
