f134adc4e5041ae7f0fc0e3a11095b36c0136ac2
Ledjon Behluli <Ledjon@notiphy.io>
2024-09-07 23:11:39 +0200

    removed old testing apps

---
+++ test/TesterInternal/ActivationRebalancingTests/GenericRebalancingTests.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTests.ActivationRebalancingTests;
+
+[TestCategory("Functional"), TestCategory("ActivationRebalancing")]
+public class GenericRebalancingTests(RebalancerFixture fixture, ITestOutputHelper output)
+    : RebalancingTestBase<RebalancerFixture>(fixture, output), IClassFixture<RebalancerFixture>
+{
+
+}
