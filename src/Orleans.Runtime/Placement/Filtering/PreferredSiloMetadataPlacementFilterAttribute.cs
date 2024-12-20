8fcf93b0f3d48f25ff0ba5e6b1e4f20dd8ecfc51
Ryan Karg <ryankarg@microsoft.com>
2024-12-20 11:44:05 -0800

    Tests for silo metadata placement filters

--- src/Orleans.Runtime/Placement/Filtering/PreferredSiloMetadataPlacementFilterAttribute.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace Orleans.Runtime.Placement.Filtering;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-public class PreferredSiloMetadataPlacementFilterAttribute(string[] orderedMetadataKeys)
-    : PlacementFilterAttribute(new PreferredSiloMetadataPlacementFilterStrategy(orderedMetadataKeys));
