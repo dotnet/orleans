0bc5dedb6d3e5d2c88f6cf746557303cb44d8525
Ryan Karg <ryankarg@microsoft.com>
2025-01-08 13:59:52 -0800

    Allowing chaining extension method

--- src/Orleans.Core/Placement/PlacementFilterExtensions.cs
+++ src/Orleans.Core/Placement/PlacementFilterExtensions.cs
@@ -12,12 +12,14 @@ public static class PlacementFilterExtensions
     /// <param name="services">The service collection.</param>
     /// <param name="strategyLifetime">The lifetime of the placement strategy.</param>
     /// <returns>The service collection.</returns>
-    public static void AddPlacementFilter<TFilter, TDirector>(this IServiceCollection services, ServiceLifetime strategyLifetime)
+    public static IServiceCollection AddPlacementFilter<TFilter, TDirector>(this IServiceCollection services, ServiceLifetime strategyLifetime)
         where TFilter : PlacementFilterStrategy
         where TDirector : class, IPlacementFilterDirector
     {
         services.Add(ServiceDescriptor.DescribeKeyed(typeof(PlacementFilterStrategy), typeof(TFilter).Name, typeof(TFilter), strategyLifetime));
         services.AddKeyedSingleton<IPlacementFilterDirector, TDirector>(typeof(TFilter));
+
+        return services;
     }
 
 }
