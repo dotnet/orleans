facb9981628941aa99f4dbb50bd4cc4af5fca822
Ryan Brooks <rbrooks@infonuncio.com>
2025-03-18 08:46:41 -0600

    Clear blob contents when DeleteStateOnClear is false

--- src/Azure/Orleans.Persistence.AzureStorage/Providers/Storage/AzureBlobStorage.cs
+++ src/Azure/Orleans.Persistence.AzureStorage/Providers/Storage/AzureBlobStorage.cs
@@ -226,13 +226,25 @@ namespace Orleans.Storage
                 if (options.DeleteStateOnClear)
                 {
                     await DoOptimisticUpdate(
-                    static state => state.blob.DeleteIfExistsAsync(DeleteSnapshotsOption.None, conditions: state.conditions),
-                    (blob, conditions),
-                    blob,
-                    grainState.ETag).ConfigureAwait(false);
+                        static state => state.blob.DeleteIfExistsAsync(DeleteSnapshotsOption.None, conditions: state.conditions),
+                        (blob, conditions),
+                        blob,
+                        grainState.ETag).ConfigureAwait(false);
+                    grainState.ETag = null;
+                }
+                else
+                {
+                    var options = new BlobUploadOptions { Conditions = conditions };
+                    var response = await DoOptimisticUpdate(
+                        static state => state.blob.UploadAsync(BinaryData.Empty, state.options),
+                        (blob, options, conditions),
+                        blob,
+                        grainState.ETag).ConfigureAwait(false);
+                    grainState.ETag = response.Value.ETag.ToString();
                 }
 
-                ResetGrainState(grainState);
+                grainState.RecordExists = false;
+                grainState.State = CreateInstance<T>();
                 if (this.logger.IsEnabled(LogLevel.Trace))
                 {
                     var properties = await blob.GetPropertiesAsync();
--- test/Extensions/TesterAzureUtils/Persistence/PersistenceStateTests_AzureBlobStore.cs
+++ test/Extensions/TesterAzureUtils/Persistence/PersistenceStateTests_AzureBlobStore.cs
@@ -41,3 +41,36 @@ public class PersistenceStateTests_AzureBlobStore : Base_PersistenceGrainTests_A
         fixture.EnsurePreconditionsMet();
     }
 }
+
+[TestCategory("Persistence"), TestCategory("AzureStorage")]
+public class PersistenceStateTests_AzureBlobStore_NoDeleteStateOnClear : Base_PersistenceGrainTests_AzureStore, IClassFixture<PersistenceStateTests_AzureBlobStore_NoDeleteStateOnClear.Fixture>
+{
+    public class Fixture : BaseAzureTestClusterFixture
+    {
+        protected override void ConfigureTestCluster(TestClusterBuilder builder)
+        {
+            builder.Options.InitialSilosCount = 4;
+            builder.Options.UseTestClusterMembership = false;
+            builder.AddSiloBuilderConfigurator<SiloBuilderConfigurator>();
+            builder.AddSiloBuilderConfigurator<StorageSiloBuilderConfigurator>();
+            builder.AddClientBuilderConfigurator<ClientBuilderConfigurator>();
+        }
+
+        private class StorageSiloBuilderConfigurator : ISiloConfigurator
+        {
+            public void Configure(ISiloBuilder hostBuilder)
+            {
+                hostBuilder.AddAzureBlobGrainStorage("GrainStorageForTest", (AzureBlobStorageOptions options) =>
+                {
+                    options.ConfigureTestDefaults();
+                    options.DeleteStateOnClear = false;
+                });
+            }
+        }
+    }
+
+    public PersistenceStateTests_AzureBlobStore_NoDeleteStateOnClear(ITestOutputHelper output, Fixture fixture) : base(output, fixture, "UnitTests.PersistentState.Grains")
+    {
+        fixture.EnsurePreconditionsMet();
+    }
+}
