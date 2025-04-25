04492b68d7d249ba9d50142409e1f2f202d77d78
ydy <517697206@qq.com>
2025-04-25 16:20:19 +0800

    feat:modify for AdoNetTransactionsSiloBuilderExtensions and adjust method

--- src/AdoNet/Orleans.Transactions.AdoNet/Utils/Constants.cs
+++ src/AdoNet/Orleans.Transactions.AdoNet/Utils/Constants.cs
@@ -1,10 +1,16 @@
 namespace Orleans.Transactions.AdoNet.Utils;
-internal static class Constants
+
+ internal static class Constants
 {
-     public static string AddKeySql = "addKeySql";
-     public static string UpdateKeySql = "updateKeySql";
-     public static string DelKeySql = "delKeySql";
-     public static string AddStateSql = "addStateSql";
-     public static string UpdateStateSql = "updateStateSql";
-     public static string DelStateSql = "delStateSql";
+    public static string AddKeySql = "addKeySql";
+    public static string UpdateKeySql = "updateKeySql";
+    public static string DelKeySql = "delKeySql";
+    public static string AddStateSql = "addStateSql";
+    public static string UpdateStateSql = "updateStateSql";
+    public static string DelStateSql = "delStateSql";
+
+    // sqlserver,mysql,postgre
+    public static string SqlParameterDot = "@";
+    // oracle
+    public static string OracleParameterDot = ":";
 }
