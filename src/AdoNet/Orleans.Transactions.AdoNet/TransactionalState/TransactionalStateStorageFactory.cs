f03ef7807f12e06f165179ecb9695bfd335a5c36
ydy <517697206@qq.com>
2025-04-25 16:20:19 +0800

    feat:modify for AdoNetTransactionsSiloBuilderExtensions and adjust method

--- src/AdoNet/Orleans.Transactions.AdoNet/TransactionalState/TransactionalStateStorageFactory.cs
+++ src/AdoNet/Orleans.Transactions.AdoNet/TransactionalState/TransactionalStateStorageFactory.cs
@@ -23,27 +23,17 @@ namespace Orleans.Transactions.AdoNet.TransactionalState
         private readonly TransactionalStateStorageOptions options;
         private readonly ClusterOptions clusterOptions;
         private readonly JsonSerializerSettings jsonSettings;
-        private readonly ILoggerFactory loggerFactory;
 
         public TransactionalStateStorageFactory(
             string name,
             TransactionalStateStorageOptions options,
             IOptions<ClusterOptions> clusterOptions,
-           IServiceProvider services,
-           ILoggerFactory loggerFactory)
+            IServiceProvider services)
         {
             this.name = name;
             this.options = options;
             this.clusterOptions = clusterOptions.Value;
             this.jsonSettings = TransactionalStateFactory.GetJsonSerializerSettings(services);
-            this.loggerFactory = loggerFactory;
-            //now just oracle sqlparameter dot is different
-            if (this.options.Invariant == AdoNetInvariants.InvariantNameOracleDatabase
-                && string.IsNullOrWhiteSpace(this.options.SqlParameterDot))
-            {
-                this.options.SqlParameterDot = ":";
-            }
-            InitExecuteSqlDic();
         }
 
         public static ITransactionalStateStorageFactory Create(IServiceProvider services, string name)
@@ -91,55 +81,6 @@ namespace Orleans.Transactions.AdoNet.TransactionalState
         private  Task Init(CancellationToken cancellationToken)
         {
            return Task.CompletedTask;
-        }
-
-        /// <summary>
-        /// init execute sql
-        /// </summary>
-        private void InitExecuteSqlDic()
-        {
-            var addKeySql = ActionToSql.InsertSql(this.options.KeyEntityTableName, new List<string>() {
-                nameof(KeyEntity.StateId),nameof(KeyEntity.CommittedSequenceId),
-                nameof(KeyEntity.Metadata),nameof(KeyEntity.Timestamp),nameof(KeyEntity.ETag) }, this.options.SqlParameterDot);
-            this.options.ExecuteSqlDcitionary.Add(Constants.AddKeySql, addKeySql);
-
-            string updateKeySql = ActionToSql.UpdateSql(this.options.KeyEntityTableName, new List<string>()
-            {
-                 nameof(KeyEntity.CommittedSequenceId),nameof(KeyEntity.Metadata),nameof(KeyEntity.Timestamp)
-            }, new List<string>() {
-                nameof(KeyEntity.StateId),nameof(KeyEntity.ETag)
-            }, this.options.SqlParameterDot);
-            this.options.ExecuteSqlDcitionary.Add(Constants.UpdateKeySql, updateKeySql);
-
-            string delKeySql = ActionToSql.DeleteSql(this.options.KeyEntityTableName, new List<string>()
-            {
-                nameof(KeyEntity.StateId),nameof(KeyEntity.ETag)
-            }, this.options.SqlParameterDot);
-            this.options.ExecuteSqlDcitionary.Add(Constants.DelKeySql, delKeySql);
-
-            string addStateSql = ActionToSql.InsertSql(this.options.StateEntityTableName, new List<string>() {
-                nameof(StateEntity.StateId), nameof(StateEntity.SequenceId),
-                nameof(StateEntity.TransactionId),nameof(StateEntity.TransactionTimestamp),
-                nameof(StateEntity.TransactionManager),nameof(StateEntity.StateJson),
-                nameof(StateEntity.ETag), nameof(StateEntity.Timestamp)
-            }, this.options.SqlParameterDot);
-            this.options.ExecuteSqlDcitionary.Add(Constants.AddStateSql, addStateSql);
-
-            string updateStateSql = ActionToSql.UpdateSql(this.options.StateEntityTableName, new List<string>()
-            {
-                nameof(StateEntity.TransactionId),nameof(StateEntity.TransactionTimestamp),
-                nameof(StateEntity.TransactionManager), nameof(StateEntity.StateJson),
-                 nameof(StateEntity.Timestamp) },
-             new List<string>() {
-                nameof(StateEntity.StateId),nameof(StateEntity.SequenceId) }
-             , this.options.SqlParameterDot);
-            this.options.ExecuteSqlDcitionary.Add(Constants.UpdateStateSql, updateStateSql);
-
-            string delStateSql = ActionToSql.DeleteSql(this.options.StateEntityTableName, new List<string>()
-            {
-                nameof(StateEntity.StateId),nameof(StateEntity.SequenceId),nameof(StateEntity.ETag)
-            }, this.options.SqlParameterDot);
-            this.options.ExecuteSqlDcitionary.Add(Constants.DelStateSql, delStateSql);
         }
     }
 }
