4f1dacd85f1f4b96b0bace6daf5a8dbb4299a522
ydy <517697206@qq.com>
2025-04-27 14:53:59 +0800

    fix:change keyEntity and stateEntity with bigdata store for byte[] replace and so on

--- src/AdoNet/Orleans.Transactions.AdoNet/PostgreSQL-Transactions.sql
+++ src/AdoNet/Orleans.Transactions.AdoNet/PostgreSQL-Transactions.sql
@@ -1,10 +1,10 @@
 -- Orleans Transaction Key Table for PostgreSQL
 CREATE TABLE OrleansTransactionKeyTable (
   StateId VARCHAR(255) NOT NULL,
-  ETag VARCHAR(255) NOT NULL,
+  ETag VARCHAR(36) NOT NULL,
   CommittedSequenceId BIGINT NOT NULL,
-  Metadata TEXT NULL,
-  Timestamp TIMESTAMP NOT NULL,
+  Metadata BYTEA NULL,
+  Timestamp timestamptz(3) NOT NULL DEFAULT now(),
   PRIMARY KEY (StateId)
 );
 
@@ -12,12 +12,12 @@ CREATE TABLE OrleansTransactionKeyTable (
 CREATE TABLE OrleansTransactionStateTable (
   StateId VARCHAR(255) NOT NULL,
   SequenceId BIGINT NOT NULL,
-  ETag VARCHAR(255) NOT NULL,
-  TransactionId VARCHAR(255) NOT NULL,
+  ETag VARCHAR(36) NOT NULL,
+  TransactionId VARCHAR(36) NOT NULL,
   TransactionTimestamp TIMESTAMP NOT NULL,
-  TransactionManager TEXT NOT NULL,
-  StateJson TEXT NULL,
-  Timestamp TIMESTAMP NOT NULL,
+  TransactionManager BYTEA NOT NULL,
+  SateData BYTEA NULL,
+  Timestamp timestamptz(3) NOT NULL DEFAULT now(),
   PRIMARY KEY (StateId, SequenceId)
 );
 
