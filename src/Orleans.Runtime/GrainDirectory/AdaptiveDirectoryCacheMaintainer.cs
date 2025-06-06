e7e2aa409214fa85f58acef5a9caabcf1dd19bd2
Meir Blachman <meirblachman@gmail.com>
2025-06-06 19:11:33 +0300

    chore(Orleans.Runtime): Use [LoggerMessage] p4 (#9541)
    * chore(Orleans.Runtime): Use [LoggerMessage] p4
    
    Cleanup `src/Orleans.Runtime/GrainDirectory/LocalGrainDirectoryPartition.cs`
    
    * chore(Orleans.Runtime): Use [LoggerMessage] p4
    
    Cleanup `src\Orleans.Runtime\GrainDirectory\LocalGrainDirectory.cs`
    
    * chore(Orleans.Runtime): Use [LoggerMessage] p4
    
    Cleanup `src/Orleans.Runtime/Networking/ConnectionListener.cs`
    
    * chore(Orleans.Runtime): Use [LoggerMessage] p4
    
    Cleanup `src\Orleans.Runtime\Networking\SiloConnection.cs`
    
    * chore(Orleans.Runtime): Use [LoggerMessage] p4
    
    Cleanup `src\Orleans.Runtime\Messaging\MessageCenter.cs`
    
    * chore(Orleans.Runtime): Use [LoggerMessage] p4
    
    Cleanup `src\Orleans.Runtime\GrainDirectory\AdaptiveDirectoryCacheMaintainer.cs`
    
    * chore(Orleans.Runtime): Refactor logging implementation in AdaptiveDirectoryCacheMaintainer

--- src/Orleans.Runtime/GrainDirectory/AdaptiveDirectoryCacheMaintainer.cs
+++ src/Orleans.Runtime/GrainDirectory/AdaptiveDirectoryCacheMaintainer.cs
@@ -14,6 +14,7 @@ namespace Orleans.Runtime.GrainDirectory
     {
         private static readonly TimeSpan SLEEP_TIME_BETWEEN_REFRESHES = Debugger.IsAttached ? TimeSpan.FromMinutes(5) : TimeSpan.FromMinutes(1); // this should be something like minTTL/4
 
+        private readonly ILogger<AdaptiveDirectoryCacheMaintainer> _log;
         private readonly AdaptiveGrainDirectoryCache cache;
         private readonly LocalGrainDirectory router;
         private readonly IInternalGrainFactory grainFactory;
@@ -29,7 +30,7 @@ namespace Orleans.Runtime.GrainDirectory
             IInternalGrainFactory grainFactory,
             ILoggerFactory loggerFactory)
         {
-            Log = loggerFactory.CreateLogger<AdaptiveDirectoryCacheMaintainer>();
+            _log = loggerFactory.CreateLogger<AdaptiveDirectoryCacheMaintainer>();
             this.grainFactory = grainFactory;
             this.router = router;
             this.cache = cache;
@@ -37,8 +38,6 @@ namespace Orleans.Runtime.GrainDirectory
             lastNumAccesses = 0;
             lastNumHits = 0;
         }
-
-        private ILogger<AdaptiveDirectoryCacheMaintainer> Log { get; }
 
         public void Start()
         {
@@ -135,7 +134,7 @@ namespace Orleans.Runtime.GrainDirectory
                         }
                     }
 
-                    LogTraceSelfOwnedAndRemoved(Log, router.MyAddress, ownedAndRemovedCount, keptCount, removedCount, refreshedCount);
+                    LogTraceSelfOwnedAndRemoved(_log, router.MyAddress, ownedAndRemovedCount, keptCount, removedCount, refreshedCount);
 
                     // Send batch requests
                     SendBatchCacheRefreshRequests(fetchInBatchList);
@@ -144,7 +143,7 @@ namespace Orleans.Runtime.GrainDirectory
                 }
                 catch (Exception ex) when (!cancellationToken.IsCancellationRequested)
                 {
-                    Log.LogError(ex, $"Error in {nameof(AdaptiveDirectoryCacheMaintainer)}.");
+                    LogErrorAdaptiveDirectoryCacheMaintainer(ex);
                 }
             }
         }
@@ -167,7 +166,7 @@ namespace Orleans.Runtime.GrainDirectory
                     ProcessCacheRefreshResponse(silo, response);
                 }).Ignore();
 
-                LogTraceSendingRequest(Log, router.MyAddress, silo, cachedGrainAndETagList.Count);
+                LogTraceSendingRequest(_log, router.MyAddress, silo, cachedGrainAndETagList.Count);
             }
         }
 
@@ -175,7 +174,7 @@ namespace Orleans.Runtime.GrainDirectory
             SiloAddress silo,
             List<AddressAndTag> refreshResponse)
         {
-            LogTraceReceivedProcessCacheRefreshResponse(Log, router.MyAddress, refreshResponse.Count);
+            LogTraceReceivedProcessCacheRefreshResponse(_log, router.MyAddress, refreshResponse.Count);
 
             int otherSiloCount = 0, updatedCount = 0, unchangedCount = 0;
 
@@ -211,7 +210,7 @@ namespace Orleans.Runtime.GrainDirectory
                 }
             }
 
-            LogTraceProcessedRefreshResponse(Log, router.MyAddress, silo, otherSiloCount, updatedCount, unchangedCount);
+            LogTraceProcessedRefreshResponse(_log, router.MyAddress, silo, otherSiloCount, updatedCount, unchangedCount);
         }
 
         /// <summary>
@@ -237,10 +236,7 @@ namespace Orleans.Runtime.GrainDirectory
                 {
                     // this may happen only if the LRU cache is full and decided to drop this grain
                     // while we try to refresh it
-                    Log.LogWarning(
-                        (int)ErrorCode.Runtime_Error_100199,
-                        "Grain {GrainId} disappeared from the cache during maintenance",
-                        grain);
+                    LogWarningGrainDisappearedFromCache(grain);
                 }
             }
 
@@ -258,11 +254,24 @@ namespace Orleans.Runtime.GrainDirectory
             long numAccesses = curNumAccesses - lastNumAccesses;
             long numHits = curNumHits - lastNumHits;
 
-            if (Log.IsEnabled(LogLevel.Trace)) Log.LogTrace("#accesses: {AccessCount}, hit-ratio: {HitRatio}%", numAccesses, (numHits / Math.Max(numAccesses, 0.00001)) * 100);
+            if (_log.IsEnabled(LogLevel.Trace)) _log.LogTrace("#accesses: {AccessCount}, hit-ratio: {HitRatio}%", numAccesses, (numHits / Math.Max(numAccesses, 0.00001)) * 100);
 
             lastNumAccesses = curNumAccesses;
             lastNumHits = curNumHits;
         }
+
+        [LoggerMessage(
+            Level = LogLevel.Error,
+            Message = $"Error in {nameof(AdaptiveDirectoryCacheMaintainer)}."
+        )]
+        private partial void LogErrorAdaptiveDirectoryCacheMaintainer(Exception ex);
+
+        [LoggerMessage(
+            EventId = (int)ErrorCode.Runtime_Error_100199,
+            Level = LogLevel.Warning,
+            Message = "Grain {GrainId} disappeared from the cache during maintenance"
+        )]
+        private partial void LogWarningGrainDisappearedFromCache(GrainId grainId);
 
         [LoggerMessage(
             Level = LogLevel.Trace,
