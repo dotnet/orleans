778a649a2541b67532b13e2073d5e0f23267da1d
Reuben Bond <reuben.bond@gmail.com>
2025-06-06 08:17:04 -0700

    fix tests

--- src/Orleans.Core.Abstractions/Placement/SiloRoleBasedPlacement.cs
+++ src/Orleans.Core.Abstractions/Placement/SiloRoleBasedPlacement.cs
@@ -5,7 +5,7 @@ namespace Orleans.Runtime
     /// <summary>
     /// The silo role placement strategy specifies that a grain should be placed on a compatible silo which has the role specified by the strategy's placement attribute.
     /// </summary>
-    [Serializable]
+    [Serializable, GenerateSerializer, Immutable, SuppressReferenceTracking]
     public class SiloRoleBasedPlacement : PlacementStrategy
     {
         /// <summary>
