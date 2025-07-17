35507ed5a791fda40c068ad6b96f285a699ddb6b
scalalang2 <scalalang2@gmail.com>
2025-07-17 19:49:51 +0900

    the trnasactional storage load keys and states upon activation

--- src/AWS/Orleans.Transactions.DynamoDB/TransactionalState/DynamoDBTransactionalStateConstants.cs
+++ src/AWS/Orleans.Transactions.DynamoDB/TransactionalState/DynamoDBTransactionalStateConstants.cs
@@ -3,9 +3,16 @@ namespace Orleans.Transactions.DynamoDB.TransactionalState;
 public class DynamoDBTransactionalStateConstants
 {
     public const int MAX_DATA_SIZE = 400 * 1024;
-    public const string GRAIN_REFERENCE_PROPERTY_NAME = "GrainReference";
+
+    public const string PARTITION_KEY_PROPERTY_NAME = "PartitionKey";
+
+    public const string ROW_KEY_PROPERTY_NAME = "RowKey";
+
     public const string BINARY_STATE_PROPERTY_NAME = "GrainState";
-    public const string GRAIN_TYPE_PROPERTY_NAME = "GrainType";
+
     public const string ETAG_PROPERTY_NAME = "ETag";
+
+    public const string TIMESTAMP_PROPERTY_NAME = "Timestamp";
+
     public const string CURRENT_ETAG_ALIAS = ":currentETag";
 }
