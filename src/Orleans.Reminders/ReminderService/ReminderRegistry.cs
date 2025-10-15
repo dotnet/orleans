249509550663c90f7729ff3b3ae5ec811776d773
Da-Teach <da_teach@thehackerwithin.com>
2025-10-15 23:55:25 +0200

    Remove infinite timespan reminder checks (#9715)
    * Timeout.InfiniteTimeSpan is not allowed for reminders
    
    * Specifically check for InfiniteTimeSpan to make clear it's usage is not allowed (and to allow for an eventual future change of the InfiniteTimeSpan value)
    
    ---------
    
    Co-authored-by: Jerremy Koot <jerremy.koot@gmail.com>

--- src/Orleans.Reminders/ReminderService/ReminderRegistry.cs
+++ src/Orleans.Reminders/ReminderService/ReminderRegistry.cs
@@ -24,14 +24,19 @@ namespace Orleans.Runtime.ReminderService
 
         public Task<IGrainReminder> RegisterOrUpdateReminder(GrainId callingGrainId, string reminderName, TimeSpan dueTime, TimeSpan period)
         {
-            // Perform input volatility checks that are consistent with System.Threading.Timer
-            // http://referencesource.microsoft.com/#mscorlib/system/threading/timer.cs,c454f2afe745d4d3,references
-            if (dueTime.Ticks < 0 && dueTime != Timeout.InfiniteTimeSpan)
+            // Perform input volatility checks 
+            if (dueTime == Timeout.InfiniteTimeSpan)
+                throw new ArgumentOutOfRangeException(nameof(dueTime), "Cannot use InfiniteTimeSpan dueTime to create a reminder");
+
+            if (dueTime.Ticks < 0)
                 throw new ArgumentOutOfRangeException(nameof(dueTime), "Cannot use negative dueTime to create a reminder");
-           
-            if (period.Ticks < 0 && period != Timeout.InfiniteTimeSpan)
+
+            if (period == Timeout.InfiniteTimeSpan)
+                throw new ArgumentOutOfRangeException(nameof(period), "Cannot use InfiniteTimeSpan period to create a reminder");
+
+            if (period.Ticks < 0)
                 throw new ArgumentOutOfRangeException(nameof(period), "Cannot use negative period to create a reminder");
-          
+            
             var minReminderPeriod = options.MinimumReminderPeriod;
             if (period < minReminderPeriod)
                 throw new ArgumentException($"Cannot register reminder {reminderName} as requested period ({period}) is less than minimum allowed reminder period ({minReminderPeriod})");
