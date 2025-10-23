5329033568485ad5afaaabfec21dc4080f5b99f1
Benjamin Petit <bpetit@microsoft.com>
2025-10-23 21:57:02 +0200

    Add comments

--- src/Orleans.ScheduledJobs/IScheduledJobHandler.cs
+++ src/Orleans.ScheduledJobs/IScheduledJobHandler.cs
@@ -3,27 +3,57 @@ using System.Threading.Tasks;
 
 namespace Orleans.ScheduledJobs;
 
+/// <summary>
+/// Provides contextual information about a scheduled job execution.
+/// </summary>
 public interface IScheduledJobContext
 {
+    /// <summary>
+    /// Gets the scheduled job being executed.
+    /// </summary>
     IScheduledJob Job { get; }
 
+    /// <summary>
+    /// Gets the unique identifier for this execution run.
+    /// </summary>
     string RunId { get; }
 
+    /// <summary>
+    /// Gets the number of times this job has been dequeued for execution, including retries.
+    /// </summary>
     int DequeueCount { get; }
 }
 
+/// <summary>
+/// Represents the execution context for a scheduled job.
+/// </summary>
 [GenerateSerializer]
 internal class ScheduledJobContext : IScheduledJobContext
 {
+    /// <summary>
+    /// Gets the scheduled job being executed.
+    /// </summary>
     [Id(0)]
     public IScheduledJob Job { get; }
 
+    /// <summary>
+    /// Gets the unique identifier for this execution run.
+    /// </summary>
     [Id(1)]
     public string RunId { get; }
 
+    /// <summary>
+    /// Gets the number of times this job has been dequeued for execution, including retries.
+    /// </summary>
     [Id(2)]
     public int DequeueCount { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScheduledJobContext"/> class.
+    /// </summary>
+    /// <param name="job">The scheduled job to execute.</param>
+    /// <param name="runId">The unique identifier for this execution run.</param>
+    /// <param name="retryCount">The number of times this job has been dequeued, including retries.</param>
     public ScheduledJobContext(IScheduledJob job, string runId, int retryCount)
     {
         Job = job;
@@ -32,7 +62,52 @@ internal class ScheduledJobContext : IScheduledJobContext
     }
 }
 
+/// <summary>
+/// Defines the interface for handling scheduled job execution.
+/// Grains implement this interface to receive and process scheduled jobs.
+/// </summary>
+/// <remarks>
+/// <para>
+/// Grains that implement this interface can be targeted by scheduled jobs.
+/// The <see cref="ExecuteJobAsync"/> method is invoked when the job's due time is reached.
+/// </para>
+/// <example>
+/// The following example demonstrates a grain that implements <see cref="IScheduledJobHandler"/>:
+/// <code>
+/// public class MyGrain : Grain, IScheduledJobHandler
+/// {
+///     public Task ExecuteJobAsync(IScheduledJobContext context, CancellationToken cancellationToken)
+///     {
+///         // Process the scheduled job
+///         var jobName = context.Job.Name;
+///         var dueTime = context.Job.DueTime;
+///         
+///         // Perform job logic here
+///         
+///         return Task.CompletedTask;
+///     }
+/// }
+/// </code>
+/// </example>
+/// </remarks>
 public interface IScheduledJobHandler
 {
+    /// <summary>
+    /// Executes the scheduled job with the provided context.
+    /// </summary>
+    /// <param name="context">The context containing information about the scheduled job execution.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous job execution operation.</returns>
+    /// <remarks>
+    /// <para>
+    /// This method is invoked by the Orleans scheduled jobs infrastructure when a job's due time is reached.
+    /// Implementations should handle job execution logic and can use information from the <paramref name="context"/>
+    /// to access job metadata, dequeue count for retry logic, and other execution details.
+    /// </para>
+    /// <para>
+    /// If the method throws an exception and a retry policy is configured, the job may be retried.
+    /// The <see cref="IScheduledJobContext.DequeueCount"/> property can be used to determine if this is a retry attempt.
+    /// </para>
+    /// </remarks>
     Task ExecuteJobAsync(IScheduledJobContext context, CancellationToken cancellationToken);
 }
--- src/Orleans.ScheduledJobs/ScheduledJob.cs
+++ src/Orleans.ScheduledJobs/ScheduledJob.cs
@@ -4,29 +4,77 @@ using Orleans.Runtime;
 
 namespace Orleans.ScheduledJobs;
 
+/// <summary>
+/// Represents a scheduled job that will be executed at a specific time.
+/// </summary>
 public interface IScheduledJob
 {
+    /// <summary>
+    /// Gets the unique identifier for this scheduled job.
+    /// </summary>
     string Id { get; }
+    
+    /// <summary>
+    /// Gets the name of the scheduled job.
+    /// </summary>
     string Name { get; }
+    
+    /// <summary>
+    /// Gets the time when this job is due to be executed.
+    /// </summary>
     DateTimeOffset DueTime { get; }
+    
+    /// <summary>
+    /// Gets the identifier of the target grain that will handle this job.
+    /// </summary>
     GrainId TargetGrainId { get; }
+    
+    /// <summary>
+    /// Gets optional metadata associated with this scheduled job.
+    /// </summary>
     IReadOnlyDictionary<string, string>? Metadata { get; }
 }
 
+/// <summary>
+/// Represents a scheduled job with shard information.
+/// </summary>
 [GenerateSerializer]
 [Alias("Orleans.ScheduledJobs.ScheduledJob")]
 public sealed class ScheduledJob : IScheduledJob
 {
+    /// <summary>
+    /// Gets the unique identifier for this scheduled job.
+    /// </summary>
     [Id(0)]
     public required string Id { get; init; }
+    
+    /// <summary>
+    /// Gets the name of the scheduled job.
+    /// </summary>
     [Id(1)]
     public required string Name { get; init; }
+    
+    /// <summary>
+    /// Gets the time when this job is due to be executed.
+    /// </summary>
     [Id(2)]
     public DateTimeOffset DueTime { get; init; }
+    
+    /// <summary>
+    /// Gets the identifier of the target grain that will handle this job.
+    /// </summary>
     [Id(3)]
     public GrainId TargetGrainId { get; init; }
+    
+    /// <summary>
+    /// Gets the identifier of the shard that manages this scheduled job.
+    /// </summary>
     [Id(4)]
     public required string ShardId { get; init; }
+    
+    /// <summary>
+    /// Gets optional metadata associated with this scheduled job.
+    /// </summary>
     [Id(5)]
     public IReadOnlyDictionary<string, string>? Metadata { get; init; }
 }
