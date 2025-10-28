2cb575a76873460f0fdeaa8bef45ae8497dc0101
Bryan Mager <Bryan.Mager@mt.com>
2025-10-28 11:51:14 -0400

    Add comments

--- src/Orleans.Streaming/Core/OldestInStreamToken.cs
+++ src/Orleans.Streaming/Core/OldestInStreamToken.cs
@@ -15,6 +15,10 @@ public sealed class OldestInStreamToken : StreamSequenceToken
     /// Always -1, which is less than any other valid sequence number.
     /// </summary>
     public override long SequenceNumber { get => -1; protected set { } }
+
+    /// <summary>
+    /// Always 0, as this is a conceptual token representing the oldest event in the stream.
+    /// </summary>
     public override int EventIndex { get => 0; protected set { } }
 
     /// <summary>
@@ -22,11 +26,15 @@ public sealed class OldestInStreamToken : StreamSequenceToken
     /// </summary>
     public static OldestInStreamToken Instance { get; } = new OldestInStreamToken();
 
+    /// <inheritdoc/>
     public override bool Equals(StreamSequenceToken other)
     {
         return other is OldestInStreamToken;
     }
 
+    /// <summary>
+    /// Always less than any other token, except another <see cref="OldestInStreamToken"/>.
+    /// </summary>
     public override int CompareTo(StreamSequenceToken other)
     {
         if (other is OldestInStreamToken) return 0;
