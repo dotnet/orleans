616e4886a42c20731a4658c788728f4e28ad2628
Benjamin Petit <bpetit@microsoft.com>
2025-10-30 13:10:13 +0100

    Move ILocalScheduledJobManager interface to its own file

--- src/Orleans.ScheduledJobs/LocalScheduledJobManager.cs
+++ src/Orleans.ScheduledJobs/LocalScheduledJobManager.cs
@@ -15,31 +15,6 @@ using Orleans.Runtime.Scheduler;
 
 namespace Orleans.ScheduledJobs;
 
-/// <summary>
-/// Provides functionality for scheduling and managing jobs on the local silo.
-/// </summary>
-public interface ILocalScheduledJobManager
-{
-    /// <summary>
-    /// Schedules a job to be executed at a specific time on the target grain.
-    /// </summary>
-    /// <param name="target">The grain identifier of the target grain that will receive the scheduled job.</param>
-    /// <param name="jobName">The name of the job for identification purposes.</param>
-    /// <param name="dueTime">The date and time when the job should be executed.</param>
-    /// <param name="metadata">Optional metadata associated with the job.</param>
-    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
-    /// <returns>A <see cref="Task"/> representing the asynchronous operation that returns the scheduled job.</returns>
-    Task<IScheduledJob> ScheduleJobAsync(GrainId target, string jobName, DateTimeOffset dueTime, IReadOnlyDictionary<string, string>? metadata, CancellationToken cancellationToken);
-
-    /// <summary>
-    /// Attempts to cancel a previously scheduled job.
-    /// </summary>
-    /// <param name="job">The scheduled job to cancel.</param>
-    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
-    /// <returns>A <see cref="Task"/> representing the asynchronous operation that returns <see langword="true"/> if the job was successfully canceled; otherwise, <see langword="false"/>.</returns>
-    Task<bool> TryCancelScheduledJobAsync(IScheduledJob job, CancellationToken cancellationToken);
-}
-
 /// <inheritdoc/>
 internal partial class LocalScheduledJobManager : SystemTarget, ILocalScheduledJobManager, ILifecycleParticipant<ISiloLifecycle>
 {
