838c9bb28f3defd4d0a93f2dc6926a3e402a69ee
Benjamin Petit <bpetit@microsoft.com>
2025-11-05 11:04:02 +0100

    Rename Orleans.ScheduledJobs to Orleans.DurableJobs
    - Moved all source files from src/Orleans.ScheduledJobs to src/Orleans.DurableJobs
    - Renamed project file to Orleans.DurableJobs.csproj and updated PackageId
    - Updated all project references and solution entries to point to Orleans.DurableJobs
    - Restored file contents after migration
    - Ensured no changes to namespaces or assembly names

--- src/Orleans.Runtime/Properties/AssemblyInfo.cs
+++ src/Orleans.Runtime/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Orleans.Streaming")]
 [assembly: InternalsVisibleTo("Orleans.Reminders")]
-[assembly: InternalsVisibleTo("Orleans.ScheduledJobs")]
+[assembly: InternalsVisibleTo("Orleans.DurableJobs")]
 [assembly: InternalsVisibleTo("Orleans.Journaling")]
 [assembly: InternalsVisibleTo("Orleans.TestingHost")]
 
