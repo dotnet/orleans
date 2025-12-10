ac3695e5f48b8b1606c4cfcce05cef7d134e07c8
大神 <xuchen@babuyo.com>
2025-12-10 17:12:50 +0800

    support memorypack serialization

---
+++ src/Orleans.Serialization.MemoryPack/MemoryPackCodecOptions.cs
@@ -0,0 +1,27 @@
+using MemoryPack;
+using System;
+using System.Collections.Generic;
+
+namespace Orleans.Serialization;
+
+/// <summary>
+/// Options for <see cref="MemoryPackCodec"/>.
+/// </summary>
+public class MemoryPackCodecOptions
+{
+    //
+    /// <summary>
+    /// Gets or sets the <see cref="MemoryPackSerializerOptions"/>.
+    /// </summary>
+    public MemoryPackSerializerOptions SerializerOptions { get; set; } = MemoryPackSerializerOptions.Default;
+
+    /// <summary>
+    /// Gets or sets a delegate used to determine if a type is supported by the MemoryPack serializer for serialization and deserialization.
+    /// </summary>
+    public Func<Type, bool?> IsSerializableType { get; set; }
+
+    /// <summary>
+    /// Gets or sets a delegate used to determine if a type is supported by the MemoryPack serializer for copying.
+    /// </summary>
+    public Func<Type, bool?> IsCopyableType { get; set; }
+}
