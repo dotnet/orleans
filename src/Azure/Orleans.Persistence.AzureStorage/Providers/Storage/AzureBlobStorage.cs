c4052c52150dfe6dce09c99c6523e560e1247fbc
ayoub-aalilija <ayoub.aalilija@gmail.com>
2025-12-16 15:46:05 +0100

    Throw InconsistentStateException only when the azure blob is missing
    This allows WriteStateAndCreateContainerIfNotExists to handle
    container creation when it does not exist

--- src/Azure/Orleans.Persistence.AzureStorage/Providers/Storage/AzureBlobStorage.cs
+++ src/Azure/Orleans.Persistence.AzureStorage/Providers/Storage/AzureBlobStorage.cs
@@ -222,7 +222,7 @@ namespace Orleans.Storage
             {
                 return await updateOperation(state).ConfigureAwait(false);
             }
-            catch (RequestFailedException ex) when (ex.IsPreconditionFailed() || ex.IsConflict() || ex.IsNotFound())
+            catch (RequestFailedException ex) when (ex.IsPreconditionFailed() || ex.IsConflict() || ex.IsBlobNotFound())
             {
                 throw new InconsistentStateException($"Blob storage condition not Satisfied. BlobName: {blob.Name}, Container: {blob.BlobContainerName}, CurrentETag: {currentETag}", "Unknown", currentETag, ex);
             }
