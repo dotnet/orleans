e26dadffa40e939d0b5de542193fa16a1edcf21b
Ryan Karg <ryankarg@microsoft.com>
2026-01-05 15:24:07 -0800

    Separating class

--- src/Orleans.Core/Diagnostics/ActivityPropagationGrainCallFilter.cs
+++ src/Orleans.Core/Diagnostics/ActivityPropagationGrainCallFilter.cs
@@ -71,16 +71,6 @@ namespace Orleans.Runtime
         }
     }
 
-    public static class ActivitySources
-    {
-        public static string ApplicationGrainActivitySourceName = "Microsoft.Orleans.Application";
-        public static string RuntimeActivitySourceName = "Microsoft.Orleans.Runtime";
-
-        internal static readonly ActivitySource ApplicationGrainSource = new(ApplicationGrainActivitySourceName, "1.0.0");
-        internal static readonly ActivitySource RuntimeGrainSource = new(RuntimeActivitySourceName, "1.0.0");
-
-    }
-
     /// <summary>
     /// Propagates distributed context information to outgoing requests.
     /// </summary>
