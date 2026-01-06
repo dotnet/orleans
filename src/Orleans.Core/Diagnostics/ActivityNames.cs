8d18289fedf3e0bdfbe791a4bd8dc8e525df630a
Ryan Karg <ryankarg@microsoft.com>
2026-01-05 19:03:19 -0800

    aligning span naming with OTel conventions
    https://opentelemetry.io/blog/2025/how-to-name-your-spans/#verb-object

---
+++ src/Orleans.Core/Diagnostics/ActivityNames.cs
@@ -0,0 +1,11 @@
+namespace Orleans.Runtime;
+
+public static class ActivityNames
+{
+    public static string PlaceGrain = "place grain";
+    public static string FilterPlacementCandidates = "filter placement candidates";
+    public static string ActivateGrain = "activate grain";
+    public static string OnActivate = "execute OnActivateAsync";
+    public static string RegisterDirectoryEntry = "register directory entry";
+
+}
