d71191562ce4b1e541275349b7ad58df300b34e8
Ryan Karg <ryankarg@microsoft.com>
2026-01-06 16:09:13 -0800

    Updating ActivitySource versions to denote changes around IAsyncEnumerable spans.
    Runtime (1.0.0 -> 2.0.0) no longer gets the Start/MoveNext/Dispose spans
    Application (1.0.0 -> 1.1.0) now gets the Start/MoveNext/Dispose spans and these are nested under a new span for the overall grain call being made.

--- src/Orleans.Core.Abstractions/Diagnostics/ActivitySources.cs
+++ src/Orleans.Core.Abstractions/Diagnostics/ActivitySources.cs
@@ -25,8 +25,8 @@ public static class ActivitySources
     /// </summary>
     public const string AllActivitySourceName = "Microsoft.Orleans.*";
 
-    internal static readonly ActivitySource ApplicationGrainSource = new(ApplicationGrainActivitySourceName, "1.0.0");
-    internal static readonly ActivitySource RuntimeGrainSource = new(RuntimeActivitySourceName, "1.0.0");
+    internal static readonly ActivitySource ApplicationGrainSource = new(ApplicationGrainActivitySourceName, "1.1.0");
+    internal static readonly ActivitySource RuntimeGrainSource = new(RuntimeActivitySourceName, "2.0.0");
     internal static readonly ActivitySource LifecycleGrainSource = new(LifecycleActivitySourceName, "1.0.0");
     internal static readonly ActivitySource StorageGrainSource = new(StorageActivitySourceName, "1.0.0");
 }
