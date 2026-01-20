83f418ebb3592df99f6d451f21945399acb95a96
Reuben Bond <reuben.bond@gmail.com>
2026-01-20 06:16:34 -0800

    WIP

--- playground/WorkflowsApp/WorkflowsApp.AppHost/Program.cs
+++ playground/WorkflowsApp/WorkflowsApp.AppHost/Program.cs
@@ -1,17 +1,13 @@
-﻿var builder = DistributedApplication.CreateBuilder(args);
+var builder = DistributedApplication.CreateBuilder(args);
 builder.AddAzureProvisioning();
 
 var azureStorage = builder.AddAzureStorage("az-storage").RunAsEmulator(builder =>
     builder
-        .WithImageTag("3.33.0"));
-        //.WithLifetime(ContainerLifetime.Persistent));
+        .WithImageTag("3.33.0")
+        .WithLifetime(ContainerLifetime.Session));
 var azureBlobs = azureStorage.AddBlobs("state");
 
-var orleans = builder.AddOrleans("orleans")
-    .WithClustering(azureStorage.AddTables("clustering"));
-
 builder.AddProject<Projects.WorkflowsApp_Service>("workflowsapp-service")
-    .WithReference(orleans)
     .WithReference(azureBlobs, "state")
     .WaitFor(azureStorage);
 
