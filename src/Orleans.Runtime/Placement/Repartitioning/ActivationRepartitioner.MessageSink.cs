a6182379837464e358574c2ccff8d7cd2f280b29
Ledjon Behluli <behluliledjon@hotmail.com>
2026-02-21 22:04:03 +0100

    Generational decaying of anchored grains in LAR

--- src/Orleans.Runtime/Placement/Repartitioning/ActivationRepartitioner.MessageSink.cs
+++ src/Orleans.Runtime/Placement/Repartitioning/ActivationRepartitioner.MessageSink.cs
@@ -1,8 +1,5 @@
 #nullable enable
-using System;
 using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Orleans.Placement.Repartitioning;
 using Orleans.Runtime.Internal;
@@ -15,7 +12,7 @@ internal sealed partial class ActivationRepartitioner : IMessageStatisticsSink
 
     // This filter contains grain ids which will are anchored to the current silo.
     // Ids are inserted when a grain is found to have a negative transfer score.
-    private readonly BlockedBloomFilter? _anchoredFilter;
+    private readonly AnchoredGrainsFilter? _anchoredFilter;
     private Task? _processPendingEdgesTask;
 
     public void StartProcessingEdges()
