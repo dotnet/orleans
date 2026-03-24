f2614f778a5d76c16773a9959f980fae19ff63e1
Ryan Karg <ryankarg@microsoft.com>
2026-03-24 11:06:56 -0700

    fixing nullable based on merge

--- src/Orleans.Core/Runtime/RequestContextExtensions.cs
+++ src/Orleans.Core/Runtime/RequestContextExtensions.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using Orleans.Diagnostics;
 using Orleans.Serialization;
 
 namespace Orleans.Runtime
--- src/Orleans.Runtime/Hosting/DefaultSiloServices.cs
+++ src/Orleans.Runtime/Hosting/DefaultSiloServices.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
