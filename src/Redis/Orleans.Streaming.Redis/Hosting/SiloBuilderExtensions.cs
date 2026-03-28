2389b34a64373e867db75189ac04a04eddeec5fc
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2026-03-28 18:05:53 -0300

    Add hosting folder

---
+++ src/Redis/Orleans.Streaming.Redis/Hosting/SiloBuilderExtensions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans.Hosting;
+
+internal class SiloBuilderExtensions
+{
+}
