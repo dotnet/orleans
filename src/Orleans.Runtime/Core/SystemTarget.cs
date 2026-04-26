13b1767c314d0dc6f7bb10299a32fc9a60c22163
Ledjon Behluli <behluliledjon@hotmail.com>
2026-04-27 00:03:07 +0200

    auto-register timer registry on sys targets

--- src/Orleans.Runtime/Core/SystemTarget.cs
+++ src/Orleans.Runtime/Core/SystemTarget.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Orleans.Runtime.Internal;
 using Orleans.Runtime.Scheduler;
 using Orleans.Serialization.Invocation;
+using Orleans.Timers;
 
 namespace Orleans.Runtime
 {
@@ -112,6 +113,10 @@ namespace Orleans.Runtime
             else if (typeof(TComponent) == typeof(PlacementStrategy))
             {
                 result = (TComponent)(object)SystemTargetPlacementStrategy.Instance;
+            }
+            else if (typeof(TComponent) == typeof(ITimerRegistry))
+            {
+                result = (TComponent)(object)_shared.TimerRegistry;
             }
             else
             {
