da314f0752aa509ffe565e9356284eb93c5f4249
Meir Blachman <meirblachman@gmail.com>
2026-05-27 19:41:18 +0300

    chore: seal watchdog instruments

--- src/Orleans.Core/Diagnostics/Metrics/WatchdogInstruments.cs
+++ src/Orleans.Core/Diagnostics/Metrics/WatchdogInstruments.cs
@@ -2,7 +2,7 @@ using System.Diagnostics.Metrics;
 
 namespace Orleans.Runtime;
 
-internal class WatchdogInstruments(OrleansInstruments instruments)
+internal sealed class WatchdogInstruments(OrleansInstruments instruments)
 {
     private readonly Counter<int> _healthChecks = instruments.Meter.CreateCounter<int>(InstrumentNames.WATCHDOG_NUM_HEALTH_CHECKS);
     private readonly Counter<int> _failedHealthChecks = instruments.Meter.CreateCounter<int>(InstrumentNames.WATCHDOG_NUM_FAILED_HEALTH_CHECKS);
