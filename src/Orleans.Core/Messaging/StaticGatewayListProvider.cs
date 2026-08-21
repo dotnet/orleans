269ae9a50a4ad3a8471fe4c1be2d4d5914759601
Reuben Bond <reuben.bond@gmail.com>
2026-08-21 11:59:07 -0700

    fix: preserve static gateway constructor compatibility

--- src/Orleans.Core/Messaging/StaticGatewayListProvider.cs
+++ src/Orleans.Core/Messaging/StaticGatewayListProvider.cs
@@ -4,37 +4,30 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Orleans.Configuration;
 
-namespace Orleans.Messaging
+namespace Orleans.Messaging;
+
+/// <summary>
+/// <see cref="IGatewayListProvider"/> implementation which returns a static list, configured via <see cref="StaticGatewayListProviderOptions"/>.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="StaticGatewayListProvider"/> class.
+/// </remarks>
+/// <param name="options">The specific options.</param>
+/// <param name="gatewayOptions">The general gateway options.</param>
+public class StaticGatewayListProvider(IOptions<StaticGatewayListProviderOptions> options, IOptions<GatewayOptions> gatewayOptions) : IGatewayListProvider
 {
-    /// <summary>
-    /// <see cref="IGatewayListProvider"/> implementation which returns a static list, configured via <see cref="StaticGatewayListProviderOptions"/>.
-    /// </summary>
-    /// <remarks>
-    /// Initializes a new instance of the <see cref="StaticGatewayListProvider"/> class.
-    /// </remarks>
-    /// <param name="optionsAccessor">The specific options.</param>
-    /// <param name="gatewayOptions">The general gateway options.</param>
-    public class StaticGatewayListProvider(IOptions<StaticGatewayListProviderOptions> optionsAccessor, IOptions<GatewayOptions> gatewayOptions) : IGatewayListProvider
-    {
-        private readonly StaticGatewayListProviderOptions options = optionsAccessor.Value;
-        private readonly TimeSpan maxStaleness = gatewayOptions.Value.GatewayListRefreshPeriod;
+    private readonly StaticGatewayListProviderOptions _options = options.Value;
+    private readonly TimeSpan _maxStaleness = gatewayOptions.Value.GatewayListRefreshPeriod;
 
-        /// <inheritdoc />
-        public Task InitializeGatewayListProvider() => Task.CompletedTask;
-        
-        /// <inheritdoc />
-        public Task<IList<Uri>> GetGateways() => Task.FromResult<IList<Uri>>(this.options.Gateways);
+    /// <inheritdoc />
+    public Task InitializeGatewayListProvider() => Task.CompletedTask;
 
-        /// <inheritdoc />
-        public TimeSpan MaxStaleness
-        {
-            get => this.maxStaleness;
-        }
+    /// <inheritdoc />
+    public Task<IList<Uri>> GetGateways() => Task.FromResult<IList<Uri>>(_options.Gateways);
 
-        /// <inheritdoc />
-        public bool IsUpdatable
-        {
-            get => true;
-        }
-    }
+    /// <inheritdoc />
+    public TimeSpan MaxStaleness => _maxStaleness;
+
+    /// <inheritdoc />
+    public bool IsUpdatable => true;
 }
