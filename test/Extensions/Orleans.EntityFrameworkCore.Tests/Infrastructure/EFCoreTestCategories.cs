6a93bfc85ce3a35af85f5a9766ed504303b89dfd
Reuben Bond <reuben.bond@gmail.com>
2026-08-22 02:48:03 -0700

    fix(efcore): isolate provider test categories

--- test/Extensions/Orleans.EntityFrameworkCore.Tests/Infrastructure/EFCoreTestCategories.cs
+++ test/Extensions/Orleans.EntityFrameworkCore.Tests/Infrastructure/EFCoreTestCategories.cs
@@ -3,7 +3,7 @@ namespace Orleans.EntityFrameworkCore.Tests.Infrastructure;
 public static class EFCoreTestCategories
 {
     public const string SqlServer = "EFCore-SqlServer";
-    public const string MySqlProvider = "MySql";
-    public const string PostgreSqlProvider = "PostgreSql";
+    public const string MySqlProvider = "EFCore-MySql";
+    public const string PostgreSqlProvider = "EFCore-PostgreSql";
     public const string Functional = "Functional";
 }
