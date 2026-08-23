267807c02b2ddebefec019165f4f08f52ea6d36c
Reuben Bond <reuben.bond@gmail.com>
2026-08-23 03:47:58 -0700

    fix: activate grain journaling providers from configuration
    (cherry picked from commit f3b03760f82d452d46d257eead3d35db64f8ee39)

--- src/Orleans.Runtime/Hosting/DefaultSiloServices.cs
+++ src/Orleans.Runtime/Hosting/DefaultSiloServices.cs
@@ -482,6 +482,7 @@ namespace Orleans.Hosting
 
             ApplySubsection(builder, cfg, knownProviderTypes, "Clustering");
             ApplySubsection(builder, cfg, knownProviderTypes, "Reminders");
+            ApplySubsection(builder, cfg, knownProviderTypes, "GrainJournaling");
             ApplyNamedSubsections(builder, cfg, knownProviderTypes, "BroadcastChannel");
             ApplyNamedSubsections(builder, cfg, knownProviderTypes, "Streaming");
             ApplyNamedSubsections(builder, cfg, knownProviderTypes, "GrainStorage");
