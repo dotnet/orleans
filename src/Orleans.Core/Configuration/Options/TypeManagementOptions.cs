1d012b3ad71932f4621572e0cbaa5a56232a586f
Reuben Bond <reuben.bond@gmail.com>
2026-08-27 14:53:44 -0700

    feat(client): wait for grain type availability

--- src/Orleans.Core/Configuration/Options/TypeManagementOptions.cs
+++ src/Orleans.Core/Configuration/Options/TypeManagementOptions.cs
@@ -11,15 +11,6 @@ namespace Orleans.Configuration
         /// The number of seconds to refresh the cluster grain interface map
         /// </summary>
         public TimeSpan TypeMapRefreshInterval { get; set; } = DEFAULT_REFRESH_CLUSTER_INTERFACEMAP_TIME;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether grain references wait for a compatible implementation to appear in the cluster manifest.
-        /// </summary>
-        /// <remarks>
-        /// When disabled, requesting a grain interface which has no known implementation throws immediately.
-        /// </remarks>
-        public bool EnableDeferredGrainTypeResolution { get; set; } = true;
-
         /// <summary>
         /// The default interval between cluster grain interface map refreshes.
         /// </summary>
