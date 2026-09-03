a1bc8211c96a550ddb419422da28589a55f15f56
Reuben Bond <reuben.bond@gmail.com>
2026-09-02 21:49:08 -0700

    docs: document core placement contracts

--- src/Orleans.Core/Placement/IPlacementFilterDirector.cs
+++ src/Orleans.Core/Placement/IPlacementFilterDirector.cs
@@ -4,7 +4,17 @@ using Orleans.Runtime.Placement;
 
 namespace Orleans.Placement;
 
+/// <summary>
+/// Filters the silos which are candidates for a grain placement operation.
+/// </summary>
 public interface IPlacementFilterDirector
 {
+    /// <summary>
+    /// Filters the candidate silos for the specified placement target.
+    /// </summary>
+    /// <param name="filterStrategy">The placement filter strategy to apply.</param>
+    /// <param name="target">The grain and request context for the placement operation.</param>
+    /// <param name="silos">The candidate silos to filter.</param>
+    /// <returns>The silos which satisfy the placement filter.</returns>
     IEnumerable<SiloAddress> Filter(PlacementFilterStrategy filterStrategy, PlacementTarget target, IEnumerable<SiloAddress> silos);
 }
--- src/Orleans.Core/Placement/Repartitioning/IActivationRepartitionerSystemTarget.cs
+++ src/Orleans.Core/Placement/Repartitioning/IActivationRepartitionerSystemTarget.cs
@@ -8,47 +8,76 @@ using System;
 
 namespace Orleans.Placement.Repartitioning;
 
+/// <summary>
+/// Defines the system-target contract used to coordinate activation exchanges between silos.
+/// </summary>
 [Alias("IActivationRepartitionerSystemTarget")]
 internal interface IActivationRepartitionerSystemTarget : ISystemTarget
 {
+    /// <summary>
+    /// Gets a reference to the activation repartitioner system target on the specified silo.
+    /// </summary>
+    /// <param name="grainFactory">The grain factory used to create the system-target reference.</param>
+    /// <param name="targetSilo">The silo hosting the target activation repartitioner.</param>
+    /// <returns>A reference to the activation repartitioner system target on <paramref name="targetSilo"/>.</returns>
     static IActivationRepartitionerSystemTarget GetReference(IGrainFactory grainFactory, SiloAddress targetSilo)
         => grainFactory.GetGrain<IActivationRepartitionerSystemTarget>(SystemTargetGrainId.Create(Constants.ActivationRepartitionerType, targetSilo).GrainId);
 
+    /// <summary>
+    /// Starts a repartitioning round by proposing an activation exchange to another active silo.
+    /// </summary>
+    /// <returns>A value task representing the exchange attempt.</returns>
     [ResponseTimeout("00:10:00")]
     [Alias("A6EE4757")]
     ValueTask TriggerExchangeRequest(CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Evaluates an activation exchange proposed by another silo.
+    /// </summary>
+    /// <param name="request">The proposed activation exchange.</param>
+    /// <returns>A response describing whether the exchange was accepted and which activations each silo will transfer.</returns>
     [ResponseTimeout("00:10:00")]
     [Alias("9D8EDC44")]
     ValueTask<AcceptExchangeResponse> AcceptExchangeRequest(AcceptExchangeRequest request, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// For use in testing only!
+    /// Resets the collected grain-call statistics. This method supports testing.
     /// </summary>
+    /// <param name="cancellationToken">The token used to cancel the operation.</param>
+    /// <returns>A value task representing the reset operation.</returns>
     [Alias("21852A09")]
     ValueTask ResetCounters(CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// For use in testing only!
+    /// Gets the current number of activations on the silo. This method supports testing.
     /// </summary>
+    /// <param name="cancellationToken">The token used to cancel the operation.</param>
+    /// <returns>The current number of activations on the silo.</returns>
     [Alias("9FB525F3")]
     ValueTask<int> GetActivationCount(CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// For use in testing only!
+    /// Sets an offset which is added to the current activation count. This method supports testing.
     /// </summary>
+    /// <param name="activationCountOffset">The offset to add to the current activation count.</param>
+    /// <param name="cancellationToken">The token used to cancel the operation.</param>
+    /// <returns>A value task representing the update operation.</returns>
     [Alias("135356E5")]
     ValueTask SetActivationCountOffset(int activationCountOffset, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// For diagnostics only.
+    /// Gets the estimated grain-call frequencies collected on the silo.
     /// </summary>
+    /// <param name="cancellationToken">The token used to cancel the operation.</param>
+    /// <returns>The communication edges and their estimated call counts.</returns>
     [Alias("C4497899")]
     ValueTask<ImmutableArray<(Edge, ulong)>> GetGrainCallFrequencies(CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// For use in testing only! Flushes buffered messages.
+    /// Waits until all buffered messages have been removed from the input buffer. This method supports testing.
     /// </summary>
+    /// <param name="cancellationToken">The token used to cancel the operation.</param>
+    /// <returns>A value task representing the flush operation.</returns>
     [Alias("11731652")]
     ValueTask FlushBuffers(CancellationToken cancellationToken = default);
 }
@@ -57,57 +86,114 @@ internal interface IActivationRepartitionerSystemTarget : ISystemTarget
 /// <summary>
 /// Data structure representing a 'communication edge' between a source and target.
 /// </summary>
+/// <param name="source">The source vertex.</param>
+/// <param name="target">The target vertex.</param>
 [GenerateSerializer, Immutable, DebuggerDisplay("Source: [{Source.Id} - {Source.Silo}] | Target: [{Target.Id} - {Target.Silo}]")]
 internal readonly struct Edge(EdgeVertex source, EdgeVertex target) : IEquatable<Edge>
 {
+    /// <summary>
+    /// Gets the source vertex.
+    /// </summary>
     [Id(0)]
     public EdgeVertex Source { get; } = source;
 
+    /// <summary>
+    /// Gets the target vertex.
+    /// </summary>
     [Id(1)]
     public EdgeVertex Target { get; } = target;
 
+    /// <summary>
+    /// Determines whether two edges are equal.
+    /// </summary>
+    /// <param name="left">The first edge to compare.</param>
+    /// <param name="right">The second edge to compare.</param>
+    /// <returns><see langword="true"/> if the edges are equal; otherwise, <see langword="false"/>.</returns>
     public static bool operator ==(Edge left, Edge right) => left.Equals(right);
+
+    /// <summary>
+    /// Determines whether two edges are not equal.
+    /// </summary>
+    /// <param name="left">The first edge to compare.</param>
+    /// <param name="right">The second edge to compare.</param>
+    /// <returns><see langword="true"/> if the edges are not equal; otherwise, <see langword="false"/>.</returns>
     public static bool operator !=(Edge left, Edge right) => !left.Equals(right);
 
+    /// <inheritdoc />
     public override bool Equals([NotNullWhen(true)] object? obj) => obj is Edge other && Equals(other);
+
+    /// <inheritdoc />
     public bool Equals(Edge other) => Source == other.Source && Target == other.Target;
 
+    /// <inheritdoc />
     public override int GetHashCode() => HashCode.Combine(Source, Target);
 
     /// <summary>
     /// Returns a copy of this but with flipped sources and targets.
     /// </summary>
+    /// <returns>An edge whose source and target are reversed.</returns>
     public Edge Flip() => new(source: Target, target: Source);
 
+    /// <inheritdoc />
     public override string ToString() => $"[{Source} -> {Target}]";
 }
 
 /// <summary>
 /// Data structure representing one side of a <see cref="Edge"/>.
 /// </summary>
+/// <param name="id">The grain identifier.</param>
+/// <param name="silo">The silo where the grain activation resides.</param>
+/// <param name="isMigratable">Whether the activation can be migrated.</param>
 [GenerateSerializer, Immutable]
 public readonly struct EdgeVertex(
     GrainId id,
     SiloAddress silo,
     bool isMigratable) : IEquatable<EdgeVertex>
 {
+    /// <summary>
+    /// The grain identifier.
+    /// </summary>
     [Id(0)]
     public readonly GrainId Id = id;
 
+    /// <summary>
+    /// The silo where the grain activation resides.
+    /// </summary>
     [Id(1)]
     public readonly SiloAddress Silo = silo;
 
+    /// <summary>
+    /// A value indicating whether the activation can be migrated.
+    /// </summary>
     [Id(2)]
     public readonly bool IsMigratable = isMigratable;
 
+    /// <summary>
+    /// Determines whether two vertices are equal.
+    /// </summary>
+    /// <param name="left">The first vertex to compare.</param>
+    /// <param name="right">The second vertex to compare.</param>
+    /// <returns><see langword="true"/> if the vertices are equal; otherwise, <see langword="false"/>.</returns>
     public static bool operator ==(EdgeVertex left, EdgeVertex right) => left.Equals(right);
+
+    /// <summary>
+    /// Determines whether two vertices are not equal.
+    /// </summary>
+    /// <param name="left">The first vertex to compare.</param>
+    /// <param name="right">The second vertex to compare.</param>
+    /// <returns><see langword="true"/> if the vertices are not equal; otherwise, <see langword="false"/>.</returns>
     public static bool operator !=(EdgeVertex left, EdgeVertex right) => !left.Equals(right);
 
+    /// <inheritdoc />
     public override bool Equals([NotNullWhen(true)] object? obj) => obj is EdgeVertex other && Equals(other);
+
+    /// <inheritdoc />
     public bool Equals(EdgeVertex other) => Id == other.Id && Silo.Equals(other.Silo) && IsMigratable == other.IsMigratable;
 
+    /// <inheritdoc />
     public override int GetHashCode() => HashCode.Combine(Id, Silo, IsMigratable);
 
+    /// <inheritdoc />
     public override string ToString() => $"[{Id}@{Silo}{(IsMigratable ? "" : "/NotMigratable")}]";
 }
 
@@ -136,29 +222,69 @@ internal sealed class CandidateVertex
     [Id(2), Immutable]
     public ImmutableArray<CandidateConnectedVertex> ConnectedVertices { get; init; } = [];
 
+    /// <inheritdoc />
     public override string ToString() => $"[{Id} * {AccumulatedTransferScore} -> [{string.Join(", ", ConnectedVertices)}]]";
 }
 
+/// <summary>
+/// Represents a vertex connected to an activation migration candidate and its contribution to the transfer score.
+/// </summary>
+/// <param name="id">The connected grain identifier.</param>
+/// <param name="transferScore">The transfer score contributed by the connection.</param>
 [GenerateSerializer, Immutable]
 public readonly struct CandidateConnectedVertex(GrainId id, long transferScore)
 {
+    /// <summary>
+    /// Gets the connected grain identifier.
+    /// </summary>
     [Id(0)]
     public GrainId Id { get; } = id;
 
+    /// <summary>
+    /// Gets the transfer score contributed by the connection.
+    /// </summary>
     [Id(1)]
     public long TransferScore { get; } = transferScore;
 
+    /// <summary>
+    /// Determines whether two connected candidate vertices are equal.
+    /// </summary>
+    /// <param name="left">The first connected vertex to compare.</param>
+    /// <param name="right">The second connected vertex to compare.</param>
+    /// <returns><see langword="true"/> if the connected vertices are equal; otherwise, <see langword="false"/>.</returns>
     public static bool operator ==(CandidateConnectedVertex left, CandidateConnectedVertex right) => left.Equals(right);
+
+    /// <summary>
+    /// Determines whether two connected candidate vertices are not equal.
+    /// </summary>
+    /// <param name="left">The first connected vertex to compare.</param>
+    /// <param name="right">The second connected vertex to compare.</param>
+    /// <returns><see langword="true"/> if the connected vertices are not equal; otherwise, <see langword="false"/>.</returns>
     public static bool operator !=(CandidateConnectedVertex left, CandidateConnectedVertex right) => !left.Equals(right);
 
+    /// <inheritdoc />
     public override bool Equals([NotNullWhen(true)] object? obj) => obj is CandidateConnectedVertex other && Equals(other);
+
+    /// <summary>
+    /// Determines whether this instance is equal to another connected candidate vertex.
+    /// </summary>
+    /// <param name="other">The connected candidate vertex to compare with this instance.</param>
+    /// <returns><see langword="true"/> if the instances are equal; otherwise, <see langword="false"/>.</returns>
     public bool Equals(CandidateConnectedVertex other) => Id == other.Id && TransferScore == other.TransferScore;
 
+    /// <inheritdoc />
     public override int GetHashCode() => HashCode.Combine(Id, TransferScore);
 
+    /// <inheritdoc />
     public override string ToString() => $"[{Id} * {TransferScore}]";
 }
 
+/// <summary>
+/// Represents a proposed activation exchange from another silo.
+/// </summary>
+/// <param name="sendingSilo">The silo proposing the exchange.</param>
+/// <param name="exchangeSet">The activations which the sending silo proposes transferring.</param>
+/// <param name="activationCountSnapshot">The sending silo's activation count when the request was created.</param>
 [GenerateSerializer, Immutable]
 internal sealed class AcceptExchangeRequest(SiloAddress sendingSilo, ImmutableArray<CandidateVertex> exchangeSet, int activationCountSnapshot)
 {
@@ -181,12 +307,28 @@ internal sealed class AcceptExchangeRequest(SiloAddress sendingSilo, ImmutableAr
     public int ActivationCountSnapshot { get; } = activationCountSnapshot;
 }
 
+/// <summary>
+/// Represents the result of evaluating a proposed activation exchange.
+/// </summary>
+/// <param name="type">The result of the exchange request.</param>
+/// <param name="acceptedGrains">The grains accepted from the requesting silo.</param>
+/// <param name="givenGrains">The grains offered to the requesting silo.</param>
 [GenerateSerializer, Immutable]
 internal sealed class AcceptExchangeResponse(AcceptExchangeResponse.ResponseType type, ImmutableArray<GrainId> acceptedGrains, ImmutableArray<GrainId> givenGrains)
 {
+    /// <summary>
+    /// Gets a cached response indicating that the receiving silo completed another exchange too recently.
+    /// </summary>
     public static readonly AcceptExchangeResponse CachedExchangedRecently = new(ResponseType.ExchangedRecently, [], []);
+
+    /// <summary>
+    /// Gets a cached response indicating that both silos attempted to initiate the same exchange.
+    /// </summary>
     public static readonly AcceptExchangeResponse CachedMutualExchangeAttempt = new(ResponseType.MutualExchangeAttempt, [], []);
 
+    /// <summary>
+    /// Gets the result of the exchange request.
+    /// </summary>
     [Id(0)]
     public ResponseType Type { get; } = type;
 
@@ -202,6 +344,9 @@ internal sealed class AcceptExchangeResponse(AcceptExchangeResponse.ResponseType
     [Id(2)]
     public ImmutableArray<GrainId> GivenGrainIds { get; } = givenGrains;
 
+    /// <summary>
+    /// Describes the result of an activation exchange request.
+    /// </summary>
     [GenerateSerializer]
     public enum ResponseType
     {
