a0d978f4a0689b4385d3e0ceb77df2faaa1ddeb9
Reuben Bond <reuben.bond@gmail.com>
2026-09-04 04:13:35 -0700

    fix(membership): preserve silo metadata API compatibility

--- src/Orleans.Runtime/MembershipService/SiloMetadata/SiloMetadata.cs
+++ src/Orleans.Runtime/MembershipService/SiloMetadata/SiloMetadata.cs
@@ -39,14 +39,12 @@ public record SiloMetadata
         AddMetadata(metadata);
     }
 
-    [Id(0)]
-    private ImmutableDictionary<string, string> MetadataStorage { get; set; } = ImmutableDictionary<string, string>.Empty;
-
     /// <summary>
     /// Gets the metadata key-value pairs associated with the silo.
     /// </summary>
-    public IReadOnlyDictionary<string, string> Metadata => MetadataStorage;
+    [Id(0)]
+    public ImmutableDictionary<string, string> Metadata { get; private set; } = ImmutableDictionary<string, string>.Empty;
 
-    internal void AddMetadata(IEnumerable<KeyValuePair<string, string>> metadata) => MetadataStorage = MetadataStorage.SetItems(metadata);
-    internal void AddMetadata(string key, string value) => MetadataStorage = MetadataStorage.SetItem(key, value);
+    internal void AddMetadata(IEnumerable<KeyValuePair<string, string>> metadata) => Metadata = Metadata.SetItems(metadata);
+    internal void AddMetadata(string key, string value) => Metadata = Metadata.SetItem(key, value);
 }
