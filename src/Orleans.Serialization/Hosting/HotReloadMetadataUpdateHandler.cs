034f4c6043937e4a19d0cb53acb30089dba91460
Reuben Bond <reuben.bond@gmail.com>
2026-09-04 05:34:14 -0700

    fix(serialization): correct hot reload refresh guards

--- src/Orleans.Serialization/Hosting/HotReloadMetadataUpdateHandler.cs
+++ src/Orleans.Serialization/Hosting/HotReloadMetadataUpdateHandler.cs
@@ -1,4 +1,4 @@
-#if NET
+#if NET6_0_OR_GREATER
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
--- src/Orleans.Serialization/Hosting/SerializationHotReloadRefresher.cs
+++ src/Orleans.Serialization/Hosting/SerializationHotReloadRefresher.cs
@@ -1,4 +1,4 @@
-#if NET
+#if NET6_0_OR_GREATER
 using System;
 using System.Collections.Generic;
 using System.Reflection;
