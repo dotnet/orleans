3d4b54f0a773e67c6be1fd7f02ad5acb7808763a
Reuben Bond <reuben.bond@gmail.com>
2026-09-04 03:07:42 -0700

    test(streaming): record delivery progress in cache stub

--- test/Orleans.Streaming.Tests/StreamingTests/PersistentStreamPullingAgentTests.cs
+++ test/Orleans.Streaming.Tests/StreamingTests/PersistentStreamPullingAgentTests.cs
@@ -1352,6 +1352,11 @@ namespace UnitTests.StreamingTests
                 HasActiveSubscriptions = hasActiveSubscriptions;
             }
 
+            public void UpdateDeliveryProgress(StreamSequenceToken? earliestSubscriptionToken, DateTime utcNow)
+            {
+                CheckpointedToken = earliestSubscriptionToken;
+            }
+
             public void Purge()
             {
                 if (cache.IsEmpty)
