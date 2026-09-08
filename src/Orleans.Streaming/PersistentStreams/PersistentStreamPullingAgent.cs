9b22465042037951e23ad580e80d8cb7468b7328
Reuben Bond <reuben.bond@gmail.com>
2026-09-08 01:37:47 -0700

    fix(streaming): detach unavailable consumers locally

--- src/Orleans.Streaming/PersistentStreams/PersistentStreamPullingAgent.cs
+++ src/Orleans.Streaming/PersistentStreams/PersistentStreamPullingAgent.cs
@@ -1873,6 +1873,7 @@ namespace Orleans.Streams
             if (exceptionOccured is ClientNotAvailableException)
             {
                 LogWarningConsumerIsDead(consumerData.StreamConsumer, consumerData.StreamId);
+                RemoveSubscriber_Impl(consumerData.SubscriptionId, consumerData.StreamId);
                 UnregisterUnavailableConsumer(consumerData, cancellationToken).Ignore();
                 return true;
             }
