69aa47dab9a7ddf5000bd962ffddc5062c09e3a2
ksemenenko <mail@ksemenenko.com>
2026-09-10 13:24:09 +0200

    feat(azure): add advanced reminders table storage provider

--- src/Azure/Shared/Storage/AzureTableUtils.cs
+++ src/Azure/Shared/Storage/AzureTableUtils.cs
@@ -12,6 +12,8 @@ using Azure.Data.Tables.Models;
 
 #if ORLEANS_CLUSTERING
 namespace Orleans.Clustering.AzureStorage
+#elif ADVANCED_REMINDERS_AZURE
+namespace Orleans.AdvancedReminders.AzureStorage
 #elif ORLEANS_PERSISTENCE
 namespace Orleans.Persistence.AzureStorage
 #elif ORLEANS_REMINDERS
