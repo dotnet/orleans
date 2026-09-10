b15d9683a4d33c15c6138e93599711204ac423c5
ksemenenko <mail@ksemenenko.com>
2026-09-10 13:24:09 +0200

    feat(redis): add advanced reminders storage provider

---
+++ src/Redis/Orleans.AdvancedReminders.Redis/Storage/RedisRemindersException.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Orleans.AdvancedReminders.Redis;
+
+/// <summary>
+/// Exception thrown from <see cref="RedisReminderTable"/>.
+/// </summary>
+[GenerateSerializer]
+public class RedisRemindersException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="RedisRemindersException"/>.
+    /// </summary>
+    public RedisRemindersException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="RedisRemindersException"/>.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    public RedisRemindersException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="RedisRemindersException"/>.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="inner">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+    public RedisRemindersException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
